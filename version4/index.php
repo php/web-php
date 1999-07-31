8917c2f24fab74bf17927c768c48cfc79e35877e
James Gingerich <jimi@php.net>
1999-07-31 13:37:32 +0000

    Someone didn't commit this

--- version4/index.php
+++ version4/index.php
@@ -9,8 +9,9 @@ echo "\n<!--$MYSITE-->\n";
   <li>Read the <a href="release_announcement.php">Release Announcement</a></li>
   <li><a href="downloads.php">Download</a> PHP 4.0 Beta 1!</li>
   <li>Read the <a href="migration.php">Migration notes</a> on how to migrate from PHP 3.0</li>
-  <li>Read about the (very few) <a href="incompatibilities.php">incompatibilities</a> between PHP3 and PHP4.</li>
+  <li>Read about the (very few) <a href="incompatibilities.php">incompatibilities</a> between PHP 3.0 and PHP 4.0.</li>
   <li><a href="http://bugs.php.net/version4/">Report bugs</a></li>
+  <li><a href="cvs.php">CVS instructions</a></li>
   <li>Subscribe to the <a href="mailto:php4beta-subscribe@lists.php.net">PHP 4.0 Beta mailing list</a></li>
   <li>Download <a href="logos.php"> PHP 4.0 Logos</a></li>
   <li>Check out the <a href="http://www.zend.com"> Zend web site</a></li>
