73e28436458900c803d180dce4b41d8884f9f483
James Gingerich <jimi@php.net>
1999-08-16 14:34:58 +0000

    Uncomitted

--- version4/downloads.php
+++ version4/downloads.php
@@ -5,7 +5,8 @@ commonHeader("PHP 4.0 Beta 2 Downloads");
 
 <ul>
   <li><a href="do_download.php?download_file=php-4.0b2.tar.gz">PHP 4.0 Beta 2 Source</a></li>
-  <li>Win32 binaries will be posted by Wednesday (11/8/1999)</li>
+  <li><a href="do_download.php?download_file=php-4.0b2-Win32.zip">PHP 4.0 Beta 2 Binaries for Win32</a><br>(both CGI and ISAPI modules included)</li>
+  <li><a href="do_download.php?download_file=mysql-php-4.0b2-Win32.zip">MySQL module for PHP 4.0 Beta 2 for Win32</a></li>
   <!--<li><a href="downloads/php4b1-Win32.zip">PHP 4.0 Beta 1 for Win32</a> - ISAPI module for IIS 4.0 and CGI binary included (installation instructions will be posted soon)</li>
 -->
 </ul>
