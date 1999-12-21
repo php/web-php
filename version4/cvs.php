1c8b88f8229bf3affba23e59a9495f934db6e65b
Egon Schmid <eschmid@php.net>
1999-12-21 15:54:12 +0000

    Removed a conflict.

--- version4/cvs.php
+++ version4/cvs.php
@@ -4,26 +4,29 @@ commonHeader("PHP 4.0 CVS");
 echo "\n<!--$MYSITE-->\n";
 ?>
 
-<p>If you wish to get the latest PHP 4.0 source tree, you can obtain it through
-CVS.  As before, you should be warned that the CVS version is a development version,
-and as such, is often unstable, and may not even compile properly.  The advantage
-of using CVS, though, is that you can get the latest fixes and updates, without
-having to wait for the official releases.</p>
+<p>If you wish to get the latest PHP 4.0 source tree, you can obtain
+it through CVS.  As before, you should be warned that the CVS version
+is a development version, and as such, is often unstable, and may not
+even compile properly.  The advantage of using CVS, though, is that
+you can get the latest fixes and updates, without having to wait for
+the official releases.</p>
 
-<p>Getting the PHP 4.0 CVS version to work is a bit more complicated than it was
-with PHP 3.0 for two main reasons - first, the engine, Zend, has been separated
-from the main language tree, and resides in a different CVS repository.  And second,
-PHP 4.0 uses a much more advanced configuration system, that requires you to have
-not only autoconf installed, but also automake and libtool.</p>
+<p>Getting the PHP 4.0 CVS version to work is a bit more complicated
+than it was with PHP 3.0 for two main reasons - first, the engine,
+Zend, has been separated from the main language tree, and resides in a
+different CVS repository.  And second, PHP 4.0 uses a much more
+advanced configuration system, that requires you to have not only
+autoconf installed, but also automake and libtool.</p>
 
-<p>All of the necessary utilities (autoconf, automake. libtool, bison and flex) can be obtained from
-<a href="ftp://ftp.gnu.org/pub/gnu/">the GNU FTP site</a>.</p>
+<p>All of the necessary utilities (autoconf, automake. libtool, bison
+and flex) can be obtained from <a
+href="ftp://ftp.gnu.org/pub/gnu/">the GNU FTP site</a>.</p>
 
-<p>If after these warnings you're still interested in obtaining the PHP 4.0 CVS,
-here's how.<p>
+<p>If after these warnings you're still interested in obtaining the
+PHP 4.0 CVS, here's how.<p>
 
 <ul>
-	<li>It is recommended that you configure CVS by putting this in your <tt>~/.cvsrc</tt> file:
+        <li>It is recommended that you configure CVS by putting this in your <tt>~/.cvsrc</tt> file:
 <pre>
 cvs -z3
 update -d -P
