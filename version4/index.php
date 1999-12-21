b2f7212a6864282c2bb64ad7e9b7a1a830221b8c
Sascha Schumann <sas@php.net>
1999-12-21 22:59:58 +0000

    Remove link from that page. People should look directly into the manual.

--- version4/index.php
+++ version4/index.php
@@ -19,7 +19,6 @@ echo "\n<!--$MYSITE-->\n";
   <li>Check out the <a href="http://www.zend.com"> Zend web site</a></li>
   <li>View the <a href="credits.php">Credits page</a></li>
   <li>License questions?  See the <a href="license-FAQ.php">License FAQ</a></li>
-  <li>David Hjortso prepared documentation about the new PHP 4.0 Session module by Sascha Schumann.  <a href="http://129.142.82.11/session/">Check it out!</a></li>
   <li><a href="mailto:feedback@beta.php.net">Give us feedback!</a>  We're interested in both negative and positive feedback.  <br>Please don't report bugs in here, though - report them via the <a href="http://bugs.php.net/version4/">bugs database</a>.
 </ul>
 
