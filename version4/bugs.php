a47a31cba850b713246b31a6372636020404832d
Andrei Zmievski <andrei@php.net>
2000-02-22 19:19:48 +0000

    Add Beta 4 and Beta 4 Patch Level 1.

--- version4/bugs.php
+++ version4/bugs.php
@@ -550,7 +550,9 @@ Or use the form below to submit a new bug report.
   <th align=right>PHP version:</th>
   <td>
    <select name="php_version">
-    <option name="4.0b3" selected>4.0 Beta 3
+    <option name="4.0b4pl1" selected>4.0 Beta 4 Patch Level 1
+    <option name="4.0b4">4.0 Beta 4
+    <option name="4.0b3">4.0 Beta 3
     <option name="4.0B2">4.0 Beta 2
     <option name="4.0CVS-<? print date("d/m/Y"); ?>">4.0 Latest CVS (<? print date("d/m/Y"); ?>)
     <option name="4.0B1">4.0 Beta 1
