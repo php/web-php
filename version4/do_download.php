88389f10b092f0b2022b16b366b80ba5ad8e0b2f
Rasmus Lerdorf <rasmus@php.net>
2000-03-07 19:12:30 +0000

    commit the 4.0pl1 stuff

--- version4/do_download.php
+++ version4/do_download.php
@@ -8,6 +8,15 @@ if (!isset($download_file)
 	exit("Invalid file requested for download!");
 }
 
+switch($download_file) {
+	case "php-4.0b4.tar.gz":
+		$download_file = "php-4.0b4pl1.tar.gz";
+		break;
+	case "php-4.0b4-Win32.zip":
+		$download_file = "php-4.0b4pl1-Win32.zip";
+		break;
+}
+
 header("Location:  http://www.php.net/version4/downloads/$download_file");
 
 $log = @fopen("logs/download.log", "a");
--- version4/downloads.php
+++ version4/downloads.php
@@ -4,8 +4,9 @@ commonHeader("PHP 4.0 Beta 4 Downloads");
 ?>
 
 <ul>
-  <li><a href="do_download.php?download_file=php-4.0b4.tar.gz">PHP 4.0 Beta 4 Source</a></li> - 20 Feb 2000
-  <li><a href="do_download.php?download_file=php-4.0b4-Win32.zip">PHP 4.0 Beta 4 Binaries for Win32</a> - 20 Feb 2000<br>(CGI binary, ISAPI module and MySQL extension included)</li>
+  <li><a href="do_download.php?download_file=php-4.0b4pl1.tar.gz">PHP 4.0 Beta 4 (pl1) Source</a></li> - 21 Feb 2000
+  <li><a href="do_download.php?download_file=php-4.0b4pl1-Win32.zip">PHP 4.0 Beta 4 (pl1) Binaries for Win32</a> - 21 Feb 2000<br>(CGI binary, ISAPI module and MySQL extension included)</li>
+  <li><a href="do_download.php?download_file=php-4.0b4-4.0b4pl1.diff.gz">Diff file from PHP 4.0 Beta 4 to PHP 4.0 Beta patch level 1</a> - 21 Feb 2000</li>
 <!--
   <li><a href="http://www.zend.com/optimizer_link.php">Zend Optimizer</a> for PHP 4.0 Beta 4 (pl1) is available on  Zend Technologies' web site.</li>
 -->
