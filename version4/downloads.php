2ba14393b2106d7ca4a9986c8a25efcd17ac9298
Zeev Suraski <zeev@php.net>
2000-03-27 22:41:09 +0000

    *** empty log message ***

--- version4/downloads.php
+++ version4/downloads.php
@@ -5,7 +5,7 @@ commonHeader("PHP 4.0 Release Candidate 1 Downloads");
 
 <ul>
   <li><a href="do_download.php?download_file=php-4.0RC1.tar.gz">PHP 4.0 Release Candidate 1 Source</a></li> - 21 Feb 2000
-  <li><a href="do_download.php?download_file=php-4.0RC1-Win32.zip">PHP 4.0 Release Candidate 1 Binaries for Win32</a> - 21 Feb 2000<br>(CGI binary, ISAPI module and MySQL extension included)</li>
+  <li><a href="do_download.php?download_file=php-4.0RC1-Win32.zip">PHP 4.0 Release Candidate 1 Binaries for Win32</a> - 21 Feb 2000<br>(CGI binary and ISAPI module included, MySQL support built-in)</li>
   <li><a href="http://www.zend.com/zend/optimizer.php">Zend Optimizer Beta</a> for PHP 4.0 (Beta 4 or later) is available on  Zend Technologies' web site.</li>
 </ul>
 
