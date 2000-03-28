bd61e057f86e8ae0fca7f736ee3cc82c9d37ff59
Sam Ruby <rubys@php.net>
2000-03-28 15:16:12 +0000

    Add Win32 java and servlet binaries

--- version4/downloads.php
+++ version4/downloads.php
@@ -5,7 +5,20 @@ commonHeader("PHP 4.0 Release Candidate 1 Downloads");
 
 <ul>
   <li><a href="do_download.php?download_file=php-4.0RC1.tar.gz">PHP 4.0 Release Candidate 1 Source</a></li> - 21 Feb 2000
+  <p>     
   <li><a href="do_download.php?download_file=php-4.0RC1-Win32.zip">PHP 4.0 Release Candidate 1 Binaries for Win32</a> - 21 Feb 2000<br>(CGI binary and ISAPI module included, MySQL support built-in)</li>
+  <p>Other add-ons:
+  <ul>
+    <li>Servlet Server API -
+      <a href="do_download.php?download_file=php-4.0RC1-Win32-servlet-jdk118.zip">jdk1.1.8</a>,
+      <a href="do_download.php?download_file=php-4.0RC1-Win32-servlet-jdk122.zip">jdk1.2.2</a>,
+      <a href="do_download.php?download_file=php-4.0RC1-Win32-servlet-jdk13.zip">jdk1.3</a>
+    <li>Java extensions -
+      <a href="do_download.php?download_file=php-4.0RC1-Win32-java-jdk118.zip">jdk1.1.8</a>,
+      <a href="do_download.php?download_file=php-4.0RC1-Win32-java-jdk122.zip">jdk1.2.2</a>,
+      <a href="do_download.php?download_file=php-4.0RC1-Win32-java-jdk13.zip">jdk1.3</a>
+  </ul>
+  <p>     
   <li><a href="http://www.zend.com/zend/optimizer.php">Zend Optimizer Beta</a> for PHP 4.0 (Beta 4 or later) is available on  Zend Technologies' web site.</li>
 </ul>
 
