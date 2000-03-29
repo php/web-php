3d38544741860a123b96508ee8cea7f39ce2ece2
Sascha Schumann <sas@php.net>
2000-03-29 17:36:06 +0000

    Add 4.0RC1

--- version4/bugs.php
+++ version4/bugs.php
@@ -550,7 +550,8 @@ Or use the form below to submit a new bug report.
   <th align=right>PHP version:</th>
   <td>
    <select name="php_version">
-    <option name="4.0b4pl1" selected>4.0 Beta 4 Patch Level 1
+    <option name="4.0RC1" selected>4.0 Release Candidate 1
+    <option name="4.0b4pl1">4.0 Beta 4 Patch Level 1
     <option name="4.0b4">4.0 Beta 4
     <option name="4.0b3">4.0 Beta 3
     <option name="4.0B2">4.0 Beta 2
