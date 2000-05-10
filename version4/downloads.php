40a9c12089974df44f12dd237e75eaacb966650f
Sam Ruby <rubys@php.net>
2000-05-10 14:16:30 +0000

    add dates

--- version4/downloads.php
+++ version4/downloads.php
@@ -9,11 +9,11 @@ commonHeader("PHP 4.0 Release Candidate 2 Downloads");
   <li><a href="do_download.php?download_file=php-4.0RC2-Win32.zip">PHP 4.0 Release Candidate 2 Binaries for Win32</a> - 09 May 2000<br>(CGI binary and ISAPI module included, MySQL support built-in)</li>
   <p>Other add-ons:
   <ul>
-    <li>Servlet Server API -
+    <li>Servlet Server API - 10 May 2000
       <a href="do_download.php?download_file=php-4.0RC2-Win32-servlet-jdk118.zip">jdk1.1.8</a>,
       <a href="do_download.php?download_file=php-4.0RC2-Win32-servlet-jdk122.zip">jdk1.2.2</a>,
       <a href="do_download.php?download_file=php-4.0RC2-Win32-servlet-jdk13.zip">jdk1.3</a>
-    <li>Java extensions -
+    <li>Java extensions - 10 May 2000
       <a href="do_download.php?download_file=php-4.0RC2-Win32-java-jdk118.zip">jdk1.1.8</a>,
       <a href="do_download.php?download_file=php-4.0RC2-Win32-java-jdk122.zip">jdk1.2.2</a>,
       <a href="do_download.php?download_file=php-4.0RC2-Win32-java-jdk13.zip">jdk1.3</a>
