67c10de113124ce2d59224b4107eb3149c8484fc
Rasmus Lerdorf <rasmus@php.net>
2000-05-18 18:58:26 +0000

    - There is an RC2 optimizer available

--- version4/index.php
+++ version4/index.php
@@ -7,7 +7,7 @@ echo "\n<!--$MYSITE-->\n";
 <p>The long anticipated next generation of PHP is here!</p>
 <ul>
   <li>Read the <a href="release_announcement.php">Beta 1 Release Announcement</a></li>
-  <li><a href="downloads.php">Download</a> PHP 4.0 Release Candidate 2! Please note that if you have been using the Zend optimizer, it does not work with RC2. You will need to disable it in your php.ini file until a new optimizer is released.</li>
+  <li><a href="downloads.php">Download</a> PHP 4.0 Release Candidate 2!</li> 
   <li>Read the <a href="roadmap.php">roadmap</a> for the 4.0 release that came out of the PDM.</li>
   <li>Read the <a href="migration.php">Migration notes</a> on how to migrate from PHP 3.0</li>
   <li>Read about the (very few) <a href="incompatibilities.php">incompatibilities</a> between PHP 3.0 and PHP 4.0.</li>
