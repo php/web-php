4115aa23ffb6ae5ad3f85194fe33be8d4598df74
Zeev Suraski <zeev@php.net>
2000-05-22 18:34:10 +0000

    Some more updates

--- version4/index.php
+++ version4/index.php
@@ -7,20 +7,16 @@ echo "\n<!--$MYSITE-->\n";
 <p>The long anticipated next generation of PHP is here!</p>
 <ul>
   <li>Read the <a href="release_announcement.php">Beta 1 Release Announcement</a></li>
-  <li><a href="downloads.php">Download</a> PHP 4.0 Release Candidate 2!</li> 
-  <li>Read the <a href="roadmap.php">roadmap</a> for the 4.0 release that came out of the PDM.</li>
   <li>Read the <a href="migration.php">Migration notes</a> on how to migrate from PHP 3.0</li>
   <li>Read about the (very few) <a href="incompatibilities.php">incompatibilities</a> between PHP 3.0 and PHP 4.0.</li>
-  <li><a href="http://bugs.php.net/version4/">Report bugs</a></li>
   <li><a href="cvs.php">CVS instructions</a></li>
   <li><a href="win32build.php">Win32 build instructions</a></li>
-  <li>Subscribe to the <a href="mailto:php4beta-subscribe@lists.php.net">PHP 4.0 Beta mailing list</a></li>
   <li>Subscribe to the <a href="mailto:php-cvs-daily-subscribe@lists.php.net">PHP 4.0 daily ChangeLog and NEWS additions</a></li>
   <li>Download <a href="logos.php"> PHP 4.0 Logos</a></li>
   <li>Check out the <a href="http://www.zend.com"> Zend web site</a></li>
   <li>View the <a href="credits.php">Credits page</a></li>
   <li>License questions?  See the <a href="license-FAQ.php">License FAQ</a></li>
-  <li><a href="mailto:feedback@beta.php.net">Give us feedback!</a>  We're interested in both negative and positive feedback.  <br>Please don't report bugs in here, though - report them via the <a href="http://bugs.php.net/version4/">bugs database</a>.
+  <li><a href="mailto:feedback@beta.php.net">Give us feedback!</a>  We're interested in both negative and positive feedback.  <br>Please don't report bugs in here, though - report them via the <a href="http://bugs.php.net/">bugs database</a>.
 </ul>
 
 
