c43ee88f7cef40a0f030dac9fd2c9fd901c57f05
Sascha Schumann <sas@php.net>
2000-06-07 02:49:30 +0000

    Change email address to feedback@php.net

--- version4/index.php
+++ version4/index.php
@@ -16,7 +16,7 @@ echo "\n<!--$MYSITE-->\n";
   <li>Check out the <a href="http://www.zend.com"> Zend web site</a></li>
   <li>View the <a href="credits.php">Credits page</a></li>
   <li>License questions?  See the <a href="license-FAQ.php">License FAQ</a></li>
-  <li><a href="mailto:feedback@beta.php.net">Give us feedback!</a>  We're interested in both negative and positive feedback.  <br>Please don't report bugs in here, though - report them via the <a href="http://bugs.php.net/">bugs database</a>.
+  <li><a href="mailto:feedback@php.net">Give us feedback!</a>  We're interested in both negative and positive feedback.  <br>Please don't report bugs in here, though - report them via the <a href="http://bugs.php.net/">bugs database</a>.
 </ul>
 
 
