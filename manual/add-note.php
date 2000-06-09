55fda64100914d6eaca4a5ddf69604f5d76897b7
Colin Viebrock <cmv@php.net>
2000-06-09 17:18:28 +0000

    For now ...

---
+++ manual/add-note.php
@@ -0,0 +1,17 @@
+<?
+
+require("shared.inc");
+commonHeader("Manual Notes");
+?>
+
+<P>
+Sorry, but adding notes to the annotated manual has been temporarily disabled.
+</P>
+
+<P>
+You can still read the annotated notes at: <A HREF="http://php.chek.com">php.chek.com</A>.
+</P>
+
+<?
+commonFooter();
+?>
