e6a282f1b9fe37ab8b86f415e65ae6118da72d9a
Sascha Schumann <sas@php.net>
2000-07-01 05:31:55 +0000

    Add data from March-June 2000 Netcraft Survey

--- usage.php
+++ usage.php
@@ -2,9 +2,10 @@
 require("shared.inc");
 commonHeader("PHP Usage Stats");
 ?>
-February 2000: 1412822 Domains, 428840 IP Addresses<br>
-Source: <a href="http://www.netcraft.com">Netcraft</a><br>
-<img src="gifs/phpstats-200002.gif"><br clear=left>
+June 2000: 2756002 Domains, 571610 IP Addresses<br>
+Source: <a href="http://www.netcraft.com/Survey/">Netcraft</a><br>
+<img src="gifs/phpstats-200006.gif" width=780 height=350 alt="Graph"
+><br clear=left>
 
 <p>You can also see how popular PHP is relative to other Apache modules
 at <a href="http://www.securityspace.com/s_survey/">E-Soft Inc.'s
