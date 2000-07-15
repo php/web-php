88a9319e640fad072facb5c834b18a82033b655b
Egon Schmid <eschmid@php.net>
2000-07-15 20:58:24 +0000

    Added the LinuxTag 2000 at Stuttgart.

--- news.php
+++ news.php
@@ -55,6 +55,11 @@ function endEntry() {
 
 <TABLE BORDER=0 CELLPADDING=0 CELLSPACING=0 WIDTH=100%>
 
+<? makeEntry( "LinuxTag - some impressions", "by <a href=\"mailto:winni@xpilot.org\">Winfried Tr&uuml;mper</a>",
+    "http://www.guug.de:8080/fotos/linuxtag/story/index.html", "July, 2000");?>
+The literal translation of LinuxTag is LinuxDay. This title is a leftover from ancient times, when the event really lasted one day. This year the event occupied the fair in Stuttgart for four whole days, beginning at thurday with a business congress. For those commercially relevant lectures you had to pay. However, a total of over 100 hours of free lectures followed from friday to sunday, addressing the interests of end-users and developers. During all days companies and free software developers showed their Linux-products on an area of 6.000 m². An estimated number of 17.000 people visited the LinuxTag, making it a big success to be repeated next year. 
+<? endEntry(); ?>
+
 <? makeEntry( "Are PHP and MySQL the Perfect Couple?", "by <a href=\"http://www.oreillynet.com/pub/au/77\">Jason Gilmore</a>",
     "http://www.oreillynet.com/pub/a/network/2000/06/16/magazine/php_mysql.html", "June 16, 2000");?>
 MySQL and PHP have developed a special sort of relationship over the past few years, perhaps due in large part to the fact that they are both freely available for download. The increased popularity of this dynamic duo, coupled with boundless enthusiasm from core developers of both technologies, culminated in a "meeting of the minds" in Israel earlier this year. This resulted in the MySQL library being packaged with the PHP 4.0 distribution, in addition to an agreement to help each other improve the performance quality of product integration whenever the opportunity arises.
