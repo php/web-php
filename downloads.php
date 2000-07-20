3d9facabcab850fbd09a1eb6fad747b223cb1a8f
Colin Viebrock <cmv@php.net>
2000-07-20 16:17:11 +0000

    A bit of organization, and links to older versions.

--- downloads.php
+++ downloads.php
@@ -2,44 +2,92 @@
 require("shared.inc");
 commonHeader("Downloads ");
 $server = ($SERVER_PORT == "81" ? $SERVER_NAME . ":81" : $SERVER_NAME);
+
+$server = 'ca.php.net';
 ?>
 
-<h2>PHP 4</h2>
-<ul>
-  <li><a href="http://www.php.net/do_download.php?download_file=php-4.0.1pl2.tar.gz&source_site=<?php echo $server; ?>">PHP 4.0.1pl2 Source</a> - 30 June 2000</li>
-  <li><a href="http://www.php.net/do_download.php?download_file=php-4.0.1pl2-Win32.zip&source_site=<?php echo $server; ?>">PHP 4.0.1pl2 Binaries for Win32</a> - 30 June 2000<br>(CGI binary and ISAPI module included, MySQL support built-in)</li>
-  <li><a href="http://www.php.net/do_download.php?download_file=php-4.0.1-4.0.1pl2.diff.gz&source_site=<?php echo $server; ?>">Diffs from PHP 4.0.1 to PHP 4.0.1pl2</a> - 30 June 2000</li>
-  <p>Other add-ons:
-  <ul>
-    <li>Servlet Server API:
-      <a href="http://www.php.net/do_download.php?download_file=php-4.0.1-Win32-servlet-jdk118.zip&source_site=<?php echo $server; ?>">jdk1.1.8</a>,
-      <a href="http://www.php.net/do_download.php?download_file=php-4.0.1-Win32-servlet-jdk122.zip&source_site=<?php echo $server; ?>">jdk1.2.2</a>,
-      <a href="http://www.php.net/do_download.php?download_file=php-4.0.1-Win32-servlet-jdk13.zip&source_site=<?php echo $server; ?>">jdk1.3</a>
-    <li>Java extensions:
-      <a href="http://www.php.net/do_download.php?download_file=php-4.0.1-Win32-java-jdk118.zip&source_site=<?php echo $server; ?>">jdk1.1.8</a>,
-      <a href="http://www.php.net/do_download.php?download_file=php-4.0.1-Win32-java-jdk122.zip&source_site=<?php echo $server; ?>">jdk1.2.2</a>,
-      <a href="http://www.php.net/do_download.php?download_file=php-4.0.1-Win32-java-jdk13.zip&source_site=<?php echo $server; ?>">jdk1.3</a>
-  </ul>
-  <p>
-<!--
-  <p>Java add-ons will be posted shortly.</p>
--->
-  <LI>For downloadable documentation, go to the <A HREF="docs.php">documentation</A> page
-  <li>To download the latest development version, see the <a href="version4/cvs.php">instructions on using CVS</a>.</li>
-  <li><a href="http://www.zend.com/zend/optimizer.php">Zend Optimizer RC</a> for PHP 4.0.1 is available on Zend Technologies' web site.</li>
-</ul>
+<h2>Latest version of PHP4</h2><BR>
 
-<hr>
-<h2>Old versions of PHP</h2>
+<b>Linux/Unix Source</b><BR>
+<UL>
+  <li><a href="http://www.php.net/do_download.php?download_file=php-4.0.1pl2.tar.gz&source_site=<?php echo $server; ?>">PHP 4.0.1pl2</a> - 30 June 2000</li>
+  <li><a href="http://www.php.net/do_download.php?download_file=php-4.0.1-4.0.1pl2.diff.gz&source_site=<?php echo $server; ?>">Diffs from PHP 4.0.1 to PHP 4.0.1pl2</a> - 30 June 2000</li>
+</UL>
+
+<b>Win32 Binaries</b><BR>
+<UL>
+  <li><a href="http://www.php.net/do_download.php?download_file=php-4.0.1pl2-Win32.zip&source_site=<?php echo $server; ?>">PHP 4.0.1pl2</a> - 30 June 2000<br>(CGI binary and ISAPI module included, MySQL support built-in)</li>
+  <li>Servlet Server API:
+    <a href="http://www.php.net/do_download.php?download_file=php-4.0.1-Win32-servlet-jdk118.zip&source_site=<?php echo $server; ?>">jdk1.1.8</a>,
+    <a href="http://www.php.net/do_download.php?download_file=php-4.0.1-Win32-servlet-jdk122.zip&source_site=<?php echo $server; ?>">jdk1.2.2</a>,
+    <a href="http://www.php.net/do_download.php?download_file=php-4.0.1-Win32-servlet-jdk13.zip&source_site=<?php echo $server; ?>">jdk1.3</a>
+  <li>Java extensions:
+    <a href="http://www.php.net/do_download.php?download_file=php-4.0.1-Win32-java-jdk118.zip&source_site=<?php echo $server; ?>">jdk1.1.8</a>,
+    <a href="http://www.php.net/do_download.php?download_file=php-4.0.1-Win32-java-jdk122.zip&source_site=<?php echo $server; ?>">jdk1.2.2</a>,
+    <a href="http://www.php.net/do_download.php?download_file=php-4.0.1-Win32-java-jdk13.zip&source_site=<?php echo $server; ?>">jdk1.3</a>
+</UL>
+
+
+<hr noshade>
+
+
+<H3>Previous Releases of PHP4</H3>
+
+<b>Linux/Unix Source</b><BR>
+<UL>
+  <li><a href="http://www.php.net/do_download.php?download_file=php-4.0.1RC2.tar.gz&source_site=<?php echo $server; ?>">PHP 4.0.1 Release Candidate 2</a></li>
+  <li><a href="http://www.php.net/do_download.php?download_file=php-4.0.1RC.tar.gz&source_site=<?php echo $server; ?>">PHP 4.0.1 Release Candidate</a></li>
+  <li><a href="http://www.php.net/do_download.php?download_file=php-4.0.0.tar.gz&source_site=<?php echo $server; ?>">PHP 4.0.0</a></li>
+</UL>
+
+<b>Win32 Binaries</b><BR>
+<UL>
+  <li><a href="http://www.php.net/do_download.php?download_file=php-4.0.1pl1-Win32.zip&source_site=<?php echo $server; ?>">PHP 4.0.1pl1</a></li>
+  <li><a href="http://www.php.net/do_download.php?download_file=php-4.0.1-Win32.zip&source_site=<?php echo $server; ?>">PHP 4.0.1</a></li>
+  <li><a href="http://www.php.net/do_download.php?download_file=php-4.0.0-Win32.zip&source_site=<?php echo $server; ?>">PHP 4.0.0</a></li>
+  <li>Servlet Server API:
+    <a href="http://www.php.net/do_download.php?download_file=php-4.0.0-Win32-servlet-jdk118.zip&source_site=<?php echo $server; ?>">jdk1.1.8</a>,
+    <a href="http://www.php.net/do_download.php?download_file=php-4.0.0-Win32-servlet-jdk122.zip&source_site=<?php echo $server; ?>">jdk1.2.2</a>,
+    <a href="http://www.php.net/do_download.php?download_file=php-4.0.0-Win32-servlet-jdk13.zip&source_site=<?php echo $server; ?>">jdk1.3</a>
+  <li>Java extensions:
+    <a href="http://www.php.net/do_download.php?download_file=php-4.0.0-Win32-java-jdk118.zip&source_site=<?php echo $server; ?>">jdk1.1.8</a>,
+    <a href="http://www.php.net/do_download.php?download_file=php-4.0.0-Win32-java-jdk122.zip&source_site=<?php echo $server; ?>">jdk1.2.2</a>,
+    <a href="http://www.php.net/do_download.php?download_file=php-4.0.0-Win32-java-jdk13.zip&source_site=<?php echo $server; ?>">jdk1.3</a>
+</UL>
+
+<P>
+<I>Previous releases are listed for informational purposes only.  You should being using the latest release.</I>
+</P>
+
+<hr noshade>
+
+
+<h3>Older Versions of PHP</h3>
 
 <UL>
-<LI>Download <a href="download-php3.php">PHP 3.0</a> (source code and Windows binaries distributions available).</li>
+  <li><a href="download-php3.php">PHP 3.0</a> Source code and Win32 binaries
+  <li><A HREF="ftp://ftp.nerosworld.com/pub/php/dist">PHP/FI 2.0</a> Source and related files
 <!--
-<li>Instructions on how to use the <a href="http://www.php.net/cvs-php3.php">PHP 3.0 CVS tree</a>.</li>
+  <li>Instructions on how to use the <a href="http://www.php.net/cvs-php3.php">PHP 3.0 CVS tree</a>
 -->
-<LI><A HREF="download-logos.php">PHP logos</A> for your site
-<LI><A HREF="ftp://ftp.nerosworld.com/pub/php/dist">PHP/FI 2.0 Distributions and related files - no longer supported</A>
 </UL>
+
+<P>
+<I>Older releases are listed for archeological purposes only.  They are no longer supported.</I>
+</P>
+
+<hr noshade>
+
+
+<h3>Other Downloads</h3>
+
+<UL>
+  <li>For downloadable documentation, go to the <A HREF="docs.php">documentation</A> page
+  <li>Get some <A HREF="download-logos.php">PHP logos</A> for your site
+  <li>To download the latest development version, see the <a href="version4/cvs.php">instructions on using CVS</a>
+  <li><a href="http://www.zend.com/zend/optimizer.php">Zend Optimizer RC</a> for PHP 4.0.1 is available on Zend Technologies' web site.
+</ul>
+
 
 <?
 commonFooter();
