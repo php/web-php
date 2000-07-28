c69d9180113116cc9ec22e18b2c19e687f4bc6c6
Rasmus Lerdorf <rasmus@php.net>
2000-07-28 06:32:30 +0000

    Add set-magic-cookie and about-notes back

---
+++ manual/about-notes.php
@@ -0,0 +1,19 @@
+<?
+require("shared.inc");
+commonHeader("Manual Notes");
+?>
+
+<P>The manual notes interface allows you to add notes to the online
+version of the PHP manual. These notes are then shared with other
+PHP users when they browse the manual online.
+
+<P>Periodically, the developers may go through the notes and incorporate
+the information in them into the documentation proper.
+
+<P>These notes are only visible at <A href="http://www.php.net/manual/">www.php.net</A>
+because that's where the database that stores them is located. We're
+working on a way of supporting this across our many mirrors.
+
+<P>These notes are <B>not</B> the place to report <A href="http://bugs.php.net">bugs</A>!
+
+<?commonFooter();?>
---
+++ manual/set-magic-cookie.php
@@ -0,0 +1,19 @@
+<?
+if (isset($user) && isset($pass)) {
+  $MAGIC_COOKIE = base64_encode("$user:$pass");
+  # we set a cookie good for five years
+  SetCookie("MAGIC_COOKIE", base64_encode("$user:$pass"), time()+157248000);
+  echo "<b>Cookie set</b>.<br>";
+}
+if (isset($MAGIC_COOKIE)) {
+  list($user,$pass) = explode(":", base64_decode($MAGIC_COOKIE));
+}
+?>
+
+<form action="<?echo $PHP_SELF;?>" method="POST">
+user: <input type=text name=user value="<?echo $user;?>">
+<br>
+pass: <input type=password name=pass value="<?echo $pass;?>">
+<br>
+<input type=submit>
+</form>
