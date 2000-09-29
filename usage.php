06cca9dcd88575ad09e2d1fd4d989a40da0e8072
Sascha Schumann <sas@php.net>
2000-09-29 17:52:38 +0000

    New August Netcraft numbers

--- usage.php
+++ usage.php
@@ -2,9 +2,9 @@
 require("shared.inc");
 commonHeader("PHP Usage Stats");
 ?>
-July 2000: PHP: 3121918 Domains, 598213 IP Addresses<br>
+August 2000: PHP: PHP: 3314634 Domains, 637746 IP Addresses<br>
 Source: <a href="http://www.netcraft.com/Survey/">Netcraft</a><br>
-<img src="gifs/phpstats-200007.gif" width=780 height=350 alt="Graph"
+<img src="gifs/phpstats-200008.gif" width=780 height=350 alt="Graph"
 ><br clear=left>
 
 <p>You can also see how popular PHP is relative to other Apache modules
