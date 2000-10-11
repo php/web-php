9229fdb4d01f24cc929fbd853677afa45d2d9b34
Zeev Suraski <zeev@php.net>
2000-10-11 20:15:14 +0000

    Go live with the .tar.gz

--- downloads.php
+++ downloads.php
@@ -6,19 +6,19 @@ $server = ($SERVER_PORT == "81" ? $SERVER_NAME . ":81" : $SERVER_NAME);
 // $server = 'ca.php.net';
 ?>
 
-<h2>Latest version of PHP4</h2><BR>
+<h2>Latest version of PHP 4.0</h2><BR>
 
 <b>Complete Source Code</b><BR>
 <UL>
-  <li><a href="http://www.php.net/do_download.php?download_file=php-4.0.2.tar.gz&source_site=<?php echo $server; ?>">PHP 4.0.2</a> - 29 August 2000</li>
+  <li><a href="http://www.php.net/do_download.php?download_file=php-4.0.3.tar.gz&source_site=<?php echo $server; ?>">PHP 4.0.3</a> - 11 October 2000</li>
 </UL>
 
 <b>Win32 Binaries</b><BR>
 <UL>
-  <li><a href="http://www.php.net/do_download.php?download_file=php-4.0.2-Win32.zip&source_site=<?php echo $server; ?>">PHP 4.0.2</a> - 29 August 2000<br>(CGI binary and ISAPI module included, MySQL support built-in)</li>
+  <li>Win32 binaries will be posted soon!</li>
 <!--
   <li>Java binaries will be posted soon!</li>
--->
+  <li><a href="http://www.php.net/do_download.php?download_file=php-4.0.2-Win32.zip&source_site=<?php echo $server; ?>">PHP 4.0.2</a> - 29 August 2000<br>(CGI binary and ISAPI module included, MySQL support built-in)</li>
   <li>Servlet Server API:
     <a href="http://www.php.net/do_download.php?download_file=php-4.0.2-Win32-servlet-jdk118.zip&source_site=<?php echo $server; ?>">jdk1.1.8</a>,
     <a href="http://www.php.net/do_download.php?download_file=php-4.0.2-Win32-servlet-jdk122.zip&source_site=<?php echo $server; ?>">jdk1.2.2</a>,
@@ -27,6 +27,7 @@ $server = ($SERVER_PORT == "81" ? $SERVER_NAME . ":81" : $SERVER_NAME);
     <a href="http://www.php.net/do_download.php?download_file=php-4.0.2-Win32-java-jdk118.zip&source_site=<?php echo $server; ?>">jdk1.1.8</a>,
     <a href="http://www.php.net/do_download.php?download_file=php-4.0.2-Win32-java-jdk122.zip&source_site=<?php echo $server; ?>">jdk1.2.2</a>,
     <a href="http://www.php.net/do_download.php?download_file=php-4.0.2-Win32-java-jdk13.zip&source_site=<?php echo $server; ?>">jdk1.3</a>
+-->
 </UL>
 
 
@@ -46,6 +47,7 @@ $server = ($SERVER_PORT == "81" ? $SERVER_NAME . ":81" : $SERVER_NAME);
 
 <b>Complete Source Code</b><BR>
 <UL>
+  <li><a href="http://www.php.net/do_download.php?download_file=php-4.0.2.tar.gz&source_site=<?php echo $server; ?>">PHP 4.0.2 </a></li>
   <li><a href="http://www.php.net/do_download.php?download_file=php-4.0.1pl2.tar.gz&source_site=<?php echo $server; ?>">PHP 4.0.1pl2 </a></li>
   <li><a href="http://www.php.net/do_download.php?download_file=php-4.0.1RC2.tar.gz&source_site=<?php echo $server; ?>">PHP 4.0.1 Release Candidate 2</a></li>
   <li><a href="http://www.php.net/do_download.php?download_file=php-4.0.1RC.tar.gz&source_site=<?php echo $server; ?>">PHP 4.0.1 Release Candidate</a></li>
@@ -54,6 +56,7 @@ $server = ($SERVER_PORT == "81" ? $SERVER_NAME . ":81" : $SERVER_NAME);
 
 <b>Win32 Binaries</b><BR>
 <UL>
+  <li><a href="http://www.php.net/do_download.php?download_file=php-4.0.2-Win32.zip&source_site=<?php echo $server; ?>">PHP 4.0.2</a></li>
   <li><a href="http://www.php.net/do_download.php?download_file=php-4.0.1pl2-Win32.zip&source_site=<?php echo $server; ?>">PHP 4.0.1pl2</a></li>
   <li><a href="http://www.php.net/do_download.php?download_file=php-4.0.1pl1-Win32.zip&source_site=<?php echo $server; ?>">PHP 4.0.1pl1</a></li>
   <li><a href="http://www.php.net/do_download.php?download_file=php-4.0.1-Win32.zip&source_site=<?php echo $server; ?>">PHP 4.0.1</a></li>
