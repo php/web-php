eb5b2e7b61730316b37d24c27c3a877afb831650
Sascha Schumann <sas@php.net>
2000-10-11 20:30:10 +0000

    Add 3.0.17 tarball. Win32 binaries will be posted later

--- download-php3.php
+++ download-php3.php
@@ -1,11 +1,11 @@
 <?
 require("shared.inc");
 
-$current = "3.0.16";
+$current = "3.0.17";
 $win32_current = "3.0.16";
 #$rpm_current = "3.0.5-1";
 
-$filesizes["php-${current}.tar.gz"]="1,993 kB";
+$filesizes["php-${current}.tar.gz"]="2,220 kB";
 $filesizes["php-${win32_current}-win32.zip"]="1,887 kB";
 #$filesizes["mod_php3-${rpm_current}.i386.rpm"]="294 kB";
 #$filesizes["mod_php3-${rpm_current}.src.rpm"]="1,503 kB";
