d613bd1cd3ea6850ad8764f3a4d93168490912a7
Rasmus Lerdorf <rasmus@php.net>
2001-01-13 01:22:24 +0000

    Let's not reveal the doc_root path here

--- error/index.php
+++ error/index.php
@@ -60,7 +60,7 @@ if ($try) {
 
 ?>
 <?Header("HTTP/1.0 404 Not Found")?>
-<?commonHeader($REDIRECT_REDIRECT_ERROR_NOTES);
+<?commonHeader(str_replace($DOCUMENT_ROOT,'',$REDIRECT_REDIRECT_ERROR_NOTES));
 
 	if(!isset($pattern)) {
 		$pattern = $uri;
