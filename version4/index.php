2a9e6e29201a1133a58525e7eb81663e9764827e
jim winstead <jimw@php.net>
2001-01-20 01:53:27 +0000

    remove old cruft

--- version4/index.php
+++ version4/index.php
@@ -6,7 +6,6 @@ echo "\n<!--$MYSITE-->\n";
 
 <p>The long anticipated next generation of PHP is here!</p>
 <ul>
-  <li>Read the <a href="release_announcement.php">Beta 1 Release Announcement</a></li>
   <li>Read the <a href="migration.php">Migration notes</a> on how to migrate from PHP 3.0</li>
   <li>Read about the (very few) <a href="incompatibilities.php">incompatibilities</a> between PHP 3.0 and PHP 4.0.</li>
   <li><a href="cvs.php">CVS instructions</a></li>
@@ -14,9 +13,7 @@ echo "\n<!--$MYSITE-->\n";
   <li>Subscribe to the <a href="mailto:php-cvs-daily-subscribe@lists.php.net">PHP 4.0 daily ChangeLog and NEWS additions</a></li>
   <li>Download <a href="logos.php"> PHP 4.0 Logos</a></li>
   <li>Check out the <a href="http://www.zend.com"> Zend web site</a></li>
-  <li>View the <a href="credits.php">Credits page</a></li>
   <li>License questions?  See the <a href="license-FAQ.php">License FAQ</a></li>
-  <li><a href="mailto:feedback@php.net">Give us feedback!</a>  We're interested in both negative and positive feedback.  <br>Please don't report bugs in here, though - report them via the <a href="http://bugs.php.net/">bugs database</a>.
 </ul>
 
 
