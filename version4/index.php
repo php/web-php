0a836a72193114408a40f2be1c31241e65a752d5
jim winstead <jimw@php.net>
2001-01-23 21:46:02 +0000

    file no longer exists

--- version4/index.php
+++ version4/index.php
@@ -6,7 +6,6 @@ echo "\n<!--$MYSITE-->\n";
 
 <p>The long anticipated next generation of PHP is here!</p>
 <ul>
-  <li><a href="cvs.php">CVS instructions</a></li>
   <li><a href="win32build.php">Win32 build instructions</a></li>
   <li>Subscribe to the <a href="mailto:php-cvs-daily-subscribe@lists.php.net">PHP 4.0 daily ChangeLog and NEWS additions</a></li>
   <li>Check out the <a href="http://www.zend.com"> Zend web site</a></li>
