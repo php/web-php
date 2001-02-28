16faa8bd95da148d62ba8b639fa29c2986d61c28
Jesus M. Castagnetto <jmcastagnetto@php.net>
2001-02-28 11:55:51 +0000

    Modified email validation function to print out debugging info. Added testing script: check_email_func.php, when used as: http://www.php.net/include/chec_email_func.php?debug=1 it will do the validation with a set of email addresses and print out debugging info.

---
+++ include/check_email_func.php
@@ -0,0 +1,31 @@
+<html>
+<head><title>email validation test</title></head>
+<body>
+<?php
+
+require "email_validation.inc";
+
+if (!$debug)
+	$debug = 0;
+
+$test_add = array (
+    "wrong-email-address@lists.php.net","jmcastagnetto@yahoo.com", 
+    "some-wrong@asdas.com", "jcastagnetto-NO-SPAM@yahoo.com", 
+    "jcastagnetto@NoSpam-yahoo.com", "jmcastagnetto@chek2.com", 
+    "jcastagnetto-i-hate-spam@NOSPAMyahoo.com", "jesusmc@scripps.edu", 
+    "asasasd324324@php.net", "jcastagnetto-delete-this-@yahoo.com",
+    "wrong-address-with@@@@-remove_me-and-some-i-hate_SPAM-stuff");
+
+while (list(,$v) = each($test_add)) {
+    echo "The address: $v (".clean_AntiSpam($v).") is";
+    if (!is_emailable_address(clean_AntiSPAM($v), $debug))
+        echo " not";
+    echo " valid\n<br>";
+}
+
+?>
+<hr>
+The jesusmc@scripps.edu, jmcastagnetto@yahoo.com and jcastagnetto@yahoo.com 
+should validate OK as of 2001-02-28 --- JMC
+</body>
+</html>
