bee75ba2882ccb98d9a5ab2010f537d52de5c3d2
jim winstead <jimw@php.net>
2001-03-01 01:29:50 +0000

    lighten the load on the mirrors by 30 megs.

--- downloads.php
+++ downloads.php
@@ -46,40 +46,6 @@ $server = ($SERVER_PORT == "81" ? $SERVER_NAME . ":81" : $SERVER_NAME);
 </ul>
 
 
-<H3>Previous Releases of PHP 4.0</H3>
-
-<b>Complete Source Code</b><BR>
-<UL>
-  <li><a href="http://www.php.net/do_download.php?download_file=php-4.0.3pl1.tar.gz&source_site=<?php echo $server; ?>">PHP 4.0.3pl1 </a></li>
-  <li><a href="http://www.php.net/do_download.php?download_file=php-4.0.2.tar.gz&source_site=<?php echo $server; ?>">PHP 4.0.2</a></li>
-  <li><a href="http://www.php.net/do_download.php?download_file=php-4.0.1pl2.tar.gz&source_site=<?php echo $server; ?>">PHP 4.0.1pl2</a></li>
-  <li><a href="http://www.php.net/do_download.php?download_file=php-4.0.1RC2.tar.gz&source_site=<?php echo $server; ?>">PHP 4.0.1 Release Candidate 2</a></li>
-  <li><a href="http://www.php.net/do_download.php?download_file=php-4.0.1RC.tar.gz&source_site=<?php echo $server; ?>">PHP 4.0.1 Release Candidate</a></li>
-  <li><a href="http://www.php.net/do_download.php?download_file=php-4.0.0.tar.gz&source_site=<?php echo $server; ?>">PHP 4.0.0</a></li>
-</UL>
-
-<b>Win32 Binaries</b><BR>
-<UL>
-  <li><a href="http://www.php.net/do_download.php?download_file=php-4.0.3pl1-Win32.zip&source_site=<?php echo $server; ?>">PHP 4.0.3pl1</a></li>
-  <li><a href="http://www.php.net/do_download.php?download_file=php-4.0.2-Win32.zip&source_site=<?php echo $server; ?>">PHP 4.0.2</a></li>
-  <li><a href="http://www.php.net/do_download.php?download_file=php-4.0.1pl2-Win32.zip&source_site=<?php echo $server; ?>">PHP 4.0.1pl2</a></li>
-  <li><a href="http://www.php.net/do_download.php?download_file=php-4.0.1pl1-Win32.zip&source_site=<?php echo $server; ?>">PHP 4.0.1pl1</a></li>
-  <li><a href="http://www.php.net/do_download.php?download_file=php-4.0.1-Win32.zip&source_site=<?php echo $server; ?>">PHP 4.0.1</a></li>
-  <li><a href="http://www.php.net/do_download.php?download_file=php-4.0.0-Win32.zip&source_site=<?php echo $server; ?>">PHP 4.0.0</a></li>
-  <li>Servlet Server API:
-    <a href="http://www.php.net/do_download.php?download_file=php-4.0.0-Win32-servlet-jdk118.zip&source_site=<?php echo $server; ?>">jdk1.1.8</a>,
-    <a href="http://www.php.net/do_download.php?download_file=php-4.0.0-Win32-servlet-jdk122.zip&source_site=<?php echo $server; ?>">jdk1.2.2</a>,
-    <a href="http://www.php.net/do_download.php?download_file=php-4.0.0-Win32-servlet-jdk13.zip&source_site=<?php echo $server; ?>">jdk1.3</a>
-  <li>Java extensions:
-    <a href="http://www.php.net/do_download.php?download_file=php-4.0.0-Win32-java-jdk118.zip&source_site=<?php echo $server; ?>">jdk1.1.8</a>,
-    <a href="http://www.php.net/do_download.php?download_file=php-4.0.0-Win32-java-jdk122.zip&source_site=<?php echo $server; ?>">jdk1.2.2</a>,
-    <a href="http://www.php.net/do_download.php?download_file=php-4.0.0-Win32-java-jdk13.zip&source_site=<?php echo $server; ?>">jdk1.3</a>
-</UL>
-
-<P>
-<I>Previous releases are listed for informational purposes only.  You should being using the latest release.</I>
-</P>
-
 <hr noshade>
 
 
