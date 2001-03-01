6450ebf517974f6ca3b7dee6ffa3c91d107724fc
jim winstead <jimw@php.net>
2001-03-01 21:42:48 +0000

    fix a couple of dumb typos to fix default language of manual on mirrors

--- manual/html/index.php
+++ manual/html/index.php
@@ -3,6 +3,6 @@ require "shared.inc";
 
 $lang = $MIRRORS[$MYSITE][6];
 if (is_dir("../$lang/html")) {
-  header("Location: ../lang/html/");
+  header("Location: ../$lang/html/");
 }
 header("Location: ../en/html/");
--- manual/index.php
+++ manual/index.php
@@ -3,6 +3,6 @@ require "shared.inc";
 
 $lang = $MIRRORS[$MYSITE][6];
 if (is_dir($lang)) {
-  header("Location: lang/");
+  header("Location: $lang/");
 }
 header("Location: en/");
