6bc92852499cd3bb7aa83e7ebc1322d42a019ff4
jim winstead <jimw@php.net>
2001-03-01 01:15:05 +0000

    matrix of language/format for downloads

--- docs.php
+++ docs.php
@@ -1,6 +1,31 @@
 <?php
 require("shared.inc");
 commonHeader("Documentation ");
+
+$languages = array(
+ "en" => "English"
+,"cs" => "Czech"
+,"de" => "German"
+,"es" => "Spanish"
+,"fr" => "French"
+,"hu" => "Hungarian"
+,"it" => "Italian"
+,"ja" => "Japanese"
+,"kr" => "Korean"
+,"nl" => "Dutch"
+,"pt_BR" => "Brazilian Portuguese"
+);
+
+$formats = array(
+ "bigmanual.html.gz" => array("Single HTML file (compressed)", "html.gz")
+,"manual.zip" => array("Seperate HTML files (zip archive)", "zip")
+,"manual.tar.gz" =>  array("Seperate HTML files (compressed tar archive)", "tar.gz")
+,"manual_doc.pdb" => array("PalmPilot DOC", "doc.pdb")
+,"manual_isilo.pdb" => array("PalmPilot iSilo", "isilo.pdb")
+,"manual.txt.gz" => array("Plain text (compressed)", "txt.gz")
+,"manual.pdf" => array("PDF", "pdf")
+,"manual.chm" => array("Windows HTML Help", "chm")
+);
 ?>
 
 <H1>PHP FAQ</H1>
@@ -23,12 +48,6 @@ $prefix = ($MYSITE=='http://bugs.php.net/') ? 'http://www.php.net' : '';
     same manual in a light-weight, HTML 3.2 version without any bells or
     whistles.
 
-<li><a href="http://snaps.php.net/manual/">Latest Version and International Versions</a>.
-    Depending on how industrious the documentation team has been, you may find more
-    up-to-date info in this version of the manual than you will at one of the above
-    links.  You can also browse or download translated versions here.
-</ul>
-
 <H2>PHP Annotated Manuals</H2>
 <ul>
 <LI><A href="http://www.php.net/manual/">PHP Annotated Manual</A>. This
@@ -37,68 +56,50 @@ $prefix = ($MYSITE=='http://bugs.php.net/') ? 'http://www.php.net' : '';
     all of the documentation, this will be the most current version,
     as it is updated (and annotated) daily.
 
-<LI><A href="http://www.php.net/manual/browse-errata.php3">PHP Manual
+<LI><A href="http://www.php.net/manual/browse-errata.php">PHP Manual
     Errata</A>. This is the collected errata from the on-line errata/note
     system.
 </ul>
 
 <H1>Downloadable PHP Manuals</H1>
-<H2>HTML Version</H2>
-<ul>
-<li><?php download_link("manual/en/bigmanual.html", "PHP HTML Manual (one BIG file)"); ?>
-<li><?php download_link("manual/en/manual.zip", "PHP HTML Manual ZIP File"); ?>
-<li><?php download_link("manual/en/manual.tar.gz", "PHP HTML Manual .tar.gz File"); ?>
-</ul>
-<H2>Latest versions (daily snapshots)</H2>
-<ul>
-<li><?php download_link("http://snaps.php.net/manual/manual-en-current.tar.gz", "PHP HTML Manual (English) .tar.gz File"); ?>
-<li><?php download_link("http://snaps.php.net/manual/manual-de-current.tar.gz", "PHP HTML Manual (German) .tar.gz File"); ?>
-<li><?php download_link("http://snaps.php.net/manual/manual-it-current.tar.gz", "PHP HTML Manual (Italian) .tar.gz File"); ?>
-<li><?php download_link("http://snaps.php.net/manual/manual-ja-current.tar.gz", "PHP HTML Manual (Japanese) .tar.gz File"); ?>
-<li><?php download_link("http://snaps.php.net/manual/manual-fr-current.tar.gz", "PHP HTML Manual (French) .tar.gz File"); ?>
-<li><?php download_link("http://snaps.php.net/manual/manual-es-current.tar.gz", "PHP HTML Manual (Spanish) .tar.gz File"); ?>
-<li><?php download_link("http://snaps.php.net/manual/manual-hu-current.tar.gz", "PHP HTML Manual (Hungarian) .tar.gz File"); ?>
-</ul>
-<H2>WinHelp (CHM) versions (January 12, 2001)</H2>
-<ul>
-<li><?php download_link("manual.chm", "PHP Manual (English)"); ?>
-<li><?php download_link("manual_de.chm", "PHP Manual (German)"); ?>
-<li><?php download_link("manual_it.chm", "PHP Manual (Italian)"); ?>
-<li><?php download_link("manual_nl.chm", "PHP Manual (Dutch)"); ?>
-<li><?php download_link("manual_fr.chm", "PHP Manual (French)"); ?>
-<li><?php download_link("manual_es.chm", "PHP Manual (Spanish)"); ?>
-<li><?php download_link("manual_hu.chm", "PHP Manual (Hungarian)"); ?>
-<li><?php download_link("manual_pt_BR.chm", "PHP Manual (Portugese)"); ?>
-</ul>
 
-<H2>PDF Versions (February 25, 2001)</H2>
-<ul>
-<li><?php download_link("http://snaps.php.net/~jah/pdf/manual-cs.pdf", "PHP PDF Manual (Czech)"); ?>
-<li><?php download_link("http://snaps.php.net/~jah/pdf/manual-nl.pdf", "PHP PDF Manual (Dutch)"); ?>
-<li><?php download_link("http://snaps.php.net/~jah/pdf/manual-en.pdf", "PHP PDF Manual (English)"); ?>
-<li><?php download_link("http://snaps.php.net/~jah/pdf/manual-fr.pdf", "PHP PDF Manual (French)"); ?>
-<li><?php download_link("http://snaps.php.net/~jah/pdf/manual-de.pdf", "PHP PDF Manual (German)"); ?>
-<li><?php download_link("http://snaps.php.net/~jah/pdf/manual-hu.pdf", "PHP PDF Manual (Hungarian)"); ?>
-<li><?php download_link("http://snaps.php.net/~jah/pdf/manual-it.pdf", "PHP PDF Manual (Italian)"); ?>
-<li><?php download_link("http://snaps.php.net/~jah/pdf/manual-pt_BR.pdf", "PHP PDF Manual (Portuguese)"); ?>
-<li><?php download_link("http://snaps.php.net/~jah/pdf/manual-es.pdf", "PHP PDF Manual (Spanish)"); ?>
-</ul>
+<p>The PHP manual is also available in a selection of downloadable
+formats for off-line use. Pick your format and language from the
+table below:</p>
 
-<H2>Other Versions</H2>
-<ul>
-<li><?php download_link("manual.rtf", "PHP Manual RTF File"); ?>
-<li><?php download_link("manual_isilo.pdb","PHP Manual - PalmPilot Isilo format"); ?>
-<li><?php download_link("manual/en/manual_doc.pdb","PHP Manual - PalmPilot DOC format"); ?>
-</ul>
-
-<H1>Translations</H1>
-<ul>
-<li><a href="http://www.cityfujisawa.ne.jp/~louis/apps/phpfi/index.html"
-    >Japanese translation of the PHP manual</a>
-
-<li><a href="http://dev.nexen.net/docs/php/annotee/manuel_toc.php">Version fran&ccedil;aise annot&eacute;e.</a><br>
-Versions t&eacute;l&eacute;chargeables <a href="http://dev.nexen.net/docs/php/chargement.html">disponibles</a>. (PDF, HTML, doc, ps, palm).
-</ul>
+<table border="0" cellpadding="2" cellspacing="1" width="100%">
+ <tr bgcolor="#cccccc">
+  <td></td>
+  <?while (list($k,$v) = each($languages)) {
+      echo "<th>$v</th>\n";
+    }?>
+ </tr>
+ <?while (list($fn,$details) = each($formats)) {
+     echo "<tr>\n<td bgcolor=\"#dddddd\"><b>$details[0]</b></td>\n";
+     reset($languages);
+     while (list($langcode,$language) = each($languages)) {
+       echo "<td align=\"center\" bgcolor=\"#eeeeee\">";
+       # temporary hack until pdf are auto-generated
+       if ($fn == "manual.pdf") {
+         echo "<a href=\"http://snaps.php.net/~jah/pdf/manual-$langcode.pdf\">$details[1]</a></td>";
+         continue;
+       }
+       # temporary hack until chm are auto-generated
+       if ($fn == "manual.chm") {
+         echo "<a href=\"distributions/manual_$langcode.chm\">$details[1]</a></td>";
+         continue;
+       }
+       $size = @filesize("manual/$langcode/$fn");
+       if ($size) {
+         echo "<a href=\"manual/$langcode/$fn\">$details[1]</a>";
+       }
+       else {
+         echo "&nbsp;";
+       }
+       echo "</td>\n";
+     }
+   }?>
+</table>
 
 <H1>More information</H1>
 <ul>
@@ -122,10 +123,8 @@ with the development of PHP, read this.
 
 <H1>Old/unsupported information</H1>
 <ul>
-<li><a href="/manual/phpfi2.html">PHP/FI 2.0 Manual</a>. This includes
-    discussions on a few topics that aren't covered in the PHP 3 manual
-    yet.
+<li><a href="/manual/phpfi2.html">PHP/FI 2.0 Manual</a>. 
 </ul>
 
 
-<? commonFooter("") ?>
+<?php commonFooter("") ?>
