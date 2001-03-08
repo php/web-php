3a879349b003e482b4d1d8cbfa75c47189df7264
jim winstead <jimw@php.net>
2001-03-08 20:33:17 +0000

    more notes

--- tips.php
+++ tips.php
@@ -34,7 +34,7 @@ Send your suggestions for tips to <A HREF="mailto:webmaster@php.net">webmaster@p
 
 <?php tipTitle('Adding the Quick Reference to most Javascript-capable Browsers', 'Jim Winstead', '07-Mar-2001'); ?>
 
-<p>Just right-click (command-click for Macintosh users) on this link: <a href="javascript:void(q=prompt('PHP Reference:',''));if(q)location.href='http://www.php.net/manual-lookup.php?lang=en&function='+escape(q)" title="PHP Quick Reference">PHP Quick Reference</A> and add it to your bookmarks.
+<p>Just right-click (command-click for Macintosh users) on this link: <a href="javascript:void(q=prompt('PHP Reference:',''));if(q)location.href='http://www.php.net/manual-lookup.php?lang=en&function='+escape(q)" title="PHP Quick Reference">PHP Quick Reference</A> and add it to your bookmarks. With some browsers, you may need to edit your bookmarks manually to give the bookmark an easy-to-remember title. You can also edit the bookmark to change the default language by changing 'lang=en' to whatever language you want to use by default. (For example, to pull up the German translation of the manual, you would replace "lang=en" with "lang=de".)
 </p>
 
 
