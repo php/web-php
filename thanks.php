4a008857220d79f5ae5e58d40bd59957cc2f816a
jim winstead <jimw@php.net>
2001-03-08 22:29:16 +0000

    clean up a tiny bit

--- thanks.php
+++ thanks.php
@@ -6,18 +6,12 @@ function makeEntry($heading) {
     global $FONTFACE;
 ?>
 <TR bgcolor='#D0D0D0' valign=top>
-<TD ALIGN=left WIDTH=18><IMG ALT=" " SRC="/gifs/gcap-lefttop.gif" WIDTH=18 HEIGHT=18 BORDER=0><BR></TD>
 <TD ALIGN=left rowspan=2 valign=middle>
 <?
     echo "<FONT FACE=\"$FONTFACE\" SIZE=-1><FONT SIZE=+0>\n";
     echo "<B>$heading</B><BR></FONT>\n";
 ?>
 </TD>
-<TD ALIGN=right WIDTH=18><IMG ALT=" " SRC="/gifs/gcap-righttop.gif" WIDTH=18 HEIGHT=18 BORDER=0><BR></TD>
-</TR>
-<TR VALIGN=bottom bgcolor='#D0D0D0'>
-<TD ALIGN=left><IMG alt=" " SRC="/gifs/gcap-leftbot.gif" WIDTH=18 HEIGHT=18 BORDER=0><BR></TD>
-<TD ALIGN=right><IMG alt=" " SRC="/gifs/gcap-rightbot.gif" WIDTH=18 HEIGHT=18 BORDER=0><BR></TD>
 </TR>
 <TR VALIGN=top>
 <TD WIDTH=18><BR></TD>
@@ -50,10 +44,6 @@ For providing DNS services for the php.* domains.
 For providing the server and bandwidth that runs the PHP mailing lists, CVS server, 
 snapshot site and a number of other services.
 <?endEntry()?>
-
-<TR VALIGN=bottom bgcolor='#D0D0D0'>
-<TD ALIGN=left><IMG alt=" " SRC="/gifs/gcap-leftbot.gif" WIDTH=18 HEIGHT=18 BORDER=0><BR></TD>
-<TD ALIGN=right><IMG alt=" " SRC="/gifs/gcap-rightbot.gif" WIDTH=18 HEIGHT=18 BORDER=0><BR></TD>
 
 </TABLE>
 <? commonFooter(); ?>
