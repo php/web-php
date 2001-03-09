06640e881ebf4e5b4406f568707321ebb8dea0fb
Colin Viebrock <cmv@php.net>
2001-03-09 19:47:44 +0000

    Credits open in the main window again ... use smarts to strip off unneeded HTML.

--- credits.php
+++ credits.php
@@ -1,10 +1,26 @@
 <?php
-# require_once 'prepend.inc';
-# commonHeader("PHP Credits");
+require_once 'prepend.inc';
 
-# print "<p align=\"center\">";
+if (function_exists('ob_start')) {
+	ob_start();
+	phpcredits();
+	$credits = ob_get_contents();
+	ob_end_clean();
+
+	# strip all but the body
+	preg_match('/<body.*?>(.*)<\/body>/ims', $credits, $m );
+
+	# strip out any style
+	$credits = preg_replace('/<style.*?>.*<\/style>/ims', '', $m[1] );
+
+	if ($credits) {
+		commonHeader("PHP Credits", 1);
+		echo $credits;
+		commonFooter(); 
+		exit;
+	}
+}
+
 phpcredits();
-# print "</p>";
 
-# commonFooter();
 ?>
