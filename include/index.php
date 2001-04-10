1509e4540d1f157097fd2411e0cb1bd154efb3ee
Colin Viebrock <cmv@php.net>
2001-04-10 17:20:35 +0000

    Fix so that xx.php.net/include is more useful :)

--- include/index.php
+++ include/index.php
@@ -1,3 +1,11 @@
-<?
-	Header("Location: /");
+<?php
+require_once 'prepend.inc';
+
+$lang = $MIRRORS[$MYSITE][6];
+if (is_dir($lang)) {
+  header("Location: /manual/$lang/function.include.php");
+}
+header("Location: /manual/en/function.include.php");
+
+
 ?>
