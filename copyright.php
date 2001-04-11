2a371c3d587cf57fadb9b173b79cf870bd201350
Colin Viebrock <cmv@php.net>
2001-04-11 15:14:38 +0000

    - fiddle with stylesheet a bit - fix copyright notice - add copyright.php page (linked from commonFooter) ... we've already got copycats :) - remove unused license.html page

---
+++ copyright.php
@@ -0,0 +1,67 @@
+<?php
+require_once 'prepend.inc';
+
+commonHeader("Copyright and License");
+  
+?>
+
+<h1>Copyright and Licence</h1>
+
+
+<h2>PHP License</h2>
+
+<P>
+For information on the PHP License (i.e. using the PHP language),
+<?php echo make_link('/license/', 'click here'); ?>.
+</P>
+
+
+<h2>Website Copyright</h2>
+
+<P>
+The code, text, PHP logo, and graphical elements on this website 
+and the mirror websites (the "Site") are Copyright &copy; 2001 
+The PHP Group.  All rights reserved.
+</P>
+
+<P>
+Except as otherwise indicated elsewhere on this Site, you are free
+view, download and print the documents and information available 
+on this Site subject to the following conditions:
+</P>
+<UL>
+<LI>The documents and information may be used solely for personal, 
+informational, internal, non-commercial purposes. 
+<LI>The documents and information may not be modified or altered 
+in any way without giving credit to the original source.
+<LI>You may not remove any copyright or other proprietary notices 
+contained in the documents and information. 
+<LI>The PHP Group reserves the right to revoke the authorization 
+to view, download and print the documents and information available 
+on this Site at any time, and any such use shall be discontinued 
+immediately upon written notice from The PHP Group.
+<LI>The rights granted to you constitute a license and not a transfer 
+of title. 
+<LI>The rights specified above to view, download and print the 
+documents and information available on this Site are not applicable 
+to the graphical elements, design or layout of this Site. Elements 
+of this Site are protected by trade dress and other laws and may not 
+be copied or imitated in whole or in part. 
+</UL>
+
+<P>
+You can contact the webmaster at 
+<?php echo make_link('mailto:webmaster@php.net', 'webmaster@php.net'); ?>.
+</P>
+
+<P>
+For more information on the PHP Group and the PHP project, please see 
+<?php echo make_link('http://www.php.net/', 'http://www.php.net/'); ?>.
+</P>
+
+<BR>
+
+<?php
+commonFooter();
+?>
+
