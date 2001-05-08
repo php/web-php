f6c7c89e03345247bc7f6a4f3e1eef79ea2c4af6
foobar <sniper@php.net>
2001-05-08 12:13:13 +0000

    Lite bug stats page.

---
+++ lstats.php
@@ -0,0 +1,53 @@
+<?php
+
+if(!strstr($MYSITE,"www.php.net")) {
+	Header("Location: http://www.php.net/lstats.php");
+	exit;
+}
+	
+	function status_print ($status, $num) 
+	{
+		$str = ucfirst($status).":";
+		$str.= str_repeat(" ", 14-strlen($str));
+		$str.= "<!-- NUM -->$num<!-- /NUM -->\n<br>";
+		echo $str;
+	}
+
+	$statuses = array (	
+						"open", 
+						"critical", 
+						"closed", 
+						"analyzed", 
+						"suspended", 
+						"duplicate", 
+						"assigned", 
+						"feedback", 
+						"bogus"
+				);
+
+
+	mysql_connect("localhost","nobody","");
+	mysql_select_db("php3");
+
+
+	/* REMOVE THIS!!! */
+	if(isset($create_index_for_status) && $create_index_for_status == "please") {
+		mysql_query("CREATE INDEX idx_status on bugdb(status)");
+	}
+
+	echo "<h3>Bug stats:</h3><pre>\n";	
+	foreach($statuses as $status) {
+		$query = "SELECT count(id) from bugdb WHERE";
+		if ($phpver > 0) {
+			$query .= " php_version LIKE '" . $phpver . "%' AND";
+		}
+		$query.= " status='$status' AND bug_type NOT LIKE '%Change Request%'";
+
+		$result=mysql_unbuffered_query($query);
+		$row=mysql_fetch_row($result);
+		mysql_freeresult($result);
+		
+		status_print($status, $row[0]);
+	}
+	echo "</pre>\n";
+?>
