9988c355d68d6f1f52b953b43d069c3d1a75e868
jim winstead <jimw@php.net>
2001-05-18 03:41:47 +0000

    update the cvs account request instructions, and expand the 'purpose' field to a textbox to encourage people to write more

--- cvs-php.php
+++ cvs-php.php
@@ -9,58 +9,109 @@ Using CVS for PHP Development
 
 <?php if(empty($fullname)){ ?>
 <p>
-All PHP development is done through a distributed revision control system called
-CVS.  This helps us track changes and it makes it possible for people located
-in all corners of the world to collaborate on a project without having to worry
-about stepping on each others' toes.
+All PHP development is done through a distributed revision control system
+called <a href="http://www.cvshome.org/">CVS</a>.  This helps us track changes
+and it makes it possible for people located in all corners of the world to
+collaborate on a project without having to worry about stepping on each others'
+toes.
 </p>
 <p>
-Please note that you do <b>not</b> need a CVS account to <b>access</b> the CVS tree.  See
-<a href="http://cvs.php.net">cvs.php.net</a> for details.  You only need your
-own CVS account if you will be regularly committing things to the CVS tree.
+Please note that you do <b>not</b> need a CVS account to <a
+href="/anoncvs.php"><b>access</b> the CVS tree</a>, to use PHP, or to write PHP
+scripts. You only need a CVS account if you will be a regular contributor to
+the development of PHP itself.
 </p>
 <p>
 And once again since people just don't seem to understand this point:
 </p>
-<p>
 <table>
-<tr><td><b>Does <u>Not</u> Require CVS Account</b></td>	<td>&nbsp;</td><td><b>Requires CVS Account</b></td></tr>
-<tr><td>Learning PHP</td>								<td>&nbsp;</td><td>Coding PHP source in C</td></tr>
-<tr><td>Coding in PHP</td>								<td>&nbsp;</td><td>Creating approved modules in the PHP tree</td></tr>
-<tr><td>Reading PHP source</td>							<td>&nbsp;</td><td>&nbsp;</td></tr>
-<tr><td>Learning coding PHP source in C</td>			<td>&nbsp;</td><td>&nbsp;</td></tr>
-<tr><td>Using PHP modules</td>							<td>&nbsp;</td><td>&nbsp;</td></tr>
-<tr><td>Creating experimental PHP modules</td>			<td>&nbsp;</td><td>&nbsp;</td></tr>
-<tr><td>One-time patch contributions</td>				<td>&nbsp;</td><td>&nbsp;</td></tr>
-<tr><td>Adding notes to documentation</td>				<td>&nbsp;</td><td>Authoring PHP documentation</td></tr>
-<tr><td>Writing web pages with PHP</td>					<td>&nbsp;</td></tr>
+ <tr bgcolor="#cccccc">
+  <td><b>Does <u>Not</u> Require CVS Account</b></td>
+  <td>&nbsp;</td>
+  <td><b>Requires CVS Account</b></td></tr>
+ </tr>
+ <tr>
+  <td bgcolor="#dddddd">Learning PHP</td>
+  <td>&nbsp;</td>
+  <td bgcolor="#eeeeee">Developing the PHP runtime</td>
+ </tr>
+ <tr>
+  <td bgcolor="#dddddd">Coding in PHP</td>
+  <td>&nbsp;</td>
+  <td bgcolor="#eeeeee">Maintaining an official, bundled PHP extension</td>
+ </tr>
+ <tr>
+  <td bgcolor="#dddddd">Reading the PHP source</td>
+  <td>&nbsp;</td>
+  <td bgcolor="#eeeeee"></td>
+ </tr>
+ <tr>
+  <td bgcolor="#dddddd">Using PHP extensions</td>
+  <td>&nbsp;</td>
+  <td bgcolor="#eeeeee"></td>
+ </tr>
+ <tr>
+  <td bgcolor="#dddddd">Creating experimental PHP extensions</td>
+  <td>&nbsp;</td>
+  <td bgcolor="#eeeeee"></td>
+ </tr>
+ <tr>
+  <td bgcolor="#dddddd">Submitting a patch to PHP</td>
+  <td>&nbsp;</td>
+  <td bgcolor="#eeeeee"></td>
+ </tr>
+ <tr>
+  <td bgcolor="#dddddd">Adding notes to the documentation</td>
+  <td>&nbsp;</td>
+  <td bgcolor="#eeeeee"></td>
+ </tr>
+ <tr>
+  <td bgcolor="#dddddd">Writing web pages with PHP</td>
+  <td>&nbsp;</td>
+  <td bgcolor="#eeeeee"></td>
+ </tr>
 </table>
-</p>
 
 <p>
-If you are contributing a patch, a small fix, or another minor change you
-are not required to obtain a CVS account before submitting it. If you are subscribed
-to <a href="mailto: php-dev@lists.php.net">php-dev@lists.php.net</a>,
-you can simply post your patch and request for it to be added to PHP. Submitting
-patches to the PHP-dev list <b>before</b> requesting full CVS access is
-suggested, so the PHP development team can develop a working relationship
-with you and your code. It is suggested that all PHP developers subscribe
-to this list.
+If you are contributing a patch, a small fix, or another minor change you do
+not need to ask for a CVS account before submitting it. Just send your patch to
+<a href="mailto:php-dev@lists.php.net">php-dev@lists.php.net</a>. You should
+subscribe to that list (send a blank mail to <a
+href="mailto:php-dev-subscribe@lists.php.net">php-dev-subscribe@lists.php.net</a>)
+to participate in any discussion your patch generates! Your patch not get
+noticed the first time. Make sure that when you send your patch, you explain
+what it does. Make sure you use a clear subject when sending your patch (you
+might even want to prefix it with "[PATCH]"). If nobody seems to take notice
+after a few days, you might want to try resubmitting it. Your original message
+might have gotten missed because of another heated discussion.
 </p>
 <p>
-Ok, if you are still reading, I assume you may actually need a CVS account.
-If you are not previously known to the PHP Development Team, then this is not 
-an automatic process.  Your best bet is to send mail to <a href="mailto: group@php.net">
-group@php.net</a> and explain what you have in mind and perhaps give a bit of
-background on yourself.  If you were told by a PHP developer to come here and 
-fill out this form, you can skip sending the email to group@php.net.
+Submitting patches and participating in the discussion on the php-dev list
+<b>before</b> requesting full CVS access is strongly suggested, so the PHP
+development team can get to know you and what you'll be contributing. It is
+suggested that all PHP developers (people developing PHP itself, not people
+developing <b>in</b> PHP) subscribe to this list. (Similarly, if you plan on
+contributing documentation, you should subscribe to the documentation mailing
+list by sending a blank mail to <a
+href="mailto:phpdoc-subscribe@lists.php.net">phpdoc-subscribe@lists.php.net</a>).
 </p>
 <p>
-If, you have already established yourself in the above manner you can submit
-a CVS account request here: (only the encrypted version of the password is sent)
-In the purpose field, put a couple of words describing what you want to work on.
-This is mostly to jog our memories and allow us to match the account request with 
-whatever previous correspondence that may taken place.
+Okay, if you are still reading, I assume you may actually need a CVS account.
+This is <b>not</b> an automatic process. Fill in the form below to request an
+account. In the box next to "Purpose", describe what it is that you intend to
+do with CVS access. If it isn't clear from what you've described already, tell
+us what parts of the CVS repository you need access to (for example, "phpdoc"
+is the documentation tree, "php4/ext/mysql" is the PHP 4 Mysql extension). If
+someone told you you to fill out the form here, make sure to mention them here!
+</p>
+<P>
+The CVS account, once granted and activated (which could take a while, so be
+patient!), gives you access to a number of things.  First, and most importantly,
+it gives you access to modify those parts of PHP CVS tree for which you have
+requested and been granted access.  It also allows you to comment on and close
+bugs in our <a href="http://bugs.php.net/">bug database</a>, and allows you to
+<a href="http://www.php.net/manual/admin-notes.php">modify they documentation
+notes in the annotated manual</a>.
 </p>
 <p>
 Please note:<br>
@@ -76,10 +127,10 @@ you don't!
 <table>
 <tr><th>Full Name: </th><td><input type=text size=50 name=fullname></td></tr>
 <tr><th>Email: </th><td><input type=text size=50 name=email></td></tr>
-<tr><th>Purpose: </th><td><input type=text size=50 name=purpose></td></tr>
+<tr><th valign="top">Purpose: </th><td><textarea cols=50 rows=5 name=purpose></textarea></td></tr>
 <tr><th>User ID: </th><td><input type=text size=10 name=id></td></tr>
 <tr><th>Requested Password: </th><td><input type=password size=10 name=password></td></tr>
-<tr><th></th><td>By checking this box you ensure having read <I>all</I> comments above <input type="checkbox" name="checkread" value="1"></td></tr>
+<tr><th></th><td>By checking this box you are claiming that you have read <I>all</I> of the comments above <input type="checkbox" name="checkread" value="1"></td></tr>
 <tr><th>&nbsp;</th><td><input type=submit value="Send It"></td></tr>
 </table>
 </form>
@@ -93,7 +144,12 @@ mail("php-dev@lists.php.net", "CVS Account Request",
 		'From: "CVS Account Request" <donotemail@php.net>');
 ?>
 <p>
-Thank you.  Your request has been sent.
+Thank you.  Your request has been sent. You should hear something within the
+next week or so. If you haven't heard anything by then, send an email to <a
+href="mailto:group@php.net">group@php.net</a> to let us know that we've
+forgotten you, but you haven't forgotten us! (It happens. There's several of
+us, and sometimes we think that someone else has taken care of your request,
+and they think that we took care of it. Sorry.)
 </p>
 <?php
 } elseif ($fullname && $email && $password && $purpose && !$checkread) { 
@@ -110,45 +166,40 @@ Thank you.  Your request has been sent.
 <?php
 }
 ?>
-<P>
-The CVS account, once granted and activated, gives you access to a number of things.  First, and most important it gives you access
-to modify the PHP CVS tree.  It also allows you to comment on and close bugs in the PHP bugs interface
-as well as modifying the documentation notes in the annotated manual.  The relevant links for these
-three things are:
-<ul>
-<li><a href="http://viewcvs.php.net/">CVS Web Interface for our CVS repository</a>
-<li><a href="http://bugs.php.net/">PHP Bugs Database</a>
-<li><a href="http://www.php.net/manual/admin-notes.php">Manual Errata Administration</a>
-</ul>
+<p>
+If you are not familiar with CVS, you should have a look at the various
+documentation resources available at <a
+href="http://www.cvshome.org/">CVShome.org</a>.  This is also where to get the
+most recent version of the CVS client.
 </p>
 <p>
-If you are not familiar with CVS, you should have a look at the various documentation resources available
-at <a href="http://www.cvshome.org/">CVShome.org</a>.  This is also where to get the most recent version
-of the CVS client.
+All CVS commit messages get sent to the php-cvs mailing list.  You should
+subscribe yourself to this mailing list.  Instructions for subscribing are on
+the <a href="/support.php">Support</a> page.
 </p>
 <p>
-All CVS commit messages get sent to the php-cvs mailing list.  You should subscribe yourself to this mailing
-list.  Instructions for subscribing are on the <a href="/support.php">Support</a> page.
-</p>
-<p>
-CVS itself is quite easy to use.  Follow the steps listed on the <a href="anoncvs.php">anonymous CVS</a>
-page for checking out your tree.  Substitute your own user name and password for the cvsread/phpfi combination listed there.
-You will not be able to do this until you receive confirmation of your account having been created.
+CVS itself is quite easy to use.  Follow the steps listed on the <a
+href="anoncvs.php">anonymous CVS</a> page for checking out your tree.
+Substitute your own user name and password for the cvsread/phpfi combination
+listed there.  You will not be able to do this until you receive confirmation
+of your account having been created.
 </p>
 
 <p>
-Next, once you have your CVS tree you need to know the following commands.  They should all be executed from within
-the checked out tree.  eg. cd php4
+Next, once you have your CVS tree you need to know the following commands.
+They should all be executed from within the checked out tree.  eg. cd php4
 <dl>
 <dt><b><tt>cvs update -dP</tt></b>
-<dd>This fetches all updates made by others and brings your tree up to date.  Before starting to edit anything
-in the tree you should do this.  Generally you would do this whenever you see a CVS commit message on the php-dev
-mailing list.
+<dd>This fetches all updates made by others and brings your tree up to date.
+Before starting to edit anything in the tree you should do this.  Generally you
+would do this whenever you see a CVS commit message on the php-cvs mailing
+list.
 
 <dt><b><tt>cvs commit</tt></b>
-<dd>This commits any changes you have made anywhere in the tree.  A text editor will pop up and you will need to
-describe the changes you made.  Please provide a good description here as it may help people in the future when
-looking at your code changes.
+<dd>This commits any changes you have made anywhere in the tree.  A text editor
+will pop up and you will need to describe the changes you made.  Please provide
+a good description here as it may help people in the future when looking at
+your code changes.
 </dl>
 </p>
 
