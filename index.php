43e6fbe908fbd18b9a6e1159c3a6627c16d74160
Zeev Suraski <zeev@php.net>
2001-06-15 12:08:54 +0000

    fixes

--- index.php
+++ index.php
@@ -46,13 +46,13 @@ PHP conventions in Europe
 </h1>
 <p>
 <font class="newsDate">[14-Jun-2001]</font>
-The second International PHP Conference, taking part in Frankfurt, Germany this November
-has been announced.  Further details are available at the 
+The second International PHP Conference, taking part in Frankfurt, Germany this coming November
+has been announced.  Further details are available on the 
 <a href="http://www.php-kongress.de/2001/index_en.php">official Web site</a>.
 </p>
 <p>
 <? print_link("http://www.linuxtag.org/2001/english/30.html", make_image("linuxtag.gif", "LinuxTag", "right") ); ?>
-Also coming in July 5th-8th - 
+Also coming between July 5th-8th - 
 <a href="http://www.linuxtag.org/2001/english/30.html">LinuxTag 2001</a>,
 in Stuttgart, Germany, with <a href="http://www.linuxtag.org/2001/english/showitem.php3?item=273&lang=en">strong PHP presence</a>.
 </p>
