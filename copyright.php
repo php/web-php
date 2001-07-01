215555d02af547a6beea89d73b1539b277b43087
Gabor Hojtsy <goba@php.net>
2001-07-01 17:04:49 +0000

    Closing HTML tags, some layout issued corrected

--- copyright.php
+++ copyright.php
@@ -1,59 +1,51 @@
 <?php
 require_once 'prepend.inc';
-
-commonHeader("Copyright and License");
-  
+commonheader("Copyright and License");
 ?>
 
-<h1>Copyright and Licence</h1>
-
+<h1>Copyright and License</h1>
 
 <h2>PHP License</h2>
 
-<P>
+<p>
 For information on the PHP License (i.e. using the PHP language),
 <?php echo make_link('/license/', 'click here'); ?>.
-</P>
-
+</p>
 
 <h2>Website Copyright</h2>
 
-<P>
+<p>
 The code, text, PHP logo, and graphical elements on this website 
 and the mirror websites (the "Site") are Copyright &copy; 2001 
-The PHP Group.  All rights reserved.
-</P>
+The PHP Group. All rights reserved.
+</p>
 
-<P>
+<p>
 Except as otherwise indicated elsewhere on this Site, you are free
 to view, download and print the documents and information available 
 on this Site subject to the following conditions:
-</P>
-<UL>
-<LI>You may not remove any copyright or other proprietary notices
-contained in the documents and information on this Site.
-<LI>The rights granted to you constitute a license and not a transfer
-of title.
-<LI>The rights specified above to view, download and print the
-documents and information available on this Site are not applicable
-to the graphical elements, design or layout of this Site.  These
-elements of the Site are protected by trade dress and other laws
-and may not be copied or imitated in whole or in part.
-</UL>
+</p>
 
-<P>
+<ul>
+<li>You may not remove any copyright or other proprietary notices
+contained in the documents and information on this Site.</li>
+<li>The rights granted to you constitute a license and not a transfer
+of title.</li>
+<li>The rights specified above to view, download and print the
+documents and information available on this Site are not applicable
+to the graphical elements, design or layout of this Site. These
+elements of the Site are protected by trade dress and other laws
+and may not be copied or imitated in whole or in part.</li>
+</ul>
+
+<p>
 You can contact the webmaster at 
 <?php echo make_link('mailto:webmaster@php.net', 'webmaster@php.net'); ?>.
-</P>
+</p>
 
-<P>
+<p>
 For more information on the PHP Group and the PHP project, please see 
 <?php echo make_link('http://www.php.net/', 'http://www.php.net/'); ?>.
-</P>
+</p>
 
-<BR>
-
-<?php
-commonFooter();
-?>
-
+<?php commonfooter(); ?>
