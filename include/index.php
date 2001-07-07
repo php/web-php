3b7cef3d140141b0d30094c1ab1cc723c4a27e91
jim winstead <jimw@php.net>
2001-07-07 00:55:37 +0000

    why does /include/ redirect to the manual? go home instead.

--- include/index.php
+++ include/index.php
@@ -1,12 +1,2 @@
 <?php
-
-require_once 'prepend.inc';
-
-$lang = $MIRRORS[$MYSITE][6];
-if (is_dir($lang)) {
-  header("Location: /manual/$lang/function.include.php");
-}
-header("Location: /manual/en/function.include.php");
-
-
-?>
+header("Location: /");
