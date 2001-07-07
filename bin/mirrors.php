3c4c95b29cbc079b905c692880b3d1a145beafb3
jim winstead <jimw@php.net>
2001-07-07 00:43:21 +0000

    set the type of inactive mirrors so they won't show up in the dropdown

--- bin/mirrors.php
+++ bin/mirrors.php
@@ -6,6 +6,8 @@ if (!$res) die("error");
 echo "<?php\n\$MIRRORS = array(\n";
 while ($row = mysql_fetch_array($res)) {
   if (!strstr("http:",$row[hostname])) $row[hostname]="http://$row[hostname]/";
+  # set inactive mirrors to type 2 so they won't show up in the drop-down.
+  if (!$row[active]) $row[mirrortype] = 2;
   echo "  \"$row[hostname]\" => array(\"$row[cc]\",\"$row[providername]\",$row[has_stats],\"$row[providerurl]\",$row[mirrortype],$row[has_search],\"$row[lang]\"),\n";
 }
 echo '  0 => array("xx", "Unknown", 0, "/", 2, 0, "en")', "\n";
