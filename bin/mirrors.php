5e313e34dad00486c80c833ab23a79d652ccdbe0
jim winstead <jimw@php.net>
2001-07-07 00:12:53 +0000

    generate list of mirrors from database

---
+++ bin/mirrors.php
@@ -0,0 +1,13 @@
+<?php
+mysql_connect("localhost","nobody","");
+mysql_select_db("php3");
+$res = mysql_query("SELECT * FROM mirrors ORDER BY cc");
+if (!$res) die("error");
+echo "<?php\n\$MIRRORS = array(\n";
+while ($row = mysql_fetch_array($res)) {
+  if (!strstr("http:",$row[hostname])) $row[hostname]="http://$row[hostname]/";
+  echo "  \"$row[hostname]\" => array(\"$row[cc]\",\"$row[providername]\",$row[has_stats],\"$row[providerurl]\",$row[mirrortype],$row[has_search],\"$row[lang]\"),\n";
+}
+echo '  0 => array("xx", "Unknown", 0, "/", 2, 0, "en")', "\n";
+echo ");\n";
+echo "?>\n";
