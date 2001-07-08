ea9a5483a4389018fadf62f1982f21072fa5f025
Gabor Hojtsy <goba@php.net>
2001-07-08 11:29:45 +0000

    Some more cosmetics... We need a correct link system in the future :))

--- links.php
+++ links.php
@@ -21,89 +21,92 @@ commonHeader("Links");
 ?>
 
 <h1>Getting Started</h1>
-<p>On these sites you will find tutorials, articles and example scripts</p>
+
+<p>
+On these sites you will find tutorials, articles and example scripts
+</p>
 
 <h4>Closely related to PHP</h4>
 
 <ul>
 <li><a href="http://www.zend.com/">Zend.com</a><br>
 Zend Technologies' Web site, with lots of PHP related resources
-(articles, tutorials, case studies, PHP function table, etc.)<br>
+(articles, tutorials, case studies, PHP function table, etc.)<br></li>
 
 <li><a href="http://conf.php.net/">PHP Conference Material</a><br>
 Some PHP conference slide shows, presented at variuos meetings
-by PHP professionals<br>
+by PHP professionals<br></li>
 </ul>
 
 <h4>PHP Link Catalogs</h4>
 
 <ul>
 <li><a href="http://www.hotscripts.com/PHP/">HotScripts.com</a><br>
-PHP Books, Magazine Articles, Communities, Scripts and Programs, Softwares, Tips<br>
+PHP Books, Magazine Articles, Communities, Scripts and Programs, Softwares, Tips<br></li>
 
 <li><a href="http://php.resourceindex.com/">PHP Resource Index</a><br>
-Hundreds of PHP resources, categorized and searchable<br>
+Hundreds of PHP resources, categorized and searchable<br></li>
 
 <li><a href="http://nav.webring.yahoo.com/hub?ring=php&list">PHP Sites Webring</a><br>
-Find PHP Sites within this webring!<br>
+Find PHP Sites within this webring!<br></li>
 
 <li><a href="http://www.LinuxGuruz.org/">LinuxGuruz.org</a><br>
 Many Links and Tutorials, Articles and FAQ about Linux, Apache Web Server,
-HTML, PHP, MySQL ... and lots more<br>
+HTML, PHP, MySQL ... and lots more<br></li>
 </ul>
 
 <h4>Complex PHP Related Sites</h4>
 
 <ul>
 <li><a href="http://www.phpwizard.net/">phpWizard</a><br>
-PHP tutorials and programs<br>
+PHP tutorials and programs<br></li>
 
 <li><a href="http://www.phpbuilder.com/">PHPBuilder.com</a><br>
-A bunch of helpful PHP resources available here<br>
+A bunch of helpful PHP resources available here<br></li>
 
 <li><a href="http://www.devshed.com/Server_Side/PHP/">Developers Shed</a><br>
-A good set of articles introducing various PHP concepts<br>
+A good set of articles introducing various PHP concepts<br></li>
 
 <li><a href="http://www.weberdev.com/">WeberDev.com</a><br>
-PHP related resources (articles, tutorials, forums, advanced search etc.)<br>
+PHP related resources (articles, tutorials, forums, advanced search etc.)<br></li>
 
 <li><a href="http://www.opendeveloper.org/">Opendeveloper.org</a><br>
-Developing Open Web Sites With Linux-Apache-MySQL-PHP-Perl<br>
+Developing Open Web Sites With Linux-Apache-MySQL-PHP-Perl<br></li>
 
 <li><a href="http://www.deskcode.com/">DeskCode</a><br>
 Community-based programming site, featuring <a href="http://www.deskcode.com/phpcompiler/">PHPCompiler</a>
-which converts PHP scripts to Windows executable files (suitable for CD-ROMs, etc.)<br>
+which converts PHP scripts to Windows executable files (suitable for CD-ROMs, etc.)<br></li>
 </ul>
 
 <h4>PHP Scripts and Programs</h4>
 
 <ul>
 <li><a href="http://www.itworks.demon.co.uk/phpeditors.htm">PHP Editors List</a><br>
-A comprehensive list of editors you can use to edit PHP programs<br>
+A comprehensive list of editors you can use to edit PHP programs<br></li>
 
 <li><a href="http://phplib.netuse.de/">PHP Base Library</a><br>
 The start of what looks to be a promising collection of base code
-for developers of large PHP sites<br>
+for developers of large PHP sites<br></li>
 
 <li><a href="http://phpclasses.upperdesign.com/">The PHP Classes Repository</a><br>
-Useful PHP classes in a categorized listing<br>
+Useful PHP classes in a categorized listing<br></li>
 
 <li><a href="http://px.sklar.com/">PX - PHP Code Exchange</a><br>
 The PHP Code Exchange is a great place to start. Here you'll find many
 example scripts and useful functions, organized for easy finding. Includes
-a search function<br>
+a search function<br></li>
 
 <li><a href="http://www.evilwalrus.com/">Evil Walrus</a><br>
-A growing list of PHP scripts<br>
+A growing list of PHP scripts<br></li>
 
 <li><a href="http://www.iometrics.com/php/phplist.php3/">PHPioneers/ioMetrics Scripts</a><br>
-A collection of PHP scripts<br>
+A collection of PHP scripts<br></li>
 
 <li><a href="http://dev.nexen.net/scripts/index.php?langue=en">Nexen's scripts librairy</a><br>
-An exhaustive PHP scripts collection, with more 400 useful functions (English and French).<br>
+An exhaustive PHP scripts collection, with more 400 useful functions (English and French).<br></li>
 
 <li><a href="http://www.roadsend.com/siteManager/">SiteManager</a><br>
-A base library for PHP sites and developers<br>
+A base library for PHP sites and developers<br></li>
 
 </ul>
 
@@ -111,16 +114,16 @@ A base library for PHP sites and developers<br>
 
 <ul>
 <li><a href="http://www.find.com.au/tutorials/webdevelopment/php/default.asp?ct=21&ob=3">Tutorialfind</a><br>
-A growing list of PHP tutorials at TutorialFind<br>
+A growing list of PHP tutorials at TutorialFind<br></li>
 
 <li><a href="http://www.thescripts.com/serversidescripting/php/">TheScripts.com</a><br>
-Web Programming Resource - PHP Tutorials<br>
+Web Programming Resource - PHP Tutorials<br></li>
 
 <li><a href="http://www.awtrey.com/support/dbeweb/">Database enabled websites tutorial</a><br>
-Introductory tutorial<br>
+Introductory tutorial<br></li>
 
 <li><a href="http://www.e-gineer.com/instructions/">E-gineer Instructions</a><br>
-Lots of tutorials/instructions related to Apache, PHP and MySQL<br>
+Lots of tutorials/instructions related to Apache, PHP and MySQL<br></li>
 </ul>
 
 <h4>Non-English PHP Sites</h4>
@@ -130,75 +133,78 @@ Lots of tutorials/instructions related to Apache, PHP and MySQL<br>
 
 <ul>
 <li><a href="http://www.php-center.de/">PHP Center</a><br>
-German speaking portal site with tutorials, tips and tricks, job advertisements, articles and more<br>
+German speaking portal site with tutorials, tips and tricks, job advertisements, articles and more<br></li>
 
 <li><a href="http://www.php-homepage.de/">PHP-Homepage</a><br>
-German site about PHP with scripts, tips, forums, MyGuestBook, articles, news and more<br>
+German site about PHP with scripts, tips, forums, MyGuestBook, articles, news and more<br></li>
 
 <li><a href="http://www.dynamic-webpages.de/">Dynamic-Webpages</a><br>
 German site with installation guides, tips &amp; scripts, BestOfGML,
-WhoIsWho - a view on german PHP-cracks, german manual, book reviews, links and more<br>
+WhoIsWho - a view on german PHP-cracks, german manual, book reviews, links and more<br></li>
 
 <li><a href="http://www.phpcrawler.de/">PHPCrawler</a><br>
-Searchengine over German PHP-sites with webaccess to newsgroups<br>
+Searchengine over German PHP-sites with webaccess to newsgroups<br></li>
 
 <li><a href="http://www.phpwelt.de/">PHPWelt</a><br>
 A German website using PHP to offer free services, a huge script-archive,
-free scripts, tutorials and many more<br>
+free scripts, tutorials and many more<br></li>
 
-</ul>
+</ul></li>
 <li><b>French (Fran&ccedil;ais)</b>
 <ul>
 
 <li><a href="http://www.nexen.net/">Nexen.net</a><br>
-Home of PHP French documentation, with comprehensive technical informations<br>
+Home of PHP French documentation, with comprehensive technical informations<br></li>
 
 <li><a href="http://www.phpindex.com/">PHPIndex</a><br>
-A french gateway to PHP related contents<br>
+A french gateway to PHP related contents<br></li>
 
 <li><a href="http://www.phpinfo.net/">PHPInfo</a><br>
 French site about PHP/MySQL: Links, Tips, Articles, Manuals,
-FAQ, Code Downloads, and much more!<br>
+FAQ, Code Downloads, and much more!<br></li>
 
 <li><a href="http://nav.webring.yahoo.com/hub?ring=phpfrancophone&list">French PHP Webring</a><br>
-A ring of French PHP Sites<br>
+A ring of French PHP Sites<br></li>
 
-<li><a href="http://johng.free.fr/">FAQ of the French PHP newsgroup</a><br>
+<li><a href="http://johng.free.fr/">FAQ of the French PHP newsgroup</a><br></li>
 
-</ul>
+</ul></li>
 <li><b>Dutch</b>
 <ul>
 
 <li><a href="http://php.pagina.nl/">Pagina</a><br>
-A Dutch PHP portal that lists a lot of PHP links, including free hosting<br>
+A Dutch PHP portal that lists a lot of PHP links, including free hosting<br></li>
 
-</ul>
+</ul></li>
 <li><b>Italian</b>
 <ul>
 
 <li><a href="http://www.tutorials.it/">Tutorials.it</a><br>
 An Italian site that provides a variety of developer tutorials,
-with a small PHP section<br>
+with a small PHP section<br></li>
 
 <li><a href="http://www.phpcenter.it/">PHPcenter.it</a><br>
 An Italian-language php site that features a variety of projects, reviews,
-courses, tutorials and other resources<br>
+courses, tutorials and other resources<br></li>
 
-</ul>
+</ul></li>
 <li><b>Brazilian</b>
 <ul>
 
 <li><a href="http://phpbrasil.com/">PHPBrasil.com</a><br>
-Brazilian PHP Portal with many link and informations<br>
+Brazilian PHP Portal with many link and informations<br></li>
+</ul></li>
 </ul>
-</ul>
+
 <?php echo hdelim(); ?>
 
 <h1>Developers and Job Opportunities</h1>
-<P>
-Brian Schaffner has put up a <a href="http://www.schaffner.net/emp">PHP Employment Listing</a> site.
-PHP developers can post their resumes, and employers can post their job notices.
-</P>
+
+<p>
+Brian Schaffner has put up a <a href="http://www.schaffner.net/emp">PHP
+Employment Listing</a> site. PHP developers can post their resumes,
+and employers can post their job notices.
+</p>
 
 <h4>Other Job Sites with PHP jobs:</h4>
 
@@ -212,24 +218,27 @@ PHP developers can post their resumes, and employers can post their job notices.
  <li><a href="http://careers.yahoo.com/">Yahoo! Careers</a></li>
 </ul>
 
-<p>For a more exhaustive list of job
-search sites on the web, take a look at <a
-href="http://www.dmoz.org/Business/Employment/Job_Search/">dmoz.org's
-Job Search listings</a>.</p>
+<p>
+For a more exhaustive list of job search sites on
+the web, take a look at
+<a href="http://www.dmoz.org/Business/Employment/Job_Search/">dmoz.org's
+Job Search listings</a>.
+</p>
 
 <?php echo hdelim(); ?>
 
-
 <h1>ISPs that support PHP</h1>
-<P>
-Looking for an ISP or hosting service that supports PHP?  Try one of the following sites:
-</P>
-<UL>
-<li><a href="http://www.od-site.com/php">www.od-site.com/php</a><br>
-<li><a href="http://hosts.php.net/">hosts.php.net</a><br>
-(features a searchable directory and comments from users)<br>
-<li>PMCW webservices (<a href="http://pmcw.dhs.org/">http://pmcw.dhs.org/</a>)
-</UL>
 
+<p>
+Looking for an ISP or hosting service that supports PHP?
+Try one of the following sites:
+</p>
+
+<ul>
+  <li><a href="http://www.od-site.com/php">www.od-site.com/php</a><br></li>
+  <li><a href="http://hosts.php.net/">hosts.php.net</a><br>
+    (features a searchable directory and comments from users)<br></li>
+  <li><a href="http://pmcw.dhs.org/">PMCW webservices</a></li>
+</ul>
 
 <?php commonFooter(); ?>
