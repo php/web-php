2b19f2aa0e3d01ddc42c722b90a3df5e771918dc
Gabor Hojtsy <goba@php.net>
2001-07-27 11:51:49 +0000

    Should be a bit smaller (H3 is bigger than H4 but not than H2)

--- index.php
+++ index.php
@@ -76,7 +76,7 @@ if($fp) {
 			if($cm) $RSIDEBAR_DATA.= "<br>\n"; 
 			else $RSIDEBAR_DATA.='<h3>Upcoming Events<br>&nbsp;&nbsp;&nbsp;&nbsp;<a href="http://www.php.net/cal.php?a=1">[add event]</a></h3><br>';
 			$cm = (int)$m;  
-			$RSIDEBAR_DATA .= "<h2>".strftime('%B',mktime(12,0,0,$cm,$d,$y))."</h2>\n"; 
+			$RSIDEBAR_DATA .= "<h4>".strftime('%B',mktime(12,0,0,$cm,$d,$y))."</h4>\n"; 
 		}
 		if(!$seen[$desc]) {
 			$RSIDEBAR_DATA .= "$d. <a href=\"$url\">$desc</a><br>\n";
