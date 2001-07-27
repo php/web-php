f8935353aa0323a681f93edc37b63c832aeb2ff1
Gabor Hojtsy <goba@php.net>
2001-07-27 12:47:15 +0000

    /pear redirects to the manual, as /include, according to our standards

--- pear/index.php
+++ pear/index.php
@@ -1,2 +1,16 @@
-<?php header("Location: http://pear.php.net/"); ?>
+<?php
 
+// This page tries to find the manual page for PEAR
+// because people can access that page by using
+// http://www.php.net/pear as a REQUEST_URI
+
+require_once 'prepend.inc';
+require_once 'manual-lookup.inc';
+
+$file = find_manual_page(default_language(), "pear");
+
+if ($file) {
+    header("Location: $file");
+} else { header("Location: /"); }
+
+?>
