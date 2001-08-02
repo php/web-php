1c971ac51ef0f8c204521a61a806b54ee1b9c7b6
Rasmus Lerdorf <rasmus@php.net>
2001-08-02 14:48:33 +0000

    July numbers

--- usage.php
+++ usage.php
@@ -3,20 +3,20 @@ require_once 'prepend.inc';
 commonHeader("PHP Usage Stats");
 ?>
 
-<h1>Usage Stats for June 2001</h1>
+<h1>Usage Stats for July 2001</h1>
 
 <p>
-PHP: 6751394 Domains, 960954 IP Addresses<br>
+PHP: 6,954,476 Domains, 983,273 IP Addresses<br>
 Source: <a href="http://www.netcraft.com/Survey/">Netcraft</a><br>
 </p>
 
 <p>
-<?php echo print_image("stats/phpstats-200106.png", "Graph"); ?>
+<?php echo print_image("stats/phpstats-200107.png", "Graph"); ?>
 </p>
 
 <p>
 You can also see how popular PHP is relative to other Apache modules
-at <?php print_link("http://www.securityspace.com/s_survey/", "E-Soft Inc.'s Web Survey"); ?>.
+at <?php print_link("http://www.securityspace.com/s_survey/", "SecuritySPace's Web Survey"); ?>.
 Spoiler: PHP is the most popular.
 </p>
 
