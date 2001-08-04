04c5703dba92f509dc8c007efb6f92dbb4cf0af3
Gabor Hojtsy <goba@php.net>
2001-08-04 14:49:09 +0000

    Huh, moving all download materials to download-docs.php Here only the online versions are listed. Tryed to put the download link to a frequental place, so we won't get any "where are the documentation downloads?" emails :)

--- docs.php
+++ docs.php
@@ -39,92 +39,44 @@ commonHeader("Documentation");
 
 $man_languages = array('en', 'pt_BR', 'cs', 'nl', 'fr', 'de', 'hu', 'it', 'ja', 'kr', 'es');
 
-# array structure: (header, link_text, show_size_for_package)
-$formats = array(
- ""                  => array("View Online",         "view",      false),
- "html/"             => array("View Online (plain)", "view",      false),
- "manual.txt.gz"     => array("Plain text",          "txt.gz",    true),
- "bigmanual.html.gz" => array("Single HTML",         "html.gz",   true),
- "manual.tar.gz"     => array("Many HTML files",     "tar.gz",    true),
- "manual.tar.bz2"    => array("Many HTML files",     "tar.bz2",   true),
- "manual.zip"        => array("Many HTML files",     "zip",       true),
- "manual.pdf.bz2"    => array("PDF",                 "pdf.bz2",   true),
- "manual.pdf.gz"     => array("PDF",                 "pdf.gz",    true),
- "manual.pdf.zip"    => array("PDF",                 "pdf.zip",   true),
- "manual_doc.pdb"    => array("PalmPilot DOC",       "doc.pdb",   true),
- "manual_isilo.pdb"  => array("PalmPilot iSilo",     "isilo.pdb", true),
- "manual.chm"        => array("Windows HTML Help",   "chm",       true)
-);
 ?>
 
 <h1>PHP Manual</h1>
 
-<p>The PHP manual is available in a selection of languages
-and formats. Pick a language and format from the table below.
-Tip: If you are using a capable browser, the file size and
-date will show up when you move the mouse above the link.
-If you use another browser, or would like to see all the
-information, you can <a href="/docs.php?sizes=1">click
-here to see all the file sizes and dates</a>.
-</p>
-
 <p>
-Note that the packaged versions of the manual (tar.gz, tar.bz2 and zip)
-don't contain any directories, so all of the files will be dumped into
-your current working directory when you expand the archive unless
-the tool you use does otherwise.
+The PHP manual is available online in a selection of languages.
+You can choose between the printer friendly and graphically
+designed versions. Please pick a language and format from
+the table below.
 </p>
 
 <table border="0" cellpadding="2" cellspacing="1" width="100%">
- <tr bgcolor="#cccccc">
-  <td>&nbsp;</td>
-  <?php 
-    while (list($k,$v) = each($formats)) {
-      echo "<th valign=\"bottom\">$v[0]</th>\n";
-    }?>
- </tr>
- <?php
-   while (list(,$langcode) = each($man_languages)) {
-     $language = $LANGUAGES[$langcode];
-     echo "<tr>\n<td bgcolor=\"#dddddd\"><b>$language</b></td>\n";
-     reset($formats);
-     while (list($fn,$details) = each($formats)) {
-       echo "<td align=\"center\" bgcolor=\"#eeeeee\">";
 
-       # temporary hacks until pdf and chm are auto-generated
-       if ($fn == "manual.pdf.bz2") {
-          $link_to = "distributions/manual/manual-$langcode.pdf.bz2";
-       } elseif ($fn == "manual.pdf.gz") {
-          $link_to = "distributions/manual/manual-$langcode.pdf.gz";
-       } if ($fn == "manual.pdf.zip") {
-          $link_to = "distributions/manual/manual-$langcode.pdf.zip";
-       } elseif ($fn == "manual.chm") {
-         $link_to = "distributions/manual_$langcode.chm";
-       } else {
-         $link_to = "manual/$langcode/$fn";
-       }
-       
-       # if no size required [pdf, online], then just print, else
-       # decide what to do according to the $size of the file
-       if (!$details[2]) { 
-         if ($link_to == 'down') echo "Format Unavailable";
-         else echo "<a href=\"$link_to\">$details[1]</a>"; 
-       }
-       else {
-         $size = @filesize($link_to);
-         $changed = @filemtime($link_to);
-         $date_format = "j M Y"; // Part of the RFC date type (to be short)
-         if ($size) {
-           echo "<a href=\"$link_to\" title=\" Size: " . (int) ($size/1024) . "Kb\n Date: " . date ($date_format, $changed) . "\">$details[1]</a>";
-           if ($sizes) { echo "<br><small>Size: " . (int) ($size/1024) . "Kb<br>Date: " . date ($date_format, $changed) . "</small>"; }
-         } else {
-           echo "&nbsp;";
-         }
-       }
+<tr><td bgcolor=\"#dddddd\">View Online</td><td bgcolor="#eeeeee">
+<?php
 
-       echo "</td>\n";
-     }
-   }?>
+  foreach ($man_languages as $langnum => $langcode) {
+    echo '<a href="/manual/' . $langcode . '/">' . $LANGUAGES[$langcode] . '</a> ';
+  }
+
+?>
+</td></tr>
+
+<tr><td bgcolor=\"#dddddd\">Printer friendly</td><td bgcolor="#eeeeee">
+<?php
+
+  foreach ($man_languages as $langnum => $langcode) {
+    echo '<a href="/manual/' . $langcode . '/html/">' . $LANGUAGES[$langcode] . '</a> ';
+  }
+
+?>
+</td></tr>
+
+<tr><td bgcolor=\"#dddddd\">Printer friendly</td><td bgcolor="#eeeeee">
+For other downloadable formats, please visit our new
+<a href="download-docs.php">documentation downloads</a> page.
+</td></tr>
+
 </table>
 
 <?php echo hdelim(); ?>
@@ -151,19 +103,14 @@ The French PHP manual is also available on an external site, in differents forma
 Documentation is the official flavor, while formating and localization has 
 been worked some more (function and examples indexes, short version, search engine..). 
 Besides, those are compressed for faster download.
+<a href="http://dev.nexen.net/docs/php/annotee/">Follow this link for
+the annotated French manual</a>
 </p>
 
 <p>
 La documentation Fran&ccedil;aise est disponible sur un site secondaire,
 dans diff&eacute;rents formats. C'est la version officielle, compl&egrave;tement 
 francis&eacute;e et retravaill&eacute;e. De plus, ces docs sont compress&eacute;es.
-</p>
-
-<p>
-<ul>
-<li><a href="http://dev.nexen.net/docs/php/annotee/">Annotated version</a></li>
-<li><a href="http://dev.nexen.net/docs/php/chargement.html">Manual downloads</a></li>
-</ul>
 </p>
 
 <?php echo hdelim(); ?>
