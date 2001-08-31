f3126a12b6860c88053bb3359b883192368a364c
jim winstead <jimw@php.net>
2001-08-31 08:01:10 +0000

    use htmlspecialchars when displaying uri that triggered a 404, too.

--- error/index.php
+++ error/index.php
@@ -6,8 +6,8 @@ function make404() {
 	global $REQUEST_URI;
 	header('HTTP/1.0 404 Not Found');
 	commonHeader('404 Not Found');
-	echo "<H1>Not Found</H1>\n";
-	echo "<P>The page <B>" . $REQUEST_URI . "</B> could not be found.</P>\n";
+	echo "<h1>Not Found</h1>\n";
+	echo "<p>the page <b>" . htmlspecialchars($request_uri) . "</b> could not be found.</p>\n";
 	commonFooter();
 }
 
