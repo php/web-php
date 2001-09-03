92bc8ba9cf7826d479450710110bd1a899f728df
Rasmus Lerdorf <rasmus@php.net>
2001-09-03 18:32:12 +0000

    August numbers - with a small explanation to head off all the doomsday preachers...

--- usage.php
+++ usage.php
@@ -3,17 +3,22 @@ require_once 'prepend.inc';
 commonHeader("PHP Usage Stats");
 ?>
 
-<h1>Usage Stats for July 2001</h1>
+<h1>Usage Stats for August 2001</h1>
 
 <p>
-PHP: 6,954,476 Domains, 983,273 IP Addresses<br>
+PHP: 6,883,004 Domains, 976,593 IP Addresses<br>
 Source: <a href="http://www.netcraft.com/Survey/">Netcraft</a><br>
 </p>
 
 <p>
-<?php echo print_image("stats/phpstats-200107.png", "Graph"); ?>
+<?php echo print_image("stats/phpstats-200108.png", "Graph"); ?>
 </p>
-
+<p>
+The drop-off in August is related to the .com crash with a number of sites
+and ISP's disappearing.  Netcraft's total number of sites found dropped in 
+August from 31,299,592 to 30,775,624.  PHP's percentage actually rose from 22.2%
+to 22.4%
+</p>
 <p>
 You can also see how popular PHP is relative to other Apache modules
 at <?php print_link("http://www.securityspace.com/s_survey/", "SecuritySpace's Web Survey"); ?>.
