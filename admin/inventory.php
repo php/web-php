b8e76a2f334b49236f6f336af94355011245c07d
jim winstead <jimw@php.net>
2001-09-20 22:23:49 +0000

    initial list

---
+++ admin/inventory.php
@@ -0,0 +1,43 @@
+<?php
+require_once 'prepend.inc';
+commonHeader("php.net machine inventory");
+?>
+
+<p>php.net is supported by a number of machines provided by a number
+of generous sponsors. this is a basic inventory of those machines and
+what services they provide.</p>
+
+<h2>chek1.php.net (208.247.106.167)</h2>
+
+<p><b>machine:</b> p2/350, 256MB RAM, 8.1GB HD, redhat 5.1</p>
+
+<p><b>aliases:</b> rsync.php.net</p>
+
+<h2>chek2.php.net (208.247.106.187)</h2>
+
+<p><b>machine:</b> dual p3/650, 512MB RAM, 40.9GB HD, redhat 6.1</p>
+
+<p><b>aliases:</b> www.php.net, bugs.php.net, conf.php.net, gtk.php.net</p>
+
+<h2>pair1.php.net (216.92.131.4)</h2>
+
+<p><b>machine:</b> dual p3/1000 xeon, 512MB RAM, 27GB HD, freebsd 4.3</p>
+
+<p><b>aliases:</b> lists.php.net, news.php.net, cvs.php.net, pear.php.net</p>
+
+<h2>pair2.php.net (216.92.131.5)</h2>
+
+<p><b>machine:</b> ?, ?MB RAM, 27GB HD, freebsd 4.3</p>
+
+<h2>rack1.php.net (209.61.157.217)</h2>
+
+<p><b>machine:</b> dual p3/650, 1GB RAM, 33GB HD, redhat 7.1</p>
+
+<p><b>aliases:</b> download.php.net, snaps.php.net</p>
+
+<h2>va1.php.net (offline)</h2>
+
+<p>originally compiled by jim winstead, september 2001</p>
+
+<?php
+commonFooter();
