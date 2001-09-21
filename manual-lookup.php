68cce3c2fa442574dba2a7d40db4990d582ebcd5
jim winstead <jimw@php.net>
2001-09-21 02:35:04 +0000

    remove magic quotes here, too

--- manual-lookup.php
+++ manual-lookup.php
@@ -4,7 +4,11 @@ require_once 'prepend.inc';
 require_once 'manual-lookup.inc';
 
 if (!$function && $pattern) $function = $pattern;
+
+if (get_magic_quotes_gpc()) $function = stripslashes($function);
+
 $function = strtolower($function);
+
 if (!isset($lang)) $lang = default_language();
 
 $file = find_manual_page($lang, $function);
