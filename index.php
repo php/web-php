395933eb52fe24a80b2441ba728ff2aead05a9ee
Zeev Suraski <zeev@php.net>
2001-09-22 19:28:41 +0000

    Update rackspace link per request

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ make_link("http://www.apache.org/","Apache Software Foundation") . '.
 &nbsp; ' . make_link("http://chek.com/", "Chek.com") . '<br>
 &nbsp; ' . make_link("http://www.easydns.com/?V=698570efeb62a6e2", "easyDNS") . '<br>
 &nbsp; ' . make_link("http://www.pair.com/", "pair Networks") . '<br>
-&nbsp; ' . make_link("http://www.rackspace.com/", "Rackspace") . '<br>
+&nbsp; ' . make_link("http://www.rackspace.com/?supbid=php.net", "Rackspace") . '<br>
 &nbsp; ' . make_link("http://valinux.com/", "VA Linux Systems") . '<br>
 <h3>Related sites</h3>
 &nbsp; ' . make_link("http://www.apache.org/", "Apache") . '<br>
@@ -76,12 +76,32 @@ echo "\n<!--$MYSITE-->\n";
 ?>
 
 <h1>Rackspace providing additional hosting for PHP project</h1>
-<?php print_link("http://www.rackspace.com/", make_image("rackspace.gif", "Rackspace", "right") ); ?>
+<?php print_link("http://www.rackspace.com/?supbid=php.net", make_image("rackspace.gif", "Rackspace", "right") ); ?>
 
+<<<<<<< index.php
+<h1>Mailing Lists, CVS temporarily unavailable</h1>
+
+<p>
+<font class="newsDate">[24-May-2001]</font>
+The PHP mailing lists, news server, and CVS repository are all temporarily
+off-line. We hope to all of these services restored as soon as possible. Thank
+you for your patience!
+<br clear="all">
+</p>
+
+<? echo hdelim(); ?>
+
+<?/* move this entry to the top when you update it! */?>
+<h1>
+<? print_link("/usage.php", make_image("stats-small.gif", "PHP Usage Stats", "right") ); ?>
+New Usage Stats For May available
+</h1>
+=======
+>>>>>>> 1.143
 <p>
 <font class="newsDate">[17-Sep-2001]</font>
 Thanks to the generous folks at <a
-href="http://www.rackspace.com/">Rackspace</a>, we have added additional
+href="http://www.rackspace.com/?supbid=php.net">Rackspace</a>, we have added additional
 capacity for hosting the PHP project's efforts. Currently, the machine they
 have provided is serving downloads for www.php.net and snaps.php.net. We will
 continue to re-balance our resource needs across all of the machines provided
