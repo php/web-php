642608986a29cdc67a14e84a9eaf96189395ede5
Gabor Hojtsy <goba@php.net>
2001-10-28 22:02:06 +0000

    Brr.. Use spaces

--- log_download.php
+++ log_download.php
@@ -2,9 +2,9 @@
 
 $log = @fopen("logs/download.log", "a");
 if ($log) {
-	$log_line = sprintf("%s %s %s %s %s", date("H:i:s d-M-Y"), $user_referer, $download_file, gethostbyaddr($user_ip), $mirror);
-	fputs($log, "$log_line\n");
-	fclose($log);
+    $log_line = sprintf("%s %s %s %s %s", date("H:i:s d-M-Y"), $user_referer, $download_file, gethostbyaddr($user_ip), $mirror);
+    fputs($log, "$log_line\n");
+    fclose($log);
 }
 
 ?>
