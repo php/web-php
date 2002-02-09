9ee671fe3fc69d63aed3927c31147babe3ba9fdb
jim winstead <jimw@php.net>
2002-02-09 23:42:37 +0000

    ko is (correctly) used for korean downloadable docs

--- download-docs.php
+++ download-docs.php
@@ -30,7 +30,7 @@ downloads of .bz2 tools for Windows</a>.
 
 commonHeader("Download documentation");
 
-$man_languages = array('en', 'pt_BR', 'cs', 'nl', 'fr', 'de', 'hu', 'it', 'ja', 'kr', 'pl', 'es');
+$man_languages = array('en', 'pt_BR', 'cs', 'nl', 'fr', 'de', 'hu', 'it', 'ja', 'ko', 'pl', 'es');
 
 # array structure: (header, link_text, show_size_for_package)
 $formats = array(
