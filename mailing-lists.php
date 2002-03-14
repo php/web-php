12f3e320a140ca984d7b2d2b78da7f9ea4227a76
Simone Cortesi <cortesi@php.net>
2002-03-14 00:19:13 +0000

    correcting the address of Michel Morelli mailinglist it had the wrong address

--- mailing-lists.php
+++ mailing-lists.php
@@ -265,7 +265,7 @@ that have been set up by others in the community to host local
   <li><a href="news:de.comp.lang.php">German Newsgroup</a></li>
   <li><a href="mailto:wl-phplista-request@gimli.externet.hu?subject=subscribe">Hungarian Mailing List</a></li>
   <li><a href="http://groups.yahoo.com/group/php_greek">Greek Mailing List</a></li>
-  <li><a href="mailto:php3-it-request@michel.enter.it">Italian Mailing List</a></li>
+  <li><a href="http://www.ziobudda.net/mailman/listinfo/php-it/">Italian Mailing List</a></li>
   <li><a href="http://groups.yahoo.com/group/php-pt">Portuguese Mailing List</a></li>
   <li><a href="mailto:lista-subscribe@phpes.com">Spanish Mailing List</a></li>
   <li><a href="http://phptr.baslangic.net/">Turkish Mailing List</a></li>
