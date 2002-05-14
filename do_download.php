2e027a4cea4326f7c5fd431bd9f9d65b5415a908
Gabor Hojtsy <goba@php.net>
2002-05-14 21:46:17 +0000

    Reorganize download stuff, put log thing to a shutdown function

--- do_download.php
+++ do_download.php
@@ -1,29 +1,47 @@
 <?php
 
-if (!isset($HTTP_REFERER)) {
-	$HTTP_REFERER="-";
-}
-
+// If there is no file, then drop out an invalid file error message
 if (!isset($download_file)
 	|| !file_exists("distributions/$download_file")) {
 	exit("Invalid file requested for download!");
 }
 
+// Redirect to the particular file
 header("Location: http://$SERVER_NAME/distributions/$download_file");
 
-$remote_addr = $HTTP_X_FORWARDED_FOR ? $HTTP_X_FORWARDED_FOR : $REMOTE_ADDR;
+// Register the log function as a shutdown function to let the
+// download starts before the log activates. This way if the
+// "log server" does not work, downloads are still working
+register_shutdown_function("log_download");
 
-$remote_log = 
-    @fopen("http://www.php.net/log_download.php".
-        "?download_file=" . urlencode($download_file).
-        "&mirror=" . urlencode($SERVER_NAME).
-        "&user_referer=" . urlencode($HTTP_REFERER).
-        "&user_ip=" . urlencode($remote_addr), 
-    'r');
+// Log downloads in a shutdown function (do not disturb download)
+function log_download ()
+{
+    global $HTTP_REFERER, $HTTP_X_FORWARDED_FOR,
+           $REMOTE_ADDR, $SERVER_NAME;
 
-if ($remote_log) {
-    fread($remote_log, 1024);
-    fclose($remote_log);
+    // Default referer
+    if (!isset($HTTP_REFERER)) {
+    	$HTTP_REFERER = "-";
+    }
+
+    // Figure out true remote address
+    $remote_addr = $HTTP_X_FORWARDED_FOR ? $HTTP_X_FORWARDED_FOR : $REMOTE_ADDR;
+    
+    // Try to open remote log on php.net
+    $remote_log = 
+        @fopen("http://www.php.net/log_download.php".
+            "?download_file=" . urlencode($download_file).
+            "&mirror=" . urlencode($SERVER_NAME).
+            "&user_referer=" . urlencode($HTTP_REFERER).
+            "&user_ip=" . urlencode($remote_addr), 
+        'r');
+    
+    // If we can open it, read from it, and close (jut to make sure it runs)
+    if ($remote_log) {
+        fread($remote_log, 1024);
+        fclose($remote_log);
+    }
 }
 
 ?>
