e6d8e99c3f3516649d265d3dcb69a188ee30e57b
Mark Kronsbein <mk@php.net>
2002-07-22 23:39:05 +0000

    Add http://rs1.php.net to the list of default mirrors

--- downloads.php
+++ downloads.php
@@ -81,7 +81,7 @@ if ($cmn > 0) {
         srand ((double)microtime()*1000000);
 	
 	if ($server == "www.php.net") {
-		$default_mirrors = array("http://us2.php.net/", "http://ca.php.net", "http://us3.php.net");
+		$default_mirrors = array("http://us2.php.net/", "http://ca.php.net", "http://us3.php.net","http://rs1.php.net");
 		$mirror = $default_mirrors[rand(0, count($default_mirrors)-1)];
 	} else {
 		$mirror = "";
