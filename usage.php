e60b780b02962422ee2251a445f1f8b8cc193e38
Gabor Hojtsy <goba@php.net>
2002-08-28 18:23:00 +0000

    Adding TIOBE index link

--- usage.php
+++ usage.php
@@ -16,7 +16,8 @@ Source: <a href="http://www.netcraft.com/Survey/">Netcraft</a><br>
 <p>
 You can also see how popular PHP is relative to other Apache modules
 at <?php print_link("http://www.securityspace.com/s_survey/", "SecuritySpace's Web Survey"); ?>.
-Spoiler: PHP is the most popular.
+Spoiler: PHP is the most popular. There is also a <a href="http://www.tiobe.com/tpci.htm">Programming
+Community Index</a> provided TIOBE.
 </p>
 
 <?php commonFooter(); ?>
