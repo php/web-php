56dd672d24da803b15fbed8b2156e16857892a43
Gabor Hojtsy <goba@php.net>
2002-12-15 18:29:09 +0000

    Another check if the htsearch program is really an executable (is in place, and PHP can excute it).
    Still commented, so my mails sent out recently will still
    be valid ;). After those problems are sorted out, this
    can be uncommented, and will provide another check for
    local search mirror sites before they go into an error.

--- search.php
+++ search.php
@@ -25,6 +25,13 @@ if (isset($_SERVER['HTSEARCH_PROG'])) {
     $htsearch_prog = $_SERVER['HTSEARCH_PROG'];
 }
 
+// If the program to use to search is not excutable, we cannot
+// get search results, regardless of what we do (uncomment as the
+// real local search providers are known [Goba])
+//if (!is_executable($htsearch_prog)) {
+//    unset($htsearch_prog);
+//}
+
 /* Form parameters expected:
      pattern - search keywords
      lang    - language to limit search to [this is not used currently!!!]
