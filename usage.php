0ba1741b2ef8be6da46faedd600e0dd9406fd423
Gabor Hojtsy <goba@php.net>
2002-12-17 20:51:58 +0000

    Standardize formatting, and fix a typo

--- usage.php
+++ usage.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'prepend.inc';
+include_once 'prepend.inc';
 commonHeader("PHP Usage Stats");
 ?>
 
@@ -13,11 +13,13 @@ Source: <a href="http://www.netcraft.com/Survey/">Netcraft</a><br>
 <p>
 <?php echo print_image("stats/phpstats-200211.png", "Graph"); ?>
 </p>
+
 <p>
 You can also see how popular PHP is relative to other Apache modules
 at <?php print_link("http://www.securityspace.com/s_survey/", "SecuritySpace's Web Survey"); ?>.
-Spoiler: PHP is the most popular. There is also a <a href="http://www.tiobe.com/tpci.htm">Programming
-Community Index</a> provided TIOBE.
+Spoiler: PHP is the most popular. There is also a
+<a href="http://www.tiobe.com/tpci.htm">Programming Community Index</a>
+provided by TIOBE.
 </p>
 
 <?php commonFooter(); ?>
