990f97182f2c24ac797f5225f97abe1935f46d6b
Gabor Hojtsy <goba@php.net>
2002-12-21 20:02:52 +0000

    Updating mirroring information:
     - Updating "the mirror number rule" to include the reason
       why we have this limitation, and advice to contact
       the mirror managers before starting to set up anything
    
     - Also linked "the rule" from below the instructions
    
     - Remove instruction on setting up configuration.inc,
       it is no more required
    
     - Adding new HTSEARCH_EXCLUDE environment variable to
       set what is excluded from the index on a mirror. This
       will be used for the search information page in the
       future.
    
     - Adding note on missing files (/tut.php or print
       or printwn directories) which caused some problems
       in the past. Mirror site should not expect them
       to be there...
    
     - Reordered and reworded what is exactly required to
       be submitted to mirrors@php.net. Guys mostly tend
       to misunderstand what the provider URL is for, so
       eplain it adequately. Also adding link to webalizer.
    
     - Reformatting of HTML code in other places...

--- mirroring.php
+++ mirroring.php
@@ -1,40 +1,47 @@
 <?php
-require_once 'prepend.inc';
-
+include_once 'prepend.inc';
 commonHeader("Mirroring php.net");
 ?>
 
 <p>
-If you want to become an official PHP mirror, please be sure to
-read and follow these instructions carefully. You should have the
-consent of your hosting company (if you aren't a hosting company
-yourself), and be prepared for some reasonably significant bandwidth
-usage (a reasonable estimate as of 2/2002 is 150MB/day). The PHP
-website requires PHP 4 with the settings <a href="#settings">outlined
-below</a>. The size of the full website is approximately 3/4 gigabytes.
+ If you want to become an official PHP mirror, please be sure to
+ read and follow these instructions carefully. You should have the
+ consent of your hosting company (if you aren't a hosting company
+ yourself), and be prepared for some reasonably significant bandwidth
+ usage (a reasonable estimate as of 2/2002 is 150MB/day). The PHP
+ website requires PHP 4 with the settings <a href="#settings">outlined
+ below</a>. The size of the full website is approximately 3/4 gigabytes.
+</p>
+
+<a name="rule"></a>
+<p>
+ <strong>
+  Please note that we are currently only accepting new official
+  mirrors in countries where we don't already have two active official
+  mirrors. For a list of official mirrors have a look at
+  <a href="/mirrors.php">mirrors.php</a>. Before you start to set up
+  an official mirror site, it is advisable to contact <a
+  href="mailto:mirrors@php.net">mirrors@php.net</a>, and ask if you
+  have chance to get your mirror site accepted. We would not like
+  to put too much pressure on our rsync server, so we need to limit
+  the number of mirror sites.
+ </strong>
 </p>
 
 <p>
-<strong>Please note that we are currently only accepting new official
-mirrors in countries where we don't already have two active official
-mirrors. For a list of official mirrors have a look at
-<a href="/mirrors.php">mirrors.php</a>.</strong>
-</p>
-
-<p>
-If you are not an official mirror (eg. mirror the site for your company's
-internal use), you should not rsync from <tt>rsync.php.net</tt> more frequently
-than every three hours, or you may find your ip blocked. Also, please make
-an effort to only mirror those parts of the site that you actually need.
-(For example, <a href="#exclude">exclude the manual in all of languages that you
-will not be using and exclude the distributions directory</a>.)
+ If you are not an official mirror (eg. mirror the site for your company's
+ internal use), you should not rsync from <tt>rsync.php.net</tt> more frequently
+ than every three hours, or you may find your ip blocked. Also, please make
+ an effort to only mirror those parts of the site that you actually need.
+ (For example, <a href="#exclude">exclude the manual in all languages that you
+ will not be using and exclude the distributions directory</a>.)
 </p>
 
 <h1>How To Mirror The PHP Website</h1>
 
 <p>
-First, you need to have a <a href="http://rsync.samba.org/">rsync</a> installed.
-As you have rsync, fetch the web files with the following:
+ First, you need to have a <a href="http://rsync.samba.org/">rsync</a> installed.
+ As you have rsync, fetch the web files with the following:
 </p>
 
 <pre>
@@ -44,8 +51,8 @@ As you have rsync, fetch the web files with the following:
 
 <a name="exclude"></a>
 <p>
-Setting up an unofficial mirror, and want to only mirror one
-language of the manual? Add:
+ Setting up an unofficial mirror, and want to only mirror one
+ language of the manual? Add:
 </p>
 
 <pre>
@@ -53,28 +60,23 @@ language of the manual? Add:
 </pre>
 
 <p>
-after <tt>"--delete-after"</tt> in the command line above (substituting your
-prefered language code in place of <tt>'en'</tt>). You can also exclude the
-distributions directory by adding <tt>"--exclude='distributions/'"</tt>
+ after <tt>"--delete-after"</tt> in the command line above (substituting your
+ prefered language code in place of <tt>'en'</tt>). You can also exclude the
+ distributions directory by adding <tt>"--exclude='distributions/'"</tt>
 </p>
 
 <p>
-If <tt>/your/local/path</tt> isn't in your web document tree (why isn't it?), 
-then symlink the <tt>phpweb/</tt> directory to the correct place on your server.
+ If <tt>/your/local/path</tt> isn't in your web document tree (why isn't it?), 
+ then symlink the <tt>phpweb/</tt> directory to the correct place on your server.
 </p>
 
 <p>
-Now, make sure your web server is set up to serve up <tt>.php</tt> files
-as PHP parsed files. If it isn't, add the mime-type to your config.
+ Now, make sure your web server is set up to serve up <tt>.php</tt> files
+ as PHP parsed files. If it isn't, add the mime-type to your config.
 </p>
 
 <p>
-Rename the <tt>configuration.inc-dist</tt> file to <tt>configuration.inc</tt>
-and edit it appropriately.
-</p>
-
-<p>
-Once you create a virtualhost which looks something like:
+ Create a virtualhost which looks something like:
 </p>
 
 <a name="settings"></a>
@@ -96,7 +98,7 @@ Once you create a virtualhost which looks something like:
      TransferLog logs/access_log
      
      # Set directory index
-     DirectoryIndex index.php index.html
+     DirectoryIndex index.php
      
      # Handle errors with local error handler script
      ErrorDocument 401 /error/index.php
@@ -112,31 +114,37 @@ Once you create a virtualhost which looks something like:
      # generating stats (see /stats/README.stats)
      Alias /stats/ /path/to/local/stats/
 
-     # The next line is only necessary if you would
+     # The next lines are only necessary if you would
      # like to provide local search support (see /Mirrors-htdig.tgz)
      SetEnv HTSEARCH_PROG /local/htdig/bin/htphp.sh
+     SetEnv HTSEARCH_EXCLUDE "/manual/zh /cal.php"
 
    &lt;/VirtualHost&gt;
 </pre>
    
 <p>
-modified to reflect your local paths and country, of course,
-then your site should start working.
+ Provide an asterisk or a hostname in the VirtualHost container's
+ header and in the ServerName directive. Consult the Apache
+ documentation for differences of the two methods. Change the
+ DocumentRoot and include_path settings as appropriate, and
+ provide settings according to your local search and stats
+ setup, if your mirror is going to provide these. After you
+ restart Apache, your mirror site should start working.
 </p>
 
 <p>
-The official names for PHP mirrors are in the convention:
-<tt>"xx.php.net"</tt>, where <tt>"xx"</tt> is replaced by the
-2-letter country code of your mirror's location. If there already
-is a <tt>"xx.php.net"</tt>, then you should set up your <tt>ServerName</tt>
-to be <tt>"xx2.php.net"</tt>. For a list of what's available and what
-is taken, have a look at the <a href="/mirrors.php">list of mirrors</a>.
+ The official names for PHP mirrors are in the convention:
+ <tt>"xx.php.net"</tt>, where <tt>"xx"</tt> is replaced by the
+ 2-letter country code of your mirror's location. If there already
+ is a <tt>"xx.php.net"</tt>, then you will probably get
+ <tt>"xx2.php.net"</tt>. <a href="#rule">Please read the note in
+ bold above</a>.
 </p>
 
 <p>
-You must also set up a cron job that periodically does an rsync
-to refresh your web directory. This will ensure that your web site
-is up to date. Something like:
+ You must also set up a cron job that periodically does an rsync
+ to refresh your web directory. This will ensure that your web site
+ is up to date. Something like:
 </p>
 
 <pre>
@@ -144,57 +152,80 @@ is up to date. Something like:
 </pre>
 
 <p>
-You should try to stagger your times a bit from the example to help spread the
-load on the <tt>rsync.php.net</tt> server.
+ You should try to stagger your times a bit from the example to help
+ spread the load on the <tt>rsync.php.net</tt> server. Don't be afraid
+ if you cannot find several pieces of the site in your local copy,
+ like the tutorial PHP page or the printed pages' directories. These
+ are handled automatically on the fly and are not real files.
 </p>
 
 <h1>Data Registered About Official Mirrors</h1>
 
-<p>
-Once you have done the above and your site appears to work, send
-a message to mirrors@php.net with the following information, and
-appropriate steps will be taken to integrate your mirror site:
+s<p>
+ Once you have done the above and your site appears to work, send
+ a message to <a href="mailto:mirrors@php.net">mirrors@php.net</a>
+ with the following information, and appropriate steps will be taken
+ to integrate your mirror site:
 </p>
 
 <ul>
- <li>Your name and email address as the admin for the mirror.</li>
- <li>Your country (we will update the DNS for php.net).</li>
- <li>A hostname that we can use as a CNAME for the country-code-based
-  name of the mirror. This is preferable to an IP address, because
-  it means you can move the mirror to another IP address without
-  coordinating with us at all.</li>
- <li>Whether or not you've installed local searching support on
+ <li>
+  Your country.
+ </li>
+ <li>
+  The xx.php.net address you used to set up the mirror site.
+ </li>
+ <li>
+  Your name and email address to be registered as the admin of the mirror.
+ </li>
+ <li>
+  A hostname that we can use as a CNAME for the country-code-based
+  name of the mirror. Please do not provide an IP address. Using a name
+  means you can move the mirror to another IP address without
+  coordinating with us at all.
+ </li>
+ <li>
+  Whether or not you've installed local searching support on
   your mirror. This uses <a href="http://www.htdig.org">ht://Dig</a>,
   for which setup instructions can be found in
-  <a href="/Mirrors-htdig.tgz">Mirrors-htdig.tgz</a></li>
- <li>Whether or not you've installed local stats support on your mirror.
-  This uses webalizer, for which instructions for setting it up
-  can be found in stats/README.stats</li>
- <li>The name of your hosting company.</li>
- <li>The URL of the site that hosts the mirror. This is where you want
-  people will go when they click on the link for your mirror at the
-  bottom of each page.</li>
- <li>The preferred default language for your country (currently only
-  the manuals are translated). Don't worry if there's no content
-  available in the preferred language yet, so long as English is
-  an acceptable secondary default.</li>
+  <a href="/Mirrors-htdig.tgz">Mirrors-htdig.tgz</a>
+ </li>
+ <li>
+  Whether or not you've installed local stats support on your mirror.
+  This uses <a href="http://www.mrunix.net/webalizer/">webalizer</a>,
+  for which instructions for setting it up can be found in
+  stats/README.stats.
+ </li>
+ <li>
+  The name of the hosting company.
+ </li>
+ <li>
+  The URL of the hosting company. This link is provided with the
+  companies name at the bottom of pages, and in the mirror listing.
+ </li>
+ <li>
+  The preferred default language for your country (currently only
+  the manuals are translated). Don't worry if there is no content
+  available in the preferred language yet, as long as English is
+  an acceptable secondary default.
+ </li>
 </ul>
 
 <p>
-There is a mailing list named <tt>"php-mirrors"</tt> at
-<tt>lists.php.net</tt>. It is not required to sign up to
-this mailing list. Besides the name, the traffic on this
-list is mainly interesting for the webmasters of php.net,
-and we are able to keep in touch with you using your given
-email address. Anyway if you would like to follow what's
-happening, you can subscribe, by sending an empty message
-to: <a href="mailto:php-mirrors-subscribe@lists.php.net">php-mirrors-subscribe@lists.php.net</a>
+ There is a mailing list named <tt>"php-mirrors"</tt> at
+ <tt>lists.php.net</tt>. It is not required to sign up to
+ this mailing list. Besides the name, the traffic on this
+ list is mainly interesting for the webmasters of php.net,
+ and we are able to keep in touch with you using your given
+ email address. Anyway if you would like to follow what's
+ happening, you can subscribe, by sending an empty message
+ to: <a href="mailto:php-mirrors-subscribe@lists.php.net">php-mirrors-subscribe@lists.php.net</a>
 </p>
 
 <p>
-We would like to thank you for providing php.net with a mirror, so
-if you would like to display a logo on the site promoting your mirror,
-you are able to do so by following these steps:
+ We would like to thank you for providing php.net with a mirror, so
+ if you would like to display a logo on the mirror site promoting your
+ company, you are able to do so by following these steps:
 </p>
 
 <ul>
@@ -204,27 +235,27 @@ you are able to do so by following these steps:
 </ul>
 
 <p>
-The PHP Group do reserve the right to refuse images based on content, but
-most things should be fine.
+ The PHP Group do reserve the right to refuse images based on content, but
+ most things should be fine.
 </p>
 
 <p>
-We have chosen a banner size which conforms with the
-<a href="http://www.iab.net/standards/adunits.asp">Internet
-Advertising Bureau standards</a>.
+ We have chosen a banner size which conforms with the
+ <a href="http://www.iab.net/standards/adunits.asp">Internet
+ Advertising Bureau standards</a>.
 </p>
 
 <p>
-And finally, don't forget to put a nice little PHP logo somewhere
-on your hosting company's site if possible. Grab one of the logos
-from the <a href="/download-logos.php">Download logos</a> page, and
-link it to your mirror.
+ And finally, don't forget to put a nice little PHP logo somewhere
+ on your hosting company's site if possible. Grab one of the logos
+ from the <a href="/download-logos.php">Download logos</a> page, and
+ link it to your mirror.
 </p>
 
 <p>
-<em>Thank you for being a mirror!</em>
+ <em>
+  Thank you for being a mirror!
+ </em>
 </p>
 
-<?php
-commonFooter();
-?>
+<?php commonFooter(); ?>
