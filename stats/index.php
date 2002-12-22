e6e5f09d94bfe7eab395059498562fb1e661900b
Gabor Hojtsy <goba@php.net>
2002-12-22 22:55:22 +0000

    Add when this page is displayed. Made code more clear.

--- stats/index.php
+++ stats/index.php
@@ -1,8 +1,10 @@
 <?php
-require_once 'prepend.inc';
+
+// This page is displayed if there is no alias to
+// override the stats directory distributed by rsync
+// => no local search is set up
+include_once 'prepend.inc';
 commonHeader("No Stats");
-?>
-<p>No stats are available from this mirror.</p>
-<?php
+echo '<p>No visitor statistics information is available on this mirror site.</p>';
 commonFooter();
 ?>
