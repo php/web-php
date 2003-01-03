351bf7a98bc4e65bc9ab6e43cd5b08ef2a6d17f0
Gabor Hojtsy <goba@php.net>
2003-01-03 21:41:13 +0000

    Add local search support test, so we can update the searchable mirrors list automatically, without needed human intervention

--- mirror-info.php
+++ mirror-info.php
@@ -1,4 +1,20 @@
 <?php
-require_once 'prepend.inc';
-echo "$MYSITE|", phpversion(), "|$LAST_UPDATED|";
+
+// Define $MYSITE and $LAST_UPDATED variables
+include_once "prepend.inc";
+
+// Try to find out if local search is possible
+unset($htsearch_prog);
+if (file_exists("configuration.inc")) {
+    include_once 'configuration.inc';
+}
+if (isset($_SERVER['HTSEARCH_PROG'])) {
+    $htsearch_prog = $_SERVER['HTSEARCH_PROG'];
+}
+if (!is_executable($htsearch_prog)) {
+    unset($htsearch_prog);
+}
+
+echo "$MYSITE|", phpversion(), "|$LAST_UPDATED|" . ((int) isset($htsearch_prog));
+
 ?>
