d12098f8ad7016f7804f52d3c16cfbcdeb6bc584
Gabor Hojtsy <goba@php.net>
2003-01-03 20:18:59 +0000

    Adding 2003 to copyright notices

--- copyright.php
+++ copyright.php
@@ -16,7 +16,7 @@ For information on the PHP License (i.e. using the PHP language),
 
 <p>
 The code, text, PHP logo, and graphical elements on this website 
-and the mirror websites (the "Site") are Copyright &copy; 2001, 2002 
+and the mirror websites (the "Site") are Copyright &copy; 2001-2003
 The PHP Group. All rights reserved.
 </p>
 
