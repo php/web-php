ac7ea2c26261ad988b0072fdc77beed393b1e11d
Gabor Hojtsy <goba@php.net>
2003-01-19 13:28:26 +0000

    Use the spacer function to print out spacers, and correct logo image (now in /images/logos and not /logos)

--- searchbar.php
+++ searchbar.php
@@ -34,10 +34,10 @@ if (preg_match('/Gecko/', $HTTP_SERVER_VARS['HTTP_USER_AGENT'])) {
  vlink="#000099" onload="document.sform.pattern.focus();">
 <table border="0" cellspacing="2" cellpadding="0">
  <tr>
-  <td width="69"><a href="/" target="<?php echo $target ?>"><img src="logos/php-logo.gif" width="69" height="36" border="0" alt="PHP" hspace="3" vspace="3" /></a></td>
+  <td width="69"><a href="/" target="<?php echo $target ?>"><img src="/images/logos/php-logo.gif" width="69" height="36" border="0" alt="PHP" hspace="3" vspace="3" /></a></td>
   <td class="title" align="left">Search</td>
  </tr>
- <tr bgcolor="#333366"><td colspan="2"><img src="gifs/spacer.gif" width="1" height="1" border="0" alt="" /></td></tr>
+ <tr bgcolor="#333366"><td colspan="2"><?php spacer(); ?></td></tr>
  <tr>
   <td colspan="2">
    <div class="indented">
@@ -49,7 +49,7 @@ if (preg_match('/Gecko/', $HTTP_SERVER_VARS['HTTP_USER_AGENT'])) {
    <select name="show" style="width: 95%">
    <option value="quickref" selected>function list
    <option value="wholesite">whole site
-   <option value="manual">online documentation
+   <option value="manual">online documentation [en]
    <option value="bugdb">bug database
    <option value="maillist">general mailing list
    <option value="devlist">developer mailing list
@@ -60,7 +60,7 @@ if (preg_match('/Gecko/', $HTTP_SERVER_VARS['HTTP_USER_AGENT'])) {
    </div>
   </td>
  </tr>
- <tr bgcolor="#333366"><td colspan="2"><img src="gifs/spacer.gif" width="1" height="1" border="0" alt="" /></td></tr>
+ <tr bgcolor="#333366"><td colspan="2"><?php spacer(); ?></td></tr>
  <tr>
   <td colspan="2">
    <div class="indented">
