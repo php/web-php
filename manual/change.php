4c0191e6260e7e1d780b3789981627cf5fbbbd4d
Gabor Hojtsy <goba@php.net>
2003-01-26 10:14:39 +0000

    Do not interfere with general $lang parameter, use a whole $page param instead.

--- manual/change.php
+++ manual/change.php
@@ -2,7 +2,7 @@
 
 include_once "prepend.inc";
 
-// Redirect to new manual page (lang can also contain en/print for example)
-mirror_redirect("/manual/" . $_GET['lang'] . "/" . $_GET['file']);
+// Redirect to new manual page
+mirror_redirect("/manual/" . $_GET['page']);
 
 ?>
