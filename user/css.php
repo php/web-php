8e59f97866384a6ac29fb5bd1723af396ab693ef
Gabor Hojtsy <goba@php.net>
2003-05-09 19:14:55 +0000

    Use proper content type

--- user/css.php
+++ user/css.php
@@ -6,6 +6,9 @@ include_once "prepend.inc";
 // The output of this page should not be cached
 header_nocache();
 
+// Proper content-type header
+header("Content-type: text/css");
+
 // If we have a valid country, output a CSS rule
 // marking the events in the user's country specially
 if (i2c_valid_country()) {
