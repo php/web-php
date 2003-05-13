a7423ced2af255510f9783d4a9769c216c5cfc39
James Cox <imajes@php.net>
2003-05-13 21:16:38 +0000

    ok, so actually we get even more of a gain by removing the /user/css.php stuff. Flame me later, but this is going to stay.. we can't afford the apache hit this causes on every page load.

--- user/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// Simulate a /user shortcut call
-$_SERVER['REQUEST_URI'] = $HTTP_SERVER_VARS['REQUEST_URI'] = '/user';
-include_once 'prepend.inc';
-include_once "$DOCUMENT_ROOT/error/index.php";
-
-?>
