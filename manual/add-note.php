22cf5a8f36ebceb665e504bc7e895e87a7d96a42
Andrew Lindeman <alindeman@php.net>
2003-05-17 20:40:45 +0000

    lang doesn't seem to be needed and would just cause issues if E_ALL errors were on

--- manual/add-note.php
+++ manual/add-note.php
@@ -1,6 +1,4 @@
 <?php
-/* $Id$ */
-
 // Sitewide functions
 include_once 'prepend.inc';
 
@@ -14,7 +12,7 @@ include_once 'shared-manual.inc';
 commonHeader("Manual Notes");
 
 // We have a submitted form to process
-if (isset($note) && isset($user) && isset($lang) &&
+if (isset($note) && isset($user) &&
     isset($sect) && isset($redirect) && isset($action)) {
 
     // Clean off leading and trailing whitespace 
@@ -76,7 +74,6 @@ if (isset($note) && isset($user) && isset($lang) &&
             array(
                 "user" => ($MQ ? stripslashes($user) : $user),
                 "note" => ($MQ ? stripslashes($note) : $note),
-                "lang" => ($MQ ? stripslashes($lang) : $lang),
                 "sect" => ($MQ ? stripslashes($sect) : $sect)
             )
         );
@@ -213,8 +210,7 @@ if (!isset($sect)) {
 else {?>
 <form method="post" action="<?php echo $PHP_SELF; ?>">
  <input type="hidden" name="sect" value="<?php echo clean($sect); ?>" />
- <input type="hidden" name="redirect" value="<?php if (isset($redirect)) { echo clean($redirect); } ?>" />
- <input type="hidden" name="lang" value="<?php if (isset($lang)) { echo clean($lang); } ?>" />
+ <input type="hidden" name="redirect" value="<?php echo clean($redirect); ?>" />
  <table border="0" cellpadding="5" cellspacing="0" bgcolor="#d0d0d0">
   <tr>
    <td colspan="2">
@@ -233,7 +229,7 @@ else {?>
   </tr>
   <tr valign="top">
    <td><b>Your notes:</b></td>
-   <td><textarea name="note" rows="20" cols="60" wrap="virtual"><?php if (isset($note)) { echo clean($note); } ?></textarea><br>
+   <td><textarea name="note" rows="20" cols="60" wrap="virtual"><?php echo clean($note); ?></textarea><br>
   </td>
   </tr>
   <tr>
