035dff4143560c4badc08cc81da7bcaa360314eb
foobar <sniper@php.net>
2003-05-29 06:16:20 +0000

    Fine tuning

--- release_4_3_2.php
+++ release_4_3_2.php
@@ -16,23 +16,28 @@ PHP 4.3.2 Release Announcement
 
 <p>[ <a href="/release_4_3_2_fr.php">Version Fran&ccedil;aise</a> ]&nbsp;</p>
 
+<p>
+ After a lengthy QA process, PHP 4.3.2 is finally out. 
+ This maintenance release solves a lot of bugs found in earlier PHP versions
+ and is <b>strongly</b> recommended upgrade for all users of PHP.
+</p>
+
 <h2>Bugfix release</h2>
 
-<p>This bug fix release solves a lot of bugs found in PHP 4.3.1 and earlier versions.
-   PHP 4.3.2 includes among others the following fixes, additions and improvements:
-<ul>
- <li>Fixed several integer overflows.</li> 
- <li>Fixed several 64-bit problems.</li>
- <li>Added new Apache 2.0 SAPI module (sapi/apache2handler, enabled with --with-apxs2).</li>
- <li>Improved dba extension.</li>
- <li>Improved thttpd SAPI module.</li>
- <li>Syncronized bundled GD library with GD 2.0.12 .</li>
- <li>Dropped support for GDLIB version 1.x.x (php_gd.dll) on Windows.</li>
- <li>Added unix man page for CLI version of PHP.</li>
- <li>Added "disable_classes" php.ini option to allow administrators to disable certain classes for security reasons.</li>
-</ul>
-For a full list of changes in PHP 4.3.2, see the
-<a href="/ChangeLog-4.php">NEWS</a> file.
+<p>
+ PHP 4.3.2 contains, among others, following important fixes, additions and improvements:
+ <ul>
+  <li>Fixes for several integer overflows.</li> 
+  <li>Fixes for several 64-bit problems.</li>
+  <li>New Apache 2.0 SAPI module (sapi/apache2handler, enabled with --with-apxs2).</li>
+  <li>Improvements to dba extension.</li>
+  <li>Improvements to thttpd SAPI module.</li>
+  <li>Syncronized bundled GD library with GD 2.0.12.</li>
+  <li>Dropped support for GDLIB version 1.x.x (php_gd.dll) on Windows.</li>
+  <li>An unix man page for CLI version of PHP.</li>
+  <li>New "disable_classes" php.ini option to allow administrators to disable certain classes for security reasons.</li>
+  </ul>
+ For a full list of changes in PHP 4.3.2, see the <a href="/ChangeLog-4.php">NEWS</a> file.
 </p>
 
 <?php commonFooter(); ?>
