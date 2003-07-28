b63886576d2ef8482818aeae7c20be6d84675058
Gabor Hojtsy <goba@php.net>
2003-07-28 09:12:51 +0000

    Reformatting and adding more information on the webmaster address publicly archived

--- copyright.php
+++ copyright.php
@@ -1,5 +1,7 @@
 <?php
-require_once 'prepend.inc';
+// $Id$
+$_SERVER['BASE_PAGE'] = 'copyright.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/include/prepend.inc';
 commonheader("Copyright and License");
 ?>
 
@@ -8,44 +10,54 @@ commonheader("Copyright and License");
 <h2>PHP License</h2>
 
 <p>
-For information on the PHP License (i.e. using the PHP language),
-<?php echo make_link('/license/', 'click here'); ?>.
+ For information on the PHP License (i.e. using the PHP language),
+ <?php print_link('/license/', 'see our licensing information page'); ?>.
 </p>
 
 <h2>Website Copyright</h2>
 
 <p>
-The code, text, PHP logo, and graphical elements on this website 
-and the mirror websites (the "Site") are Copyright &copy; 2001-2003
-The PHP Group. All rights reserved.
+ The code, text, PHP logo, and graphical elements on this website 
+ and the mirror websites (the "Site") are Copyright &copy; 2001-2003
+ The PHP Group. All rights reserved.
 </p>
 
 <p>
-Except as otherwise indicated elsewhere on this Site, you are free
-to view, download and print the documents and information available 
-on this Site subject to the following conditions:
+ Except as otherwise indicated elsewhere on this Site, you are free
+ to view, download and print the documents and information available 
+ on this Site subject to the following conditions:
 </p>
 
 <ul>
-<li>You may not remove any copyright or other proprietary notices
-contained in the documents and information on this Site.</li>
-<li>The rights granted to you constitute a license and not a transfer
-of title.</li>
-<li>The rights specified above to view, download and print the
-documents and information available on this Site are not applicable
-to the graphical elements, design or layout of this Site. These
-elements of the Site are protected by trade dress and other laws
-and may not be copied or imitated in whole or in part.</li>
+ <li>
+  You may not remove any copyright or other proprietary notices
+  contained in the documents and information on this Site.
+ </li>
+ <li>
+  The rights granted to you constitute a license and not a transfer
+  of title.
+ </li>
+ <li>
+  The rights specified above to view, download and print the
+  documents and information available on this Site are not applicable
+  to the graphical elements, design or layout of this Site. These
+  elements of the Site are protected by trade dress and other laws
+  and may not be copied or imitated in whole or in part.
+ </li>
 </ul>
 
+<h2>Contact</h2>
+
 <p>
-You can contact the webmaster at 
-<?php echo make_link('mailto:webmaster@php.net', 'webmaster@php.net'); ?>.
+ You can contact the webmaster at 
+ <?php print_link('mailto:webmaster@php.net', 'webmaster@php.net'); ?>.
+ Note that this address is mapped to a mailing list and a newsgroup, so
+ every message you send will be stored in public archives.
 </p>
 
 <p>
-For more information on the PHP Group and the PHP project, please see 
-<?php echo make_link('http://www.php.net/', 'http://www.php.net/'); ?>.
+ For more information on the PHP Group and the PHP project, please see 
+ <?php print_link('http://www.php.net/'); ?>.
 </p>
 
 <?php commonfooter(); ?>
