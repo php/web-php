6472bfbc5907564f680e0c3cd75e044d4a6fe9fc
Gabor Hojtsy <goba@php.net>
2003-07-30 10:06:22 +0000

    Change link of German ML as requested

--- mailing-lists.php
+++ mailing-lists.php
@@ -328,7 +328,7 @@ that have been set up by others in the community to host local
   <li><a href="news:dk.edb.internet.webdesign.serverside.php">Danish Newsgroup</a></li>
   <li><a href="http://www.ilovephp.com/mailinglist/">French Mailing List</a></li>
   <li><a href="news:fr.comp.infosystemes.www.auteurs.php">French Newsgroup</a></li>
-  <li><a href="http://www.php-center.de/">German Mailing List</a></li>
+  <li><a href="http://www.phpbar.de/">German Mailing List</a></li>
   <li><a href="news:de.comp.lang.php.misc">German Newsgroup</a> (also other topics under de.comp.lang.php)</li>
   <li><a href="http://weblabor.hu/wl-phplista/etikett">Hungarian Mailing List</a></li>
   <li><a href="http://groups.yahoo.com/group/php_greek">Greek Mailing List</a></li>
