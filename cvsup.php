683912d1e00a9291088220e764639e186a923791
Gabor Hojtsy <goba@php.net>
2003-08-02 10:10:13 +0000

    Indentation, inclusion independence, and replacing php3 and php4 with php-src in the instructions

--- cvsup.php
+++ cvsup.php
@@ -1,41 +1,55 @@
 <?php
-require_once 'prepend.inc';
-commonHeader("Using CVSup to maintain local CVS repository");
+// $Id$
+$_SERVER['BASE_PAGE'] = 'cvsup.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/include/prepend.inc';
+commonHeader("Using CVSup to maintain a local CVS repository");
 ?>
 
 <h1>Using CVSup To Maintain A Local CVS Repository</h1>
 
 <p>
-In addition to anonymous CVS, we also provide read-only access to
-our CVS repository by using CVSup. While CVSup is faster at updating
-large trees, it is not as widely available as the standard cvs client.
-We will describe now how you can use CVSup.
+ In addition to anonymous CVS, we also provide read-only access to
+ our CVS repository by using CVSup. While CVSup is faster at updating
+ large trees, it is not as widely available as the standard cvs client.
+ We will describe now how you can use CVSup.
 </p>
 
 <ol>
-<li>Download the appropiate binary distribution from
-<a href="http://www.polstra.com/projects/freeware/CVSup/">the
-CVSup homepage</a>.</li>
-<li>Follow the instructions in the Install file of the binary distribution</li>
-<li>Create a php.cvsup file containing the following content:
-<pre>
+ <li>
+  Download the appropiate binary distribution from
+  <a href="http://www.polstra.com/projects/freeware/CVSup/">the
+  CVSup homepage</a>.
+ </li>
+ <li>
+  Follow the instructions in the Install file of the binary distribution
+ </li>
+ <li>
+  Create a php.cvsup file containing the following content:
+  <pre>
 *default host=CVSup.php.net
 *default base=/usr/src/php
 *default release=cvs
 *default compress
 *default tag=.
 *default delete use-rel-suffix
-php3
-php4
-</pre>
-This will cause the php3 and php4 trees to be stored in /usr/src/php
-(you need to create that directory manually).</li>
-<li>At a command prompt, simply type:
-<pre>cvsup php.cvsup</pre></li>
-<li>If you are behind a firewall, you might need to use:
-<pre>cvsup -P - php.cvsup</pre></li>
-<li>To build PHP, you need to install a few utilities (GNU autoconf,
-libtool, bison). Cd into php3 or php4 and run <code>./buildconf</code>.</li>
+php-src
+  </pre>
+  This will cause the php-src tree to be stored in <tt>/usr/src/php</tt>
+  (you need to create that directory manually).
+ </li>
+ <li>
+  At a command prompt, simply type:
+  <pre>cvsup php.cvsup</pre>
+ </li>
+ <li>
+  If you are behind a firewall, you might need to use:
+  <pre>cvsup -P - php.cvsup</pre>
+ </li>
+ <li>
+  To build PHP, you need to install a few utilities (GNU autoconf,
+  libtool, bison). Cd into <tt>php-src</tt> and run
+  <code>./buildconf</code>.
+ </li>
 </ol>
 
 <?php commonFooter(); ?>
