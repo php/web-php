4a6b37b4169f71a5dd1bc1a8d16c0fd71dfd104c
Gabor Hojtsy <goba@php.net>
2003-08-03 11:29:08 +0000

    Updating the prepend include, add base page support and WS fixing to reflect the common layout

--- support.php
+++ support.php
@@ -1,104 +1,114 @@
 <?php
-/* $Id$ */
-
-include './include/prepend.inc';
-
+// $Id$
+$_SERVER['BASE_PAGE'] = 'support.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/include/prepend.inc';
 commonHeader("Getting Help");
 ?>
 
 <h1>Documentation</h1>
+
 <p>
-A good place to start is by skimming through the ever-growing list of <a
-href="/FAQ.php">frequently asked questions (with answers, of course)</a>. Then
-have a look at the rest of the online manual and other resources in the <a
-href="/docs.php">documentation</a> section.  </p>
+ A good place to start is by skimming through the ever-growing list of <a
+ href="/FAQ.php">frequently asked questions (with answers, of course)</a>. Then
+ have a look at the rest of the online manual and other resources in the <a
+ href="/docs.php">documentation</a> section.
+</p>
 
 <h1>Books</h1>
+
 <p>
-<a href="/books.php">Books</a> are convenient resources to begin exploring 
-PHP. <a href="/books.php">The titles listed here</a> will help you to start 
-learning PHP, as well as extending your existing knowledge.
+ <a href="/books.php">Books</a> are convenient resources to begin exploring 
+ PHP. <a href="/books.php">The titles listed here</a> will help you to start 
+ learning PHP, as well as extending your existing knowledge.
 </p>
 
 <h1>Mailing Lists</h1>
+
 <p>
-There are a number of mailing lists devoted to talking about PHP and related
-projects. <a href="mailing-lists.php">This list</a> describes them all, has
-links to searchable archives for all of the lists, and explains how to
-subscribe to the lists.
+ There are a number of mailing lists devoted to talking about PHP and related
+ projects. <a href="mailing-lists.php">This list</a> describes them all, has
+ links to searchable archives for all of the lists, and explains how to
+ subscribe to the lists.
 </p>
 
 <h1>Newsgroups</h1>
+
 <p>
-The PHP language newsgroup is <tt>comp.lang.php</tt>, available on any
-news server around the globe. In addition to this many of our mailing
-lists are also reflected onto the news server at
-<a href="news://news.php.net">news://news.php.net/</a>. The
-server also has a read only web interface at
-<a href="http://news.php.net/">http://news.php.net/</a>.
+ The PHP language newsgroup is <tt>comp.lang.php</tt>, available on any
+ news server around the globe. In addition to this many of our mailing
+ lists are also reflected onto the news server at
+ <a href="news://news.php.net">news://news.php.net/</a>. The
+ server also has a read only web interface at
+ <a href="http://news.php.net/">http://news.php.net/</a>.
 </p>
+
 <p>
-Mailing list messages are transfered to newsgroup posts and
-newsgroup posts are sent to the mailing lists. Please note
-that these newsgroups are only available on this server.
+ Mailing list messages are transfered to newsgroup posts and
+ newsgroup posts are sent to the mailing lists. Please note
+ that these newsgroups are only available on this server.
 </p>
 
 <h1>User Groups</h1>
+
 <p>
-Christopher R. Moewes-Bystrom is running a PHP user group registry at <a
-href="http://www.phpusergroups.org/">http://www.phpusergroups.org/</a>.
-There's also a list of user groups in Germany, as well as news from them,
-and link to other countries user groups at
-<a href="http://www.phpug.de/">http://www.phpug.de/</a>.
-<a href="http://php.meetup.com/">PHP Meetup</a> is another great
-opportunity to get together.
+ Christopher R. Moewes-Bystrom is running a PHP user group registry at <a
+ href="http://www.phpusergroups.org/">http://www.phpusergroups.org/</a>.
+ There's also a list of user groups in Germany, as well as news from them,
+ and link to other countries user groups at
+ <a href="http://www.phpug.de/">http://www.phpug.de/</a>.
+ <a href="http://php.meetup.com/">PHP Meetup</a> is another great
+ opportunity to get together.
 </p>
 
 <h1>Events &amp; Training</h1>
+
 <p>
-A list of upcoming events (such as user group meetings and PHP training
-sessions) is included in the right-hand column of the front page.
-If you want to list an upcoming event, just fill out the form <a
-href="http://www.php.net/submit-event.php">on this page</a>.
+ A list of upcoming events (such as user group meetings and PHP training
+ sessions) is included in the right-hand column of the front page, and
+ on the <a href="/cal.php">event calendar page</a>. If you want to list
+ an upcoming event, just fill out the form <a
+ href="http://www.php.net/submit-event.php">on this page</a>.
 </p>
 
 <h1>Sample Code</h1>
+
 <p>
-Looking for some more sample PHP scripts? Our <a
-href="/links.php">links page</a> lists some archives of sample PHP code - 
-great places to find many example scripts and useful functions, organized for 
-your searching pleasure!
+ Looking for some more sample PHP scripts? Our <a
+ href="/links.php">links page</a> lists some archives of sample PHP code - 
+ great places to find many example scripts and useful functions, organized for 
+ your searching pleasure!
 </p>
 
 <h1>Knowledge Base</h1>
+
 <p>
-The knowledge base is a growing collection of PHP related information in
-a searchable question and answer format. Anyone can contribute, and
-everyone is encouraged to do so. You can visit the Knowledge Base
-at <a href="http://php.faqts.com/">http://php.faqts.com/</a>.
+ The knowledge base is a growing collection of PHP related information in
+ a searchable question and answer format. Anyone can contribute, and
+ everyone is encouraged to do so. You can visit the Knowledge Base
+ at <a href="http://php.faqts.com/">http://php.faqts.com/</a>.
 </p>
 
 <h1>Instant Resource Center</h1>
+
 <p>
-Otherwise known as IRC or Internet Relay Chat. Here you can usually find
-experienced PHP people sitting around doing nothing on the #php channel.
-On <a href="http://oftc.net">OFTC</a> as well as on all major networks
-(<a href="http://www.efnet.org/">EFNet</a>,
-<a href="http://www.ircnet.com/">IRCNet</a>,
-<a href="http://www.quakenet.org">QuakeNet</a>,
-<a href="http://www.dal.net/">DALNet</a> and
-<a href="http://www.freenode.net/">freenode</a>).
+ Otherwise known as IRC or Internet Relay Chat. Here you can usually find
+ experienced PHP people sitting around doing nothing on the #php channel.
+ On <a href="http://oftc.net">OFTC</a> as well as on all major networks
+ (<a href="http://www.efnet.org/">EFNet</a>,
+ <a href="http://www.ircnet.com/">IRCNet</a>,
+ <a href="http://www.quakenet.org">QuakeNet</a>,
+ <a href="http://www.dal.net/">DALNet</a> and
+ <a href="http://www.freenode.net/">freenode</a>).
 </p>
 
 <h1>PHP.net webmasters</h1>
+
 <p>
-If you have a problem or suggestion with the PHP.net website,
-please contact the webmasters mailing list at
-<a href="mailto:webmaster@php.net">webmaster@php.net</a>.
-This mailing list is archived and also available as a newsgroup,
-much like all the other lists.
+ If you have a problem or suggestion with the PHP.net website,
+ please contact the webmasters' mailing list at
+ <a href="mailto:webmaster@php.net">webmaster@php.net</a>.
+ This mailing list is archived and also available as a newsgroup,
+ much like all the other lists.
 </p>
 
-<?php
-commonFooter();
-?>
+<?php commonFooter(); ?>
