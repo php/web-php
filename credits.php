02655718a299fd4cd4e54faca34e4d312ea4f216
Gabor Hojtsy <goba@php.net>
2003-08-07 11:12:44 +0000

    Add credits page syles to the sitewide CSS rules, and move the credits page to the new CSS based layout...

--- credits.php
+++ credits.php
@@ -3,32 +3,21 @@
 $_SERVER['BASE_PAGE'] = 'credits.php';
 include_once $_SERVER['DOCUMENT_ROOT'] . '/include/prepend.inc';
 
-// Output buffering supported
-if (function_exists('ob_start')) {
-    
-    // Put credits information to $credits
-    ob_start();
-    phpcredits();
-    $credits = ob_get_contents();
-    ob_end_clean();
+// Put credits information to $credits
+ob_start();
+phpcredits();
+$credits = ob_get_contents();
+ob_end_clean();
 
-    // Strip all but the body
-    preg_match('!<body.*?>(.*)</body>!ims', $credits, $m);
+// Strip all but the body and drop styles
+preg_match('!<body.*?>(.*)</body>!ims', $credits, $m);
+$credits = preg_replace('!<style.*?>.*</style>!ims', '', $m[1]);
 
-    // Strip out any style
-    $credits = preg_replace('!<style.*?>.*</style>!ims', '', $m[1]);
-
-    // If there is something left, print it out
-    if ($credits) {
-        commonHeader("Credits");
-        echo $credits;
-        commonFooter(); 
-    }
-}
- 
-// No output buffer support, standard credits page
-else {
-    phpcredits();
+// If there is something left, print it out
+if ($credits) {
+    site_header("Credits");
+    echo $credits;
+    site_footer(); 
 }
 
 ?>
