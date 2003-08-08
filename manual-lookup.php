8a28534c34adc68065f6dd607424a90c106e7754
Gabor Hojtsy <goba@php.net>
2003-08-08 13:46:28 +0000

    Manual lookup remake:  - use new type of includes  - add base page functionality  - use $_GET vars instead of register_globals  - use existing information ($MQ, $LANG)  - use mirror_redirect() for redirection

--- manual-lookup.php
+++ manual-lookup.php
@@ -1,25 +1,27 @@
-<?php // -*- C++ -*-
+<?php
+// $Id$
+$_SERVER['BASE_PAGE'] = 'manual-lookup.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/include/prepend.inc';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/include/loadavg.inc';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/include/manual-lookup.inc';
 
-require_once 'prepend.inc';
-require_once 'loadavg.inc';
-require_once 'manual-lookup.inc';
-
-if (!$function && $pattern) $function = $pattern;
-
-if (get_magic_quotes_gpc()) $function = stripslashes($function);
-
-$function = strtolower($function);
-
-if (empty($lang)) $lang = default_language();
-
-$file = find_manual_page($lang, $function);
-
-if ($file) {
-    header("Location: $file");
-    exit;
+// BC code, so patter and function can equaly be used as
+// parameters to specify the function name
+if (empty($_GET['function']) && !empty($_GET['pattern'])) {
+    $_GET['function'] = $_GET['pattern'];
 }
 
-$notfound = $function;
-include 'quickref.php';
+// Prepare data for search
+if ($MQ) { $_GET['function'] = stripslashes($_GET['function']); }
+$_GET['function'] = strtolower($_GET['function']);
+
+// Try to find appropriate manual page
+if ($file = find_manual_page($LANG, $_GET['function'])) {
+    mirror_redirect($file);
+}
+
+// Fall back to a quick reference search
+$notfound = $_GET['function'];
+include_once $_SERVER['DOCUMENT_ROOT'] . '/quickref.php';
 
 ?>
