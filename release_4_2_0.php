25931ebe1c3bc7d3bdfe13f9fc43463501b586f3
Gabor Hojtsy <goba@php.net>
2003-08-14 12:21:39 +0000

    Release annoucement fixes:  - make these pages really stand on their own, not look    like as if they were copied from the homepage...  - use new headers and footers  - use correct identation and adhere to standards

--- release_4_2_0.php
+++ release_4_2_0.php
@@ -1,77 +1,82 @@
 <?php
-/* if you're reading this, it isn't because you've found a security hole.
-   this is an open source website. read and learn! */
-
-header("Cache-Control: public, max-age=600");
-
-require_once 'prepend.inc';
-
-commonHeader("Hypertext Preprocessor");
-echo "\n<!--$MYSITE-->\n";
+// $Id$
+$_SERVER['BASE_PAGE'] = 'release_4_2_0.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/include/prepend.inc';
+site_header("PHP 4.2.0 Release Announcement");
 ?>
 
-<h1>
-PHP 4.2.0 Release Announcement
-</h1>
+<h1>PHP 4.2.0 Release Announcement</h1>
 
-
-<p>After an orderly QA process, PHP 4.2.0 is <a href="/downloads.php">out</a>!<br>
+<p>After an orderly QA process, PHP 4.2.0 is <a href="/downloads.php">out</a>!<br />
 [ <a href="/release_4_2_0_fr.php">Version Fran&ccedil;aise</a> ]&nbsp;</p>
 
 <h2>External variables</h2>
 
-<p>The biggest change in PHP 4.2.0 concerns variable handling. <b>External
+<p>
+ The biggest change in PHP 4.2.0 concerns variable handling. <strong>External
  variables (from the environment, the HTTP request, cookies or the web server)
- are no longer registered in the global scope by default.</b> The preferred
+ are no longer registered in the global scope by default.</strong> The preferred
  method of accessing these external variables is by using the new Superglobal
- arrays, introduced in PHP 4.1.0. More information about this change:
+ arrays, introduced in PHP 4.1.0. More information about this change:</p>
+ 
 <ul>
  <li><a href="/manual/en/language.variables.predefined.php">PHP Manual: Predefined variables</a></li>
  <li><a href="/release_4_1_0.php">The PHP 4.1.0 release announcement</a></li>
  <li><a href="http://www.zend.com/zend/art/art-oertli.php">Thomas Oertli's article on secure programming in PHP</a></li>
 </ul>
-</p>
 
 <h2>Compatibility</h2>
 
-<p>The Apache Software Foundation recently released their first
+<p>
+ The Apache Software Foundation recently released their first
  General Availability version of Apache 2. PHP 4.2.0 will have
- <b>EXPERIMENTAL</b> support for this version. You can
- build a DSO module for Apache 2 with --with-apxs2. We do <b>not</b> recommend
- that you use this in a production environment.
+ <strong>EXPERIMENTAL</strong> support for this version. You can
+ build a DSO module for Apache 2 with --with-apxs2. We do
+ <strong>not</strong> recommend that you use this in a production
+ environment.
 </p>
-<p>PHP 4.2.0 still lacks certain key features on Mac OS X and
+<p>
+ PHP 4.2.0 still lacks certain key features on Mac OS X and
  Darwin, and isn't officially supported by the PHP Group on
  these platforms. Specifically, building PHP as a dynamically
  loaded Apache module isn't supported at this time. PHP 4.3.0,
  due to be released in August, 2002, will be the first PHP
  release to officially support Mac OS X. It, along with future
  Mac OS X and Apache releases, will enable full feature parity
- with other PHP platforms. <b>Update:</b> 
+ with other PHP platforms. <strong>Update:</strong> 
  <a href="http://www.entropy.ch/software/macosx/php/">Instructions on
- overcoming these limitations</a></p>
+ overcoming these limitations</a>
+</p>
 
 <h2>Improvements</h2>
+
 <p>PHP 4.2.0 includes several improvements:</p>
+
 <ul>
- <li><b>External variables (from the environment, the HTTP request, cookies or
-  the web server) are no longer registered as global variables</b></li>
+ <li>
+  <strong>External variables (from the environment, the HTTP request, cookies or
+  the web server) are no longer registered as global variables</strong>
+ </li>
  <li>Overhaul of the sockets extension</li>
  <li>Highly improved performance with file uploads</li>
- <li>The satellite and mailparse extensions were moved to PECL and are no longer 
-  bundled with the official PHP release</li>
+ <li>
+  The satellite and mailparse extensions were moved to PECL and are no longer 
+  bundled with the official PHP release
+ </li>
  <li>The posix extension has been cleaned up</li>
  <li>iconv handling has been improved</li>
- <li>Output buffering support, which was introduced in PHP 4.1.0 has
-  been stabilized</li>
+ <li>
+  Output buffering support, which was introduced in PHP 4.1.0 has
+  been stabilized
+ </li>
  <li>Improved performance and stability of the domxml extension</li>
  <li>New multibyte regular expression support</li>
- <li><b>LOTS</b> of fixes and new functions</li>
+ <li><strong>LOTS</strong> of fixes and new functions</li>
 </ul>
 
 <p>
-For a full list of changes in PHP 4.2.0, see the
-<a href="/ChangeLog-4.php">NEWS</a> file.
+ For a full list of changes in PHP 4.2.0,
+ <a href="/ChangeLog-4.php#4.0.2">see the ChangeLog</a>.
 </p>
 
-<?php commonFooter(); ?>
+<?php site_footer(); ?>
--- release_4_2_0_fr.php
+++ release_4_2_0_fr.php
@@ -1,77 +1,91 @@
 <?php
-header("Cache-Control: public, max-age=600");
-require_once 'prepend.inc';
-header("Content-language: fr");
-commonHeader("Hypertext Preprocessor");
-echo "\n<!--$MYSITE-->\n";
+// $Id$
+$_SERVER['BASE_PAGE'] = 'release_4_2_0_fr.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/include/prepend.inc';
+site_header("Annonce de publication de PHP 4.2.0");
 ?>
 
-<h1>
-Annonce de PHP 4.2.0
-</h1>
+<h1>Annonce de publication de PHP 4.2.0</h1>
 
-<p>Apr&egrave;s avoir pass&eacute; avec succ&egrave;s le processus qualit&eacute;, PHP 4.2.0 est officiellement <a href="http://www.php.net/downloads.php">publi&eacute;</a>!</p>
+<p>
+ Apr&egrave;s avoir pass&eacute; avec succ&egrave;s le processus
+ qualit&eacute;, PHP 4.2.0 est officiellement <a href="/downloads.php">publi&eacute;</a>!
+</p>
 
 <h2>Variables externes</h2>
 
 <p>
-Le changement le plus important de PHP 4.2.0 concerne la gestion des
- variables. Les <b>variables externes (issues de l'environnement d'ex&eacute;cution,
-des requ&ecirc;tes HTTP, des cookies ou du serveur web) ne sont plus enregistr&eacute;es
-dans l'environnement d'ex&eacute;cution global par d&eacute;faut.</b> La m&eacute;thode recommand&eacute;e
-pour acc&eacute;der aux variables externes est d'utiliser les nouveaux tableaux globaux,
-introduits en PHP 4.1.0. Pour plus d'informations sur ces modifications : 
+ Le changement le plus important de PHP 4.2.0 concerne la gestion des
+ variables. Les <strong>variables externes (issues de l'environnement d'ex&eacute;cution,
+ des requ&ecirc;tes HTTP, des cookies ou du serveur web) ne sont plus enregistr&eacute;es
+ dans l'environnement d'ex&eacute;cution global par d&eacute;faut.</strong>
+ La m&eacute;thode recommand&eacute;e pour acc&eacute;der aux variables
+ externes est d'utiliser les nouveaux tableaux globaux, introduits en
+ PHP 4.1.0. Pour plus d'informations sur ces modifications:
+</p>
 <ul>
  <li><a href="http://www.php.net/manual/en/language.variables.predefined.php">PHP Manual : Predefined variables</a></li>
  <li><a href="http://www.php.net/release_4_1_0.php">Annonce de PHP 4.1.0</a></li>
  <li><a href="http://www.zend.com/zend/art/art-oertli.php">Article de Thomas Oertli sur la programmation s&eacute;curis&eacute;e en PHP</a></li>
  <li><a href="http://www.phpinfo.net/?p=articles&rub=globals">Article de Jean Pierre D&eacute;zelus sur les variables globales</a></li>
 </ul>
-</p>
 
 <h2>Compatibilit&eacute;</h2>
 
 <p>
-L'ASF (Apache Software Foundation) a r&eacute;cemment publi&eacute; sa premi&egrave;re
-version publique d'Apache 2. PHP 4.2.0 dispose du support
- <b>EXPERIMENTAL</b> d'Apache 2. Vous pouvez compiler un module
+ L'ASF (Apache Software Foundation) a r&eacute;cemment publi&eacute; sa premi&egrave;re
+ version publique d'Apache 2. PHP 4.2.0 dispose du support
+ <strong>EXPERIMENTAL</strong> d'Apache 2. Vous pouvez compiler un module
  DSO pour Apache 2 avec l'option --with-apxs2. Nous recommandons
- de <b>ne pas</b> utiliser cette combinaison en environnement de
+ de <strong>ne pas</strong> utiliser cette combinaison en environnement de
  production.
 </p>
-<p>Il manque encore &agrave; PHP 4.2.0 des fonctionnalit&eacute;s cl&eacute;s
-sur MacOSX et sur Darwin. PHP n'est donc pas officiellement
-support&eacute; par le PHP group sur ces plates-formes. Sp&eacute;cifiquement,
-compiler PHP comme module Apache dynamiquement charg&eacute; n'est pas
-encore support&eacute;. PHP 4.3.0, dont la publication est pr&eacute;vue pour
-Ao&ucirc;t 2002, sera la premi&egrave;re version qui supportera officiellement 
-Mac OS X. Cette version, aussi bien pour les futures versions de
+<p>
+ Il manque encore &agrave; PHP 4.2.0 des fonctionnalit&eacute;s cl&eacute;s
+ sur MacOSX et sur Darwin. PHP n'est donc pas officiellement
+ support&eacute; par le PHP group sur ces plates-formes. Sp&eacute;cifiquement,
+ compiler PHP comme module Apache dynamiquement charg&eacute; n'est pas
+ encore support&eacute;. PHP 4.3.0, dont la publication est pr&eacute;vue pour
+ Ao&ucirc;t 2002, sera la premi&egrave;re version qui supportera officiellement 
+ Mac OS X. Cette version, aussi bien pour les futures versions de
  Mac OS X et Apache, sera totalement synchronis&eacute; avec les autres
- plates-formes PHP.</p>
+ plates-formes PHP.
+</p>
 
 <h2>Am&eacute;liorations</h2>
-<p>PHP 4.2.0 inclut de nombreuses innovations : </p>
+
+<p>PHP 4.2.0 inclut de nombreuses innovations:</p>
 <ul>
- <li><b>Les variables externes (issues de l'environnement d'ex&eacute;cution,
-des requ&ecirc;tes HTTP, des cookies ou du serveur web) ne sont plus enregistr&eacute;es
-dans l'environnement d'ex&eacute;cution global par d&eacute;faut.
-</b></li>
+ <li>
+  <strong>Les variables externes (issues de l'environnement
+  d'ex&eacute;cution, des requ&ecirc;tes HTTP, des cookies ou du serveur web)
+  ne sont plus enregistr&eacute;es dans l'environnement d'ex&eacute;cution
+  global par d&eacute;faut.
+  </strnong>
+ </li>
  <li>Remise en &eacute;tat g&eacute;n&eacute;rale de l'extension socket</li>
  <li>Performances r&eacute;volutionnaires des uploads de fichiers</li>
- <li>Les extensions satellite (corba) et mailparse ont &eacute;t&eacute; plac&eacute;es sous PECL,
- et ne sont plus fournies directement avec la distribution officielle de PHP.</li>
+ <li>
+  Les extensions satellite (corba) et mailparse ont &eacute;t&eacute; plac&eacute;es sous PECL,
+  et ne sont plus fournies directement avec la distribution officielle de PHP.
+ </li>
  <li>L'extension POSIX a &eacute;t&eacute; nettoy&eacute;e</li>
  <li>L'extension iconv a &eacute;t&eacute; am&eacute;lior&eacute;e</li>
- <li>Le support de la bufferisation d'affichage, introduite en PHP 4.1.0,
- a &eacute;t&eacute; stabilis&eacute;e.</li>
- <li>Gain de performance notable et am&eacute;lioration de la stabilit&eacute; de l'extension domxml</li>
+ <li>
+  Le support de la bufferisation d'affichage, introduite en PHP 4.1.0,
+  a &eacute;t&eacute; stabilis&eacute;e.
+ </li>
+ <li>
+  Gain de performance notable et am&eacute;lioration de la
+  stabilit&eacute; de l'extension domxml
+ </li>
  <li>Support des expressions r&eacute;guli&egrave;res multi-octets</li>
- <li><b>ENORMÉMENT</b> de corrections, et de nouvelles fonctions.</li>
+ <li><strong>ENORMMENT</strong> de corrections, et de nouvelles fonctions.</li>
 </ul>
 
 <p>
-Pour une liste compl&egrave;te de changements en PHP 4.2.0, voyez le fichier
-<a href="/ChangeLog-4.php">NEWS</a>, dans la distribution.
+ Pour une liste compl&egrave;te de changements en PHP 4.2.0, voyez le fichier
+ <a href="/ChangeLog-4.php#4.2.0">NEWS</a>, dans la distribution.
 </p>
 
-<?php commonFooter(); ?>
+<?php site_footer(); ?>
