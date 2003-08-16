6fb8a811efc529334d9d289d0430a33febdc9a1c
Gabor Hojtsy <goba@php.net>
2003-08-16 13:56:42 +0000

    Use new header/footer/basepage and add a title to the page

--- stats/index.php
+++ stats/index.php
@@ -1,10 +1,18 @@
 <?php
-
-// This page is displayed if there is no alias to
-// override the stats directory distributed by rsync
-// => no local search is set up
-include_once 'prepend.inc';
-commonHeader("No Stats");
-echo '<p>No visitor statistics information is available on this mirror site.</p>';
-commonFooter();
+// $Id$
+/*
+ This page is displayed if there is no alias to
+ override the stats directory distributed by rsync,
+ which means there is no local search is set up
+*/
+$_SERVER['BASE_PAGE'] = 'stats/index.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/include/prepend.inc';
+site_header("No Stats");
 ?>
+
+<h1>No Stat</h1>
+<p>
+ No visitor statistics information is available on this mirror site.
+</p>
+
+<?php site_footer(); ?>
