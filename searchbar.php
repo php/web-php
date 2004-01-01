3786955d413da9c4a6201ead666230dc4c646579
Gabor Hojtsy <goba@php.net>
2004-01-01 00:20:31 +0000

    and one more 2004 fix

--- searchbar.php
+++ searchbar.php
@@ -47,7 +47,7 @@ echo <<<SEARCHBAR_END
  </p>
 </form>
 <p class="copyright">
- <a href="/copyright.php"{$target}>Copyright &copy; 2002, 2003 The PHP Group</a><br />All rights reserved.
+ <a href="/copyright.php"{$target}>Copyright &copy; 2002 - 2004 The PHP Group</a><br />All rights reserved.
 </p>
 </body>
 </html>
