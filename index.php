2871fac41ddfa515bb3bee9f1b3cd38d5a8a35bb
Zeev Suraski <zeev@php.net>
2004-03-30 10:11:59 +0000

    Trim down to the size appropriate for bugfix releases :)

--- index.php
+++ index.php
@@ -150,44 +150,18 @@ commonHeader("Hypertext Preprocessor");
 // DO NOT REMOVE THIS COMMENT (the RSS parser is dependant on it)
 ?>
 
-<?php news_image("/downloads.php", "php4_logo.gif", "PHP 4"); ?>
-
 <h1> PHP 4.3.5 released!</h1>
 <p>
  <span class="newsdate">[26-Mar-2004]</span>
  The PHP Development Team is proud to announce the release of <a href="/downloads.php">PHP 4.3.5</a>.
- This is primarily a bug fix release, without any new features or additions. PHP 4.3.5
+ This is a bug fix release, without any new features or additions. It
  is by far the most stable release of PHP to date and it is recommended that 
  all users upgrade to this release where possible.
 </p>
 
 <p>
- PHP 4.3.5 contains, among others, the following important fixes, additions and improvements:
-</p>
-
-<ul>
- <li>Fixed INI leak between Apache virtual hosts.</li>
- <li>Fixed crashes inside fgetcsv() and make the function binary safe.</li>
- <li>Fixed compilation with early versions of GCC 3.0.</li>
- <li>Fixed a bug that prevented feof() from working correctly with sockets.</li>
- <li>Improved the matching algorithm inside the get_browser() function.</li>
- <li>Fixed resolving of open_basedir on Win32 systems.</li>
- <li>Fixed incorrect errors for non-existent directories when safe_mode is enabled.</li>
- <li>Bundled OpenSSL dlls on Win32 upgraded to 0.9.7c</li>
- <li>Updated bundled PostgreSQL library to version 7.4 in Windows distribution.</li>
- <li>Bundled PCRE library upgraded to 4.5</li>
- <li>Synchronized bundled GD library with GD 2.0.17</li>
- <li>A number of fixes for 64bit systems.</li>
-
-</ul>
-
-<p>
-Aside from the above mentioned fixes, this release resolves over 140 various bugs and implementational problems.
-</p>
-
-<p>
- For a full list of changes in PHP 4.3.5, see the
- <a href="/ChangeLog-4.php#4.3.5">ChangeLog</a>.
+This release resolves over a hundred various bugs and problems with previous versions.
+ For a full list of changes in PHP 4.3.5, see the <a href="/ChangeLog-4.php#4.3.5">ChangeLog</a>.
 </p>
 
 <hr />
