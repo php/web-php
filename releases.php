51368e529c7a328fc21fe8f9086bf4d9745a69c2
Gabor Hojtsy <goba@php.net>
2004-04-02 16:25:46 +0000

    PHP 5 is no more beta

--- releases.php
+++ releases.php
@@ -9,7 +9,7 @@ $SIDEBAR_DATA = '
 <h3>PHP 5 Releases</h3>
 <p>
  There is no stable PHP 5 release yet. You can find the latest
- beta as well as links to more information on
+ release candidate as well as links to more information on
  <a href="/downloads.php">our downloads page</a>.
 </p>
 
@@ -122,7 +122,6 @@ site_header("Releases");
   </ul>
  </li>
 </ul>
-
 
 <a name="4.3.3"></a>
 <h2>4.3.3</h2>
@@ -328,109 +327,114 @@ site_header("Releases");
  <li>
   Download:
   <a href="http://museum.php.net/php4/php-4.0.5.tar.gz">Source (tar.gz)</a>,
+  <a href="http://museuSource (tar.gz)</a>,
+  <a href="http://museum.php.net/win32/php-4.2.3-Win32.zip">Windows binary</a>,
+  <a href="http://museum.php.net/win32/php-4.2.3-installer.exe">Windows installer</a>
+ </li>
+</ul>
+
+<a name="4.2.2"></a>
+<h2>4.2.2</h2>
+
+<ul>
+ <li>Released: 22 July 2002</li>
+ <li>Announcement: <a href="/release_4_2_2.php">English</a>, <a href="/release_4_2_2_fr.php">French</a></li>
+ <li><a href="/ChangeLog-4.php#4.2.2">ChangeLog</a></li>
+ <li>
+  Download:
+  <a href="http://museum.php.net/php4/php-4.2.2.tar.gz">Source (tar.gz)</a>,
+  <a href="http://museum.php.net/win32/php-4.2.2-Win32.zip">Windows binary</a>,
+  <a href="http://museum.php.net/win32/php-4.2.2-installer.exe">Windows installer</a>
+ </li>
+</ul>
+
+<a name="4.2.1"></a>
+<h2>4.2.1</h2>
+
+<ul>
+ <li>Released: 13 May 2002</li>
+ <li>Announcement: <a href="/release_4_2_1.php">English</a>, <a href="/release_4_2_1_fr.php">French</a></li>
+ <li><a href="/ChangeLog-4.php#4.2.1">ChangeLog</a></li>
+ <li>
+  Download:
+  <a href="http://museum.php.net/php4/php-4.2.1.tar.gz">Source (tar.gz)</a>,
+  <a href="http://museum.php.net/win32/php-4.2.1-Win32.zip">Windows binary</a>,
+  <a href="http://museum.php.net/win32/php-4.2.1-installer.exe">Windows installer</a>
+ </li>
+</ul>
+
+<a name="4.2.0"></a>
+<h2>4.2.0</h2>
+
+<ul>
+ <li>Released: 22 April 2002</li>
+ <li>Announcement: <a href="/release_4_2_0.php">English</a>, <a href="/release_4_2_0_fr.php">French</a></li>
+ <li><a href="/ChangeLog-4.php#4.2.0">ChangeLog</a></li>
+ <li>
+  Download:
+  <a href="http://museum.php.net/php4/php-4.2.0.tar.gz">Source (tar.gz)</a>,
+  <a href="http://museum.php.net/win32/php-4.2.0-Win32.zip">Windows binary</a>,
+  <a href="http://museum.php.net/win32/php-4.2.0-installer.exe">Windows installer</a>
+ </li>
+</ul>
+
+<a name="4.1.2"></a>
+<h2>4.1.2</h2>
+
+<ul>
+ <li>Released: 12 March 2002</li>
+ <li>Announcement: <a href="/release_4_1_2_win32.php">English</a></li>
+ <li><a href="/ChangeLog-4.php#4.1.2">ChangeLog</a></li>
+ <li>
+  Download:
+  <a href="http://museum.php.net/php4/php-4.1.2.tar.gz">Source (tar.gz)</a>,
+  <a href="http://museum.php.net/win32/php-4.1.2-Win32.zip">Windows binary</a>,
+  <a href="http://museum.php.net/win32/php-4.1.2-installer.exe">Windows installer</a>
+ </li>
+</ul>
+
+<a name="4.1.1"></a>
+<h2>4.1.1</h2>
+
+<ul>
+ <li>Released: 26 Dec 2001</li>
+ <li>Announcement: <a href="/release_4_1_1.php">English</a></li>
+ <li><a href="/ChangeLog-4.php#4.1.1">ChangeLog</a></li>
+ <li>
+  Download:
+  <a href="http://museum.php.net/php4/php-4.1.1.tar.gz">Source (tar.gz)</a>,
+  <a href="http://museum.php.net/win32/php-4.1.1-Win32.zip">Windows binary</a>,
+  <a href="http://museum.php.net/win32/php-4.1.1-installer.exe">Windows installer</a>
+ </li>
+</ul>
+
+<a name="4.0.6"></a>
+<h2>4.0.6</h2>
+
+<ul>
+ <li>Released: 23 June 2001</li>
+ <li>Announcement: None</li>
+ <li><a href="/ChangeLog-4.php#4.0.6">ChangeLog</a></li>
+ <li>
+  Download:
+  <a href="http://museum.php.net/php4/php-4.0.6.tar.gz">Source (tar.gz)</a>,
+  <a href="http://museum.php.net/win32/php-4.0.6-Win32.zip">Windows binary</a>
+ </li>
+</ul>
+
+<a name="4.0.5"></a>
+<h2>4.0.5</h2>
+
+<ul>
+ <li>Released: 30 April 2001</li>
+ <li>Announcement: None</li>
+ <li><a href="/ChangeLog-4.php#4.0.5">ChangeLog</a></li>
+ <li>
+  Download:
+  <a href="http://museum.php.net/php4/php-4.0.5.tar.gz">Source (tar.gz)</a>,
   <a href="http://museum.php.net/win32/php-4.0.5-Win32.zip">Windows binary</a>
  </li>
 </ul>
 
 <a name="4.0.4"></a>
-<h2>4.0.4</h2>
-
-<ul>
- <li>Released: 19 December 2000</li>
- <li>Announcement: <a href="http://news.php.net/article.php?group=php.announce&amp;article=28">English</a></li>
- <li><a href="/ChangeLog-4.php#4.0.4">ChangeLog</a></li>
- <li>
-  Download:
-  <a href="http://museum.php.net/php4/php-4.0.4.tar.gz">Source (tar.gz)</a>,
-  <a href="http://museum.php.net/win32/php-4.0.4-Win32.zip">Windows binary</a>,
-  <a href="http://museum.php.net/win32/php-4.0.4-installer.zip">Windows installer</a>,
-  <a href="http://museum.php.net/php4/php-4.0.4pl1.tar.gz">4.0.4pl1 Source (tar.gz)</a>,
-  <a href="http://museum.php.net/win32/php-4.0.4pl1-Win32.zip">4.0.4pl1 Windows binary</a>
- </li>
-</ul>
-
-<a name="4.0.3"></a>
-<h2>4.0.3</h2>
-
-<ul>
- <li>Released: 11 October 2000</li>
- <li>
-  Announcement: <a href="http://news.php.net/article.php?group=php.announce&amp;article=26">English</a>,
-  <a href="http://news.php.net/article.php?group=php.announce&amp;article=27">English (pl1)</a>,
- </li>
- <li><a href="/ChangeLog-4.php#4.0.3">ChangeLog</a></li>
- <li>
-  Download:
-  <a href="http://museum.php.net/php4/php-4.0.3.tar.gz">Source (tar.gz)</a>,
-  <a href="http://museum.php.net/win32/php-4.0.3-Win32.zip">Windows binary</a>,
-  <a href="http://museum.php.net/php4/php-4.0.3pl1.tar.gz">4.0.3pl1 Source (tar.gz)</a>,
-  <a href="http://museum.php.net/win32/php-4.0.3pl1-installer.exe">4.0.3pl1 Windows installer</a>
- </li>
-</ul>
-
-<a name="4.0.2"></a>
-<h2>4.0.2</h2>
-
-<ul>
- <li>Released: 29 August 2000</li>
- <li>Announcement: <a href="http://news.php.net/article.php?group=php.announce&amp;article=24">English</a></li>
- <li><a href="/ChangeLog-4.php#4.0.2">ChangeLog</a></li>
- <li>
-  Download:
-  <a href="http://museum.php.net/php4/php-4.0.2.tar.gz">Source (tar.gz)</a>,
-  <a href="http://museum.php.net/win32/php-4.0.2-Win32.zip">Windows binary</a>
- </li>
-</ul>
-
-<a name="4.0.1"></a>
-<h2>4.0.1</h2>
-
-<ul>
- <li>Released: 28 June 2000</li>
- <li>Announcement: <a href="http://news.php.net/article.php?group=php.announce&amp;article=23">English</a></li>
- <li><a href="/ChangeLog-4.php#4.0.1">ChangeLog</a></li>
- <li>
-  Download:
-  <a href="http://museum.php.net/php4/php-4.0.1.tar.gz">Source (tar.gz)</a>,
-  <a href="http://museum.php.net/win32/php-4.0.1-Win32.zip">Windows binary</a>,
-  <a href="http://museum.php.net/php4/php-4.0.1pl2.tar.gz">4.0.1pl2 Source (tar.gz)</a>,
-  <a href="http://museum.php.net/win32/php-4.0.1pl1-Win32.zip">4.0.1pl1 Windows binary</a>
- </li>
-</ul>
-
-<a name="4.0.0"></a>
-<h2>4.0.0</h2>
-
-<ul>
- <li>Released: 22 May 2000</li>
- <li>Announcement: None</li>
- <li><a href="/ChangeLog-4.php#4.0.0">ChangeLog</a></li>
- <li>
-  Download:
-  <a href="http://museum.php.net/php4/php-4.0.0.tar.gz">Source (tar.gz)</a>,
-  <a href="http://museum.php.net/win32/php-4.0.0-Win32.zip">Windows binary</a>
- </li>
-</ul>
-
-<a name="3.0.x"></a>
-<h2>3.0.x (latest)</h2>
-
-<ul>
- <li>Released: 20 Oct 2000</li>
- <li>Announcement: None</li>
- <li>
-  Download:
-  <ul>
-   <li>
-    <?php download_link("php-3.0.18.tar.gz", "PHP 3.0.18 Source Code"); ?><br />
-    <span class="md5sum">md5: b4b8f7f1151ce66d5f3910a066651133</span>
-   </li>
-   <li>
-    <?php download_link("php-3.0.17-win32.zip", "PHP 3.0.17 Windows Binary"); ?><br />
-    <span class="md5sum">md5: 29029ac1c3c2075dce38bbd804c42f72</span>
-   </li>
-  </ul>
- </li>
-</ul>
-
-<?php site_footer(); ?>
+<
