2866e625aecc0be7088481b4a9cfe156395daad2
Mehdi Achour <didou@php.net>
2004-04-26 14:14:27 +0000

    put back RC1 news

--- index.php
+++ index.php
@@ -223,6 +223,61 @@ commonHeader("Hypertext Preprocessor");
 
 <hr />
 
+<?php news_image("/downloads.php#5", "php-version5.gif", "PHP 5"); ?>
+<a name="5.0.0RC1"></a>
+<h1>PHP 5 Release Candidate 1 Released!</h1>
+<p>
+ <span class="newsdate">[18-Mar-2004]</span>
+ The <a href="/downloads.php#v5">first Release Candidate of PHP 5</a> is finally
+ here!  The move from Beta stage to RC stage means that PHP 5 is now feature
+ complete, and is quite stable - stable enough for everyone to start playing
+ with.  Note that it is still <i>not</i> recommended for mission-critical
+ use.
+</p>
+
+<p>Some of the key features of PHP 5 include:</p>
+
+<ul>
+ <li>
+  The <a href="http://www.zend.com/php5/zend-engine2.php">Zend Engine II</a>
+  with a new object model and dozens of new features.
+ </li>
+ <li>
+  XML support has been completely redone in PHP 5, all extensions are now
+  focused around the excellent libxml2 library
+  (<a href="http://www.xmlsoft.org/">http://www.xmlsoft.org/</a>).
+ </li>
+ <li>
+  A new MySQL extension named MySQLi for developers using MySQL 4.1 and later.
+  This new extension includes an object-oriented interface in addition to
+  a traditional interface; as well as support for many of MySQL's new features,
+  such as prepared statements.
+ </li>
+ <li>
+  SQLite has been bundled with PHP. For more information on SQLite,
+  please visit <a href="http://www.sqlite.org/">their website</a>.
+ </li>
+ <li>
+  A brand new built-in SOAP extension for interoperability with Web Services.
+ </li>
+ <li>
+  A new SimpleXML extension for easily accessing and manipulating XML as
+  PHP objects. It can also interface with the DOM extension and vice-versa.
+ </li>
+ <li>
+  Streams have been greatly improved, including the ability to access
+  low-level socket operations on streams.
+ </li>
+ <li>And lots more...</li>
+</ul>
+
+<p>
+ For changes since Beta 4, please consult the
+ <a href="/ChangeLog-5.php#5.0.0RC1">ChangeLog</a>.
+</p>
+
+<hr />
+
 <h1>Spanish PHP Mailing List Now at PHP.net</h1>
 <p>
  <span class="newsdate">[29-Feb-2004]</span>
