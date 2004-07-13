babea72f5fa95e86ecc40980fb2f615cb79bcf62
Andi Gutmans <andi@php.net>
2004-07-13 21:45:28 +0000

    - Fix an RC1 left over

--- index.php
+++ index.php
@@ -154,7 +154,7 @@ commonHeader("Hypertext Preprocessor");
 <a name="5.0.0"></a>
 <h1>PHP 5.0.0 Released!</h1>
 <p>
- <span class="newsdate">[18-Mar-2004]</span>
+ <span class="newsdate">[13-Jul-2004]</span>
  The <a href="/downloads.php#v5">final release of PHP 5</a> is finally
  here!
 </p>
