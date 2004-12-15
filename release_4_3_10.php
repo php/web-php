dabcf84cf82bf951860f7c0ef9bed954043535c6
Damien Seguy <dams@php.net>
2004-12-15 15:50:14 +0000

    translation

--- release_4_3_10.php
+++ release_4_3_10.php
@@ -6,7 +6,7 @@ site_header("PHP 4.3.10 Release Announcement");
 ?>
 
 <h1>PHP 4.3.10 Release Announcement</h1>
-<!-- <p>[ <a href="/release_4_3_10_fr.php">Version Fran&ccedil;aise</a> ]</p> -->
+<p>[ <a href="/release_4_3_10_fr.php">Version Fran&ccedil;aise</a> ]</p> 
 <p>
 PHP Development Team would like to announce the immediate release of <a href="/downloads.php">PHP 4.3.10</a>. This is a 
 maintenance release that in addition to over 30 non-critical bug fixes addresses several very 
@@ -18,7 +18,7 @@ These include the following:
 <a href="http://www.cve.mitre.org/cgi-bin/cvename.cgi?name=CAN-2004-1018">CAN-2004-1018</a> - shmop_write() out of bounds memory write access.<br />
 <a href="http://www.cve.mitre.org/cgi-bin/cvename.cgi?name=CAN-2004-1018">CAN-2004-1018</a> - integer overflow/underflow in pack() and unpack() functions.<br />
 <a href="http://www.cve.mitre.org/cgi-bin/cvename.cgi?name=CAN-2004-1019">CAN-2004-1019</a> - possible information disclosure, double free and negative reference index array underflow in deserialization code.<br />
-<a href="http://www.cve.mitre.org/cgi-bin/cvename.cgi?name=CAN-2004-1020">CAN-2004-1020</a> - addslashes not escaping \0 correctly.<br />
+<a href="http://www.cve.mitre.org/cgi-bin/cvename.cgi?name=CAN-2004-1020">CAN-2004-1020</a> - addslashes() not escaping \0 correctly.<br />
 <a href="http://www.cve.mitre.org/cgi-bin/cvename.cgi?name=CAN-2004-1063">CAN-2004-1063</a> - safe_mode execution directory bypass.<br />
 <a href="http://www.cve.mitre.org/cgi-bin/cvename.cgi?name=CAN-2004-1064">CAN-2004-1064</a> - arbitrary file access through path truncation.<br />
 <a href="http://www.cve.mitre.org/cgi-bin/cvename.cgi?name=CAN-2004-1065">CAN-2004-1065</a> - exif_read_data() overflow on long sectionname.<br />
@@ -35,7 +35,7 @@ All Users of PHP are strongly encouraged to upgrade to this release as soon as p
 <ul>
 <li> Possible crash inside ftp_get().</li>
 <li> get_current_user() crashes on Windows.</li>
-<li> Possible crash in ctype_digit on large numbers.</li>
+<li> Possible crash in ctype_digit() on large numbers.</li>
 <li> Crash when parsing <i>?getvariable[][</i>.</li>
 <li> Possible crash in the curl_getinfo() function.</li>
 <li> Double free when openssl_csr_new fails.</li>
