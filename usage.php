9e80f1a96bde97a8a44163018cdc6cdd9145a376
Rasmus Lerdorf <rasmus@php.net>
2005-09-03 19:50:59 +0000

    September numbers

--- usage.php
+++ usage.php
@@ -5,15 +5,15 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/include/prepend.inc';
 site_header("PHP Usage Stats");
 ?>
 
-<h1>Usage Stats for August 2005</h1>
+<h1>Usage Stats for September 2005</h1>
 
 <p>
-PHP: 22,267,442 Domains, 1,291,738 IP Addresses<br>
+PHP: 22,167,075 Domains, 1,283,102 IP Addresses<br>
 Source: <a href="http://www.netcraft.com/Survey/">Netcraft</a><br />
 </p>
 
 <p>
-<?php echo print_image("stats/phpstats-200508.png", "Graph"); ?>
+<?php echo print_image("stats/phpstats-200509.png", "Graph"); ?>
 </p>
 
 <p>
