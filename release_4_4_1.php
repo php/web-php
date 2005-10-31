052def6922959dd837f675b232384695e0626035
Derick Rethans <derick@php.net>
2005-10-31 15:29:34 +0000

    - Add reference change to the RA as requested by Andi.

--- release_4_4_1.php
+++ release_4_4_1.php
@@ -40,7 +40,9 @@ security issues that this release fixes are:
 </ul>
 </p>
 <p>
-Besides those security issues, this release also fixes 35 other defects. 
+This release also fixes 35 other defects, where the most important is the
+the fix that removes a notice when passing a by-reference result of a function
+as a by-reference value to another function. (Bug #<a href='http://bugs.php.net/33558'>33558</a>).
 </p>
 <p>
  For a full list of changes in PHP 4.4.1, see the
