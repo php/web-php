97abfeb81bb5a6dd7212a875250ff568b3d2a41d
Rasmus Lerdorf <rasmus@php.net>
2005-11-02 22:04:40 +0000

    October numbers.  November numbers are in too, but they look suspect, so I am checking with Netcraft on them.

--- usage.php
+++ usage.php
@@ -5,15 +5,15 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/include/prepend.inc';
 site_header("PHP Usage Stats");
 ?>
 
-<h1>Usage Stats for September 2005</h1>
+<h1>Usage Stats for October 2005</h1>
 
 <p>
-PHP: 22,167,075 Domains, 1,283,102 IP Addresses<br>
+PHP: 23,299,550 Domains, 1,290,179 IP Addresses<br>
 Source: <a href="http://www.netcraft.com/Survey/">Netcraft</a><br />
 </p>
 
 <p>
-<?php echo print_image("stats/phpstats-200509.png", "Graph"); ?>
+<?php echo print_image("stats/phpstats-200510.png", "Graph"); ?>
 </p>
 
 <p>
