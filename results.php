e21c9b2c5c1cf341247c2a679ac237ca6498f12e
Gabor Hojtsy <goba@php.net>
2006-02-26 21:44:05 +0000

    add some HTML list markup, so that we can hook some CSS to it (will add the CSS later)

--- results.php
+++ results.php
@@ -6,27 +6,38 @@ $_SERVER['BASE_PAGE'] = 'results.php';
 
 // Prepare data for search
 if ($MQ) { $q = stripslashes($_GET['q']); }
-else $q = $_GET['q'];
+else { $q = $_GET['q']; }
 $q = urlencode($q);
-$s = (int)$_GET['start'];
+$s = (int) $_GET['start'];
 
 $data = file_get_contents("http://www.php.net/~rasmus/ws.php?q=$q&results=10&start=$s");
 $res = unserialize($data);
 $pos = $res['ResultSet']['firstResultPosition'];
 
-echo <<<EOB
-<h2>Showing {$res['ResultSet']['totalResultsReturned']} of {$res['ResultSet']['totalResultsAvailable']}</h2>
-EOB;
+// No results
+if ($res['ResultSet']['totalResultsAvailable'] == 0) {
+  echo '<h2>No pages matched your query</h2>';
+}
 
-foreach($res['ResultSet']['Result'] as $i=>$hit) {
-  $cnt = $pos+$i; 
-  $d = date('r',$hit['ModificationDate']);
+// Got results
+else {
   echo <<<EOB
-$cnt. <a href="{$hit['Url']}">{$hit['Title']}</a><br />
-&nbsp; &nbsp; {$hit['Summary']}<br />
-&nbsp; &nbsp; <font size=-1>Modified $d
-&nbsp; &nbsp; <a href="{$hit['Cache']['Url']}">Cache ({$hit['Cache']['Size']} bytes)</a></font><br />
-
+<h2>Showing {$res['ResultSet']['totalResultsReturned']} of {$res['ResultSet']['totalResultsAvailable']}</h2>
+<ul id="search-results">
+EOB;
+  foreach($res['ResultSet']['Result'] as $i => $hit) {
+    $cnt = $pos + $i; 
+    $d = date('r', $hit['ModificationDate']);
+    echo <<<EOB
+<li>
+ <p class="result">$cnt. <a href="{$hit['Url']}">{$hit['Title']}</a></p>
+ <p class="summary">{$hit['Summary']}</p>
+ <p class="meta">Modified $d <a href="{$hit['Cache']['Url']}">Cache ({$hit['Cache']['Size']} bytes)</a></p>
+</li>
+EOB;
+  }
+ echo <<<EOB
+</ul>
 EOB;
 }
 ?>
