8273bfb7c4cdde14074a89270400c63b13540568
Rasmus Lerdorf <rasmus@php.net>
2006-03-04 20:13:20 +0000

    New numbers

--- usage.php
+++ usage.php
@@ -5,15 +5,15 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/include/prepend.inc';
 site_header("PHP Usage Stats");
 ?>
 
-<h1>Usage Stats for February 2006</h1>
+<h1>Usage Stats for March 2006</h1>
 
 <p>
-PHP: 21,340,831 Domains, 1,268,048 IP Addresses<br>
+PHP: 21,439,178 Domains, 1,277,736 IP Addresses<br>
 Source: <a href="http://www.netcraft.com/Survey/">Netcraft</a><br />
 </p>
 
 <p>
-<?php echo print_image("stats/phpstats-200602.png", "Graph"); ?>
+<?php echo print_image("stats/phpstats-200603.png", "Graph"); ?>
 </p>
 
 <p>
