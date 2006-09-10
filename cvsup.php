c2264ee00158eae9f5ec347e695821a71308e0b8
Gabor Hojtsy <goba@php.net>
2006-09-10 18:29:42 +0000

    update cvsup link, interlink anoncvs, snaps and the cvs wiki

--- cvsup.php
+++ cvsup.php
@@ -2,23 +2,44 @@
 // $Id$
 $_SERVER['BASE_PAGE'] = 'cvsup.php';
 include_once $_SERVER['DOCUMENT_ROOT'] . '/include/prepend.inc';
+$SIDEBAR_DATA = '
+<h3>What is CVS?</h3>
+<p>
+ You can find more information about CVS, and
+ download clients for most major platforms, at
+ <a href="http://ximbiot.com/cvs/wiki/index.php">the CVS Wiki</a>.
+</p>
+
+<h3>PHP.net CVS access</h3>
+<p>
+ If you would like to grab PHP sources or other PHP.net
+ hoster project data from PHP.net, read our 
+ <a href="/anoncvs.php">anonymous CVS</a> instructions.
+</p>
+
+<h3>Source and binary snapshots</h3>
+<p>
+ You may also be interested in a PHP snapshot, see
+ <a href="http://snaps.php.net/">snaps.php.net</a>.
+ Compiled snapshots for Windows users are also included.
+</p>
+';
 site_header("Using CVSup to maintain a local CVS repository");
 ?>
 
 <h1>Using CVSup To Maintain A Local CVS Repository</h1>
 
 <p>
- In addition to anonymous CVS, we also provide read-only access to
- our CVS repository by using CVSup. While CVSup is faster at updating
- large trees, it is not as widely available as the standard cvs client.
- We will describe now how you can use CVSup.
+ In addition to <a href="/anoncvs.php">anonymous CVS</a>, we also provide
+ read-only access to our CVS repository by using CVSup. While CVSup is faster
+ at updating large trees, it is not as widely available as the standard CVS
+ client. We will describe now how you can use CVSup.
 </p>
 
 <ol>
  <li>
   Download the appropiate binary distribution from
-  <a href="http://www.polstra.com/projects/freeware/CVSup/">the
-  CVSup homepage</a>.
+  <a href="http://www.cvsup.org/">the CVSup homepage</a>.
  </li>
  <li>
   Follow the instructions in the Install file of the binary distribution
@@ -52,4 +73,4 @@ php-src
  </li>
 </ol>
 
-<?php site_footer(); ?>
+<?php site_footer();
