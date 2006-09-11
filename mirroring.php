9d1b5048bcc0411ad329cb2a12c7b5e38b55053a
Gabor Hojtsy <goba@php.net>
2006-09-11 21:12:02 +0000

    point out that uk.php.net is a busy mirror, and use the more exact 12GB number

--- mirroring.php
+++ mirroring.php
@@ -21,11 +21,11 @@ $minute = rand(0, 59);
  read and follow these instructions carefully. You should have the
  consent of your hosting company (if you aren't a hosting company
  yourself), and be prepared for some reasonably significant bandwidth
- usage (a reasonable estimate as of September 2006 is 10GB/day, based on
- access data from uk.php.net). The PHP website requires at least PHP 4.3.x,
- but PHP 5 is preferred. Set it up as an Apache module with the settings
- <a href="#settings">outlined below</a>. The size of the full website is
- approximately 1.7 gigabyte.
+ usage. A reasonable estimate for an often visited mirror as of September
+ 2006 is 12GB/day, based on access data from uk.php.net. The PHP website
+ requires at least PHP 4.3.x, but PHP 5 is preferred. Set it up as an Apache
+ module with the settings <a href="#settings">outlined below</a>. The size
+ of the full website is approximately 1.7 gigabyte.
 </p>
 
 <a name="rule"></a>
