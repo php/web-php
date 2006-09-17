dbc41374391230b9853c4e43869ca139bb01e26b
Gabor Hojtsy <goba@php.net>
2006-09-17 20:07:23 +0000

    adding more info to this simple file

--- images/index.php
+++ images/index.php
@@ -1,6 +1,7 @@
 <?php
-// Simulate a /images shortcut call
+// $Id$
+
+// Simulate a /images shortcut call (which will lead to a manual page)
 $_SERVER['REQUEST_URI'] = '/images';
 include_once $_SERVER['DOCUMENT_ROOT'] . '/include/prepend.inc';
 include_once $_SERVER['DOCUMENT_ROOT'] . '/error.php';
-?>
