4fcabbd994517b74a261fd2f68cf066e3695021d
Damien Seguy <dams@php.net>
2007-02-06 16:53:43 +0000

    moving to november, since its available, and adding nexens stats

--- usage.php
+++ usage.php
@@ -5,23 +5,34 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/include/prepend.inc';
 site_header("PHP Usage Stats");
 ?>
 
-<h1>Usage Stats for November 2006</h1>
+<h1>Usage Stats for December 2006</h1>
 
 <p>
-PHP: 19,562,759 Domains, 1,305,799 IP Addresses<br />
+PHP: 19,404,533 domains, 1,319,743 IP addresses<br />
 Source: <a href="http://www.netcraft.com/Survey/">Netcraft</a><br />
 </p>
 
 <p>
-<?php echo print_image("stats/phpstats-200610.png", "Graph"); ?>
+<?php echo print_image("stats/phpstats-200611.png", "Graph"); ?>
 </p>
 
 <p>
  You can also see how popular PHP is relative to other Apache modules
  at <a href="http://www.securityspace.com/s_survey/">SecuritySpace's
- Web Survey</a>. Spoiler: PHP is the most popular. There is also a
+ Web Survey</a>. Spoiler: PHP is the most popular. 
+</p>
+
+<p>
+ There is also a
  <a href="http://www.tiobe.com/tpci.htm">Programming Community Index</a>
  provided by TIOBE.
+</p>
+
+<p>
+ There are <a href="http://www.nexen.net/the_english_speaking_nexen.net.php">
+ detailled stats about PHP</a>, by version, by countries, by web servers
+ and evolution from the &quot;PHP version tracker&quot;, run by 
+ <a href="http://www.nexen.net/">nexen.net</a>.
 </p>
 
 <?php site_footer(); ?>
