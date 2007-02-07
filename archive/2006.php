f99f86b5d198ec744263ccac763ea700e4dd47f0
Hannes Magnusson <bjori@php.net>
2007-02-07 18:21:40 +0000

    Yearly cleanup (move news from 2006 into the archive)

--- archive/2006.php
+++ archive/2006.php
@@ -13,6 +13,116 @@ site_header("News Archive - 2006");
 </p>
 
 <hr />
+<?php news_image("http://www.prophp.com.br/phpconference.php", "phpconfbrasil.jpg", "PHP Brasil 2007"); ?>
+<h1>PHP Conference Brazil</h1>
+<p>
+ <span class="newsdate">[27-Nov-2006]</span>
+ The PHP Conference Brasil is the first Brazilian conference related exclusively
+ to the PHP language in Sao Paulo, on December 1st and 2nd, 2006.
+</p>
+
+<p>
+ It will be a 
+ great opportunity to establish a sustainable Brazilian PHP community and to
+ exchange ideas among our professionals.
+ More info is available at
+ <a href="http://www.prophp.com.br/phpconference.php">the Brazilian PHP Conference</a> Web site.
+</p>
+
+<hr />
+
+<?php news_image("http://www.afup.org/forumphp/", "afup2006.jpg", "AFUP Paris 2006"); ?>
+
+<h1>Forum PHP in Paris, France, November 9 &amp; 10th, 2006</h1>
+<p>
+<span class="newsdate">[04-Oct-2006]</span>
+The <a href="http://afup.org/">French AFUP association</a> is proud to announce
+the <a href="http://afup.org/forumphp/">fifth annual PHP</a> meeting 
+in Paris, on November 9th and 10th, 2006. Developers and managers will gather 
+to meet Rasmus Lerdorf, Andrei Zmievski and other prominent community experts 
+for two days of sessions, packed with enterprise solutions and 
+advanced techniques (in French).
+</p>
+
+<hr />
+
+<?php news_image("http://phpconference.com/", "intconf2006fall.png", "International PHP Conference 2006"); ?>
+
+<h1>International PHP Conference 2006</h1>
+<p>
+ <span class="newsdate">[10-Sep-2006]</span>
+ Software &amp; Support media is pleased to announce the
+ <a href="http://www.phpconference.com/">International PHP Conference 2006</a>.
+ It will happen from Nov. 05 to Nov. 08, 2006 at Frankfurt/Main.
+ This year's sessions include topics on Security, PHP Internals,
+ PHP &amp; Business and Webservice topics. The Call for Papers has
+ been closed and the programme is now online.
+</p>
+<p>
+ On Nov. 05 and Nov. 06 there will be 6-hour PowerWorkshops that
+ deliver hands-on PHP knowledge. A Management Day, dedicated to
+ Managers who want to get informed about the usage of PHP, contains
+ several sessions about the usage of PHP in large companies. The
+ "PHP Code Camp @ Nite" with PHP Core Devs Marcus Börger and Sara
+ Golemon. They go through from PHP basics to PHP OOP and implementing
+ your own PHP extension. Delicious pizza and beer will do the rest.
+ Registration is now open.
+</p>
+
+<hr />
+
+<h1>PHP 5.1.6 Released</h1>
+<p>
+ <span class="newsdate">[24-Aug-2006]</span>
+ The PHP development team would like to announce the immediate availability of
+ PHP <a href="/downloads.php#v5">5.1.6</a>. This release contains a fix for 
+ memory_limit restriction on 64 bit systems that was not included in PHP 5.1.5.
+</p>
+
+<hr />
+
+<?php news_image("http://www.amazon.com/exec/obidos/ASIN/0596528124/wwwphpnet", "mre3cover_sm.jpg", "Mastering Regular Expressions, 3rd edition"); ?>
+<h1>Mastering Regular Expressions - now with PHP coverage</h1>
+<p>
+ <span class="newsdate">[24-Aug-2006]</span>
+ The third edition of
+ <a href="http://www.amazon.com/exec/obidos/ASIN/0596528124/wwwphpnet">Mastering Regular Expressions</a>
+ is finally out and includes a full 48-page chapter on PHP. This is an excellent
+ resource from Jeffrey Friedl who has been writing about regexps for years.
+</p>
+
+<hr />
+
+<h1>PHP 4.4.4 and PHP 5.1.5 Released</h1>
+<p>
+ <span class="newsdate">[17-Aug-2006]</span>
+ The PHP development team would like to announce the immediate availability of
+ PHP <a href="/downloads.php#v5">5.1.5</a> and <a
+ href="/downloads.php#v4">4.4.4</a>. These two releases address a series of
+ security problems that were discovered since the release of PHP 5.1.4 and
+ 4.4.3. The new releases include the following changes:
+</p>
+<ul>
+<li>Added missing safe_mode/open_basedir checks inside the error_log(),
+    file_exists(), imap_open() and imap_reopen() functions.</li>
+<li>Fixed overflows inside str_repeat() and wordwrap() functions on 64bit
+    systems.</li>
+<li>Fixed possible open_basedir/safe_mode bypass in cURL extension and on PHP
+    5.1.5 with realpath cache.</li>
+<li>Fixed overflow in GD extension on invalid GIF images.</li>
+<li>Fixed a buffer overflow inside sscanf() function.</li>
+<li>Fixed an out of bounds read inside stripos() function.</li>
+<li>Fixed memory_limit restriction on 64 bit system.</li>
+</ul>
+<p>
+Further details about this release can be found in the release announcements
+(<a href="/release_5_1_5.php">5.1.5</a> and
+<a href="/release_4_4_4.php">4.4.4</a>), and the full list of changes is
+available in the ChangeLogs (<a href="/ChangeLog-4.php#4.4.4">PHP 4</a>, 
+<a href="/ChangeLog-5.php#5.1.5">PHP 5</a>).
+</p>
+
+<hr />
 
 <h1>PHP 4.4.3 Released</h1>
 <p>
