543d376f35844f4018c0878df193a0a45a37b4ef
Hannes Magnusson <bjori@php.net>
2007-03-07 21:02:04 +0000

    Misuse the 'has_search' field to check for SQLite availability

--- mirror-info.php
+++ mirror-info.php
@@ -13,11 +13,14 @@ $mirror_stats = (int) (isset($_SERVER['MIRROR_STATS']) && $_SERVER['MIRROR_STATS
 // Check for an existing exe file (identifies rsync setup problems)
 $exefile = (int) file_exists($_SERVER['DOCUMENT_ROOT'] . '/distributions/php-5.1.5-installer.exe');
 
+// Does this mirror have sqlite?
+$sqlite = (int) function_exists("sqlite_open");
+
 echo join('|', array(
     $MYSITE,            // 0: ServerName problems
     phpversion(),       // 1: PHP version overview
     $LAST_UPDATED,      // 2: Update problems
-    0,                  // 3: Search support is not tracked anymore
+    $sqlite,            // 3: SQLite support?
     $mirror_stats,      // 4: Optional local stats support
     default_language(), // 5: Mirror language
     'manual-noalias',   // 6: /manual alias check is done elsewhere now
