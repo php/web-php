746f3f9fdd86a1e6e0d9cfe569719747ad806842
Hannes Magnusson <bjori@php.net>
2007-03-29 18:24:31 +0000

    Improve the rsync problem detection  - Remove the hardcode filename (now uses the last released php5 .tar.bz2)  - Compare the md5 sum against the registered official md5

--- mirror-info.php
+++ mirror-info.php
@@ -4,14 +4,18 @@
 // Define $MYSITE and $LAST_UPDATED variables
 include_once $_SERVER['DOCUMENT_ROOT'] . '/include/prepend.inc';
 
+// Define $PHP_5_VERSION, $PHP_5_MD5 & $RELEASES variables
+include_once $_SERVER['DOCUMENT_ROOT'] . '/include/version.inc';
+
 // Text/plain content type for better readability in browsers
 header("Content-type: text/plain; charset=utf-8");
 
 // Provide information on local stats setup
 $mirror_stats = (int) (isset($_SERVER['MIRROR_STATS']) && $_SERVER['MIRROR_STATS'] == '1');
 
-// Check for an existing exe file (identifies rsync setup problems)
-$exefile = (int) file_exists($_SERVER['DOCUMENT_ROOT'] . '/distributions/php-5.1.5-installer.exe');
+// MD5 check last release file (identifies rsync setup problems)
+$filename = $_SERVER['DOCUMENT_ROOT'] . '/distributions/' . $RELEASES[5][$PHP_5_VERSION]["source"][0]["filename"];
+$md5_ok = (int) (file_exists($filename) && md5_file($filename) == $PHP_5_MD5["tar.bz2"]);
 
 // Does this mirror have sqlite?
 $sqlite = (int) function_exists("sqlite_open");
@@ -24,5 +28,5 @@ echo join('|', array(
     $mirror_stats,      // 4: Optional local stats support
     default_language(), // 5: Mirror language
     'manual-noalias',   // 6: /manual alias check is done elsewhere now
-    $exefile            // 7: Rsync setup problems
+    $md5_ok,            // 7: Rsync setup problems
 ));
