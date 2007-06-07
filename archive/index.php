32c395f3b4fbb1ef0b615891cd0a722377e913d5
Hannes Magnusson <bjori@php.net>
2007-06-07 10:33:39 +0000

    Don't hard code the file to redirect to

--- archive/index.php
+++ archive/index.php
@@ -1,5 +1,15 @@
 <?php
 // $Id$
 include_once $_SERVER['DOCUMENT_ROOT'] . '/include/prepend.inc';
-mirror_redirect("/archive/2006.php");
-?>
+$i = 0;
+do {
+	$y = date("Y")-$i;
+	if (file_exists("./$y.php")) {
+		mirror_redirect("/archive/$y.php");
+		break;
+	}
+} while(++$i<3);
+
+include_once $_SERVER['DOCUMENT_ROOT'] . '/include/errors.inc';
+error_noservice();
+
