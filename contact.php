e76371e13392dc75b4fe4301a3a2b4da3a0c0ec2
Hannes Magnusson <bjori@php.net>
2008-03-19 23:09:33 +0000

    php-webmaster@ is the prefered webmaster list

--- contact.php
+++ contact.php
@@ -35,7 +35,7 @@ site_header("Contact");
 </p>
 <p>
  If you would like to contact the webmasters for some other reason, please
- write to <a href="mailto:webmaster@php.net">webmaster@php.net</a>.
+ write to <a href="mailto:php-webmaster@lists.php.net">php-webmaster@lists.php.net</a>.
  Note that this address is mapped to a mailing list and a newsgroup, so
  <strong>every message you send will be stored in public archives at multiple
  servers</strong>.
