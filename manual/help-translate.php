6f46bac48f7f73f75a25aa33fa94f59a827e3d76
Philip Olson <philip@php.net>
2008-04-09 17:18:31 +0000

    Added information about why some translations are missing, and how people can help. This will be found via the lang select box as "Other"

---
+++ manual/help-translate.php
@@ -0,0 +1,36 @@
+<?php
+// $Id$
+
+$_SERVER['BASE_PAGE'] = 'manual/help-translate.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/include/prepend.inc';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/include/shared-manual.inc';
+site_header("Help translate the PHP Manual");
+?>
+
+<h3>Looking for a translation?</h3>
+<p>
+The PHP Manual has over 30 translations already setup, but due to inactivity many have been taken offline. The odds are high that your language has already started a translation, but for various reasons it's no longer being updated or shown at php.net.
+</p>
+
+<h3>How to help translate the PHP Manual</h3>
+<p>
+If you're interested in helping translate a specific language, then please read the translation section of the <a href="http://doc.php.net/php/dochowto/">PHP Documentation HOWTO</a> and contact the appropriate mailing list. Whether or not your language is shown below, you are very welcome to help translate the PHP Manual from English to another language.
+</p>
+
+<h3>Using outdated translations</h3>
+<p>
+The following list of languages already contain CVS modules, and will show up on the documentation development server. Warning: These translations are considered outdated, so content within each may be completely wrong or insecure!
+</p>
+<p>
+<strong>Inactive languages already in CVS</strong>
+<ul>
+<?php
+foreach ($INACTIVE_ONLINE_LANGUAGES as $cc => $lang) {
+    echo '<li>', $lang, ' : (<a href="http://docs.php.net/manual/', $cc, '>archive</a>)</li>';
+}
+?>
+</ul>
+</p>
+
+<?php
+site_footer();
