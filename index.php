b18d9bc6153a3d424b5aacffc2dc2efbf32939c4
Hannes Magnusson <bjori@php.net>
2008-05-11 19:11:37 +0000

    add eukhost to the thanks section

--- index.php
+++ index.php
@@ -81,6 +81,7 @@ $SIDEBAR_DATA = '
  <li><a href="http://www.binarysec.com/">BinarySEC</a></li>
  <li><a href="http://www.nexcess.net/">NEXCESS.NET</a></li>
  <li><a href="http://www.rackspace.com/">Rackspace</a></li>
+ <li><a href="http://www.eukhost.com/">EUKhost</a></li>
 </ul>
 <h3>Related sites</h3>
 <ul class="simple">
