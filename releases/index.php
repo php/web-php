f5a19726f5981f3e4b98924b1b89dc690abca31a
Hannes Magnusson <bjori@php.net>
2009-08-10 14:01:03 +0000

    Quick workaround for missing date key in the latest stable

--- releases/index.php
+++ releases/index.php
@@ -6,6 +6,7 @@ include_once $_SERVER["DOCUMENT_ROOT"] . "/include/releases.inc";
 
 if (isset($_GET["serialize"])) {
 	include_once $_SERVER["DOCUMENT_ROOT"] . "/include/version.inc";
+  $RELEASES[5][$PHP_5_3_VERSION]["date"] = $PHP_5_3_DATE;
 	$RELEASES = $RELEASES + $OLDRELEASES;
 
 	if (isset($_GET["version"])) {
