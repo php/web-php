30cbcc6e60fd66e4dff6835d97803410537dddca
Stewart Lord <stewartlord@php.net>
2009-12-19 19:57:58 +0000

    Implemented search auto-complete.
    The auto-completing search feature uses the popular
    jquery.autocomplete plugin:
    
     http://docs.jquery.com/Plugins/Autocomplete
    
    The auto-complete data set is held client-side. This
    incurs one http request to load the search index when
    the page is loaded (via jQuery.getScript()), thereby 
    avoiding additional requests as the user types in the
    search field. This makes the auto-complete feature 
    very responsive.
    
    The search index file defines a 'searchIndex' array 
    containing the name and page (filename) of each entry 
    in the manual. The file is 382 KB uncompressed. Gzip
    compression (via the cli) reduces this to 53 KB. Proper
    configuration of server-side compression and expire 
    headers should make this download relatively painless.
    
    The search index file resides under the js folder 
    and is named: 'search-index-<lang>.js'. There is only
    one index file at this time ('en') and it was manually
    generated.
    
    The auto-complete feature has one major short-coming;
    the results are not sorted/ranked by relevance. This
    will required modification of the auto-complete 
    plugin or a server-side suggest script to overcome.

--- js/common.js
+++ js/common.js
@@ -2,23 +2,58 @@ $(document).ready(function() {
 
     // fade in/out mega drop-downs.
     $("#headmenu li:has(div.children)").hoverIntent({    
-        interval:   200,
+        interval:   100,
         timeout:    200,
         over:       function(){$(this).find("div.children").fadeIn(250);},
         out:        function(){$(this).find("div.children").fadeOut(250);}
     });
 
-    // remove 'Search...' text on focus.
-    // @todo support multilingual default text.
-    var defaultSearchText = 'Search...';
+    // remove default search text on focus.
     $('#headsearch-keywords').focus(function(){
-        if ($(this).attr("value") == defaultSearchText) {
+        if ($(this).attr("value") == getDefaultSearchText()) {
             $(this).attr("value", "");
         }
     }).blur(function(){
         if (!$(this).attr("value")) {
-            $(this).attr("value", defaultSearchText);
+            $(this).attr("value", getDefaultSearchText());
         }
     });
 
+    // load the search index and enable auto-complete.
+    jQuery.getScript("js/search-index-" + getLanguage() + ".js", function(){
+        $('#headsearch-keywords').autocomplete(searchIndex, {
+            matchContains:  true,
+            scrollHeight:   240,
+            formatItem:     function(item) {
+                return item.name;
+            }
+        }).result(
+            function(event, item){
+                alert("User selected: " + item['name'] + "\n" +
+                      "Direct user to: " + item['page']);
+            }
+        );
+    });
+
 });
+
+/**
+ * Get the default search text to use (e.g. 'Search...').
+ */
+function getDefaultSearchText()
+{
+    switch (getLanguage()) {
+        case "en":
+            return "Search...";
+    }
+}
+
+/**
+ * Determine what language to present to the user.
+ * 
+ * @todo    make this detect the user's language.
+ */
+function getLanguage()
+{
+    return "en";
+}
