bec86c95b9ab003ec6184c7c6b25a642b2ee11b8
Jordi Boggiano <seld@php.net>
2010-03-15 00:46:40 +0000

    Filled in correct urls for links and reorganized things a little bit

--- footer.php
+++ footer.php
@@ -2,15 +2,13 @@
          
          <div id="footer">
           <ul class="footmenu copyright">
-            <li><a href="/copyright.php">Copyright © 2001-2010 The PHP Group</a></li>
-            <li><a href="/credits.php">Credits</a></li>
+            <li><a href=""<?php echo $baseUrl ?>copyright.php">Copyright © 2001-2010 The PHP Group</a></li>
           </ul>
           <ul class="footmenu">
-            <li><a href="/my.php">My PHP.net</a></li>
-            <li><a href="/contact.php">Contact</a></li>
-            <li><a href="/sites.php">Other PHP.net sites</a></li>
-            <li><a href="/mirros.php">Mirror sites</a></li>
-            <li><a href="/stats/">Stats</a></li>
+            <li><a href=""<?php echo $baseUrl ?>my.php">My PHP.net</a></li>
+            <li><a href=""<?php echo $baseUrl ?>contact.php">Contact</a></li>
+            <li><a href=""<?php echo $baseUrl ?>sites.php">Other PHP.net sites</a></li>
+            <li><a href=""<?php echo $baseUrl ?>mirrors.php">Mirror sites</a></li>
           </ul>
         </div>
   </div>
