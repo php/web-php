0990bcdb1da4c292ef5c32d1b66762934ca56e71
Hannes Magnusson <bjori@php.net>
2010-03-19 19:18:56 +0000

    Fix wrong closing quotes

--- footer.php
+++ footer.php
@@ -2,13 +2,13 @@
          
          <div id="footer">
           <ul class="footmenu copyright">
-            <li><a href=""<?php echo $baseUrl ?>copyright.php">Copyright © 2001-2010 The PHP Group</a></li>
+            <li><a href="<?php echo $baseUrl ?>copyright.php">Copyright © 2001-2010 The PHP Group</a></li>
           </ul>
           <ul class="footmenu">
-            <li><a href=""<?php echo $baseUrl ?>my.php">My PHP.net</a></li>
-            <li><a href=""<?php echo $baseUrl ?>contact.php">Contact</a></li>
-            <li><a href=""<?php echo $baseUrl ?>sites.php">Other PHP.net sites</a></li>
-            <li><a href=""<?php echo $baseUrl ?>mirrors.php">Mirror sites</a></li>
+            <li><a href="<?php echo $baseUrl ?>my.php">My PHP.net</a></li>
+            <li><a href="<?php echo $baseUrl ?>contact.php">Contact</a></li>
+            <li><a href="<?php echo $baseUrl ?>sites.php">Other PHP.net sites</a></li>
+            <li><a href="<?php echo $baseUrl ?>mirrors.php">Mirror sites</a></li>
           </ul>
         </div>
   </div>
@@ -16,3 +16,4 @@
 
 </body>
 </html>
+
