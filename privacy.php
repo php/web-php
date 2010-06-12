fca1de461856271751a5575a90f67b7f22de592a
Philip Olson <philip@php.net>
2010-06-12 03:54:38 +0000

    - Initial [very] rough draft of a privacy policy, as mostly taken directly from pear.php.net. - This may or may not be true, yet, and requires additional research (e.g., cover all mirrors?) - Also, not sure what this will look like with the prototype layout.... we'll see :)

---
+++ privacy.php
@@ -0,0 +1,34 @@
+<?php include 'header.php'; ?>
+
+<h1>Privacy Policy</h1>
+
+<p>
+ This privacy policy covers www.php.net and its associated mirrors.
+</p>
+
+<h2>Email</h2>
+<p>
+ We will not give away your email address to anyone, who is not related to
+ the operations of php.net. We will also never ask you to send us any of
+ your passwords via email.
+</p>
+
+
+<h2>Logfiles</h2>
+<p>
+ Most mirrors maintain standard logs of the requests that reach the web servers,
+ but we do only use those files for statistical purposes.
+</p>
+<p>
+ And to improve your search experience, we store anonymised search terms that
+ are submitted to the site.
+</p>
+
+<h2>Cookies</h2>
+<p>
+ php.net uses cookies to keep track of <a href="/my.php">user preferences</a>. Unless
+ you login on the site, the cookies will not be used to store personal information and
+ we do not give away the information from the cookies.
+</p>
+
+<?php include 'footer.php'; ?>
