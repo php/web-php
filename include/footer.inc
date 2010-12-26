83301bce52cd3230e9aecebae56b9655fe7d7f3d
Hannes Magnusson <bjori@php.net>
2010-12-26 16:11:06 +0000

    Merge in new styles from PROTOTYPE_2010     (not includes/used anywhere yet)

---
+++ include/footer.inc
@@ -0,0 +1,20 @@
+         <div class="cleaner">&nbsp;</div>
+         
+         <div id="footer">
+          <ul class="footmenu copyright">
+            <li><a href="/copyright.php">Copyright © 2001-2010 The PHP Group</a></li>
+          </ul>
+          <ul class="footmenu">
+            <li><a href="/my.php">My PHP.net</a></li>
+            <li><a href="/contact.php">Contact</a></li>
+            <li><a href="/sites.php">Other PHP.net sites</a></li>
+            <li><a href="/mirrors.php">Mirror sites</a></li>
+            <li><a href="/privacy.php">Privacy policy</a></li>
+          </ul>
+        </div>
+  </div>
+<!--script type="text/javascript" src="js/search-index-en.js" id=search-index></script-->
+
+</body>
+</html>
+
---
+++ js/common.js
@@ -0,0 +1,60 @@
+$(document).ready(function() {
+
+    // fade in/out mega drop-downs.
+    $("#headmenu li:has(div.children)").hoverIntent({    
+        interval:   100,
+        timeout:    200,
+        over:       function(){$(this).find("div.children").fadeIn(250);},
+        out:        function(){$(this).find("div.children").fadeOut(250);}
+    });
+
+    // remove default search text on focus.
+    $('#headsearch-keywords').focus(function(){
+        if ($(this).attr("value") == getDefaultSearchText()) {
+            $(this).attr("value", "");
+        }
+    }).blur(function(){
+        if (!$(this).attr("value")) {
+            $(this).attr("value", getDefaultSearchText());
+        }
+    });
+
+    // load the search index and enable auto-complete.
+    jQuery.getScript("/js/search-index-" + getLanguage() + ".js", function(){
+        $('#headsearch-keywords').autocomplete(searchIndex, {
+            matchContains:  true,
+            scrollHeight:   240,
+            formatItem:     function(item) {
+                return item.name;
+            }
+        }).result(
+            function(event, item){
+                alert("User selected: " + item['name'] + "\n" +
+                      "Direct user to: " + item['page']);
+                window.location = '/manual/' + getLanguage() + '/' + item['page'];
+            }
+        );        
+    });
+
+});
+
+/**
+ * Get the default search text to use (e.g. 'Search...').
+ */
+function getDefaultSearchText()
+{
+    switch (getLanguage()) {
+        case "en":
+            return "Search...";
+    }
+}
+
+/**
+ * Determine what language to present to the user.
+ * 
+ * @todo    make this detect the user's language.
+ */
+function getLanguage()
+{
+    return "en";
+}
