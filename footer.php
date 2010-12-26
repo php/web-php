22168ef8f166d12633dec68011e999a5facaddc6
Hannes Magnusson <bjori@php.net>
2010-12-26 11:57:57 +0000

    Kill baseUrl

--- footer.php
+++ footer.php
@@ -2,14 +2,14 @@
          
          <div id="footer">
           <ul class="footmenu copyright">
-            <li><a href="<?php echo $baseUrl ?>copyright.php">Copyright © 2001-2010 The PHP Group</a></li>
+            <li><a href="/copyright.php">Copyright © 2001-2010 The PHP Group</a></li>
           </ul>
           <ul class="footmenu">
-            <li><a href="<?php echo $baseUrl ?>my.php">My PHP.net</a></li>
-            <li><a href="<?php echo $baseUrl ?>contact.php">Contact</a></li>
-            <li><a href="<?php echo $baseUrl ?>sites.php">Other PHP.net sites</a></li>
-            <li><a href="<?php echo $baseUrl ?>mirrors.php">Mirror sites</a></li>
-            <li><a href="<?php echo $baseUrl ?>privacy.php">Privacy policy</a></li>
+            <li><a href="/my.php">My PHP.net</a></li>
+            <li><a href="/contact.php">Contact</a></li>
+            <li><a href="/sites.php">Other PHP.net sites</a></li>
+            <li><a href="/mirrors.php">Mirror sites</a></li>
+            <li><a href="/privacy.php">Privacy policy</a></li>
           </ul>
         </div>
   </div>
