771c12cc9495fd9280d363cd8022d5f63b833dde
Hannes Magnusson <bjori@php.net>
2010-12-26 14:41:40 +0000

    List available files rather then redirect

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
-<?php
+<ul>
+    <li><a href="./manual/en/function.file-get-contents.php">./manual/en/function.file-get-contents.php</a></li>
+    <li><a href="./manual/en/function.date.php">./manual/en/function.date.php</a></li>
+    <li><a href="./manual/en/function.strpos.php">./manual/en/function.strpos.php</a></li>
+    <li><a href="./manual/en/ref.strings.php">./manual/en/ref.strings.php</a></li>
+    <li><a href="./manual/en/book.strings.php">./manual/en/book.strings.php</a></li>
+    <li><a href="./manual/en/refs.basic.text.php">./manual/en/refs.basic.text.php</a></li>
+    <li><a href="./manual/en/funcref.php">./manual/en/funcref.php</a></li>
+    <li><a href="./manual/en/index.php">./manual/en/index.php</a></li>
 
-header('Location: /manual/en/function.strpos.php');
+    <li><a href="./mailing-lists.php">./mailing-lists.php</a></li>
+    <li><a href="./support.php">./support.php</a></li>
+    <li><a href="./urlhowto.php">./urlhowto.php</a></li>
+    <li><a href="./tips.php">./tips.php</a></li>
+    <li><a href="./sites.php">./sites.php</a></li>
+    <li><a href="./privacy.php">./privacy.php</a></li>
+    <li><a href="./sidebars.php">./sidebars.php</a></li>
+</ul>
