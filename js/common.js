410cb1bb55ff8788fdae0e2f56f291e7a1ab67b2
Paul Dragoonis <dragoonis@php.net>
2011-01-01 21:40:25 +0000

    Adding the pointer icon to the buttons that lack it

--- js/common.js
+++ js/common.js
@@ -154,6 +154,11 @@ $(document).ready(function() {
         });
 
         if (foundToc) {
+
+            var userNotesHeader = $('#usernotes');
+            $('<a class="toc_item" href="#' + userNotesHeader.attr('id') + '">User Contributed Notes</a>')
+                .appendTo( $('<li>') ).parent().appendTo(l);
+
             jQuery.getScript("/js/jquery.scrollto.min.js", function(){
                 l.delegate("a.toc_item","click keypress", function(e) {
                     // Escape dots in ids so they won't be treated as class selectors
@@ -178,6 +183,10 @@ $(document).ready(function() {
             }
         });
     });
+
+    if($('.docs table.doctable').length) {
+        $(this).find('tbody tr:even').addClass("alt-row")
+    }
 
 });
 
