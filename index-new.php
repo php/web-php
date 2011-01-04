d9d63d4657fd411f763f5989b46f50192ad6362b
Stewart Lord <stewartlord@php.net>
2011-01-04 04:55:51 +0000

    Deleting files that were mistakenly submitted (svn playing tricks on me!)

--- index-new.php
+++
@@ -1,8 +0,0 @@
-<?php // vim: et
-
-include_once $_SERVER['DOCUMENT_ROOT'] . '/include/prepend.inc';
-if (myphpnet_beta()) {
-    include $_SERVER['DOCUMENT_ROOT'] . '/index-beta.php';
-} else {
-    include $_SERVER['DOCUMENT_ROOT'] . '/index-stable.php';
-}
