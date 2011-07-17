4b17dcd20faf4d3e6c7af200c9b9fa3fde60dbe9
Rasmus Lerdorf <rasmus@php.net>
2011-07-17 04:17:46 +0000

    Log if we returned a cached result

--- ws.php
+++ ws.php
@@ -39,6 +39,7 @@ if(isset($sites[$_REQUEST['profile']])) {
 // mtime is stored on the first line of the result file to
 // avoid needing to call clearstatcache()
 function limited_intermediate_result_cache($url,$ttl=1800, &$error=NULL) {
+	global $cached;
 	$cfile = '/var/tmp/bing_'.md5($url).'.json';
 	$mtime = 0;
 	$data = '';
@@ -63,6 +64,7 @@ function limited_intermediate_result_cache($url,$ttl=1800, &$error=NULL) {
 			if(strlen($data)) return $data;
 			else return false;
 		} else $data = $tmp;
+		$cached = 1;
 		$fp = fopen($cfile, 'w');
 		fputs($fp, $_SERVER['REQUEST_TIME']."\n");	
 		fputs($fp, $data);
@@ -74,6 +76,7 @@ function limited_intermediate_result_cache($url,$ttl=1800, &$error=NULL) {
 
 $request =  "{$conf['svc']}?appid={$conf['appid']}&query=$q%20site:{$sites[$scope]}&version=2.2&Sources=Web&web.offset=$s&web.count=$r&market=$market";
 $error = '';
+$cached = 0;
 $data = limited_intermediate_result_cache($request, 1800, $error);
 if($data) echo ws_bing_massage($data);
 else echo serialize($error);
@@ -112,8 +115,8 @@ $dbh = new PDO('mysql:host=localhost;dbname=ws', $conf['db_user'], $conf['db_pw'
                                                                                          PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => true));
 $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 try {
-  $stmt = $dbh->prepare("INSERT INTO log (query,profile,mirror,lang) VALUES (:query,:profile,:mirror,:lang)");
-  $stmt->execute(array(':query'=>$raw,':profile'=>$scope,':mirror'=>$m,':lang'=>$l));
+  $stmt = $dbh->prepare("INSERT INTO log (query,profile,mirror,lang,cached) VALUES (:query,:profile,:mirror,:lang,:cached)");
+  $stmt->execute(array(':query'=>$raw,':profile'=>$scope,':mirror'=>$m,':lang'=>$l,':cached'=>$cached));
 } catch (PDOException $e) {
    
 }
