f5e3f5850cf3481150f58dce8eae12bfc0c45ccf
Pierre Joye <pajoye@php.net>
2011-10-13 13:39:44 +0000

    - and here too

--- index-stable.php
+++ index-stable.php
@@ -87,6 +87,7 @@ $SIDEBAR_DATA = '
  <li><a href="http://www.redpill-linpro.com">Redpill Linpro</a></li>
  <li><a href="http://www.facebook.com">Facebook</a></li>
  <li><a href="http://krystal.co.uk">Krystal.co.uk</a></li>
+ <li><a href="http://servergrove.com/">ServerGrove</a></li>
 </ul>
 <h3>Related sites</h3>
 <ul class="simple">
