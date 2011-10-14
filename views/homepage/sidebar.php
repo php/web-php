a5b8ac0b30eeb19cc34069f9a86d0e59df000336
Paul Dragoonis <dragoonis@php.net>
2011-10-14 13:25:19 +0000

    Keeping the thanks list up-to-date with the live one, as per changes by pajoye on r318081. Additionally adding title attributes to the <a> tags to make it W3C compliant.

--- views/homepage/sidebar.php
+++ views/homepage/sidebar.php
@@ -31,22 +31,24 @@
     <h2>Thanks To</h2>
     <ul>
         <li><a target="_blank" href="http://www.easydns.com/?V=698570efeb62a6e2" title="DNS Hosting provided by easyDNS">easyDNS</a></li>
-        <li><a target="_blank" href="http://www.directi.com/">Directi</a></li>
-        <li><a target="_blank" href="http://promote.pair.com/direct.pl?php.net">pair Networks</a></li>
-        <li><a target="_blank" href="http://www.servercentral.net/">Server Central</a></li>
-        <li><a target="_blank" href="http://www.hostedsolutions.com/">Hosted Solutions</a></li>
-        <li><a target="_blank" href="http://www.spry.com/">Spry VPS Hosting</a></li>
-        <li><a target="_blank" href="http://ez.no/">eZ Systems</a> / <a href="http://www.hit.no/">HiT</a></li>
-        <li><a target="_blank" href="http://www.osuosl.org">OSU Open Source Lab</a></li>
-        <li><a target="_blank" href="http://www.yahoo.com/">Yahoo! Inc.</a></li>
-        <li><a target="_blank" href="http://www.binarysec.com/">BinarySEC</a></li>
-        <li><a target="_blank" href="http://www.nexcess.net/">NEXCESS.NET</a></li>
-        <li><a target="_blank" href="http://www.rackspace.com/">Rackspace</a></li>
-        <li><a target="_blank" href="http://www.eukhost.com/">EUKhost</a></li>
-        <li><a target="_blank" href="http://www.micfo.com/">micfo</a></li>
-        <li><a target="_blank" href="http://www.sohosted.nl/webhosting/">Sohosted</a></li>
-        <li><a target="_blank" href="http://www.redpill-linpro.com">Redpill Linpro</a></li>
-        <li><a target="_blank" href="http://www.facebook.com">Facebook</a></li>
+        <li><a target="_blank" href="http://www.directi.com/" title="Directi">Directi</a></li>
+        <li><a target="_blank" href="http://promote.pair.com/direct.pl?php.net" title="pair Networks">pair Networks</a></li>
+        <li><a target="_blank" href="http://www.servercentral.net/" title="Server Central">Server Central</a></li>
+        <li><a target="_blank" href="http://www.hostedsolutions.com/" title="Hosted Solutions">Hosted Solutions</a></li>
+        <li><a target="_blank" href="http://www.spry.com/" title="Spry VPS Hosting">Spry VPS Hosting</a></li>
+        <li><a target="_blank" href="http://ez.no/" title="eZ Systems">eZ Systems</a> / <a href="http://www.hit.no/">HiT</a></li>
+        <li><a target="_blank" href="http://www.osuosl.org" title="OSU Open Source Lab">OSU Open Source Lab</a></li>
+        <li><a target="_blank" href="http://www.yahoo.com/" title="Yahoo! Inc.">Yahoo! Inc.</a></li>
+        <li><a target="_blank" href="http://www.binarysec.com/" title="BinarySEC">BinarySEC</a></li>
+        <li><a target="_blank" href="http://www.nexcess.net/" title="NEXCESS.NET">NEXCESS.NET</a></li>
+        <li><a target="_blank" href="http://www.rackspace.com/" title="Rackspace">Rackspace</a></li>
+        <li><a target="_blank" href="http://www.eukhost.com/" title="EUKhost">EUKhost</a></li>
+        <li><a target="_blank" href="http://www.micfo.com/" title="micfo">micfo</a></li>
+        <li><a target="_blank" href="http://www.sohosted.nl/webhosting/" title="Sohosted">Sohosted</a></li>
+        <li><a target="_blank" href="http://www.redpill-linpro.com" title="Redpill Linpro">Redpill Linpro</a></li>
+        <li><a target="_blank" href="http://www.facebook.com" title="Facebook">Facebook</a></li>
+	<li><a target="_blank" href="http://krystal.co.uk" title="Krystal.co.uk">Krystal.co.uk</a></li>
+	<li><a target="_blank" href="http://servergrove.com/" title="ServerGrove">ServerGrove</a></li>
     </ul>
 
 </div>
