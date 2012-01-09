b37cb043a88be6377e4d5f0a235cda08ecf8614a
Adam Harvey <aharvey@php.net>
2012-01-09 09:47:01 +0000

    Add a 2012 archive page.

---
+++ archive/2012.php
@@ -0,0 +1,23 @@
+<?php
+// $Id: 2010.php 293613 2010-01-16 13:38:42Z bjori $
+$_SERVER['BASE_PAGE'] = 'archive/2012.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/include/prepend.inc';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/include/pregen-news.inc';
+news_archive_sidebar();
+site_header("News Archive - 2012");
+?>
+
+<h1>News Archive - 2012</h1>
+
+<p>
+ Here are the most important news items we have published in 2012 on PHP.net.
+</p>
+
+<hr />
+
+<?php
+
+print_news($NEWS_ENTRIES, array("conferences", "cfp", "frontpage"), 50, 2012);
+/* %s/<a href="\(.*\)"><img src="\/images\/news\/\(.*\)" alt="\(.*\)" width.*><\/a>/<?php news_image("\1", "\2", "\3"); ?>/g */
+site_footer();
+
