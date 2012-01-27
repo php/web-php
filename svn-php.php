f62667d0e01987310fa944f74658a958e2315cb8
Rasmus Lerdorf <rasmus@php.net>
2012-01-27 00:42:46 -0800

    Back to master

--- svn-php.php
+++ svn-php.php
@@ -83,7 +83,7 @@ if (count($_POST) && (!isset($_POST['purpose']) || !is_array($_POST['purpose']) 
     // Post the request if there is no error
     if (!$error) {
         $error = posttohost(
-            "http://master2.php.net/entry/svn-account.php",
+            "http://master.php.net/entry/svn-account.php",
             array(
                 "username" => $cleaned['id'],
                 "name"     => $cleaned['fullname'],
