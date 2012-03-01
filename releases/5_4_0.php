17ec8a0970e6961e0ac2a213cba5dc4f8a2d1dc2
David Soria Parra <dsp@php.net>
2012-03-01 21:54:40 +0100

    Do not link to /en/ directly

--- releases/5_4_0.php
+++ releases/5_4_0.php
@@ -18,11 +18,11 @@ which includes a large number of new features and bug fixes.
 </p>
 <ul>
  <li>New language syntax including <a href="http://php.net/traits">Traits</a>,
-    <a href="http://docs.php.net/manual/en/language.types.array.php">shortened array syntax</a>
-    and <a href="http://docs.php.net/manual/en/migration54.new-features.php">more</a></li>
+    <a href="http://docs.php.net/manual/language.types.array.php">shortened array syntax</a>
+    and <a href="http://docs.php.net/manual/migration54.new-features.php">more</a></li>
  <li>Improved performance and reduced memory consumption</li>
  <li>Support for multibyte languages now available in all builds of PHP at the flip of a runtime switch</li>
- <li><a href="http://php.net/manual/en/features.commandline.webserver.php">
+ <li><a href="http://php.net/manual/features.commandline.webserver.php">
     Built-in webserver</a> in CLI mode to simplify development workflows and testing</li>
  <li>Cleaner code base thanks to the removal of multiple deprecated language features</li>
  <li>Many more improvements and fixes</li>
@@ -32,17 +32,17 @@ which includes a large number of new features and bug fixes.
 <b>Changes that affect compatibility:</b>
 </p>
 <ul>
-  <li><a href="http://www.php.net/manual/en/security.globals.php">Register globals</a>, <a href="http://www.php.net/manual/en/security.magicquotes.php">magic quotes</a> and <a href="http://www.php.net/manual/en/features.safe-mode.php">safe mode</a> were removed</li>
-  <li>The <a href="http://php.net/manual/en/control-structures.break.php">break</a>/<a href="http://php.net/manual/en/control-structures.continue.php">continue</a> $var syntax was removed</li>
-  <li>The ini option <a href=""http://www.php.net/manual/en/ini.core.php#ini.allow-call-time-pass-reference>allow_call_time_pass_reference</a> was removed</li>
-  <li>The PHP <a href="http://www.php.net/manual/en/ini.core.php#ini.default-charset">default_charset</a> is now "UTF-8"</li>
+  <li><a href="http://www.php.net/manual/security.globals.php">Register globals</a>, <a href="http://www.php.net/manual/security.magicquotes.php">magic quotes</a> and <a href="http://www.php.net/manual/features.safe-mode.php">safe mode</a> were removed</li>
+  <li>The <a href="http://php.net/manual/control-structures.break.php">break</a>/<a href="http://php.net/manual/control-structures.continue.php">continue</a> $var syntax was removed</li>
+  <li>The ini option <a href="http://www.php.net/manual/ini.core.php#ini.allow-call-time-pass-reference">allow_call_time_pass_reference</a> was removed</li>
+  <li>The PHP <a href="http://www.php.net/manual/ini.core.php#ini.default-charset">default_charset</a> is now "UTF-8"</li>
 </ul>
 
 <p>
 <b>Extensions moved to <a href="http://pecl.php.net">PECL</a>:</b>
 </p>
 <ul>
-  <li><a href="http://www.php.net/manual/en/ref.sqlite.php">ext/sqlite</a> (<a href="http://www.php.net/manual/en/book.sqlite3.php">ext/sqlite3</a> and <a href="http://www.php.net/manual/en/ref.pdo-sqlite.php">ext/pdo_sqlite</a> are not affected)</li>
+  <li><a href="http://www.php.net/manual/ref.sqlite.php">ext/sqlite</a> (<a href="http://www.php.net/manual/book.sqlite3.php">ext/sqlite3</a> and <a href="http://www.php.net/manual/ref.pdo-sqlite.php">ext/pdo_sqlite</a> are not affected)</li>
 </ul>
 
 <p>
