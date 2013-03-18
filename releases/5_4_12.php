77352c308a9ff9bba12335bdd683b96f815e5aad
Stanislav Malyshev <smalyshev@gmail.com>
2013-03-18 00:53:24 -0700

    fix typoes

--- releases/5_4_12.php
+++ releases/5_4_12.php
@@ -13,7 +13,7 @@ users of PHP are encouraged to upgrade to this release.</p>
 
 <p>For source downloads of PHP 5.4.12 please visit our <a href="http://www.php.net/downloads.php">downloads page</a>,
 Windows binaries can be found on <a href="http://windows.php.net/download/">windows.php.net/download/</a>.
-The list of changes is recorded in the <a href="http://www.php.net/ChangeLog-5.php#5.4.11">ChangeLog</a>.
+The list of changes is recorded in the <a href="http://www.php.net/ChangeLog-5.php#5.4.12">ChangeLog</a>.
 </p>
 
 <?php site_footer(); ?>
