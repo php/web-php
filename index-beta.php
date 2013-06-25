769cd8593bbd29e36a23d082a791ecc5358e73d9
Paul Dragoonis <dragoonis@gmail.com>
2013-06-25 20:20:43 +0100

    Removing thanks to section from homepage

--- index-beta.php
+++ index-beta.php
@@ -135,39 +135,6 @@ print_view('homepage/sidebar.php', array(
 ));
 print $content;
 
-$thanksTo = <<<THANKS
-<section id='thanks-to'>
-    <h3>Thanks To</h3>
-    <ul>
-        <li><a target="_blank" href="http://www.easydns.com/?V=698570efeb62a6e2" title="DNS Hosting provided by easyDNS">easyDNS</a></li>
-        <li><a target="_blank" href="http://www.directi.com/" title="Directi">Directi</a></li>
-        <li><a target="_blank" href="http://promote.pair.com/direct.pl?php.net" title="pair Networks">pair Networks</a></li>
-        <li><a target="_blank" href="http://www.servercentral.net/" title="Server Central">Server Central</a></li>
-        <li><a target="_blank" href="http://www.hostedsolutions.com/" title="Hosted Solutions">Hosted Solutions</a></li>
-        <li><a target="_blank" href="http://www.spry.com/" title="Spry VPS Hosting">Spry VPS Hosting</a></li>
-        <li class="thanks-two">
-            <a target="_blank" href="http://ez.no/" title="eZ Systems">
-                <span>eZ Systems</span>
-            </a>
-            <a target="_blank" href="http://www.hit.no/">
-                <span>HiT</span>
-            </a>
-        </li>
-        <li><a target="_blank" href="http://www.osuosl.org" title="OSU Open Source Lab">OSU Open Source Lab</a></li>
-        <li><a target="_blank" href="http://www.yahoo.com/" title="Yahoo! Inc.">Yahoo! Inc.</a></li>
-        <li><a target="_blank" href="http://www.binarysec.com/" title="BinarySEC">BinarySEC</a></li>
-        <li><a target="_blank" href="http://www.nexcess.net/" title="NEXCESS.NET">NEXCESS.NET</a></li>
-        <li><a target="_blank" href="http://www.rackspace.com/" title="Rackspace">Rackspace</a></li>
-        <li><a target="_blank" href="http://www.eukhost.com/" title="EUKhost">EUKhost</a></li>
-        <li><a target="_blank" href="http://www.sohosted.nl/webhosting/" title="Sohosted">Sohosted</a></li>
-        <li><a target="_blank" href="http://www.redpill-linpro.com" title="Redpill Linpro">Redpill Linpro</a></li>
-        <li><a target="_blank" href="http://www.facebook.com" title="Facebook">Facebook</a></li>
-        <li><a target="_blank" href="http://krystal.co.uk" title="Krystal.co.uk">Krystal.co.uk</a></li>
-        <li><a target="_blank" href="http://servergrove.com/" title="ServerGrove">ServerGrove</a></li>
-    </ul>
-</section>
-THANKS;
-
 // Print the common footer.
 site_footer(
     array(
