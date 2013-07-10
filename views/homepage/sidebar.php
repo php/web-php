72e6b7738de91ebf97cecba605e833194ec9835d
Levi Morrison <levim@php.net>
2013-07-10 15:31:16 -0600

    Added option to site_header to allow a custom span(for twitter bootstrap). Calendar page needed this. Other minor updates.

--- views/homepage/sidebar.php
+++ views/homepage/sidebar.php
@@ -3,9 +3,8 @@
         echo $announcements;
     endif;?>
 
-    <h3 class='panel news'><a href='/archive/'>Recent News</a></h3>
-    <h3 class='panel documentation'><a href='/manual/'>Documentation</a></h3>
-    <h3 class='panel events'><a href='/conferences/'>Conferences</a></h3>
-    <h3 class='panel events'><a href='/cal.php'>User Group Events</a></h3>
-    <h3 class='panel tips'><a href='/tips.php'>Tips and Tricks</a></h3>
+    <h3 class='panel'><a href='/tips.php'>Tips and Tricks</a></h3>
+    <h3 class='panel'><a href='/conferences/'>Conferences</a></h3>
+    <h3 class='panel'><a href='/cal.php'>User Group Events</a></h3>
+    <h3 class='panel'><a href='/thanks.php'>Special Thanks</a></h3>
 
