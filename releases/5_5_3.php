e77e0f9d6a9f2fbfca3292167d0305d15e1771d3
Julien Pauli <jpauli@php.net>
2013-09-19 15:04:37 +0200

    Fixed a copy/paste error

--- releases/5_5_3.php
+++ releases/5_5_3.php
@@ -5,7 +5,7 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/include/prepend.inc';
 site_header("PHP 5.5.3 Release Announcement");
 ?>
 
-<h1>PHP 5.4.19 Release Announcement</h1>
+<h1>PHP 5.5.3 Release Announcement</h1>
 
 <p>The PHP development team announces the immediate availability of PHP 5.5.3. 
 This release fixes a bug in the patch for CVE-2013-4248 in OpenSSL module.</p>
