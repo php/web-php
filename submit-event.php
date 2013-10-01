ccbfdb71e13c0e563fdea16dc7c8ba5e3ac2a1fb
danbrown <danbrown@php.net>
2013-10-01 15:09:37 -0400

    Though it doesn't make much of a difference, really, we don't need to specify 2001 as the year.

--- submit-event.php
+++ submit-event.php
@@ -152,10 +152,10 @@ if (count($errors)) { display_errors($errors); }
 
 // Generate days and months arrays for form
 for ($i = 1; $i <= 7; $i++) {
-    $days[$i] = strftime('%A', mktime(12, 0, 0, 4, $i, 2001));
+    $days[$i] = strftime('%A', mktime(12, 0, 0, 4, $i));
 }
 for ($i = 1; $i <= 12; $i++) {
-    $months[$i] = strftime('%B', mktime(12, 0, 0, $i, 1, 2001));
+    $months[$i] = strftime('%B', mktime(12, 0, 0, $i, 1));
 }
 
 // Possibilities to recur
