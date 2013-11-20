a4a8a6eb4693112058e6d39750337a3fa8976b09
Paul Dragoonis <dragoonis@gmail.com>
2013-11-20 11:27:25 +0000

    Adding UserVoice and GoSquared to the thanks page as part of the new site launch.

--- thanks.php
+++ thanks.php
@@ -8,97 +8,107 @@ site_header("Thanks", array("current" => "community"));
 <h1>Thanks</h1>
 
 <p>
- <a href="http://www.easydns.com/?V=698570efeb62a6e2">easyDNS</a>
+ <a target="_blank" href="http://www.easydns.com/?V=698570efeb62a6e2">easyDNS</a>
  provides DNS services for the PHP domains.
 </p>
 
 <p>
- <a href="http://www.directi.com/">Directi</a> provides
+ <a target="_blank" href="http://www.directi.com/">Directi</a> provides
  IP address to country lookup information.
 </p>
 
 <p>
- <a href="http://promote.pair.com/direct.pl?php.net">pair Networks</a>
+ <a target="_blank" href="http://promote.pair.com/direct.pl?php.net">pair Networks</a>
  provides the servers and bandwidth for PEAR, PECL and mailing list services.
 </p>
 
 <p>
- <a href="http://www.rackspace.com/">Rackspace</a> provides
+ <a target="_blank" href="http://www.rackspace.com/">Rackspace</a> provides
  the server and bandwidth for various php.net services.
 </p>
 
 <p>
- <a href="http://www.hostedsolutions.com/">Hosted Solutions</a> provides
+ <a target="_blank" href="http://www.hostedsolutions.com/">Hosted Solutions</a> provides
  the server and bandwidth that run various services and websites
  for php.net.
 </p>
 
 <p>
- <a href="http://www.servercentral.net/">Server Central</a> provides 
+ <a target="_blank" href="http://www.servercentral.net/">Server Central</a> provides 
  a box and connection which runs various services and sites for php.net
 </p>
 
 <p>
- <a href="http://www.spry.com/">Spry VPS Hosting</a> provides a server and 
+ <a target="_blank" href="http://www.spry.com/">Spry VPS Hosting</a> provides a server and 
  bandwidth for various php.net services.
 </p>
 
 <p>
- <a href="http://www.osuosl.org/">OSU Open Source Lab</a> provides a server and
+ <a target="_blank" href="http://www.osuosl.org/">OSU Open Source Lab</a> provides a server and
  bandwidth for various php.net services.
 </p>
 
 <p>
- <a href="http://www.yahoo.com/">Yahoo! Inc.</a> provides bandwidth and 
+ <a target="_blank" href="http://www.yahoo.com/">Yahoo! Inc.</a> provides bandwidth and 
  hardware for www.php.net and git.php.net.
 </p>
 
 <p>
- <a href="http://www.nexcess.net/">NEXCESS.NET</a> provides a server and bandwidth
+ <a target="_blank" href="http://www.nexcess.net/">NEXCESS.NET</a> provides a server and bandwidth
  for various php.net services.
 </p>
 
 <p>
- <a href="http://www.eukhost.com/">EUKhost</a> provides a server and bandwidth
+ <a target="_blank" href="http://www.eukhost.com/">EUKhost</a> provides a server and bandwidth
  for various php.net services.
 </p>
 
 <p>
- <a href="http://www.34sp.com/">34SP.com</a> provides the servers and bandwidth
+ <a target="_blank" href="http://www.34sp.com/">34SP.com</a> provides the servers and bandwidth
  used for buildbot testing.
 </p>
 
 <p>
- <a href="http://www.sohosted.nl/webhosting/">SoHosted Webhosting</a> provides the 
+ <a target="_blank" href="http://www.sohosted.nl/webhosting/">SoHosted Webhosting</a> provides the 
  servers and bandwidth used for buildbot testing and various windows based servers.
 </p>
 
 <p>
- <a href="http://www.redpill-linpro.com">Redpill Linpro</a> provides managed servers and bandwidth for various php.net services.
+ <a target="_blank" href="http://www.redpill-linpro.com">Redpill Linpro</a> provides managed servers and bandwidth for various php.net services.
 </p>
 
 <p>
- <a href="http://facebook.com">Facebook</a> provides us with SSL certificates.
+ <a target="_blank" href="http://facebook.com">Facebook</a> provides us with SSL certificates.
 </p>
 
 <p>
- <a href="http://krystal.co.uk">Krystal.co.uk</a> provides a server and bandwidth
+ <a target="_blank" href="http://krystal.co.uk">Krystal.co.uk</a> provides a server and bandwidth
  for build testing and various PHP testing and quality assurance services.
 </p>
 
 <p>
- <a href="http://servergrove.com/">ServerGrove</a> provides managed servers and bandwidth for various php.net services.
+ <a target="_blank" href="http://servergrove.com/">ServerGrove</a> provides managed servers and bandwidth for various php.net services.
 </p>
 
 <p>
- <a href="http://www.bauer-kirch.de/">Bauer + Kirch GmbH</a> provides a server and bandwidth
+ <a target="_blank" href="http://www.bauer-kirch.de/">Bauer + Kirch GmbH</a> provides a server and bandwidth
  for the php.net monitoring infrastructure.
+</p>
+
+<p>
+ <a target="_blank" href="http://www.gosquared.com">GoSquared</a> provides us with their premium package for high qualiy real-time 
+ analytics for the php.net website.
+</p>
+
+<p>
+ <a target="_blank" href="http://www.uservoice.com">UserVoice</a> provides us with their premium package for easy reporting of bugs
+ through the interactive widget on the php.net website
 </p>
 
 <p>
  And special thanks to all the companies who donate server space and
  bandwidth to host our international array of 
- <a href="/mirrors.php">mirror sites</a>.
+ <a target="_blank" href="/mirrors.php">mirror sites</a>.
 </p>
 
 <h1>Thanks Emeritus</h1>
@@ -109,13 +119,13 @@ site_header("Thanks", array("current" => "community"));
 </p>
 
 <p>
- <a href="http://www.synacor.com">Synacor</a> provided us with many
+ <a target="_blank" href="http://www.synacor.com">Synacor</a> provided us with many
  terabytes of bandwidth and hosting for www.php.net and others for
  many years.
 </p>
 
 <p>
- <a href="http://www.vasoftware.com">VA Software Corp.</a> helped us
+ <a target="_blank" href="http://www.vasoftware.com">VA Software Corp.</a> helped us
  by donating a server and resources to enable us to build manuals
  and distribute our content via rsync.
 </p>
