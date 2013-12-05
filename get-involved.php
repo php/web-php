d3b330b55d232db722eff3889241c2581d724c71
philip <philip@php.net>
2013-12-05 13:35:52 -0800

    Fixed a few typos, of sorts

--- get-involved.php
+++ get-involved.php
@@ -9,7 +9,7 @@ $SIDEBAR_DATA ='
       Who is in charge and how decisions are made? The PHP.net project has a lot of 
       things in the works and is always looking for new talent to join the project, 
       share new ideas, discuss improvements, fix broken features, look after the
-      websites, documentation, and so on and on...
+      websites, documentation, and so on....
     </p>
 ';
 
@@ -23,22 +23,22 @@ site_header("Get Involved", array("current" => "community"));
     With every release, or release candidate comes a wave of work, which takes a lot of organization and co-ordination.   
 </p>
 <p>
-    With the introduction of release managers comes a smooth release process, but still help is needed: 
-        testing release candidates, finding and squishing bugs in tests, documentation, and sources.
+    With the introduction of release managers comes a smoother release process, but help is still needed: 
+        testing release candidates, finding and squashing bugs in tests, documentation, and sources.
 </p>
 
 <h3>Four Best Ways to Contribute</h3>
 <p>
     <ol>
         <li>Running test suites in <a href="http://qa.php.net/">RC</a> and <a href="http://php.net/releases">release</a> distributions of PHP</li>
-        <li>Help finding and diagnosing failed tests <a href="http://qa.php.net/write-test.php">phpt documentation</a></li>
+        <li>Help finding and diagnosing failed tests, see the <a href="http://qa.php.net/write-test.php">phpt documentation</a></li>
         <li>Filing and resolving bug reports at <a href="http://bugs.php.net">bugs.php.net</a></li>
         <li>Help maintain and or translate documentation files at <a href="http://edit.php.net">edit.php.net</a></li>
     </ol>
 </p>
 <h3>Development of the PHP source</h3>
 <p>
-    Someone hoping to become involved in the maintanence and development of the source should be experienced in all of the areas mentioned above, 
+    Someone hoping to become involved in the maintenance and development of the source should be experienced in all of the areas mentioned above, as
         this creates a strong team; everyone knows how every other part of the project works. 
 </p>
 <p>
