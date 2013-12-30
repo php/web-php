abbacca6515fef899725f8b86f107e6f5e03ab7f
Hannes Magnusson <bjori@mongodb.com>
2013-12-29 22:23:28 -0800

    Prep for 2014

---
+++ archive/2014.php
@@ -0,0 +1,25 @@
+<?php
+
+$_SERVER['BASE_PAGE'] = 'archive/2014.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/include/prepend.inc';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/include/pregen-news.inc';
+news_archive_sidebar();
+site_header("News Archive - 2014");
+//site_header("News Archive - 2014", array("cache" => true));
+?>
+
+<h1>News Archive - 2014</h1>
+
+<p>
+ Here are the most important news items we have published in 2014 on PHP.net.
+</p>
+
+<hr />
+
+<?php
+
+print_news($NEWS_ENTRIES, array("conferences", "cfp", "frontpage"), 500, 2014);
+
+/* %s/<a href="\(.*\)"><img src="\/images\/news\/\(.*\)" alt="\(.*\)" width.*><\/a>/<?php news_image("\1", "\2", "\3"); ?>/g */
+site_footer(array('elephpants' => true, 'sidebar' => $SIDEBAR_DATA));
+
