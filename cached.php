efc35ac0f036067081e13aabca445f68d6e7b9b4
Rasmus Lerdorf <rasmus@php.net>
2014-03-12 08:11:21 -0700

    Prevent response header splitting

--- cached.php
+++ cached.php
@@ -11,7 +11,7 @@ $abs = $pwd. "/" .(string)$_GET["f"];
 $abs = realpath($abs);
 
 if (strncmp($abs, $pwd, strlen($pwd)) != 0) {
-    header("Location: http://php.net/" . $_GET["f"]);
+    header("Location: http://php.net/" . strtr($_GET["f"],array("\r"=>"","\n"=>"")));
     exit;
 }
 
