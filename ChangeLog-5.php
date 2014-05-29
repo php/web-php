ba8fe364f4bfe4e2978458aa1d3e51b6d9bc1247
Julien Pauli <jpauli@php.net>
2014-05-29 11:30:06 +0200

    PHP 5.5.13 release

--- ChangeLog-5.php
+++ ChangeLog-5.php
@@ -16,6 +16,71 @@ function release_date($in) {
 ?>
 
 <h1>PHP 5 ChangeLog</h1>
+<section class="version" id="5.5.13"><!-- {{{ 5.5.13 -->
+<h3>Version 5.5.13</h3>
+<?php release_date('29-May-2014'); ?>
+<ul><li>CLI server:
+<ul>
+  <li><?php bugfix(67079); ?> (Missing MIME types for XML/XSL files).</li>
+</ul></li>
+<ul><li>COM:
+<ul>
+  <li><?php bugfix(66431); ?> (Special Character via COM Interface (CP_UTF8)).</li>
+</ul></li>
+<ul><li>Core:
+<ul>
+  <li><?php bugfix(65701); ?> (copy() doesn't work when destination filename is created
+    by tempnam()).</li>
+  <li><?php bugfix(67072); ?> (Echoing unserialized "SplFileObject" crash).</li>
+  <li><?php bugfix(67245); ?> (usage of memcpy() with overlapping src and dst in
+    zend_exceptions.c).</li>
+  <li><?php bugfix(67247); ?> (spl_fixedarray_resize integer overflow).</li>
+  <li><?php bugfix(67249); ?> (printf out-of-bounds read).</li>
+  <li><?php bugfix(67250); ?> (iptcparse out-of-bounds read).</li>
+</ul></li>
+<li>cURL:
+<ul>
+  <li><?php bugfix(64247); ?> (CURLOPT_INFILE doesn't allow reset).</li>
+</ul></li>
+<li>Date:
+<ul>
+  <li><?php bugfix(67118); ?> (DateTime constructor crash with invalid data).</li>
+  <li><?php bugfix(67251); ?> (date_parse_from_format out-of-bounds read).</li>
+  <li><?php bugfix(67253); ?> (timelib_meridian_with_check out-of-bounds read).</li>
+</ul></li>
+<li>DOM:
+<ul>
+  <li><?php bugfix(67081); ?> (DOMDocumentType->internalSubset returns entire DOCTYPE tag,
+    not only the subset).</li>
+</ul></li>
+<li>Fileinfo:
+<ul>
+  <li><?php bugfix(66307); ?> (Fileinfo crashes with powerpoint files).</li>
+  <li><?php bugfix(67327); ?> (CDF infinite loop in nelements DoS) (CVE-2014-0238).</li>
+  <li><?php bugfix(67328); ?> (numerous file_printf calls resulting in
+    performance degradation) (CVE-2014-0237).</li>
+</ul></li>
+<li>FPM:
+<ul>
+  <li><?php bugfix(66908); ?> (php-fpm reload leaks epoll_create() file descriptor).</li>
+</ul></li>
+<li>GD:
+<ul>
+  <li><?php bugfix(67248); ?> (imageaffinematrixget missing check of parameters).</li>
+</ul></li>
+<li>PCRE:
+<ul>
+  <li><?php bugfix(67248); ?> Ungreedy and min/max quantifier bug, applied patch
+    from the upstream.</li>
+</ul></li>
+<li>Phar:
+<ul>
+  <li><?php bugfix(64498); ?> ($phar->buildFromDirectory can't compress file with an accent
+    in its name).</li>
+</ul></li>
+</ul>
+<!-- }}} --></section>
+
 <section class="version" id="5.5.12"><!-- {{{ 5.5.12 -->
 <h3>Version 5.5.12</h3>
 <?php release_date('1-May-2014'); ?>
