097a075644c96d372f07a6ddc0a529ed6010645d
Julien Pauli <jpauli@php.net>
2014-07-24 12:33:35 +0200

    5.5.15 release

---
+++ releases/5_5_15.php
@@ -0,0 +1,21 @@
+<?php
+// $Id$
+$_SERVER['BASE_PAGE'] = 'releases/5_5_15.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/include/prepend.inc';
+site_header("PHP 5.5.15 Release Announcement");
+?>
+
+<h1>PHP 5.5.15 Release Announcement</h1>
+
+<p>The PHP Development Team announces the immediate availability of PHP 5.5.15.
+This release fixes several bugs against PHP 5.5.14.
+</p>
+
+<p>All PHP users are encouraged to upgrade to this new version.</p>
+
+<p>For source downloads of PHP 5.5.15, please visit our <a href="http://www.php.net/downloads.php">downloads page</a>.
+Windows binaries can be found on <a href="http://windows.php.net/download/">windows.php.net/download/</a>.
+The list of changes is recorded in the <a href="http://www.php.net/ChangeLog-5.php#5.5.15">ChangeLog</a>.
+</p>
+
+<?php site_footer(); ?>
