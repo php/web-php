71a8f34ddd520e83b4057a13e19fee869191a8b1
Stanislav Malyshev <smalyshev@gmail.com>
2015-09-04 12:56:17 -0700

    typofix

--- releases/5_4_45.php
+++ releases/5_4_45.php
@@ -19,7 +19,7 @@ The list of changes is recorded in the <a href="http://www.php.net/ChangeLog-5.p
 
 <p>Please note that according to the <a href="http://php.net/supported-versions.php">PHP version support timelines</a>, 
 PHP 5.4.45 is the last scheduled release of PHP 5.4 branch. There may be additional release if we discover 
-important security issues that warrant it, otherwise this release will be the final one in the PHP 5.4 
+important security issues that warrant it, otherwise this release will be the final one in the PHP 5.4 branch. 
 If your PHP installation is based on PHP 5.4, it may be a good time to start making the plans for the upgrade to PHP 5.5 or PHP 5.6.
 </p> 
 
