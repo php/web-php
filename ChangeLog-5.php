a7eb5db356ddb63332f5cbbe97d6560e0b18278f
Lior Kaplan <kaplanlior@gmail.com>
2016-01-21 13:41:36 +0200

    Add CVE for #70976 (PHP 5.5.31, 5.6.17)

--- ChangeLog-5.php
+++ ChangeLog-5.php
@@ -24,7 +24,7 @@ site_header("PHP 5 ChangeLog", array("current" => "docs", "css" => array("change
 </ul></li>
 <li>GD:
 <ul>
-  <li><?php bugfix(70976); ?> (Memory Read via gdImageRotateInterpolated Array Index Out of Bounds).</li>
+  <li><?php bugfix(70976); ?> (Memory Read via gdImageRotateInterpolated Array Index Out of Bounds). (CVE-2016-1903)</li>
 </ul></li>
 <li>Mysqlnd:
 <ul>
@@ -63,7 +63,7 @@ site_header("PHP 5 ChangeLog", array("current" => "docs", "css" => array("change
 </ul></li>
 <li>GD:
 <ul>
-  <li><?php bugfix(70976); ?> (Memory Read via gdImageRotateInterpolated Array Index Out of Bounds).</li>
+  <li><?php bugfix(70976); ?> (Memory Read via gdImageRotateInterpolated Array Index Out of Bounds). (CVE-2016-1903)</li>
 </ul></li>
 <li>WDDX:
 <ul>
