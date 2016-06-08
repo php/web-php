eb63d62b6d8890c7c3f2c33c0e7e20232ff280ab
Anatol Belski <ab@php.net>
2016-06-08 08:26:26 +0200

    fix changelogs in regard to PR #105

--- ChangeLog-7.php
+++ ChangeLog-7.php
@@ -70,7 +70,7 @@ site_header("PHP 7 ChangeLog", array("current" => "docs", "css" => array("change
 <ul>
   <li><?php bugfix(72028); ?> (pg_query_params(): NULL converts to empty string).</li>
   <li><?php bugfix(71062); ?> (pg_convert() doesn't accept ISO 8601 for datatype timestamp).</li>
-  <li><?php bugfix(72151); ?> (mysqli_fetch_object changed behaviour).</li>
+  <li><?php bugfix(72151); ?> (mysqli_fetch_object changed behaviour). Patch to <?php bugfix(71820) ?> is reverted.</li>
 </ul></li>
 <li>Reflection:
 <ul>
