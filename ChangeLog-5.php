0aa94f922a6ab0cd157ff2d1cbe992d36f78028c
Ferenc Kovacs <tyra3l@gmail.com>
2016-07-03 23:46:16 +0200

    add missing Changelog entry

--- ChangeLog-5.php
+++ ChangeLog-5.php
@@ -17,6 +17,10 @@ site_header("PHP 5 ChangeLog", array("current" => "docs", "css" => array("change
   <li><?php bugfix(72400); ?> (Integer Overflow in addcslashes/addslashes).</li>
   <li><?php bugfix(72403); ?> (Integer Overflow in Length of String-typed ZVAL).</li>
 </ul></li>
+<li>Date:
+<ul>
+  <li><?php bugfix(63740); ?> (strtotime seems to use both sunday and monday as start of week).</li>
+</ul></li>
 <li>GD:
 <ul>
   <li><?php bugfix(66387); ?> (Stack overflow with imagefilltoborder). (CVE-2015-8874)</li>
