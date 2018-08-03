f478a437692c1e1f2eb869662ef6c7e0ac6c2942
Lior Kaplan <kaplanlior@gmail.com>
2018-08-03 07:56:29 +0300

    Add CVE ID to bug #76409 (PHP 7.2.8)

--- ChangeLog-7.php
+++ ChangeLog-7.php
@@ -21,7 +21,7 @@ site_header("PHP 7 ChangeLog", array("current" => "docs", "css" => array("change
 </ul></li>
 <li>EXIF:
 <ul>
-  <li><?php bugfix(76409); ?> (heap use after free in _php_stream_free).</li>
+  <li><?php bugfix(76409); ?> (heap use after free in _php_stream_free). (CVE-2018-12882)</li>
   <li><?php bugfix(76423); ?> (Int Overflow lead to Heap OverFlow in exif_thumbnail_extract of exif.c).</li>
   <li><?php bugfix(76557); ?> (heap-buffer-overflow (READ of size 48) while reading exif data). (CVE-2018-14851)</li>
 </ul></li>
