e341f5895e6ec79bddac668a5b4c85bc1d9da354
Kalle Sommer Nielsen <kalle@php.net>
2019-04-19 14:10:46 +0300

    Fix typo and remove the mirroring part of requesting a Git account

--- git-php.php
+++ git-php.php
@@ -237,10 +237,6 @@ EOT;
   <td class="sub">Writing web pages with PHP</td>
   <td>&nbsp;</td>
  </tr>
- <tr>
-  <td class="sub">Setting up a php.net mirror site</td>
-  <td>&nbsp;</td>
- </tr>
 </table>
 
 <h3 class="content-header">Contributing patches</h3>
@@ -325,7 +321,7 @@ EOT;
 </p>
 
 <p>
- If you get no resposne to an account request after a while, remember this is a
+ If you get no response to an account request after a while, remember this is a
  manual process, then please contact the relevant mailing list that belongs to
  the part of PHP you requested access to.
 </p>
@@ -377,7 +373,7 @@ EOT;
 $purposes = array("Learning PHP", "Coding in PHP", "Reading the PHP source",
 	"Using PHP extensions", "Creating experimental PHP extensions",
 	"Submitting a patch to PHP", "Adding notes to the documentation",
-	"Writing web pages with PHP", "Setting up a php.net mirror site");
+	"Writing web pages with PHP");
 
 foreach ($purposes as $i => $p) { ?>
   <input type="checkbox" name="purpose[<?php echo $i?>]" value="1"
