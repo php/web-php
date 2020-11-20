c72e74c178619829440e6ad48bfb406783776fde
Roman Pronskiy <roman@pronskiy.com>
2020-11-20 12:29:36 +0200

    Add doc link for union types

--- releases/8_0_x_pt_BR.php
+++ releases/8_0_x_pt_BR.php
@@ -151,7 +151,7 @@ class User
         <div class="php8-compare">
             <h2 class="php8-h2" id="union-types">
                 União de tipos
-                <a class="php8-rfc" href="https://wiki.php.net/rfc/union_types_v2">RFC</a>
+                <a class="php8-rfc" href="https://wiki.php.net/rfc/union_types_v2">RFC</a> <a class="php8-rfc" href="/manual/en/language.types.declarations.php#language.types.declarations.union">Doc</a>
             </h2>
             <div class="php8-compare__main">
                 <div class="php8-compare__block example-contents">
