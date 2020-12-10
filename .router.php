2f12574e6ef45204401e2391ff73ab663b21bb82
Christoph M. Becker <cmbecker69@gmx.de>
2020-12-10 17:42:03 +0100

    Allow to access symlinked docs
    Since `realpath()` resolves links, symlinked docs as suggested by the
    "Setting up Documentation environment" page[1] are incompatible with
    the router script.  Of course, the Webserver can be run without the
    router script, but in that case shortcuts (e.g. `/json_decode`) won't
    work.
    
    Thus, we remove the `realpath()` resolution.
    
    [1] <http://doc.php.net/tutorial/local-setup.php>

--- .router.php
+++ .router.php
@@ -3,7 +3,6 @@ $_SERVER["SERVER_ADDR"] = $_SERVER["HTTP_HOST"];
 
 $filename = isset($_SERVER["PATH_INFO"]) ? $_SERVER["PATH_INFO"] : $_SERVER["SCRIPT_NAME"];
 $afilename = $_SERVER["DOCUMENT_ROOT"] . $filename;
-$afilename = realpath($afilename);
 $len = strlen($_SERVER["DOCUMENT_ROOT"]);
 
 if (strncmp($_SERVER["DOCUMENT_ROOT"], $afilename, $len) == 0) {
