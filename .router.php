6905f975bc80978bcc5475b4cd2abd37895f6af5
Christoph M. Becker <cmbecker69@gmx.de>
2020-12-11 00:35:04 +0100

    Simplify
    There is no more need for the `strncmp()` check.  This suggest that we
    can get rid of the `$afilename` and `$len` variables; hopefully, they
    are not used by any of the included files.

--- .router.php
+++ .router.php
@@ -2,14 +2,11 @@
 $_SERVER["SERVER_ADDR"] = $_SERVER["HTTP_HOST"];
 
 $filename = isset($_SERVER["PATH_INFO"]) ? $_SERVER["PATH_INFO"] : $_SERVER["SCRIPT_NAME"];
-$afilename = $_SERVER["DOCUMENT_ROOT"] . $filename;
-$len = strlen($_SERVER["DOCUMENT_ROOT"]);
 
-if (strncmp($_SERVER["DOCUMENT_ROOT"], $afilename, $len) == 0) {
-    if (file_exists($afilename)) {
-        /* This could be an image or whatever, so don't try to compress it */
-        ini_set("zlib.output_compression", 0);
-        return false;
-    }
+if (file_exists($_SERVER["DOCUMENT_ROOT"] . $filename)) {
+    /* This could be an image or whatever, so don't try to compress it */
+    ini_set("zlib.output_compression", 0);
+    return false;
 }
+
 include_once "error.php";
