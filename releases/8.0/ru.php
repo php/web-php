996de6e86f6486cfb41f96e84f2691bef29a5310
Sergey Panteleev <sergey@php.net>
2020-12-24 09:36:10 +0300

    docs(ru): Updated PHP 8 announcement page

--- releases/8.0/ru.php
+++ releases/8.0/ru.php
@@ -365,10 +365,10 @@ array_chunk([], -1); // ValueError: array_chunk(): Argument #2 ($length) must be
     производительность приложения находится на одном уровне с PHP 7.4.
   </p>
   <h3 class="php8-h3">
-    Relative JIT contribution to PHP 8 performance
+    Относительный вклад JIT в производительность PHP 8
   </h3>
   <p>
-    <img src="/images/php8/scheme.svg" width="900" alt="Ккомпиляция Just-In-Time">
+    <img src="/images/php8/scheme.svg" width="900" alt="Компиляция Just-In-Time">
   </p>
 
   <div class="php8-columns">
