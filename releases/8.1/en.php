a2d9e61da6ae1007036bf8bc91fb15e35c7ee032
Roman Pronskiy <roman@pronskiy.com>
2021-11-23 18:12:15 +0200

    Update releases/8.1/en.php
    Co-authored-by: Sergey Panteleev <sergey@php.net>

--- releases/8.1/en.php
+++ releases/8.1/en.php
@@ -556,7 +556,7 @@ PHP
             <li>finfo Extension: <code>file_info</code> resource are migrated to existing finfo objects.</li>
             <li>IMAP: imap resources are <code>IMAP\Connection</code> class objects.</li>
             <li>FTP Extension: Connection resources are FTP\Connection class objects.</li>
-            <li>GD Extension: Font identifiers are GdFont class objects.</li>
+            <li>GD Extension: Font identifiers are <code>GdFont</code> class objects.</li>
             <li>LDAP: resources migrated to LDAP\Connection, LDAP\Result, and LDAP\ResultEntry objects.</li>
             <li>PostgreSQL: resources migrated to \PgSql\Connection, \PgSql\Result, and \PgSql\Lob objects.</li>
             <li>Pspell: pspell, pspell config resources are PSpell\Dictionary, PSpell\Config class objects.</li>
