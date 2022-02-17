d101a0437e60b1d7dceeb95af9062f77e162aa6b
Sara Golemon <pollita@php.net>
2022-02-17 23:16:20 +0000

    Announce 8.0.16

--- ChangeLog-8.php
+++ ChangeLog-8.php
@@ -398,6 +398,48 @@ changelog_header(8, $MINOR_VERSIONS);
 
 <a id="PHP_8_0"></a>
 
+<section class="version" id="8.0.16"><!-- {{{ 8.0.16 -->
+<h3>Version 8.0.16</h3>
+<b><?php release_date('17-Feb-2022'); ?></b>
+<ul><li>Core:
+<ul>
+  <li><?php bugfix(81430); ?> (Attribute instantiation leaves dangling pointer).</li>
+  <li>Fixed bug <?php githubissuel('php/php-src',7896); ?> (Environment vars may be mangled on Windows).</li>
+</ul></li>
+<li>FFI:
+<ul>
+  <li>Fixed bug <?php githubissuel('php/php-src',7867); ?> (FFI::cast() from pointer to array is broken).</li>
+</ul></li>
+<li>Filter:
+<ul>
+  <li>Fix #81708: UAF due to php_filter_float() failing for ints.</li>
+</ul></li>
+<li>FPM:
+<ul>
+  <li>Fixed memory leak on invalid port.</li>
+</ul></li>
+<li>MBString:
+<ul>
+  <li>Fixed bug <?php githubissuel('php/php-src',7902); ?> (mb_send_mail may delimit headers with LF only).</li>
+</ul></li>
+<li>MySQLnd:
+<ul>
+  <li>Fixed bug <?php githubissuel('php/php-src',7972); ?> (MariaDB version prefix 5.5.5- is not stripped).</li>
+</ul></li>
+<li>Sockets:
+<ul>
+  <li>Fixed ext/sockets build on Haiku.</li>
+  <li>Fixed bug <?php githubissuel('php/php-src',7978); ?> (sockets extension compilation errors).</li>
+</ul></li>
+<li>Standard:
+<ul>
+  <li>Fixed bug <?php githubissuel('php/php-src',7875); ?> (mails are sent even if failure to log throws exception).</li>
+</ul></li>
+</ul>
+<!-- }}} --></section>
+
+
+
 <section class="version" id="8.0.15"><!-- {{{ 8.0.15 -->
 <h3>Version 8.0.15</h3>
 <b><?php release_date('20-Jan-2022'); ?></b>
