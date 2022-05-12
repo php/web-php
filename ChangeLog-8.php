9f796a96c65f07e45845ec248933bfb0010b94a9
Ben Ramsey <ramsey@php.net>
2022-05-12 16:56:58 -0500

    Announce PHP 8.1.6

--- ChangeLog-8.php
+++ ChangeLog-8.php
@@ -8,6 +8,71 @@ changelog_header(8, $MINOR_VERSIONS);
 ?>
 <a id="PHP_8_1"></a>
 
+<section class="version" id="8.1.6"><!-- {{{ 8.1.6 -->
+<h3>Version 8.1.6</h3>
+<b><?php release_date('12-May-2022'); ?></b>
+<ul><li>Core:
+<ul>
+  <li>Fixed bug <?php githubissuel('php/php-src',8310); ?> (Registry settings are no longer recognized).</li>
+  <li>Fixed potential race condition during resource ID allocation.</li>
+  <li>Fixed bug <?php githubissuel('php/php-src',8133); ?> (Preloading of constants containing arrays with enums segfaults).</li>
+  <li>Fixed Haiku ZTS builds.</li>
+</ul></li>
+<li>Date:
+<ul>
+  <li>Fixed bug <?php githubissuel('php/php-src',7752); ?> (DateTimeZone::getTransitions() returns insufficient data).</li>
+  <li>Fixed bug <?php githubissuel('php/php-src',8108); ?> (Timezone doesn't work as intended).</li>
+  <li><?php bugfix(81660); ?> (DateTimeZone::getTransitions() returns invalid data).</li>
+  <li>Fixed bug <?php githubissuel('php/php-src',8289); ?> (Exceptions thrown within a yielded from iterator are not rethrown into the generator).</li>
+</ul></li>
+<li>FFI:
+<ul>
+  <li>Fixed bug <?php githubissuel('php/php-src',8433); ?> (Assigning function pointers to structs in FFI leaks).</li>
+</ul></li>
+<li>FPM:
+<ul>
+  <li><?php bugfix(76003); ?> (FPM /status reports wrong number of active processe).</li>
+  <li><?php bugfix(77023); ?> (FPM cannot shutdown processes).</li>
+  <li>Fixed comment in kqueue remove callback log message.</li>
+</ul></li>
+<li>Hash:
+<ul>
+  <li><?php bugfix(81714); ?> (segfault when serializing finalized HashContext).</li>
+</ul></li>
+<li>Iconv:
+<ul>
+  <li>Fixed bug <?php githubissuel('php/php-src',8218); ?> (ob_end_clean does not reset Content-Encoding header).</li>
+</ul></li>
+<li>Intl:
+<ul>
+  <li>Fixed bug <?php githubissuel('php/php-src',8364); ?> (msgfmt_format $values may not support references).</li>
+</ul></li>
+<li>MBString:
+<ul>
+  <li>Number of error markers emitted for invalid UTF-8 text matches WHATWG specification. This is a return to the behavior of PHP 8.0 and earlier.</li>
+</ul></li>
+<li>MySQLi:
+<ul>
+  <li>Fixed bug <?php githubissuel('php/php-src',8267); ?> (MySQLi uses unsupported format specifier on Windows).</li>
+</ul></li>
+<li>SPL:
+<ul>
+  <li>Fixed bug <?php githubissuel('php/php-src',8366); ?> (ArrayIterator may leak when calling __construct()).</li>
+  <li>Fixed bug <?php githubissuel('php/php-src',8273); ?> (SplFileObject: key() returns wrong value).</li>
+</ul></li>
+<li>Streams:
+<ul>
+  <li>Fixed php://temp does not preserve file-position when switched to temporary file.</li>
+</ul></li>
+<li>zlib:
+<ul>
+  <li>Fixed bug <?php githubissuel('php/php-src',8218); ?> (ob_end_clean does not reset Content-Encoding header).</li>
+</ul></li>
+</ul>
+<!-- }}} --></section>
+
+
+
 <section class="version" id="8.1.5"><!-- {{{ 8.1.5 -->
 <h3>Version 8.1.5</h3>
 <b><?php release_date('14-Apr-2022'); ?></b>
