be98d3d3a7fd04a3600aedfd61d4ec76b5905ce1
Sergey Panteleev <sergey@php.net>
2022-06-06 12:43:10 +0300

    Start PHP 8.2 Release page

---
+++ releases/8.2/languages/en.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'common_header' => 'PHP 8.2 is a major update of the PHP language. Readonly classes, null, false, and true as stand-alone types, deprecated dynamic properties, performance improvements and more',
+    'documentation' => 'Doc',
+];
