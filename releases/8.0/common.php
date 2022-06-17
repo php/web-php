655b9f1b41604dcb75333af5596b147214a72a3d
MathiasReker <mathias@reker.dk>
2022-06-17 15:39:43 +0200

    No useless return
    There should not be an empty return statement at the end of a function.

--- releases/8.0/common.php
+++ releases/8.0/common.php
@@ -6,7 +6,7 @@ include_once __DIR__ . '/../../include/prepend.inc';
 function language_redirect(string $currentLang): void {
     // We don't use the general language selection of php.net,
     // so soldier on with this one.
-    return;
+    
 }
 
 function common_header(string $description): void {
