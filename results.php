f24a9f3906cf0cd55779a380da04151ed490e063
Andreas Möller <am@localheinz.com>
2022-07-11 18:39:48 +0200

    Fix: Remove unused parameters
    Closes GH-577.

--- results.php
+++ results.php
@@ -3,18 +3,6 @@ $_SERVER['BASE_PAGE'] = 'results.php';
 include __DIR__ . '/include/prepend.inc';
 include __DIR__ . '/include/results.inc';
 
-if (!isset($_GET['l']) || !is_string($_GET['l'])) {
-  $_GET['l'] = null;
-}
-
-$lang = isset($_GET["l"]) ? (string)$_GET["l"] : "en";
-$query = isset($_GET["q"]) ? (string)$_GET["q"] : '';
-
-if (!isset($LANGUAGES[$lang])) {
-    $lang = "en";
-}
-
-
 // HTTP status line is passed on, signifies an error
 site_header(
     'Search results',
@@ -27,6 +15,6 @@ site_header(
 
 echo '<h1>Search results</h1>';
 
-google_cse($query, $lang);
+google_cse();
 
 site_footer();
