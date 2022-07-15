ce186ff71ae88ed6ddb912dd915606b6f5ae46ab
Andreas Möller <am@localheinz.com>
2022-07-15 11:42:52 +0200

    Enhancement: Enable strict_param fixer

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -15,18 +15,21 @@ $finder = $config->getFinder()
     ->notPath('include/pregen-news.inc')
     ->notPath('tests/run-tests.php');
 
-$config->setRules([
-    'array_indentation' => true,
-    'constant_case' => true,
-    'indentation_type' => true,
-    'line_ending' => true,
-    'no_extra_blank_lines' => true,
-    'no_trailing_whitespace' => true,
-    'ordered_class_elements' => true,
-    'single_space_after_construct' => true,
-    'trim_array_spaces' => true,
-    'visibility_required' => true,
-    'whitespace_after_comma_in_array' => true,
-]);
+$config
+    ->setRiskyAllowed(true)
+    ->setRules([
+        'array_indentation' => true,
+        'constant_case' => true,
+        'indentation_type' => true,
+        'line_ending' => true,
+        'no_extra_blank_lines' => true,
+        'no_trailing_whitespace' => true,
+        'ordered_class_elements' => true,
+        'single_space_after_construct' => true,
+        'strict_param' => true,
+        'trim_array_spaces' => true,
+        'visibility_required' => true,
+        'whitespace_after_comma_in_array' => true,
+    ]);
 
 return $config;
