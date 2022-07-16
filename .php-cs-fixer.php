94cc9697f00ede261005e95233a0e8a77fbeada5
Andreas Möller <am@localheinz.com>
2022-07-16 19:32:09 +0200

    Enhancement: Enable switch_case_space fixer

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -27,6 +27,7 @@ $config
         'ordered_class_elements' => true,
         'single_space_after_construct' => true,
         'strict_param' => true,
+        'switch_case_space' => true,
         'trim_array_spaces' => true,
         'visibility_required' => true,
         'whitespace_after_comma_in_array' => true,
