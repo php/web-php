f3ff09248b84d1a23c18411dd33229216a09db47
Protocol Corporation <protocollive@outlook.com>
2022-12-11 11:30:37 -0300

    Fix some logics

--- releases/8.2/languages/pt_BR.php
+++ releases/8.2/languages/pt_BR.php
@@ -1,22 +1,22 @@
 <?php
 
 return [
-    'common_header' => 'PHP 8.2 é uma grande atualização da linguagem PHP. Classe somente leitura, tipo stand alone, null, false, e true ele inclui recursos como depreciação de propriedades dinâmicas e melhorias de desempenho',
+    'common_header' => 'PHP 8.2 é a grande atualização da linguagem PHP. Classe somente leitura, null, false e true como tipos stand alone, depreciação de propriedades dinâmicas, melhorias de desempenho e mais',
     'documentation' => 'Doc',
     'main_title' => 'Lançado!',
-    'main_subtitle' => 'PHP 8.2 é uma grande atualização da linguagem PHP.<br class="display-none-md">Esta atualização inclui muitos novos recursos e otimizações. Classe somente leitura, tipo independente, null, false, e true como tipos stand alone, propriedades dinâmicas obsoletas, melhorias de desempenho e muito mais.',
+    'main_subtitle' => 'PHP 8.2 é a grande atualização da linguagem PHP.<br class="display-none-md">Esta atualização inclui muitos novos recursos e otimizações. Classe somente leitura, tipo independente, null, false e true como tipos stand alone, propriedades dinâmicas obsoletas, melhorias de desempenho e muito mais.',
     'upgrade_now' => 'Atualize para PHP 8.2 agora!',
     'readonly_classes_title' => 'Classes somente leitura',
     'dnf_types_title' => 'Tipos DNF (Disjunctive Normal Form)',
-    'dnf_types_description' => 'Tipos DNF nos permite <a href="/manual/en/language.types.type-system.php#language.types.type-system.composite.union">union</a> e <a href="/manual/en/language.types.type-system.php#language.types.type-system.composite.intersection">intersection</a> tipos, seguindo uma regra estrita: ao combinar tipos de união e interseção, os tipos de interseção devem ser agrupados com colchetes',
-    'null_false_true_types_title' => 'Permite <code>null</code>, <code>false</code>, e <code>true</code> como tipos stand alone',
+    'dnf_types_description' => 'Tipos DNF nos permite <a href="/manual/en/language.types.type-system.php#language.types.type-system.composite.union">união</a> e <a href="/manual/en/language.types.type-system.php#language.types.type-system.composite.intersection">interseção</a> de tipos, seguindo uma regra estrita: ao combinar tipos de união e interseção, os tipos de interseção devem ser agrupados com colchetes',
+    'null_false_true_types_title' => 'Permite <code>null</code>, <code>false</code> e <code>true</code> como tipos stand alone',
     'random_title' => 'Nova extensão "Random"',
     'random_description' => '<p>A extensão "random" fornece uma nova API orientada a objetos para geração de números aleatórios. Em vez de depender de um gerador de números aleatórios globalmente semeado (RNG) usando o algoritmo Mersenne Twister, a API orientada a objetos fornece várias classes ("Engine"s) que fornecem acesso a algoritmos modernos que armazenam seu estado em objetos para permitir várias sequências semeáveis ​​independentes .</p>
         <p>A classe <code>\Random\Randomizer</code> fornece uma interface de alto nível para usar a aleatoriedade do mecanismo para gerar um número inteiro aleatório, embaralhar um array ou string, selecionar chaves de array aleatórias e muito mais.</p >',
     'constants_in_traits_title' => 'Constantes em Traits',
-    'constants_in_traits_description' => 'Você não pode acessar a constante através do nome da Trait, mas, você pode acessar a constante através da classe que usa a Trait.',
+    'constants_in_traits_description' => 'Você não pode acessar a constante através do nome da Trait, mas você pode acessar a constante através da classe que usa a Trait.',
     'deprecate_dynamic_properties_title' => 'Propriedades dinâmicas obsoletas',
-    'deprecate_dynamic_properties_description' => '<p>A criação de propriedades dinâmicas está obsoleta para ajudar a evitar erros e erros de digitação, a menos que a classe opte por usar o atributo de <code>#[\AllowDynamicProperties]</code>. <code>stdClass</code> permite propriedades dinâmicas.</p>
+    'deprecate_dynamic_properties_description' => '<p>A criação de propriedades dinâmicas está obsoleta para ajudar a evitar enganos e erros de digitação, a menos que a classe opte por usar o atributo de <code>#[\AllowDynamicProperties]</code>. <code>stdClass</code> permite propriedades dinâmicas.</p>
         <p>O uso dos métodos mágicos <code>__get</code>/<code>__set</code> não é afetado por esta alteração.</p>',
     'new_classes_title' => 'Novas classes, Interfaces, e Funções',
     'new_mysqli' => 'Nova função <a href="/manual/en/mysqli.execute-query.php"><code>mysqli_execute_query</code></a> e método <a href="/manual/en/mysqli.execute-query. php"><code>mysqli::execute_query</code></a> .',
