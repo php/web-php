70988eaed329a51504320fe0697b0997b29ce653
Ben Ramsey <ramsey@php.net>
2023-02-14 11:53:48 -0600

    Announce PHP 8.1.16

---
+++ releases/8_1_16.php
@@ -0,0 +1,16 @@
+<?php
+$_SERVER['BASE_PAGE'] = 'releases/8_1_16.php';
+include_once __DIR__ . '/../include/prepend.inc';
+site_header('PHP 8.1.16 Release Announcement');
+?>
+<h1>PHP 8.1.16 Release Announcement</h1>
+
+<p>The PHP development team announces the immediate availability of PHP 8.1.16. This is a security release that addresses CVE-2023-0567, CVE-2023-0568, and CVE-2023-0662.</p>
+
+<p>All PHP 8.1 users are advised to upgrade to this version.</p>
+
+<p>For source downloads of PHP 8.1.16 please visit our <a href="https://www.php.net/downloads.php">downloads page</a>,
+Windows source and binaries can be found on <a href="https://windows.php.net/download/">windows.php.net/download/</a>.
+The list of changes is recorded in the <a href="https://www.php.net/ChangeLog-8.php#8.1.16">ChangeLog</a>.
+</p>
+<?php site_footer();
