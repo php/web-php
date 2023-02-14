941873e4613fa1423a11f2bb457fbb4a8670d65f
Gabriel Caruso <carusogabriel34@gmail.com>
2023-02-14 15:31:53 +0100

    Improve text for PHp 8.0.28 release

--- releases/8_0_28.php
+++ releases/8_0_28.php
@@ -5,9 +5,10 @@ site_header('PHP 8.0.28 Release Announcement');
 ?>
 <h1>PHP 8.0.28 Release Announcement</h1>
 
-<p>The PHP development team announces the immediate availability of PHP 8.0.28. This is a security release.</p>
+<p>The PHP development team announces the immediate availability of PHP 8.0.28. This is a security release
+that addresses CVE-2023-0567, CVE-2023-0568, and CVE-2023-0662</p>
 
-<p>All PHP 8.0 users are encouraged to upgrade to this version.</p>
+<p>All PHP 8.0 users are advised to upgrade to this version.</p>
 
 <p>For source downloads of PHP 8.0.28 please visit our <a href="https://www.php.net/downloads.php">downloads page</a>,
 Windows source and binaries can be found on <a href="https://windows.php.net/download/">windows.php.net/download/</a>.
