8619152eda0614636b6925e11ede8dab73287765
Sergey Panteleev <sergey@php.net>
2023-11-17 13:57:35 +0300

    Typos

--- releases/8.3/languages/ru.php
+++ releases/8.3/languages/ru.php
@@ -13,9 +13,9 @@ return [
     'json_validate_description' => 'Функция <code>json_validate()</code> позволяет проверить, является ли строка синтаксически корректным JSON, при этом она более эффективна, чем функция <code>json_decode()</code>.',
     'typed_class_constants_title' => 'Типизированные константы классов',
     'override_title' => 'Атрибут <code>#[\Override]</code>',
-    'override_description' => 'Добавив методу атрибут <code>#[\Override]</code>, PHP убедится, что метод с таким же именем существует в родительском классе или в реализованном интерфейсе. Добавление атрибута даёт понять, что переопределение родительского метода является намеренным и упрощает рефакторинг, поскольку удаление переопределённого родительского метода будет обнаружено.',
+    'override_description' => 'Добавив методу атрибут <code>#[\Override]</code>, PHP убедится, что метод с таким же именем существует в родительском классе или в реализованном интерфейсе. Добавление атрибута даёт понять, что переопределение родительского метода является намеренным, а также упрощает рефакторинг, поскольку удаление переопределённого родительского метода будет обнаружено.',
     'randomizer_getbytesfromstring_title' => 'Новый метод <code>Randomizer<span style="word-break: break-all;">::</span>getBytesFromString()</code>',
-    'randomizer_getbytesfromstring_description' => '<a href="/releases/8.2/ru.php#random_extension">Модуль Random</a>, добавленный в PHP 8.3, был дополнен новым методом генерации случайных строк, состоящих только из определённых байтов. Этот метод позволяет легко генерировать случайные идентификаторы, например, имена доменов и числовые строки произвольной длины.',
+    'randomizer_getbytesfromstring_description' => '<a href="/releases/8.2/ru.php#random_extension">Модуль Random</a>, добавленный в PHP 8.2, был дополнен новым методом генерации случайных строк, состоящих только из определённых байтов. Этот метод позволяет легко генерировать случайные идентификаторы, например, имена доменов и числовые строки произвольной длины.',
     'randomizer_getfloat_nextfloat_title' => 'Новые методы <code>Randomizer::getFloat()</code> и <code>Randomizer::nextFloat()</code>',
     'randomizer_getfloat_nextfloat_description' => 'В качестве алгоритма используется алгоритм γ-секции, опубликованный в статье:
         <a href="https://doi.org/10.1145/3503512" target="_blank" rel="noopener noreferrer">Drawing Random Floating-Point Numbers from an Interval</a>.
@@ -39,7 +39,7 @@ return [
     'bc_arrays' => 'Присвоение отрицательного индекса <code>n</code> пустому массиву теперь гарантирует, что следующим индексом будет <code>n + 1</code>, а не <code>0</code>.',
     'bc_range' => 'Изменения в функции <code>range()</code>.',
     'bc_traits' => 'Изменения в повторном объявлении статических свойств в трейтах.',
-    'bc_umultipledecimalseparators' => 'Констант <code>U_MULTIPLE_DECIMAL_SEPERATORS</code> объявлена устаревшей, вместо неё рекомендуется использовать константу <code>U_MULTIPLE_DECIMAL_SEPARATORS</code>.',
+    'bc_umultipledecimalseparators' => 'Константа <code>U_MULTIPLE_DECIMAL_SEPERATORS</code> объявлена устаревшей, вместо неё рекомендуется использовать константу <code>U_MULTIPLE_DECIMAL_SEPARATORS</code>.',
     'bc_mtrand' => 'Вариант Mt19937 <code>MT_RAND_PHP</code> объявлен устаревшим.',
     'bc_reflection' => '<a href="/manual/ru/reflectionclass.getstaticproperties.php"><code>ReflectionClass::getStaticProperties()</code></a> теперь не возвращает значение <code>null</code>.',
 
