93c74f5b50b72e4cf2917c86182a28d6205ec8ae
Sergey Panteleev <sergey@php.net>
2023-11-26 10:15:50 +0300

    PHP 8.0 is now EOL (#840)

--- eol.php
+++ eol.php
@@ -6,6 +6,7 @@ include_once __DIR__ . '/include/branches.inc';
 
 // Notes for specific branches can be added here, and will appear in the table.
 $BRANCH_NOTES = [
+    '8.0' => '<a href="/migration81">A guide is available for migrating from PHP 8.0 to 8.1.</a>',
     '7.4' => '<a href="/migration80">A guide is available for migrating from PHP 7.4 to 8.0.</a>',
     '7.3' => '<a href="/migration74">A guide is available for migrating from PHP 7.3 to 7.4.</a>',
     '7.2' => '<a href="/migration73">A guide is available for migrating from PHP 7.2 to 7.3.</a>',
