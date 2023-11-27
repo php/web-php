1f6e3ae4d455815ffdb42c4dbe23d6a03b0e0c1b
Alexander King Woollam <alexjhwoollam@gmail.com>
2023-11-27 16:20:02 +0000

    Update en.php (#843)

--- releases/8.3/languages/en.php
+++ releases/8.3/languages/en.php
@@ -4,7 +4,7 @@ return [
     'common_header' => 'PHP 8.3 is a major update of the PHP language. It contains many new features, such as explicit typing of class constants, deep-cloning of readonly properties and additions to the randomness functionality. As always it also includes performance improvements, bug fixes, and general cleanup.',
     'documentation' => 'Doc',
     'main_title' => 'Released!',
-    'main_subtitle' => 'PHP 8.3 is a major update of the PHP language.<br class="display-none-md">It contains many new features, such as explicit typing of class constants, deep-cloning of readonly properties and additions to the randomness functionality. As always it also includes performance improvements, bug fixes, and general cleanup.',
+    'main_subtitle' => 'PHP 8.3 is a major update of the PHP language.<br class="display-none-md"> It contains many new features, such as explicit typing of class constants, deep-cloning of readonly properties and additions to the randomness functionality. As always it also includes performance improvements, bug fixes, and general cleanup.',
     'upgrade_now' => 'Upgrade to PHP 8.3 now!',
 
     'readonly_title' => 'Deep-cloning of readonly properties',
