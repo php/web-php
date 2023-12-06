926ffd2c5d76110a86acffeb64ede86644963dd3
Andreas Möller <am@localheinz.com>
2023-12-06 14:34:12 +0100

    Enhancement: Use constructor property promotion

--- src/UserNotes/UserNote.php
+++ src/UserNotes/UserNote.php
@@ -8,47 +8,15 @@ namespace phpweb\UserNotes;
  */
 final class UserNote
 {
-    /** @var string $id */
-    public $id;
-
-    /** @var string $sect */
-    public $sect;
-
-    /** @var string $rate */
-    public $rate;
-
-    /** @var string $ts */
-    public $ts;
-
-    /** @var string $user */
-    public $user;
-
-    /** @var string $text */
-    public $text;
-
-    /** @var int $upvotes */
-    public $upvotes;
-
-    /** @var int $downvotes */
-    public $downvotes;
-
     public function __construct(
-        string $id,
-        string $sect,
-        string $rate,
-        string $ts,
-        string $user,
-        string $text,
-        int $upvotes = 0,
-        int $downvotes = 0
+        public string $id,
+        public string $sect,
+        public string $rate,
+        public string $ts,
+        public string $user,
+        public string $text,
+        public int $upvotes = 0,
+        public int $downvotes = 0
     ) {
-        $this->id = $id;
-        $this->sect = $sect;
-        $this->rate = $rate;
-        $this->ts = $ts;
-        $this->user = $user;
-        $this->text = $text;
-        $this->upvotes = $upvotes;
-        $this->downvotes = $downvotes;
     }
 }
