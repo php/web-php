ede0692b3ef68003b09054956f3c282dc8a6b3f3
Andreas Möller <am@localheinz.com>
2023-12-06 19:31:09 +0100

    Enhancement: Expose happy path (#406)
    * Enhancement: Expose happy path
    
    * Fix: Require file with absolute path
    
    * Fix: Return null instead of false

--- .router.php
+++ .router.php
@@ -3,10 +3,12 @@ $_SERVER["SERVER_ADDR"] = $_SERVER["HTTP_HOST"];
 
 $filename = $_SERVER["PATH_INFO"] ?? $_SERVER["SCRIPT_NAME"];
 
-if (file_exists($_SERVER["DOCUMENT_ROOT"] . $filename)) {
-    /* This could be an image or whatever, so don't try to compress it */
-    ini_set("zlib.output_compression", 0);
-    return false;
+if (!file_exists($_SERVER["DOCUMENT_ROOT"] . $filename)) {
+    require_once __DIR__ . '/error.php';
+
+    return;
 }
 
-include_once "error.php";
+/* This could be an image or whatever, so don't try to compress it */
+ini_set("zlib.output_compression", 0);
+return null;
