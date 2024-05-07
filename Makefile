1be4239d105a6b21dbe82b66ab78708dba1e3bb9
Sergey Panteleev <sergey@php.net>
2024-05-08 00:11:53 +0300

    Add visual regression tests
    Co-authored-by: Roman Pronskiy <roman@pronskiy.com>

--- Makefile
+++ Makefile
@@ -21,7 +21,20 @@ help: ## Displays this list of targets with descriptions
 tests: vendor ## Runs unit and end-to-end tests with phpunit/phpunit
 	vendor/bin/phpunit --configuration=tests/phpunit.xml --testsuite=unit
 	rm -rf tests/server.log
-	tests/server start; vendor/bin/phpunit --configuration=tests/phpunit.xml --testsuite=end-to-end; tests/server stop
+	tests/server start;
+	vendor/bin/phpunit --configuration=tests/phpunit.xml --testsuite=end-to-end;
+	npx playwright test
+	tests/server stop
+
+tests_visual:
+	tests/server start;
+	npx playwright test
+	tests/server stop
+
+tests_update_snapshots:
+	tests/server start;
+	npx playwright test --update-snapshots
+	tests/server stop
 
 vendor: composer.json composer.lock
 	composer validate --strict
---
+++ tests/Visual/SmokeTest.spec.ts
@@ -0,0 +1,61 @@
+import {expect, test} from '@playwright/test';
+
+export type TestPageOptions = {
+    path: string
+    options?: object
+    evaluate?: () => any
+}
+
+const items: TestPageOptions[] = [
+    {
+        path: 'index.php',
+        evaluate: () => {
+            const selector = document.querySelector('.elephpants');
+            selector.remove()
+        }
+    },
+    {
+        path: 'archive/1998.php',
+        evaluate: () => {
+            const selector = document.querySelector('.elephpants');
+            selector.remove()
+        }
+    },
+    {path: 'releases/8_3_6.php'},
+    {path: 'releases/8.0/index.php'},
+    {path: 'releases/8.1/index.php'},
+    {path: 'releases/8.2/index.php'},
+    {path: 'releases/8.3/index.php'},
+    {path: 'manual/index.php'},
+    {path: 'manual/php5.php'},
+    {
+        path: 'conferences/index.php',
+        options: {
+            fullPage: false,
+        }
+    },
+]
+
+for (const item of items) {
+    test(`testing with ${item.path}`, async ({page}) => {
+        const httpHost = process.env.HTTP_HOST
+
+        if (typeof httpHost !== 'string') {
+            throw new Error('Environment variable "HTTP_HOST" is not set.')
+        }
+
+        await page.goto(`${httpHost}/${item.path}`)
+
+        if (typeof item.evaluate === 'function') {
+            await page.evaluate(item.evaluate)
+        }
+
+        await expect(page).toHaveScreenshot(
+            `tests/screenshots/${item.path}.png`,
+            item.options ?? {
+                fullPage: true,
+                timeout: 10000,
+            }
+        )
+    })
+}
