ca4bca796cdd7efd619a1ee9b2fa127d27e55d0f
Adecap <75079448+alejandrodecap@users.noreply.github.com>
2024-06-15 18:39:07 +0200

    Update get-involved.php (#1009)
    * Update get-involved.php
    
    Changed http to https link as the pages they link to does not redirect to their own https versions automatically
    
    * Update get-involved.php
    
    Fixed error

--- get-involved.php
+++ get-involved.php
@@ -26,10 +26,10 @@ site_header("Get Involved", ["current" => "community"]);
 <h3 class="content-title" id="ways-to-contribute">Four Best Ways to Contribute</h3>
 
  <ol class="content-box listed">
-  <li>Running test suites in <a href="http://qa.php.net/">RC</a>
-      and <a href="http://php.net/releases">release</a> distributions of PHP</li>
+  <li>Running test suites in <a href="https://qa.php.net/">RC</a>
+      and <a href="https://php.net/releases">release</a> distributions of PHP</li>
   <li>Help finding and diagnosing failed tests, see
-      the <a href="http://qa.php.net/write-test.php">phpt documentation</a></li>
+      the <a href="https://qa.php.net/write-test.php">phpt documentation</a></li>
   <li>Filing and resolving bug reports
       on <a href="https://github.com/php/php-src/issues/">GitHub Issues</a>.</li>
   <li>Help maintain and or translate documentation files
