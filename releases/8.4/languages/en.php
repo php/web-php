b58da6a338a41b28ae7fd7fb7cb7e36c957097c1
Sergey Panteleev <sergey@php.net>
2024-09-19 09:16:38 +0300

    PHP 8.4 page

---
+++ releases/8.4/languages/en.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'common_header' => 'PHP 8.4 is a major update of the PHP language. It contains many new features, such as ... As always it also includes performance improvements, bug fixes, and general cleanup.',
+    'documentation' => 'Doc',
+    'main_title' => 'Released!',
+    'main_subtitle' => 'PHP 8.4 is a major update of the PHP language.<br class="display-none-md"> It contains many new features, such as ... As always it also includes performance improvements, bug fixes, and general cleanup.',
+    'upgrade_now' => 'Upgrade to PHP 8.4 now!',
+
+    'new_classes_title' => 'New Classes, Interfaces, and Functions',
+
+    'bc_title' => 'Deprecations and backward compatibility breaks',
+
+    'footer_title' => 'Better performance, better syntax, improved type safety.',
+    'footer_description' => '<p>For source downloads of PHP 8.4 please visit the <a href="/downloads">downloads</a> page. Windows binaries can be found on the <a href="https://windows.php.net/download">PHP for Windows</a> site. The list of changes is recorded in the <a href="/ChangeLog-8.php#PHP_8_4">ChangeLog</a>.</p>
+        <p>The <a href="/manual/en/migration84.php">migration guide</a> is available in the PHP Manual. Please consult it for a detailed list of new features and backward-incompatible changes.</p>',
+];
