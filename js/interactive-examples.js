51dbc7b818e347c723661bb5a415c1b5e7c22c1d
soyuka <soyuka@users.noreply.github.com>
2024-10-16 15:37:50 +0200

    Run examples with php wasm

---
+++ js/interactive-examples.js
@@ -0,0 +1,58 @@
+import phpBinary from "/js/php-web.mjs";
+
+function createOutput(output) {
+  const container = document.createElement('div');
+  container.classList.add('screen', 'example-contents');
+  const title = document.createElement('p');
+  title.innerText = 'Output: ';
+  container.appendChild(title)
+  const div = document.createElement('div');
+  div.classList.add('examplescode');
+  container.appendChild(div);
+  const pre = document.createElement('pre');
+  pre.classList.add('examplescode');
+  pre.innerText = output;
+  div.appendChild(pre)
+  return container;
+}
+
+async function main() {
+  const buffer = [];
+	const {ccall} = await phpBinary({
+    print(data) {
+      if (!data) {
+        return;
+      }
+
+      if (buffer.length) {
+        buffer.push('\n');
+      }
+      buffer.push(data);
+    }
+  })
+
+  console.log("PHP wasm %s loaded.", ccall("phpw_exec", "string", ["string"], ["phpversion();"]));
+  let lastOutput = null
+
+	document.querySelectorAll('.example').forEach((example) => {
+		const button = document.createElement('button');
+		const phpcode = example.querySelector('.phpcode');
+
+		button.innerText = 'Run code';
+		button.onclick = function() {
+      if (lastOutput && lastOutput.parentNode) {
+        lastOutput.parentNode.removeChild(lastOutput)
+      }
+
+      ccall("phpw_run", null, ["string"], ['?>' + phpcode.innerText]);
+      lastOutput = createOutput(buffer.join(''))
+      phpcode.parentNode.appendChild(lastOutput);
+      buffer.length = 0;
+		};
+
+		phpcode.parentNode.insertBefore(button, phpcode);
+	});
+
+}
+
+main()
