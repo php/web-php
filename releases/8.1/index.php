a20418eb354aff795e3d53775e70eca5f3c2ae96
Takuya Aramaki <takaram71@gmail.com>
2024-10-18 23:50:53 +0900

    Auto-detect user's language on PHP8.0-8.3 release pages (#1098)

--- releases/8.1/index.php
+++ releases/8.1/index.php
@@ -1,6 +1,13 @@
 <?php
 
- $_SERVER['BASE_PAGE'] = 'releases/8.1/index.php';
-include __DIR__ . '/../../include/site.inc';
+use phpweb\LangChooser;
+use const releases\php81\LANGUAGES;
 
-mirror_redirect('/releases/8.1/en.php');
+$_SERVER['BASE_PAGE'] = 'releases/8.1/index.php';
+require_once __DIR__ . '/common.php';
+require_once __DIR__ . '/../../src/autoload.php';
+
+$langChooser = new LangChooser(LANGUAGES, [], "", "");
+[$lang,] = $langChooser->chooseCode("", "", $_SERVER['HTTP_ACCEPT_LANGUAGE']);
+
+mirror_redirect("/releases/8.1/$lang.php");
--- releases/8.2/index.php
+++ releases/8.2/index.php
@@ -1,6 +1,13 @@
 <?php
 
- $_SERVER['BASE_PAGE'] = 'releases/8.2/index.php';
-include __DIR__ . '/../../include/site.inc';
+use phpweb\LangChooser;
+use const releases\php82\LANGUAGES;
 
-mirror_redirect('/releases/8.2/en.php');
+$_SERVER['BASE_PAGE'] = 'releases/8.2/index.php';
+require_once __DIR__ . '/common.php';
+require_once __DIR__ . '/../../src/autoload.php';
+
+$langChooser = new LangChooser(LANGUAGES, [], "", "");
+[$lang,] = $langChooser->chooseCode("", "", $_SERVER['HTTP_ACCEPT_LANGUAGE']);
+
+mirror_redirect("/releases/8.2/$lang.php");
