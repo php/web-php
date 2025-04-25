f2fb561ae457a03717f673920d891e1d0ce04727
Jakub Vrána <jakub@vrana.cz>
2025-04-25 11:14:49 +0200

    Link named arguments doc from release announcement (#1263)
    Fix https://bugs.php.net/80555

--- releases/8.0/ru.php
+++ releases/8.0/ru.php
@@ -38,6 +38,7 @@ releases\php80\common_header(
     <h2 class="php8-h2" id="named-arguments">
       Именованные аргументы
       <a class="php8-rfc" href="https://wiki.php.net/rfc/named_params">RFC</a>
+      <a class="php8-rfc" href="/manual/ru/functions.arguments.php#functions.named-arguments">Документация</a>
     </h2>
     <div class="php8-compare__main">
       <div class="php8-compare__block example-contents">
