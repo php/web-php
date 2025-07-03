3fd6ca5dea36ca96ada01eeb231ac4593048bb3b
Sergey Panteleev <sergey@php.net>
2025-07-03 12:12:36 +0300

    Update layout

--- downloads-get-instructions.php
+++ downloads-get-instructions.php
@@ -25,31 +25,31 @@ if ($_GET['os'] === 'osx') {
 <p>
     On the OSX command line shell, enter:
 </p>
-<div class="example"><div class="example-contents screen"><pre>
+<div class="phpcode"><pre>
 brew install php{$version}
-</pre></div></div>
+</pre></div>
 <p>
 To enable PHP in Apache add the following to httpd.conf and restart Apache:
 </p>
-<div class="example"><div class="example-contents screen"><pre>
+<div class="phpcode"><pre>
 LoadModule php_module \$HOMEBREW_PREFIX/opt/php/lib/httpd/modules/libphp.so
 
 &lt;FilesMatch \.php$>
 SetHandler application/x-httpd-php
 &lt;/FilesMatch>
-</pre></div></div>
+</pre></div>
 <p>
 Finally, check DirectoryIndex includes index.php
 </p>
-<div class="example"><div class="example-contents screen"><pre>
+<div class="phpcode"><pre>
 DirectoryIndex index.php index.html
-</pre></div></div>
+</pre></div>
 <p>
 The php.ini and php-fpm.ini file can be found in:
 </p>
-<div class="example"><div class="example-contents screen"><pre>
+<div class="phpcode"><pre>
 \$HOMEBREW_PREFIX/etc/php/{$versionDir}/
-</pre></div></div>
+</pre></div>
 </p>
 ENDOSX;
     return;
@@ -62,10 +62,10 @@ if ($_GET['os'] === 'linux' && str_starts_with($_GET['osvariant'], 'linux-deb'))
 <p>
     On the command line shell, enter:
 </p>
-<div class="example"><div class="example-contents screen"><pre>
+<div class="phpcode"><pre>
 sudo apt-get update
 sudo apt-get install php
-</pre></div></div>
+</pre></div>
 ENDAPT;
     } else {
         $version = match($_GET['version']) {
@@ -79,12 +79,12 @@ ENDAPT;
 <p>
     On the command line shell, enter:
 </p>
-<div class="example"><div class="example-contents screen"><pre>
+<div class="phpcode"><pre>
 sudo apt -y install software-properties-common
 sudo add-apt-repository ppa:ondrej/php
 sudo apt update
 sudo apt install php{$version}
-</pre></div></div>
+</pre></div>
 ENDAPT;
     }
     return;
