3a5fa8bbc57a18614280193c0bc5c7e8f49d9275
Calvin Buckley <calvinb@php.net>
2025-07-17 13:40:24 -0300

    Announce PHP 8.4.11RC1

--- include/release-qa.php
+++ include/release-qa.php
@@ -84,16 +84,16 @@ $QA_RELEASES = [
         ],
     ],
 
-    '8.4.9' => [
+    '8.4.11' => [
         'active'  => true,
         'release' => [
             'type'       => 'RC',
-            'number'     => 0,
-            'sha256_bz2' => '',
-            'sha256_gz'  => '',
-            'sha256_xz'  => '',
-            'date'       => '19 Jun 2025',
-            'baseurl'    => 'https://downloads.php.net/~saki/',
+            'number'     => 1,
+            'sha256_bz2' => '641fcd2da3c066a0799cf469ee8491d98502cf5877be5cf07b3d648ac74f83f8',
+            'sha256_gz'  => 'c19c5daa94c8171c5ceb04214212cc7cc5c284804de0f60e2a78d3aec5e65900',
+            'sha256_xz'  => '7568a75d1f220fe224dc56b17f9252bdc366ad9afcc35d8785d58cd1222f6e1f',
+            'date'       => '17 Jul 2025',
+            'baseurl'    => 'https://downloads.php.net/~calvinb/',
         ],
     ],
 
