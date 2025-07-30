68d6879045940cd5b79ec1a73ce77c45835785de
Shivam Mathur <shivam_jpr@hotmail.com>
2025-07-30 20:22:22 +0530

    Fix instructions for windows-native

--- include/download-instructions/windows-native.php
+++ include/download-instructions/windows-native.php
@@ -2,5 +2,5 @@
 On the command line, run the following commands:
 </p>
 <pre><code class="language-powershell line-numbers">
-powershell -c "irm https://php.net/downloads/install.ps1 <?= $version; ?> | iex"
+powershell -c "& ([ScriptBlock]::Create((irm 'https://php.net/install/windows.ps1'))) -Version <?= $version; ?>"
 </code></pre>
