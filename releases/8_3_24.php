f9250657bcff83e4e533312c9d907af351712fe6
Remi Collet <remi@php.net>
2025-08-01 10:11:45 +0200

    8.3.24 is bugfix

--- releases/8_3_24.php
+++ releases/8_3_24.php
@@ -5,7 +5,7 @@ site_header('PHP 8.3.24 Release Announcement');
 ?>
 <h1>PHP 8.3.24 Release Announcement</h1>
 
-<p>The PHP development team announces the immediate availability of PHP 8.3.24. This is a security release.</p>
+<p>The PHP development team announces the immediate availability of PHP 8.3.24. This is a bug fix release.</p>
 
 <p>All PHP 8.3 users are encouraged to upgrade to this version.</p>
 
