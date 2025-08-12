72926fc33576cad7afa6c0885cf02c0f0c2e9f61
Derick Rethans <github@derickrethans.nl>
2025-08-12 11:33:02 +0100

    Use spaces for indentation

--- downloads-get-instructions.php
+++ downloads-get-instructions.php
@@ -11,52 +11,52 @@ if (!isset($options)) {
 }
 
 if ($options['os'] === 'windows') {
-	if ($options['osvariant'] === 'windows-wsl-debian') {
-		$options['os'] = 'linux';
-	    $options['osvariant'] = 'linux-debian';
-	}
-	if ($options['osvariant'] === 'windows-wsl-ubuntu') {
-		$options['os'] = 'linux';
-	    $options['osvariant'] = 'linux-ubuntu';
-	}
+    if ($options['osvariant'] === 'windows-wsl-debian') {
+        $options['os'] = 'linux';
+        $options['osvariant'] = 'linux-debian';
+    }
+    if ($options['osvariant'] === 'windows-wsl-ubuntu') {
+        $options['os'] = 'linux';
+        $options['osvariant'] = 'linux-ubuntu';
+    }
 }
 if ($options['os'] === 'osx' || $options['os'] === 'windows') {
     if ($options['version'] === 'default') {
-		$options['version'] = $latestPhpVersion;
-	}
+        $options['version'] = $latestPhpVersion;
+    }
 }
 
 if (in_array($options['usage'], ['fw-drupal', 'fw-laravel', 'fw-symfony', 'fw-wordpress'])) {
-	$file = "{$options['usage']}";
-	$options['os'] = null;
+    $file = "{$options['usage']}";
+    $options['os'] = null;
 }
 
 $multiversion = false;
 
 if (array_key_exists('multiversion', $options)) {
-	$multiversion = $options['multiversion'] === 'Y';
+    $multiversion = $options['multiversion'] === 'Y';
 }
 
 $source = false;
 
 if (array_key_exists('source', $options)) {
-	if ($options['source'] === 'Y') {
-		$source = $options['source'] === 'Y';
-	}
+    if ($options['source'] === 'Y') {
+        $source = $options['source'] === 'Y';
+    }
 }
 
 switch ($options['os']) {
-	case 'linux':
-		$defaultOrCommunity = ($options['version'] !== 'default' || $multiversion) ? 'community' : 'default';
-		if ($defaultOrCommunity === 'community' && $options['version'] == 'default') {
-			$options['version'] = $latestPhpVersion;
-		}
-		$file = "{$options['osvariant']}-{$options['usage']}-{$defaultOrCommunity}";
-		break;
-	case 'osx':
-	case 'windows':
-		$file = "{$options['osvariant']}";
-		break;
+    case 'linux':
+        $defaultOrCommunity = ($options['version'] !== 'default' || $multiversion) ? 'community' : 'default';
+        if ($defaultOrCommunity === 'community' && $options['version'] == 'default') {
+            $options['version'] = $latestPhpVersion;
+        }
+        $file = "{$options['osvariant']}-{$options['usage']}-{$defaultOrCommunity}";
+        break;
+    case 'osx':
+    case 'windows':
+        $file = "{$options['osvariant']}";
+        break;
 }
 
 if ($source) {
@@ -67,17 +67,17 @@ $version = $options['version'];
 $versionNoDot = str_replace('.', '', $version);
 
 if (file_exists(__DIR__ . "/include/download-instructions/{$file}.php")) {
-	include __DIR__ . "/include/download-instructions/{$file}.php";
-	if ($source) {
-		return false;
-	}
-	return true;
+    include __DIR__ . "/include/download-instructions/{$file}.php";
+    if ($source) {
+        return false;
+    }
+    return true;
 } else {
 ?>
 <p>
     There are no instructions yet. Try using the <a href="https://www.php.net/manual/en/install.php">generic installation from source</a>.
 </p>
 <?php
-	return false;
+    return false;
 }
 ?>
