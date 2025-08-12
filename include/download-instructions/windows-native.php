52d389cc29a6f1d9268adbec634c251ecb0b4a77
Derick Rethans <github@derickrethans.nl>
2025-08-12 11:31:14 +0100

    Use full domain to prevent redirects

--- include/download-instructions/windows-native.php
+++ include/download-instructions/windows-native.php
@@ -3,5 +3,5 @@ On the command line, run the following commands:
 </p>
 <pre><code class="language-powershell line-numbers">
 # Download and install PHP.
-powershell -c "& ([ScriptBlock]::Create((irm 'https://php.net/install/windows.ps1'))) -Version <?= $version; ?>"
+powershell -c "& ([ScriptBlock]::Create((irm 'https://www.php.net/install/windows.ps1'))) -Version <?= $version; ?>"
 </code></pre>
