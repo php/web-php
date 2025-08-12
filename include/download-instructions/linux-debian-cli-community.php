beef6c5d8b2557fc30104dcf6c54e2db9018e8a8
Shivam Mathur <shivam_jpr@hotmail.com>
2025-08-12 17:33:08 +0530

    Revert to full Debian community instructions

--- include/download-instructions/linux-debian-cli-community.php
+++ include/download-instructions/linux-debian-cli-community.php
@@ -3,9 +3,12 @@ On the command line, run the following commands:
 </p>
 <pre><code class="language-bash line-numbers">
 # Add the packages.sury.org/php repository.
-sudo apt update
-sudo apt install -y curl
-curl -sSL https://packages.sury.org/php/README.txt | sudo bash -x
+sudo apt-get update
+sudo apt-get install -y lsb-release ca-certificates apt-transport-https curl
+sudo curl -sSLo /tmp/debsuryorg-archive-keyring.deb https://packages.sury.org/debsuryorg-archive-keyring.deb
+sudo dpkg -i /tmp/debsuryorg-archive-keyring.deb
+sudo sh -c 'echo "deb [signed-by=/usr/share/keyrings/debsuryorg-archive-keyring.gpg] https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list'
+sudo apt-get update
 
 # Install PHP.
 sudo apt-get install -y php<?= $version; ?>
