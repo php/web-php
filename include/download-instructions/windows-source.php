3570eeffe005105878a165d7ddb057ec0ad2ccf9
Derick Rethans <github@derickrethans.nl>
2025-08-13 13:07:06 +0100

    Link to the wiki, which has updated instructions

--- include/download-instructions/windows-source.php
+++ include/download-instructions/windows-source.php
@@ -1,3 +1,5 @@
 <p>
-	The instructions for compiling from source on Windows are described in the <a href="https://github.com/php/php-windows-builder#local-setup">php/php-windows-builder</a> repository.
+	The instructions for compiling from source on Windows are described in the
+	<a href="https://wiki.php.net/internals/windows/php-windows-builder">php/php-windows-builder</a>
+	repository.
 </p>
