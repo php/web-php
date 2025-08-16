a7445f14a9b5a83ef783302c82842aba1b912572
Shivam Mathur <shivam_jpr@hotmail.com>
2025-08-17 00:11:07 +0530

    Fix spacing on macports instructions

--- include/download-instructions/osx-macports.php
+++ include/download-instructions/osx-macports.php
@@ -5,7 +5,9 @@ On the command line, run the following commands:
 # Please refer to https://guide.macports.org/chunked/installing.macports.html for installing MacPorts.
 
 # Install PHP <?= $version; ?>
+
 sudo port install php<?= $versionNoDot; ?>
+
 
 # Switch Active PHP <?= $version; ?> version
 sudo port select php php<?= $versionNoDot; ?>
