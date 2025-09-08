1db0b73233e76259aab67e72b94960603efb32f2
alikon <optimus4joomla@gmail.com>
2025-09-08 16:37:35 +0200

    add joomla

--- downloads-get-instructions.php
+++ downloads-get-instructions.php
@@ -26,7 +26,7 @@ if ($options['os'] === 'osx' || $options['os'] === 'windows') {
     }
 }
 
-if (in_array($options['usage'], ['fw-drupal', 'fw-laravel', 'fw-symfony', 'fw-wordpress'])) {
+if (in_array($options['usage'], ['fw-drupal', 'fw-laravel', 'fw-symfony', 'fw-wordpress', 'fw-joomla'])) {
     $file = "{$options['usage']}";
     $options['os'] = null;
 }
