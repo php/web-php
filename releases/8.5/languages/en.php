26141911a34f3d65a89d43d67679a157b6b39663
Sergey Panteleev <sergey@php.net>
2025-09-25 19:18:38 +0300

    Add PHP 8.5 release page

---
+++ releases/8.5/languages/en.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'common_header' => 'PHP 8.5 is a major update of the PHP language. It contains many new features, such as Pipe operator, Final Property Promotion, Attributes on Constants, performance improvements, bug fixes, and general cleanup.',
+    'documentation' => 'Doc',
+    'main_title' => 'Released!',
+    'main_subtitle' => 'PHP 8.5 is a major update of the PHP language.<br class="display-none-md"> It contains many new features, such as Pipe operator, Final Property Promotion, Attributes on Constants, performance improvements, bug fixes, and general cleanup.',
+    'upgrade_now' => 'Upgrade to PHP 8.5 now!',
+
+    'pipe_operator_title' => 'Pipe operator',
+    'url_parsing_api_title' => 'Add RFC 3986 and WHATWG URL compliant API',
+    'url_parsing_api_description' => '<code>Uri\UriException</code>, <code>Uri\InvalidUriException</code>, <code>Uri\UriComparisonMode</code>,
+    <code>Uri\Rfc3986\Uri</code>, <code>Uri\WhatWg\InvalidUrlException</code>, <code>Uri\WhatWg\UrlValidationErrorType</code>, <code>Uri\WhatWg\UrlValidationError</code>, and <code>Uri\WhatWg\Url</code> are now available.',
+
+    'new_classes_title' => 'New Classes, Interfaces, and Functions',
+    'new_core_attributes' => '<code>#[\NoDiscard]</code> and <code>#[\DelayedTargetValidation]</code> attributes',
+
+    'bc_title' => 'Deprecations and backward compatibility breaks',
+
+    'footer_title' => 'Better performance, better syntax, improved type safety.',
+    'footer_description' => '<p>For source downloads of PHP 8.5 please visit the <a href="/downloads">downloads</a> page. Windows binaries can be found on the <a href="https://windows.php.net/download">PHP for Windows</a> site. The list of changes is recorded in the <a href="/ChangeLog-8.php#PHP_8_5">ChangeLog</a>.</p>
+        <p>The <a href="/manual/en/migration85.php">migration guide</a> is available in the PHP Manual. Please consult it for a detailed list of new features and backward-incompatible changes.</p>',
+];
