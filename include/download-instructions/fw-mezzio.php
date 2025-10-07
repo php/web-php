9e8d62f4acc782a802a9604a5476ef81f4b77e39
arhimede <julian@dotkernel.com>
2025-10-07 14:45:30 +0300

    Add AlmaLinux WSL2 and generic to downloads page
    Signed-off-by: arhimede <julian@dotkernel.com>

--- include/download-instructions/fw-mezzio.php
+++
@@ -1,6 +0,0 @@
-<p>
-Instructions for installing PHP for Mezzio development can be found on:
-</p>
-<p>
-» <a href='https://docs.mezzio.dev/mezzio/v3/getting-started/quick-start/'>https://docs.mezzio.dev/mezzio/v3/getting-started/quick-start/</a>
-</p>
