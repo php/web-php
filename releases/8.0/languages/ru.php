7804a3d66ca941bf0a709cb4ae559add70ac242b
Theodore Brown <theodorejb@outlook.com>
2025-10-19 13:59:59 -0500

    Share common markup for PHP 8.0 release page translations
    Adds a `release.inc` file for PHP 8.0 like the other release pages have, in order to avoid duplicating all the markup for every translation (making it much easier to maintain consistent content). Apart from the following exceptions, no content or translations have been changed:
    
    Content and layout improvements:
    
    * Added IDs to headings in section above footer for direct linking.
    * Added `<code>` tag around operators and keywords for better readability.
    * Moved Just-In-Time Compilation section to a half-width column rather than full-width. This is more consistent with the other release pages, and makes it easier to read the description on large screens since the paragraph isn't so excessively wide.
    * Updated code examples to use 4-space indentation rather than 2-space, for consistency with all the other release pages.
    * Updated the "New Classes, Interfaces, and Functions" section to show the actual class name for `WeakMap` and `PhpToken` (which removed the need for translation there).
    * Moved both RFC links after the text for "Allow a trailing comma in parameter lists and closure use lists" for simplicity and consistency across translations.
    
    Translation-specific fixes and changes:
    
    * Fixed bug where header links in the French and Japanese translations unexpectedly switched to the English translation of the page (caused by incorrect `$_SERVER['BASE_PAGE']` value).
    * Spanish, Italian, and Turkish pages previously incorrectly linked to English documentation for some or all of the documentation links.
    * French translation previously had a different code example for attributes, which didn't really make sense (it showed the same `/api/posts/{id}` Route attribute, but above a User class rather than on a `get` method in a PostsController class).
    * Fixed incorrect Static Return Type RFC link in Portuguese translation.
    * Fixed a couple minor spacing bugs in Spanish translation.
    * Chinese page previously was missing translation for Documentation link in a couple places.
    * Fixed missing blank lines in a few Chinese translation code blocks.
    * Georgian translation previously changed strings/comments in the code examples for Union Types and Match Expression. If this is still wanted we could add translation strings to include in these code blocks, but I don't think it's worth it since the examples already have translated descriptions, and these code blocks weren't altered for any other languages.

---
+++ releases/8.0/languages/ru.php
@@ -0,0 +1,79 @@
+<?php
+
+return [
+    'common_header' => 'PHP 8.0 — большое обновление языка PHP. Оно содержит множество новых возможностей и оптимизаций, включая именованные аргументы, тип union, атрибуты, упрощённое определение свойств в конструкторе, выражение match, оператор nullsafe, JIT и улучшения в системе типов, обработке ошибок и консистентности.',
+    'documentation' => 'Документация',
+    'main_title' => 'доступен!',
+    'main_subtitle' => 'PHP 8.0 — большое обновление языка PHP.<br class="display-none-md"> Оно содержит множество новых возможностей и оптимизаций, включая именованные аргументы, union type, атрибуты, упрощённое определение свойств в конструкторе, выражение match, оператор nullsafe, JIT и улучшения в системе типов, обработке ошибок и консистентности.',
+    'upgrade_now' => 'Переходите на PHP 8!',
+
+    'named_arguments_title' => 'Именованные аргументы',
+    'named_arguments_description' => '<li>Указывайте только необходимые параметры, пропускайте необязательные.</li><li>Порядок аргументов не важен, аргументы самодокументируемы.</li>',
+    'attributes_title' => 'Атрибуты',
+    'attributes_description' => 'Вместо аннотаций PHPDoc вы можете использовать структурные метаданные с нативным синтаксисом PHP.',
+    'constructor_promotion_title' => 'Объявление свойств в конструкторе',
+    'constructor_promotion_description' => 'Меньше шаблонного кода для определения и инициализации свойств.',
+    'union_types_title' => 'Тип Union',
+    'union_types_description' => 'Вместо аннотаций PHPDoc для объединённых типов вы можете использовать объявления типа union, которые проверяются во время выполнения.',
+    'match_expression_title' => 'Выражение Match',
+    'match_expression_description' => '<p>Новое выражение <code>match</code> похоже на оператор <code>switch</code> со следующими особенностями:</p>
+        <ul>
+            <li>Match — это выражение, его результат может быть сохранён в переменной или возвращён.</li>
+            <li>Условия match поддерживают только однострочные выражения, для которых не требуется управляющая конструкция <code>break</code>.</li>
+            <li>Выражение match использует строгое сравнение.</li>
+        </ul>',
+
+    'nullsafe_operator_title' => 'Оператор Nullsafe',
+    'nullsafe_operator_description' => 'Вместо проверки на <code>null</code> вы можете использовать последовательность вызовов с новым оператором Nullsafe. Когда один из элементов в последовательности возвращает null, выполнение прерывается и вся последовательность возвращает <code>null</code>.',
+    'saner_string_number_comparisons_title' => 'Улучшенное сравнение строк и чисел',
+    'saner_string_number_comparisons_description' => 'При сравнении с числовой строкой PHP 8 использует сравнение чисел. В противном случае число преобразуется в строку и используется сравнение строк.',
+    'consistent_internal_function_type_errors_title' => 'Ошибки согласованности типов для встроенных функций',
+    'consistent_internal_function_type_errors_description' => 'Большинство внутренних функций теперь выбрасывают исключение <code>Error</code>, если при проверке параметра возникает ошибка.',
+
+    'jit_compilation_title' => 'Компиляция Just-In-Time',
+    'jit_compilation_description' => 'PHP 8 представляет два механизма JIT-компиляции. Трассировка JIT, наиболее перспективная из них, на синтетических бенчмарках показывает улучшение производительности примерно в 3 раза и в 1,5–2 раза на некоторых долго работающих приложениях. Стандартная производительность приложения находится на одном уровне с PHP 7.4.',
+    'jit_performance_title' => 'Относительный вклад JIT в производительность PHP 8',
+
+    'type_improvements_title' => 'Улучшения в системе типов и обработке ошибок',
+    'arithmetic_operator_type_checks' => 'Более строгие проверки типов для арифметических/побитовых операторов',
+    'abstract_trait_method_validation' => 'Проверка методов абстрактных трейтов',
+    'magic_method_signatures' => 'Правильные сигнатуры магических методов',
+    'engine_warnings' => 'Реклассификация предупреждений движка',
+    'lsp_errors' => 'Фатальная ошибка при несовместимости сигнатур методов',
+    'at_operator_no_longer_silences_fatal_errors' => 'Оператор <code>@</code> больше не подавляет фатальные ошибки.',
+    'inheritance_private_methods' => 'Наследование с private методами',
+    'mixed_type' => 'Новый тип <code>mixed</code>',
+    'static_return_type' => 'Возвращаемый тип <code>static</code>',
+    'internal_function_types' => 'Типы для стандартных функций',
+    'email_thread' => 'E-mail Тема',
+    'opaque_objects_instead_of_resources' => 'Непрозрачные объекты вместо ресурсов для
+        <a href="https://php.watch/versions/8.0/resource-CurlHandle">Curl</a>,
+        <a href="https://php.watch/versions/8.0/gdimage">Gd</a>,
+        <a href="https://php.watch/versions/8.0/sockets-sockets-addressinfo">Sockets</a>,
+        <a href="https://php.watch/versions/8.0/OpenSSL-resource">OpenSSL</a>,
+        <a href="https://php.watch/versions/8.0/xmlwriter-resource">XMLWriter</a>, e
+        <a href="https://php.watch/versions/8.0/xmlwriter-resource">XML</a>
+        расширения',
+
+    'other_improvements_title' => 'Прочие улучшения синтаксиса',
+    'allow_trailing_comma' => 'Разрешена запятая в конце списка параметров и в списке use замыканий',
+    'non_capturing_catches' => 'Блок catch без указания переменной',
+    'variable_syntax_tweaks' => 'Изменения синтаксиса переменных',
+    'namespaced_names_as_token' => 'Имена в пространстве имен рассматриваются как единый токен',
+    'throw_expression' => 'Выражение <code>throw</code>',
+    'class_name_literal_on_object' => 'Добавление <code>::class</code> для объектов',
+
+    'new_classes_title' => 'Новые классы, интерфейсы и функции',
+    'new_dom_apis' => 'Новые API для обходения и обработки DOM',
+
+    'footer_title' => 'Выше производительность, лучше синтаксис, надежнее система типов.',
+    'footer_description' => '<p>
+            Для загрузки исходного кода PHP 8 посетите страницу <a href="http://www.php.net/downloads">downloads</a>.
+            Бинарные файлы Windows находятся на сайте <a href="http://windows.php.net/download">PHP для Windows</a>.
+            Список изменений представлен в <a href="http://www.php.net/ChangeLog-8.php">ChangeLog</a>.
+        </p>
+        <p>
+            <a href="/manual/ru/migration80.php">Руководство по миграции</a> доступно в разделе документации. Пожалуйста,
+            изучите его для получения подробного списка новых возможностей и обратно несовместимых изменений.
+        </p>',
+];
