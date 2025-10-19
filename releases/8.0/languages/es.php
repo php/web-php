e6ef5af728d57dd0aedcf0009ff876dbd8ce6f7b
Theodore Brown <theodorejb@outlook.com>
2025-10-19 13:59:59 -0500

    Share common markup for PHP 8.0 release page translations
    Adds a `release.inc` file for PHP 8.0 like the other release pages have, in order to avoid duplicating all the markup for every translation (making it much easier to maintain consistent content). Apart from the following exceptions, no content or translations have been changed:
    
    Consistency improvements:
    
    * Added IDs to headings in section above footer for direct linking (like the other release pages have).
    * Added `<code>` tag around operators and keywords for better readability (the other release pages do this).
    * Updated code examples to consistently use 4-space indentation (previously the attributes code example used four spaces and others used two spaces).
    
    Translation-specific fixes:
    
    * Fixed bug where heading links in the French and Japanese translations unexpectedly switched to the English translation of the page (caused by incorrect `$_SERVER['BASE_PAGE']` value).
    * Spanish, Italian, and Turkish pages previously incorrectly linked to English documentation for some or all of the documentation links.
    * French translation previously had a different code example for attributes, which didn't really make sense (it showed the same `/api/posts/{id}` Route attribute, but above a User class rather than on a `get` method in a PostsController class).
    * Fixed incorrect Static Return Type RFC link in Portuguese translation.
    * Fixed a couple minor spacing bugs in Spanish translation.
    * Chinese page previously was missing translation for Documentation link in a couple places.
    * Fixed missing blank lines in a few Chinese translation code blocks.
    * Fixed inconsistent strings in Georgian match expression example.

---
+++ releases/8.0/languages/es.php
@@ -0,0 +1,81 @@
+<?php
+
+return [
+    'common_header' => 'PHP 8.0 es una actualización importante del lenguaje php que contiene nuevos recursos y optimizaciones incluyendo argumentos nombrados, tipos de uniones, atributos, promoción de propiedades constructivas, expresiones de equivalencia, operador nullsafe, JIT (traducción dinámica) y también mejoras en el sistema de tipos, manejo de errores y consistencia en general.',
+    'documentation' => 'Doc',
+    'main_title' => 'Released!',
+    'main_subtitle' => 'PHP 8.0 es una actualización importante del lenguaje PHP que contiene nuevos recursos y optimizaciones incluyendo argumentos nombrados, tipos de uniones, atributos, promoción de propiedades constructivas, expresiones match, operador nullsafe, JIT (traducción dinámica) y también mejoras en el sistema de tipos, manejo de errores y consistencia en general.',
+    'upgrade_now' => 'Actualizate a PHP 8!',
+
+    'named_arguments_title' => 'Argumentos nombrados',
+    'named_arguments_description' => '<li>Solamente especifica los parámetros requeridos, omite los opcionales.</li><li>Los argumentos son independientes del orden y se documentan automáticamente.</li>',
+    'attributes_title' => 'Atributos',
+    'attributes_description' => 'En vez de anotaciones en PHPDoc, puedes usar metadatos estructurados con la sintaxis nativa de PHP.',
+    'constructor_promotion_title' => 'Promoción de propiedades constructivas',
+    'constructor_promotion_description' => 'Menos código repetitivo para definir e inicializar una propiedad.',
+    'union_types_title' => 'Tipos de unión',
+    'union_types_description' => 'En vez de anotaciones en PHPDoc para combinar tipos, puedes usar una declaración de tipo unión nativa que será validada en el momento de ejecución.',
+    'match_expression_title' => 'Expresiones match',
+    'match_expression_description' => '<p>Las nuevas expresiones <code>match</code> son similares a <code>switch</code> y tienen las siguientes características: </p>
+        <ul>
+            <li>Match es una expresión; esto quiere decir que pueden ser almacenadas como variables o devueltas.</li>
+            <li>Match soporta expresiones de una línea y no necesitan romper declarar un <code>break</code>.</li>
+            <li>Match hace comparaciones estrictas.</li>
+        </ul>',
+
+    'nullsafe_operator_title' => 'Operador Nullsafe',
+    'nullsafe_operator_description' => 'En vez de verificar condiciones nulas, tu puedes utilizar una cadena de llamadas con el nuevo operador nullsafe. Cuando la evaluación de un elemento falla, la ejecución de la entire cadena es abortada y la cadena entera es evaluada como <code>null</code>.',
+    'saner_string_number_comparisons_title' => 'Comparaciones inteligentes entre “strings” y números',
+    'saner_string_number_comparisons_description' => 'Cuando comparas con un “string” numérico, PHP8 usa comparación numérica o de otro caso convierte el número a un "string" y asi los compara.',
+    'consistent_internal_function_type_errors_title' => 'Errores consistentes para funciones internas.',
+    'consistent_internal_function_type_errors_description' => 'La mayoría de las funciones internas ahora proveen un <code>Error</code> de excepción si el parámetro no es validado.',
+
+    'jit_compilation_title' => 'JIT (traducciones dinámicas)',
+    'jit_compilation_description' => 'PHP8 introduce 2 motores de compilación JIT. Transit JIT, es el más prometedor de los  dos y performa 3 veces mejor en benchmarks sintéticos y 1.5-2 mejor en algunas aplicaciones específicas a largo plazo. Performancia de aplicaciones típicas es a la par de las de PHP7.4',
+    'jit_performance_title' => 'JIT contribuciones al funcionamiento relativo de PHP8',
+
+    'type_improvements_title' => 'Mejorias en los tipos de sistemas y manejo de errores',
+    'arithmetic_operator_type_checks' => 'Verificaciones estrictas de operadores aritméticos/bitwise.',
+    'abstract_trait_method_validation' => 'Validación de métodos con características abstractas',
+    'magic_method_signatures' => 'Firmas correctas de métodos mágicos',
+    'engine_warnings' => 'Reclacificamiento de errores fatales',
+    'lsp_errors' => 'Errores fatales incompatibles con el método de firma',
+    'at_operator_no_longer_silences_fatal_errors' => 'El operador <code>@</code> no omitirá errores fatales.',
+    'inheritance_private_methods' => 'Herencia con métodos privados',
+    'mixed_type' => 'Tipo <code>mixed</code>',
+    'static_return_type' => 'Tipo retorno <code>static</code>',
+    'internal_function_types' => 'Tipos para funciones internas',
+    'email_thread' => 'Email thread',
+    'opaque_objects_instead_of_resources' => 'Objetos opacos en ves de recursos para
+        <a href="https://php.watch/versions/8.0/resource-CurlHandle">Curl</a>,
+        <a href="https://php.watch/versions/8.0/gdimage">Gd</a>,
+        <a href="https://php.watch/versions/8.0/sockets-sockets-addressinfo">Sockets</a>,
+        <a href="https://php.watch/versions/8.0/OpenSSL-resource">OpenSSL</a>,
+        <a href="https://php.watch/versions/8.0/xmlwriter-resource">XMLWriter</a>,
+        y <a href="https://php.watch/versions/8.0/xmlwriter-resource">XML</a> extensiones',
+
+    'other_improvements_title' => 'Otros ajustes y mejoras del sintax',
+    'allow_trailing_comma' => 'Permitir una coma al final de una lista de parámetros <a href="https://wiki.php.net/rfc/trailing_comma_in_parameter_list">RFC</a>
+        y lista de use en closures <a href="https://wiki.php.net/rfc/trailing_comma_in_closure_use_list">RFC</a>',
+    'non_capturing_catches' => 'Catches que no capturan',
+    'variable_syntax_tweaks' => 'Ajustes al syntax variable',
+    'namespaced_names_as_token' => 'Tratamiento de nombres de namespace como tokens únicos',
+    'throw_expression' => '<code>throw</code> es ahora una expresión',
+    'class_name_literal_on_object' => 'Permitir <code>::class</code> on objects',
+
+    'new_classes_title' => 'Nuevas clases, interfaces y funciones',
+    'weak_map_class' => '<a href="https://wiki.php.net/rfc/weak_maps">Weak Map</a> clase',
+    'token_as_object' => '<a href="https://wiki.php.net/rfc/token_as_object">token_get_all()</a> Implementación de objeto',
+    'new_dom_apis' => 'New DOM Traversal and Manipulation APIs',
+
+    'footer_title' => 'Mejor performancia, mejor syntax, aumentada seguridad de tipos.',
+    'footer_description' => '<p>
+            Para bajar el código fuente de PHP8 visita la página <a href="http://www.php.net/downloads">downloads</a>.
+            Código binario para windows lo puedes encontrar en la página <a href="http://windows.php.net/download">PHP for Windows</a>.
+            La lista de cambios está disponible en la página <a href="http://www.php.net/ChangeLog-8.php">ChangeLog</a>.
+        </p>
+        <p>
+            La <a href="/manual/en/migration80.php">guía de migración</a> está disponible en el manual de PHP.
+            Por favor consultala para una lista detallada de alteraciones cambios y compatibilidad.
+        </p>',
+];
---
+++ releases/8.0/languages/ka.php
@@ -0,0 +1,84 @@
+<?php
+
+return [
+    'common_header' => 'PHP 8.0 — PHP ენის დიდი განახლება. ის შეიცავს ბევრ ახალ შესაძლებლობას და ოპტიმიზაციებს, მათ შორის დასახელებული არგუმენტები, union type, ატრიბუტები, თვისებების გამარტივებული განსაზღვრა კონსტრუქტორში, გამოსახულება match, ოპერატორი nullsafe, JIT და გაუმჯობესებები ტიპის სისტემაში, შეცდომების დამუშავება და თანმიმდევრულობა.',
+    'documentation' => 'დოკუმენტაცია',
+    'main_title' => 'რელიზი!',
+    'main_subtitle' => 'PHP 8.0 — PHP ენის დიდი განახლება.<br class="display-none-md"> ის შეიცავს ბევრ ახალ შესაძლებლობას და ოპტიმიზაციებს, მათ შორის დასახელებული არგუმენტები, union type, ატრიბუტები, თვისებების გამარტივებული განსაზღვრა კონსტრუქტორში, გამოსახულება match, ოპერატორი nullsafe, JIT და გაუმჯობესებები ტიპის სისტემაში, შეცდომების დამუშავება და თანმიმდევრულობა.',
+    'upgrade_now' => 'გადადით PHP 8-ზე!',
+
+    'named_arguments_title' => 'დასახელებული არგუმენტები',
+    'named_arguments_description' => '<li>მიუთითეთ მხოლოდ საჭირო პარამეტრები, გამოტოვეთ არასავალდებულო.</li><li>არგუმენტების თანმიმდევრობა არ არის მნიშვნელოვანი, არგუმენტები თვითდოკუმენტირებადია.</li>',
+    'attributes_title' => 'Attributes',
+    'attributes_description' => 'PHPDoc ანოტაციების ნაცვლად, შეგიძლიათ გამოიყენოთ სტრუქტურული მეტამონაცემები ნატიური PHP სინტაქსით.',
+    'constructor_promotion_title' => 'თვისებების განახლება კონსტრუქტორში',
+    'constructor_promotion_description' => 'ნაკლები შაბლონური კოდი თვისებების განსაზღვრისა და ინიციალიზაციისთვის.',
+    'union_types_title' => 'Union types',
+    'union_types_description' => 'PHPDoc ანოტაციების ნაცვლად, გაერთიანებული ტიპებისთვის  შეგიძლიათ გამოიყენოთ განცხადება union type, რომლებიც მოწმდება შესრულების დროს.',
+    'ok' => 'შეცდომები არაა',
+    'oh_no' => 'ოოო არა!',
+    'this_is_expected' => 'ის, რასაც მე ველოდი',
+    'match_expression_title' => 'გამოსახულება Match',
+    'match_expression_description' => '<p>ახალი გამოსახულება <code>match</code>, <code>switch</code> ოპერატორის მსგავსია შემდეგი მახასიათებლებით:</p>
+        <ul>
+            <li>Match — ეს არის გამოსახულება, მისი შედეგი შეიძლება შენახული იყოს ცვლადში ან დაბრუნდეს.</li>
+            <li>პირობა <code>match</code>  მხარს უჭერერს მხოლოდ ერთსტრიქონიან გამოსახულებებს, რომლებიც არ საჭიროებენ <code>break</code> კონტროლის კონსტრუქციას.</li>
+            <li>გამოსახულება <code>match</code> იყენებს მკაცრ შედარებას.</li>
+        </ul>',
+
+    'nullsafe_operator_title' => 'ოპერატორი Nullsafe',
+    'nullsafe_operator_description' => 'null-ის შემოწმების ნაცვლად, შეგიძლიათ გამოიყენოთ გამოძახების თანმიმდევრობა ახალ Nullsafe ოპერატორით. როდესაც ერთ-ერთი ელემენტი თანმიმდევრობაში აბრუნებს null-ს, შესრულება ჩერდება და მთელი თანმიმდევრობა აბრუნებს null-ს.',
+    'saner_string_number_comparisons_title' => 'სტრიქონებისა და რიცხვების გაუმჯობესებული შედარება',
+    'saner_string_number_comparisons_description' => 'PHP 8 რიცხვითი სტრიქონის შედარებისას იყენებს რიცხვების შედარებას. წინააღმდეგ შემთხვევაში, რიცხვი გარდაიქმნება სტრიქონად და გამოიყენება სტრიქონების შედარება.',
+    'consistent_internal_function_type_errors_title' => 'ტიპების თანმიმდევრულობის შეცდომები ჩაშენებული ფუნქციებისთვის',
+    'consistent_internal_function_type_errors_description' => 'შიდა ფუნქციების უმეტესობა უკვე გამორიცხავს <code>Error</code> გამონაკლისს, თუ შეცდომა მოხდა პარამეტრის შემოწმებისას.',
+
+    'jit_compilation_title' => 'კომპილაცია Just-In-Time',
+    'jit_compilation_description' => 'PHP 8 წარმოგიდგენთ JIT-კომპილაციის ორ მექანიზმს. JIT ტრასირება, მათგან ყველაზე პერსპექტიულია, სინთეზურ ბენჩმარკზე აჩვენებს მუშაობის გაუმჯობესებას დაახლოებით 3-ჯერ და 1.5-2-ჯერ ზოგიერთ დიდ ხანს მომუშავე აპლიკაციებში. აპლიკაციის სტანდარტული წარმადობა ერთ და იგივე დონეზეა PHP 7.4-თან.',
+    'jit_performance_title' => 'JIT-ის შედარებითი წვლილი PHP 8-ის წარმადობაში',
+
+    'type_improvements_title' => 'გაუმჯობესებები ტიპის სისტემაში და შეცდომების დამუშავება',
+    'arithmetic_operator_type_checks' => 'ტიპის უფრო მკაცრი შემოწმება არითმეტიკული/ბიტიური ოპერატორებისთვის',
+    'abstract_trait_method_validation' => 'აბსტრაქტული თვისებების მეთოდების შემოწმება',
+    'magic_method_signatures' => 'ჯადოსნური მეთოდების სწორი სიგნატურები',
+    'engine_warnings' => 'ძრავის გაფრთხილებების ხელახალი კლასიფიკაცია',
+    'lsp_errors' => 'ფატალური შეცდომა, როდესაც მეთოდის სიგნატურა შეუთავსებელია',
+    'at_operator_no_longer_silences_fatal_errors' => '<code>@</code> ოპერატორი აღარ აჩუმებს ფატალურ შეცდომებს.',
+    'inheritance_private_methods' => 'მემკვიდრეობა private მეთოდებთან',
+    'mixed_type' => 'ახალი ტიპი <code>mixed</code>',
+    'static_return_type' => 'დაბრუნების ტიპი <code>static</code>',
+    'internal_function_types' => 'ტიპები სტანდარტული ფუნქციებისთვის',
+    'email_thread' => 'Email თემა',
+    'opaque_objects_instead_of_resources' => 'გაუმჭვირვალე ობიექტები რესურსების ნაცვლად
+        <a href="https://php.watch/versions/8.0/resource-CurlHandle">Curl</a>,
+        <a href="https://php.watch/versions/8.0/gdimage">Gd</a>,
+        <a href="https://php.watch/versions/8.0/sockets-sockets-addressinfo">Sockets</a>,
+        <a href="https://php.watch/versions/8.0/OpenSSL-resource">OpenSSL</a>,
+        <a href="https://php.watch/versions/8.0/xmlwriter-resource">XMLWriter</a>, and
+        <a href="https://php.watch/versions/8.0/xmlwriter-resource">XML</a>
+        გაფართოებებისთვის',
+
+    'other_improvements_title' => 'სინტაქსის სხვა გაუმჯობესება',
+    'allow_trailing_comma' => 'მძიმე დაშვებულია პარამეტრების სიის ბოლოს <a href="https://wiki.php.net/rfc/trailing_comma_in_parameter_list">RFC</a>
+        და use დამოკლების სიაში <a href="https://wiki.php.net/rfc/trailing_comma_in_closure_use_list">RFC</a>',
+    'non_capturing_catches' => 'ბლოკი catch ცვლადის მითითების გარეშე',
+    'variable_syntax_tweaks' => 'ცვლადის სინტაქსის ცვლილება',
+    'namespaced_names_as_token' => 'სახელების სივრცეში სახელები განიხილება, როგორც ერთიამნი ტოკენი',
+    'throw_expression' => 'გამოსახულება <code>throw</code>',
+    'class_name_literal_on_object' => 'დამატება <code>::class</code> ობიექტებისთვის',
+
+    'new_classes_title' => 'ახალი კლასები, ინტერფეისები და ფუნქციები',
+    'token_as_object' => '<a href="https://wiki.php.net/rfc/token_as_object">token_get_all()</a> ობიექტზე-ორიენტირებული ფუნქცია',
+    'new_dom_apis' => 'ახალი API-ები DOM-ის გადასასვლელად და დასამუშავებლად',
+
+    'footer_title' => 'უკეთესი წარმადობა, უკეთესი სინტაქსი, უფრო საიმედო ტიპების სისტემა.',
+    'footer_description' => '<p>
+            PHP 8 წყაროს კოდის ჩამოსატვირთად ეწვიეთ <a href="http://www.php.net/downloads">ჩამოტვირთვის</a> გვერდს.
+            Windows-ის ბინარული ფაილები განთავსებულია საიტზე <a href="http://windows.php.net/download">PHP Windows-თვის</a>.
+            ცვლილებების სია წარმოდგენილია <a href="http://www.php.net/ChangeLog-8.php">ChangeLog-ში</a>.
+        </p>
+        <p>
+            <a href="/manual/en/migration80.php">მიგრაციის გზამკვლევი</a> ხელმისაწვდომია დოკუმენტაციის განყოფილებაში. გთხოვთ,
+            შეისწავლოთ იგი ახალი ფუნქციების დეტალური ჩამონათვალის მისაღებად და უკუ შეუთავსებელი ცვლილებებისთვის.
+        </p>',
+];
