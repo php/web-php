658fafec3eefa3f4d10846fc3ca5c887879fa7a1
AllenJB <AllenJB@users.noreply.github.com>
2025-11-04 17:17:06 +0000

    Update search to use new pre-combined indexes + local docs support (#1586)
    * Update search.js to use new pre-combined indexes; Support for running search on local docs
    
    * Update search.js to use new pre-combined indexes; Support for running search on local docs

--- js/search-index.php
+++ js/search-index.php
@@ -11,28 +11,9 @@ if (!isset(Languages::ACTIVE_ONLINE_LANGUAGES[$lang])) {
     header("Location: http://php.net");
 }
 
-/*
-$types = array(
-    "phpdoc:varentry",
-    "refentry",
-    "phpdoc:exceptionref",
-    "phpdoc:classref",
-    "section",
-    "chapter",
-    "book",
-    "reference",
-    "set",
-    "appendix",
-    "article",
-);
- */
+$combinedIndex = $_SERVER["DOCUMENT_ROOT"] . "/manual/$lang/search-combined.json";
+$tsstring = gmdate("D, d M Y H:i:s ", filemtime($combinedIndex)) . "GMT";
 
-$indexfile = $_SERVER["DOCUMENT_ROOT"] . "/manual/$lang/search-index.json";
-$descfile = $_SERVER["DOCUMENT_ROOT"] . "/manual/$lang/search-description.json";
-
-/* {{{ Cache this */
-$time = max(filemtime($indexfile), filemtime($descfile));
-$tsstring = gmdate("D, d M Y H:i:s ", $time) . "GMT";
 if (isset($_SERVER["HTTP_IF_MODIFIED_SINCE"]) &&
     ($_SERVER["HTTP_IF_MODIFIED_SINCE"] == $tsstring)) {
     header("HTTP/1.1 304 Not Modified");
@@ -41,26 +22,4 @@ if (isset($_SERVER["HTTP_IF_MODIFIED_SINCE"]) &&
 
 header("Last-Modified: " . $tsstring);
 header("Content-Type: application/javascript");
-/* }}} */
-
-$s = file_get_contents($indexfile);
-$js = json_decode($s, true);
-
-$index = [];
-foreach ($js as $item) {
-    if ($item[0]) {
-        /* key: ID/filename, 0=>*/
-        $index[$item[1]] = [$item[0], "", $item[2]];
-    }
-}
-
-$s = file_get_contents($descfile);
-$js = json_decode($s, true);
-
-foreach ($js as $k => $item) {
-    if ($item && isset($index[$k])) {
-        $index[$k][1] = $item;
-    }
-}
-
-echo json_encode($index);
+readfile($combinedIndex);
