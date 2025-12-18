0fa26a1db82758f554f376de1fdc48c2e4d17b33
Volker Dusch <edorian@php.net>
2025-12-18 15:43:22 +0100

    Announce PHP 8.5.1

---
+++ releases/8_5_1.php
@@ -0,0 +1,16 @@
+<?php
+$_SERVER['BASE_PAGE'] = 'releases/8_5_1.php';
+include_once __DIR__ . '/../include/prepend.inc';
+site_header('PHP 8.5.1 Release Announcement');
+?>
+<h1>PHP 8.5.1 Release Announcement</h1>
+
+<p>The PHP development team announces the immediate availability of PHP 8.5.1. This is a security release.</p>
+
+<p>All PHP 8.5 users are encouraged to upgrade to this version.</p>
+
+<p>For source downloads of PHP 8.5.1 please visit our <a href="https://www.php.net/downloads.php">downloads page</a>,
+Windows source and binaries can be found on <a href="https://windows.php.net/download/">windows.php.net/download/</a>.
+The list of changes is recorded in the <a href="https://www.php.net/ChangeLog-8.php#8.5.1">ChangeLog</a>.
+</p>
+<?php site_footer();
