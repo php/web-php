0c8818e22fbd337692d6db4c9d4b2f2a95467906
Remi Collet <remi@remirepo.net>
2025-12-19 15:19:39 +0100

    add instructions for fedora/redhat and multiple versions

--- include/download-instructions/linux-fedora-cli-community.php
+++ include/download-instructions/linux-fedora-cli-community.php
@@ -5,9 +5,15 @@ On the command line, run the following commands:
 # Add the Remi's RPM repository.
 sudo dnf install -y dnf-plugins-core
 sudo dnf install -y https://rpms.remirepo.net/fedora/remi-release-$(rpm -E %fedora).rpm
+<?php if ($multiversion) { ?>
+
+# Install PHP (multiple versions).
+sudo dnf install -y php<?= $versionNoDot; ?>
+<?php } else { ?>
 sudo dnf module reset php -y
 sudo dnf module enable php:remi-<?= $version; ?> -y
 
-# Install PHP.
+# Install PHP (single/default version).
 sudo dnf install -y php
+<?php } ?>
 </code></pre>
