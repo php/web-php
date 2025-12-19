2fdb205aee5c16c709939dedd0223ed9726d1569
Remi Collet <remi@remirepo.net>
2025-12-19 15:19:39 +0100

    add instructions for fedora/redhat and multiple versions

--- include/download-instructions/linux-redhat-cli-community.php
+++ include/download-instructions/linux-redhat-cli-community.php
@@ -7,9 +7,15 @@ sudo subscription-manager repos --enable codeready-builder-for-rhel-$(rpm -E %rh
 sudo dnf install -y dnf-plugins-core
 sudo dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-$(rpm -E %rhel).noarch.rpm
 sudo dnf install -y https://rpms.remirepo.net/enterprise/remi-release-$(rpm -E %rhel).rpm
+<?php if ($multiversion) { ?>
+
+# Install PHP (multiple versions).
+sudo dnf install -y php<?= $versionNoDot; ?>
+<?php } else { ?>
 sudo dnf module reset php -y
 sudo dnf module enable php:remi-<?= $version; ?> -y
 
-# Install PHP.
+# Install PHP (single/default version).
 sudo dnf install -y php
+<?php } ?>
 </code></pre>
