8f195515459e268779428a8690c87ab8f9c0b68d
Ben Ramsey <ramsey@php.net>
2025-12-19 11:17:02 -0600

    Announce PHP 8.1.34

---
+++ releases/8_1_34.php
@@ -0,0 +1,16 @@
+<?php
+$_SERVER['BASE_PAGE'] = 'releases/8_1_34.php';
+include_once __DIR__ . '/../include/prepend.inc';
+site_header('PHP 8.1.34 Release Announcement');
+?>
+<h1>PHP 8.1.34 Release Announcement</h1>
+
+<p>The PHP development team announces the immediate availability of PHP 8.1.34. This is a security release.</p>
+
+<p>All PHP 8.1 users are encouraged to upgrade to this version.</p>
+
+<p>For source downloads of PHP 8.1.34 please visit our <a href="https://www.php.net/downloads.php">downloads page</a>,
+Windows source and binaries can also be found <a href="https://www.php.net/downloads.php?os=windows&amp;version=8.1">there</a>.
+The list of changes is recorded in the <a href="https://www.php.net/ChangeLog-8.php#8.1.34">ChangeLog</a>.
+</p>
+<?php site_footer();
