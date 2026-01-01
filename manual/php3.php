c82ddd7256f3ab2951efd9160f63d1ffb5a74590
Jordi Kroon <jkroon@onyourmarks.agency>
2026-01-01 23:34:51 +0100

    fix: update museum.php.net links to https

--- manual/php3.php
+++ manual/php3.php
@@ -29,7 +29,7 @@ site_header('PHP Version 3 Documentation');
 </p>
 
 <p>
- See the <a href="http://museum.php.net/">PHP Museum</a> for downloads, and
+ See the <a href="https://museum.php.net/">PHP Museum</a> for downloads, and
  also read the <a href="http://php.net/history">history</a> for further
  information about PHP 3.
 </p>
