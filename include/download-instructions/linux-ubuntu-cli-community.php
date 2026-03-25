92618aa3b304d44dac8a225293b4fe8b44da568e
Derick Rethans <github@derickrethans.nl>
2026-03-25 14:52:38 +0000

    Specifically call out telemetry

--- include/download-instructions/linux-ubuntu-cli-community.php
+++ include/download-instructions/linux-ubuntu-cli-community.php
@@ -5,6 +5,11 @@ On the command line, run the commands below.
 This installation method uses binaries provided by a third-party provider,
 adding patches to the PHP source code that the PHP project has no control over.
 </p>
+<p>
+Specifically, this provider includes
+<a href="https://salsa.debian.org/php-team/php/-/commit/aa12fa4540c8733ab6d68763b2107f39ec48fb37">a patch</a>
+that adds telemetry to the Apache 2 and PHP-FPM modules.
+</p>
 <pre><code class="language-bash line-numbers">
 # Add the ondrej/php repository.
 sudo apt update
