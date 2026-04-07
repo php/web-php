4030269b2e5d7ad33487aa21467d1e635c3cc454
Jordi Kroon <jkroon@onyourmarks.agency>
2026-04-07 19:14:02 +0200

    update Laravel installation link to use latest version

--- include/download-instructions/fw-laravel.php
+++ include/download-instructions/fw-laravel.php
@@ -2,5 +2,5 @@
 Instructions for installing PHP for Laravel development can be found on:
 </p>
 <p>
-» <a href='https://laravel.com/docs/12.x/installation#installing-php'>https://laravel.com/docs/12.x/installation#installing-php</a>
+» <a href='https://laravel.com/docs/installation#installing-php'>https://laravel.com/docs/installation#installing-php</a>
 </p>
