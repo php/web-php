a9f6aa443c3f398c18501ce8ef8c4d95a8dc6db1
Ben Ramsey <ramsey@php.net>
2026-04-15 07:52:45 -0500

    license-update: update website with new license info (#1762)
    RFC: https://wiki.php.net/rfc/php_license_update

--- license/contrib-guidelines-code.php
+++ license/contrib-guidelines-code.php
@@ -1,32 +1,31 @@
 <?php
 $_SERVER['BASE_PAGE'] = 'license/contrib-guidelines-code.php';
 include_once __DIR__ . '/../include/prepend.inc';
-site_header("License Information", ["current" => "help"]);
+site_header("Contributor Guidelines", ["current" => "help"]);
 ?>
 <h2>PHP Contributor Guidelines for Code Developers</h2>
 
 <p>
-  Before you complete the form to request a Git account, you must understand and accept the principles under which PHP
+  Before you contribute code to PHP, you must understand and accept the principles under which PHP
   itself is developed. These are summarised in the next paragraph.
 </p>
 <p>
-  Any code contributed to PHP must be contributed under the PHP license (or a compatible BSD-style license).
-  PHP will be redistributed under the PHP license (currently
-  <a href="http://www.php.net/license/3_01.txt">version 3.01</a>).
+  Any code contributed to PHP must be contributed under the Modified BSD License or other, compatible permissive license.
+  PHP is distributed under the <a href="/license/#bsd-3-clause">Modified BSD License</a>.
   This includes implementation code, test cases, utility scripts and tools - that is, all code associated
   with the PHP project. If you contribute code that isn't entirely your own
   (for example it may be partially derived from other Open Source software) you are asked to add a comment
   into the source code to indicate the origin and the license of the original code.
-  If you are unsure of the license you are asked to confirm with the owner of the code that they are
-  happy for it to be contributed to PHP under the PHP license.
+  If you are unsure of the license, you are asked to confirm with the owner of the code that they
+  grant permission for it to be contributed to PHP under the
+  terms of the Modified BSD License or other compatible license.
 </p>
 <p>
-  Specifically regarding GPL and LGPL source code:
+  Specifically regarding source code licensed under the GNU General Public License (GPL):
 </p>
 <ul>
-  <li>GPL or LGPL licensed code cannot be used as a basis for any derived work contributed to PHP.</li>
-  <li>Extensions which link  GPL'd libraries will not be accepted.</li>
-  <li>Extensions which link to LGPL libraries  will be strongly discouraged.</li>
+  <li>GPL-licensed code cannot be used as a basis for any work contributed to PHP.</li>
+  <li>Extensions linking to GPL-licensed libraries will not be accepted.</li>
 </ul>
 
 
