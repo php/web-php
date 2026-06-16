99ef60b3b901be1303c16c958b74884e08667633
Mark Randall <marandall@php.net>
2026-06-16 20:35:27 +0100

    Fix: Throw on realpath failures on ProjectGlobals (should never happen).

--- src/ProjectGlobals.php
+++ src/ProjectGlobals.php
@@ -9,11 +9,13 @@ class ProjectGlobals
 {
     public static function getProjectRoot(): string
     {
-        return realpath(__DIR__ . '/../');
+        return realpath(__DIR__ . '/../')
+            ?: throw new Error('Unable to locate project root');
     }
 
     public static function getPublicRoot(): string
     {
-        return realpath(__DIR__ . '/../public') ?: throw new Error('eh');
+        return realpath(__DIR__ . '/../public')
+            ?: throw new Error('Unable to locate public root');
     }
 }
