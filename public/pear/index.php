27cb0ccde9887fb98c64e2b2035e8b9dc370f2a6
Mark Randall <marandall@php.net>
2026-06-16 20:17:58 +0100

    Fix: Pear include path

--- public/pear/index.php
+++ public/pear/index.php
@@ -2,5 +2,5 @@
 
 // Simulate a /pear shortcut call (which will lead to a manual page)
 $_SERVER['REQUEST_URI'] = '/pear';
-require_once __DIR__ . '/../include/prepend.inc';
+require_once __DIR__ . '/../../include/prepend.inc';
 require_once __DIR__ . '/../error.php';
