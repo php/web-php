62279af8d8419f538929d130b506fc0510751033
Mark Randall <marandall@php.net>
2026-06-20 16:43:42 +0100

    Most of the front page

--- src/Navigation/NavItem.php
+++ src/Navigation/NavItem.php
@@ -4,11 +4,15 @@ namespace phpweb\Navigation;
 
 final readonly class NavItem
 {
+    /**
+     * @param list<self> $children
+     */
     public function __construct(
         public string $name,
         public string $href,
         public string $id,
         public ?string $image = null,
+        public array $children = [],
     ) {
     }
 }
