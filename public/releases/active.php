da0b49d37d5ce9fc7a788a973ad79c0251e615b9
Derick Rethans <github@derickrethans.nl>
2026-08-10 13:50:36 +0100

    Fixed root

--- public/releases/active.php
+++ public/releases/active.php
@@ -5,7 +5,7 @@ use phpweb\ProjectGlobals;
 $_SERVER['BASE_PAGE'] = 'releases/active.php';
 
 require_once __DIR__ . '/../../include/prepend.inc';
-require_once ProjectGlobals::getPublicRoot(). '/include/branches.inc';
+require_once ProjectGlobals::getProjectRoot(). '/include/branches.inc';
 
 header('Content-Type: application/json; charset=UTF-8');
 
