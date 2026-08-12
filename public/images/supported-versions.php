php/web-php

commit b24d3106d45cc39e34ae0055386a1ae387019a20

Create the public/ directory, and move all the files.
- Update the includes
- Swap out docroot to use a function

public/images/supported-versions.php
@@ -1,0 +1,167 @@
+<?php
+require_once __DIR__ . '/../include/prepend.inc';
+require_once __DIR__ . '/../include/branches.inc';
+
+// Sizing constants.
+$margin_left = 80;
+$margin_right = 50;
+$header_height = 24;
+$year_width = 120;
+$branch_height = 30;
+$footer_height = 24;
+
+function branches_to_show() {
+    // Basically: show all 5.3+ branches with EOL dates > min_date().
+    $branches = [];
+
+    // Flatten out the majors.
+    foreach (get_all_branches() as $major_branches) {
+        foreach ($major_branches as $branch => $version) {
+            if (version_compare($branch, '5.3', 'ge') && get_branch_security_eol_date($branch) > min_date()) {
+                $branches[$branch] = $version;
+            }
+        }
+    }
+
+    ksort($branches);
+    return $branches;
+}
+
+function min_date(): DateTime
+{
+    $now = new DateTime('January 1');
+    return $now->sub(new DateInterval('P3Y'));
+}
+
+function max_date(): DateTime
+{
+    $now = new DateTime('January 1');
+    return $now->add(new DateInterval('P5Y'));
+}
+
+function date_horiz_coord(DateTime $date) {
+    $diff = $date->diff(min_date());
+    if (!$diff->invert) {
+        return $GLOBALS['margin_left'];
+    }
+    return $GLOBALS['margin_left'] + ($diff->days / (365.24 / $GLOBALS['year_width']));
+}
+
+$branches = branches_to_show();
+$i = 0;
+foreach ($branches as $branch => $version) {
+    $branches[$branch]['top'] = $header_height + ($branch_height * $i++);
+}
+
+if (!isset($non_standalone)) {
+    header('Content-Type: image/svg+xml');
+    echo '<?xml version="1.0"?>';
+}
+
+$years = iterator_to_array(new DatePeriod(min_date(), new DateInterval('P1Y'), max_date()));
+$width = $margin_left + $margin_right + ((count($years) - 1) * $year_width);
+$height = $header_height + $footer_height + (count($branches) * $branch_height);
+?>
+<svg xmlns="http://www.w3.org/2000/svg" viewbox="0 0 <?php echo $width ?> <?php echo $height ?>" width="<?php echo $width ?>" height="<?php echo $height ?>">
+	<style type="text/css">
+		<![CDATA[
+			@import url(/fonts/Fira/fira.css);
+
+			text {
+				fill: #333;
+				font-family: var(--font-family-sans-serif);
+				font-size: <?php echo (2 / 3) * $header_height; ?>px;
+			}
+
+			g.eol rect,
+			.branches rect.eol {
+				fill: #f33;
+			}
+
+			g.eol text {
+				fill: white;
+			}
+
+			g.security rect,
+			.branches rect.security {
+				fill: #f93;
+			}
+
+			g.stable rect,
+			.branches rect.stable {
+				fill: #9c9;
+			}
+
+			.branch-labels text {
+				dominant-baseline: central;
+				text-anchor: middle;
+			}
+
+			.today line {
+				stroke: #333;
+				stroke-dasharray: 7,7;
+				stroke-width: 3px;
+			}
+
+			.today text {
+				fill: #333;
+				text-anchor: middle;
+			}
+
+			.years line {
+				stroke: black;
+			}
+
+			.years text {
+				text-anchor: middle;
+			}
+		]]>
+	</style>
+
+	<!-- Branch labels -->
+	<g class="branch-labels">
+		<?php foreach ($branches as $branch => $version): ?>
+			<g class="<?php echo get_branch_support_state($branch) ?>">
+				<rect x="0" y="<?php echo $version['top'] ?>" width="<?php echo 0.5 * $margin_left ?>" height="<?php echo $branch_height ?>" />
+				<text x="<?php echo 0.25 * $margin_left ?>" y="<?php echo $version['top'] + (0.5 * $branch_height) ?>">
+					<?php echo htmlspecialchars($branch) ?>
+				</text>
+			</g>
+		<?php endforeach ?>
+	</g>
+
+	<!-- Branch blocks -->
+	<g class="branches">
+		<?php foreach ($branches as $branch => $version): ?>
+			<?php
+            $x_release = date_horiz_coord(get_branch_release_date($branch));
+            $x_bug = date_horiz_coord(get_branch_bug_eol_date($branch));
+            $x_eol = date_horiz_coord(get_branch_security_eol_date($branch));
+            ?>
+			<rect class="stable" x="<?php echo $x_release ?>" y="<?php echo $version['top'] ?>" width="<?php echo $x_bug - $x_release ?>" height="<?php echo $branch_height ?>" />
+			<rect class="security" x="<?php echo $x_bug ?>" y="<?php echo $version['top'] ?>" width="<?php echo $x_eol - $x_bug ?>" height="<?php echo $branch_height ?>" />
+		<?php endforeach ?>
+	</g>
+
+	<!-- Year lines -->
+	<g class="years">
+		<?php foreach ($years as $date): ?>
+			<line x1="<?php echo date_horiz_coord($date) ?>" y1="<?php echo $header_height ?>" x2="<?php echo date_horiz_coord($date) ?>" y2="<?php echo $header_height + (count($branches) * $branch_height) ?>" />
+			<text x="<?php echo date_horiz_coord($date) ?>" y="<?php echo 0.8 * $header_height; ?>">
+				<?php echo $date->format('j M Y') ?>
+			</text>
+		<?php endforeach ?>
+	</g>
+
+	<!-- Today -->
+	<g class="today">
+		<?php
+        $now = new DateTime();
+        $x = date_horiz_coord($now);
+        ?>
+		<line x1="<?php echo $x ?>" y1="<?php echo $header_height ?>" x2="<?php echo $x ?>" y2="<?php echo $header_height + (count($branches) * $branch_height) ?>" />
+		<text x="<?php echo $x ?>" y="<?php echo $header_height + (count($branches) * $branch_height) + (0.8 * $footer_height) ?>">
+			<?php echo 'Today: ' . $now->format('j M Y') ?>
+		</text>
+	</g>
+</svg>

images/supported-versions.php
@@ -1,167 +1,0 @@
-<?php
-include_once __DIR__ . '/../include/prepend.inc';
-include_once __DIR__ . '/../include/branches.inc';
-
-// Sizing constants.
-$margin_left = 80;
-$margin_right = 50;
-$header_height = 24;
-$year_width = 120;
-$branch_height = 30;
-$footer_height = 24;
-
-function branches_to_show() {
-    // Basically: show all 5.3+ branches with EOL dates > min_date().
-    $branches = [];
-
-    // Flatten out the majors.
-    foreach (get_all_branches() as $major_branches) {
-        foreach ($major_branches as $branch => $version) {
-            if (version_compare($branch, '5.3', 'ge') && get_branch_security_eol_date($branch) > min_date()) {
-                $branches[$branch] = $version;
-            }
-        }
-    }
-
-    ksort($branches);
-    return $branches;
-}
-
-function min_date(): DateTime
-{
-    $now = new DateTime('January 1');
-    return $now->sub(new DateInterval('P3Y'));
-}
-
-function max_date(): DateTime
-{
-    $now = new DateTime('January 1');
-    return $now->add(new DateInterval('P5Y'));
-}
-
-function date_horiz_coord(DateTime $date) {
-    $diff = $date->diff(min_date());
-    if (!$diff->invert) {
-        return $GLOBALS['margin_left'];
-    }
-    return $GLOBALS['margin_left'] + ($diff->days / (365.24 / $GLOBALS['year_width']));
-}
-
-$branches = branches_to_show();
-$i = 0;
-foreach ($branches as $branch => $version) {
-    $branches[$branch]['top'] = $header_height + ($branch_height * $i++);
-}
-
-if (!isset($non_standalone)) {
-    header('Content-Type: image/svg+xml');
-    echo '<?xml version="1.0"?>';
-}
-
-$years = iterator_to_array(new DatePeriod(min_date(), new DateInterval('P1Y'), max_date()));
-$width = $margin_left + $margin_right + ((count($years) - 1) * $year_width);
-$height = $header_height + $footer_height + (count($branches) * $branch_height);
-?>
-<svg xmlns="http://www.w3.org/2000/svg" viewbox="0 0 <?php echo $width ?> <?php echo $height ?>" width="<?php echo $width ?>" height="<?php echo $height ?>">
-	<style type="text/css">
-		<![CDATA[
-			@import url(/fonts/Fira/fira.css);
-
-			text {
-				fill: #333;
-				font-family: var(--font-family-sans-serif);
-				font-size: <?php echo (2 / 3) * $header_height; ?>px;
-			}
-
-			g.eol rect,
-			.branches rect.eol {
-				fill: #f33;
-			}
-
-			g.eol text {
-				fill: white;
-			}
-
-			g.security rect,
-			.branches rect.security {
-				fill: #f93;
-			}
-
-			g.stable rect,
-			.branches rect.stable {
-				fill: #9c9;
-			}
-
-			.branch-labels text {
-				dominant-baseline: central;
-				text-anchor: middle;
-			}
-
-			.today line {
-				stroke: #333;
-				stroke-dasharray: 7,7;
-				stroke-width: 3px;
-			}
-
-			.today text {
-				fill: #333;
-				text-anchor: middle;
-			}
-
-			.years line {
-				stroke: black;
-			}
-
-			.years text {
-				text-anchor: middle;
-			}
-		]]>
-	</style>
-
-	<!-- Branch labels -->
-	<g class="branch-labels">
-		<?php foreach ($branches as $branch => $version): ?>
-			<g class="<?php echo get_branch_support_state($branch) ?>">
-				<rect x="0" y="<?php echo $version['top'] ?>" width="<?php echo 0.5 * $margin_left ?>" height="<?php echo $branch_height ?>" />
-				<text x="<?php echo 0.25 * $margin_left ?>" y="<?php echo $version['top'] + (0.5 * $branch_height) ?>">
-					<?php echo htmlspecialchars($branch) ?>
-				</text>
-			</g>
-		<?php endforeach ?>
-	</g>
-
-	<!-- Branch blocks -->
-	<g class="branches">
-		<?php foreach ($branches as $branch => $version): ?>
-			<?php
-            $x_release = date_horiz_coord(get_branch_release_date($branch));
-            $x_bug = date_horiz_coord(get_branch_bug_eol_date($branch));
-            $x_eol = date_horiz_coord(get_branch_security_eol_date($branch));
-            ?>
-			<rect class="stable" x="<?php echo $x_release ?>" y="<?php echo $version['top'] ?>" width="<?php echo $x_bug - $x_release ?>" height="<?php echo $branch_height ?>" />
-			<rect class="security" x="<?php echo $x_bug ?>" y="<?php echo $version['top'] ?>" width="<?php echo $x_eol - $x_bug ?>" height="<?php echo $branch_height ?>" />
-		<?php endforeach ?>
-	</g>
-
-	<!-- Year lines -->
-	<g class="years">
-		<?php foreach ($years as $date): ?>
-			<line x1="<?php echo date_horiz_coord($date) ?>" y1="<?php echo $header_height ?>" x2="<?php echo date_horiz_coord($date) ?>" y2="<?php echo $header_height + (count($branches) * $branch_height) ?>" />
-			<text x="<?php echo date_horiz_coord($date) ?>" y="<?php echo 0.8 * $header_height; ?>">
-				<?php echo $date->format('j M Y') ?>
-			</text>
-		<?php endforeach ?>
-	</g>
-
-	<!-- Today -->
-	<g class="today">
-		<?php
-        $now = new DateTime();
-        $x = date_horiz_coord($now);
-        ?>
-		<line x1="<?php echo $x ?>" y1="<?php echo $header_height ?>" x2="<?php echo $x ?>" y2="<?php echo $header_height + (count($branches) * $branch_height) ?>" />
-		<text x="<?php echo $x ?>" y="<?php echo $header_height + (count($branches) * $branch_height) + (0.8 * $footer_height) ?>">
-			<?php echo 'Today: ' . $now->format('j M Y') ?>
-		</text>
-	</g>
-</svg>

public/supported-versions.php
@@ -1,0 +1,130 @@
+<?php
+$_SERVER['BASE_PAGE'] = 'supported-versions.php';
+
+require_once __DIR__ . '/../include/prepend.inc';
+require_once __DIR__ . '/../include/branches.inc';
+
+site_header('Supported Versions', ['css' => ['supported-versions.css']]);
+
+// Version notes: if you need to make a note about a version having an unusual
+// support lifetime, add it under a heading with an anchor, and add the anchor
+// and branch names to the array below ('x.y' => '#anchor-name').
+$VERSION_NOTES = [
+	'8.5' => 'https://www.php.net/manual/migration85.php',
+	'8.4' => 'https://www.php.net/manual/migration84.php',
+	'8.3' => 'https://www.php.net/manual/migration83.php',
+	'8.2' => 'https://www.php.net/manual/migration82.php',
+];
+?>
+
+<h1>Supported Versions</h1>
+
+<p>
+	Each release branch of PHP is fully supported for two years from its initial
+	stable release. During this period, bugs and security issues that have been
+	reported are fixed and are released in regular point releases.
+</p>
+
+<p>
+	After this two year period of active support, each branch is then supported
+	for two additional years for critical security issues only. Releases during
+	this period are made on an as-needed basis: there may be multiple point
+	releases, or none, depending on the number of reports.
+</p>
+
+<p>
+	Once the four years of support are completed, the branch reaches its end of
+	life and is no longer supported. <a href="/eol.php">A table of end of life
+	branches is available.</a>
+</p>
+
+<h3>Currently Supported Versions</h3>
+
+<table class="standard">
+	<thead>
+		<tr>
+			<th>Branch</th>
+			<th colspan="2">Initial Release</th>
+			<th colspan="2">Active Support Until</th>
+			<th colspan="2">Security Support Until</th>
+			<th colspan="2">Notes</th>
+		</tr>
+	</thead>
+	<tbody>
+		<?php foreach (get_active_branches(false) as $major => $releases): ?>
+			<?php ksort($releases) ?>
+			<?php foreach ($releases as $branch => $release): ?>
+				<?php
+                $state = get_branch_support_state($branch);
+                $initial = get_branch_release_date($branch);
+                $until = get_branch_bug_eol_date($branch);
+                $eol = get_branch_security_eol_date($branch);
+                $now = new DateTime('now');
+                ?>
+				<tr class="<?php echo $state ?>">
+					<td>
+						<a href="/downloads.php?version=<?php echo htmlspecialchars($branch) ?>"><?php echo htmlspecialchars($branch) ?></a>
+					</td>
+					<td><?php echo htmlspecialchars($initial->format('j M Y')) ?></td>
+					<td class="collapse-phone"><em><?php echo htmlspecialchars(format_interval($initial, $now)) ?></em></td>
+					<td><?php echo htmlspecialchars($until->format('j M Y')) ?></td>
+					<td class="collapse-phone"><em><?php echo htmlspecialchars(format_interval($until, $now)) ?></em></td>
+					<td><?php echo htmlspecialchars($eol->format('j M Y')) ?></td>
+					<td class="collapse-phone"><em><?php echo htmlspecialchars(format_interval($eol, $now)) ?></em></td>
+					<td>
+						<?php if (isset($VERSION_NOTES[$branch])): ?>
+							<a href="<?php echo htmlspecialchars($VERSION_NOTES[$branch]) ?>">
+								Migration guide for PHP <?php echo htmlspecialchars($branch) ?>
+							</a>
+						<?php else: ?>
+							 —
+						<?php endif; ?>
+					</td>
+				</tr>
+			<?php endforeach ?>
+		<?php endforeach ?>
+	</tbody>
+</table>
+
+<p>
+	Or, visualised as a calendar:
+</p>
+
+<?php
+/* Chrome doesn't handle @font-face directives in SVG files included via <img>
+ * tags (which is odd, since it does handle them when you view the SVG files by
+ * themselves). Instead, we'll just pull the SVG into the page inline, thereby
+ * ensuring that we have the same text formatting as the rest of the page. */
+
+$non_standalone = true;
+include __DIR__ . '/images/supported-versions.php';
+?>
+
+<h4>Key</h4>
+
+<table class="standard">
+	<tr class="stable">
+		<td>Active support</td>
+		<td>
+			A release that is being actively supported. Reported bugs and security
+			issues are fixed and regular point releases are made.
+		</td>
+	</tr>
+	<tr class="security">
+		<td>Security fixes only</td>
+		<td>
+			A release that is supported for critical security issues only. Releases
+			are only made on an as-needed basis.
+		</td>
+	</tr>
+	<tr class="eol">
+		<td>End of life</td>
+		<td>
+			A release that is no longer supported. Users of this release should
+			upgrade as soon as possible, as they may be exposed to unpatched security
+			vulnerabilities.
+		</td>
+	</tr>
+</table>
+
+<?php site_footer(); ?>

supported-versions.php
@@ -1,130 +1,0 @@
-<?php
-$_SERVER['BASE_PAGE'] = 'supported-versions.php';
-
-include_once __DIR__ . '/include/prepend.inc';
-include_once __DIR__ . '/include/branches.inc';
-
-site_header('Supported Versions', ['css' => ['supported-versions.css']]);
-
-// Version notes: if you need to make a note about a version having an unusual
-// support lifetime, add it under a heading with an anchor, and add the anchor
-// and branch names to the array below ('x.y' => '#anchor-name').
-$VERSION_NOTES = [
-	'8.5' => 'https://www.php.net/manual/migration85.php',
-	'8.4' => 'https://www.php.net/manual/migration84.php',
-	'8.3' => 'https://www.php.net/manual/migration83.php',
-	'8.2' => 'https://www.php.net/manual/migration82.php',
-];
-?>
-
-<h1>Supported Versions</h1>
-
-<p>
-	Each release branch of PHP is fully supported for two years from its initial
-	stable release. During this period, bugs and security issues that have been
-	reported are fixed and are released in regular point releases.
-</p>
-
-<p>
-	After this two year period of active support, each branch is then supported
-	for two additional years for critical security issues only. Releases during
-	this period are made on an as-needed basis: there may be multiple point
-	releases, or none, depending on the number of reports.
-</p>
-
-<p>
-	Once the four years of support are completed, the branch reaches its end of
-	life and is no longer supported. <a href="/eol.php">A table of end of life
-	branches is available.</a>
-</p>
-
-<h3>Currently Supported Versions</h3>
-
-<table class="standard">
-	<thead>
-		<tr>
-			<th>Branch</th>
-			<th colspan="2">Initial Release</th>
-			<th colspan="2">Active Support Until</th>
-			<th colspan="2">Security Support Until</th>
-			<th colspan="2">Notes</th>
-		</tr>
-	</thead>
-	<tbody>
-		<?php foreach (get_active_branches(false) as $major => $releases): ?>
-			<?php ksort($releases) ?>
-			<?php foreach ($releases as $branch => $release): ?>
-				<?php
-                $state = get_branch_support_state($branch);
-                $initial = get_branch_release_date($branch);
-                $until = get_branch_bug_eol_date($branch);
-                $eol = get_branch_security_eol_date($branch);
-                $now = new DateTime('now');
-                ?>
-				<tr class="<?php echo $state ?>">
-					<td>
-						<a href="/downloads.php?version=<?php echo htmlspecialchars($branch) ?>"><?php echo htmlspecialchars($branch) ?></a>
-					</td>
-					<td><?php echo htmlspecialchars($initial->format('j M Y')) ?></td>
-					<td class="collapse-phone"><em><?php echo htmlspecialchars(format_interval($initial, $now)) ?></em></td>
-					<td><?php echo htmlspecialchars($until->format('j M Y')) ?></td>
-					<td class="collapse-phone"><em><?php echo htmlspecialchars(format_interval($until, $now)) ?></em></td>
-					<td><?php echo htmlspecialchars($eol->format('j M Y')) ?></td>
-					<td class="collapse-phone"><em><?php echo htmlspecialchars(format_interval($eol, $now)) ?></em></td>
-					<td>
-						<?php if (isset($VERSION_NOTES[$branch])): ?>
-							<a href="<?php echo htmlspecialchars($VERSION_NOTES[$branch]) ?>">
-								Migration guide for PHP <?php echo htmlspecialchars($branch) ?>
-							</a>
-						<?php else: ?>
-							 —
-						<?php endif; ?>
-					</td>
-				</tr>
-			<?php endforeach ?>
-		<?php endforeach ?>
-	</tbody>
-</table>
-
-<p>
-	Or, visualised as a calendar:
-</p>
-
-<?php
-/* Chrome doesn't handle @font-face directives in SVG files included via <img>
- * tags (which is odd, since it does handle them when you view the SVG files by
- * themselves). Instead, we'll just pull the SVG into the page inline, thereby
- * ensuring that we have the same text formatting as the rest of the page. */
-
-$non_standalone = true;
-include __DIR__ . '/images/supported-versions.php';
-?>
-
-<h4>Key</h4>
-
-<table class="standard">
-	<tr class="stable">
-		<td>Active support</td>
-		<td>
-			A release that is being actively supported. Reported bugs and security
-			issues are fixed and regular point releases are made.
-		</td>
-	</tr>
-	<tr class="security">
-		<td>Security fixes only</td>
-		<td>
-			A release that is supported for critical security issues only. Releases
-			are only made on an as-needed basis.
-		</td>
-	</tr>
-	<tr class="eol">
-		<td>End of life</td>
-		<td>
-			A release that is no longer supported. Users of this release should
-			upgrade as soon as possible, as they may be exposed to unpatched security
-			vulnerabilities.
-		</td>
-	</tr>
-</table>
-
-<?php site_footer(); ?>

commit 2bd91b6088c31447d764211b41938529d6325221

Fix include paths in supported versions visualization
Likely broken in b24d3106d45cc39e34ae0055386a1ae387019a20

public/images/supported-versions.php
@@ -1,6 +1,6 @@
 <?php
-require_once __DIR__ . '/../include/prepend.inc';
-require_once __DIR__ . '/../include/branches.inc';
+require_once __DIR__ . '/../../include/prepend.inc';
+require_once __DIR__ . '/../../include/branches.inc';
 
 // Sizing constants.
 $margin_left = 80;