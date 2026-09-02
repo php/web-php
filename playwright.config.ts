479564e4609e9ed8a6bec2b97fab677c3ee41252
Luffy <52o@qq52o.cn>
2026-09-02 15:30:36 +0800

    Fix Playwright E2E test server startup

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
         trace: 'on-first-retry',
     },
-    timeout: 0,
+    timeout: 120_000,
 
     projects: [
         {
